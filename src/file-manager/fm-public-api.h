ecba0751d87378b121e5629e8fc55986de8d0bc9
Darin Adler <darin@src.gnome.org>
2000-01-27 02:18:56 +0000

    Dead again!

--- src/file-manager/fm-public-api.h
+++
@@ -1,9 +0,0 @@
-#ifndef FM_PUBLIC_API_H
-#define FM_PUBLIC_API_H 1
-
-#include "fm-directory-view.h"
-
-GtkType fm_directory_view_icons_get_type(void);
-GtkType fm_directory_view_list_get_type(void);
-
-#endif
