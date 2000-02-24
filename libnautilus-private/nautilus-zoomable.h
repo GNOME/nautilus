839744dac32e481ea13f40e857a2b809c893fcc0
Maciej Stachowiak <mstachow@src.gnome.org>
2000-02-24 04:02:44 +0000

    Added Nautilus::Zoomable and Nautilus::ZoomableFrame interfaces. New files
    	* idl/nautilus.idl: Added Nautilus::Zoomable and
    	Nautilus::ZoomableFrame interfaces.
    	* nautilus-zoomable.c, nautilus-zoomable.h: New files for the
    	NautilusZoomable class. This class is used to implement the
    	Nautilus::Zoomable interface on a nautilus view or other control.
    	* Makefile.am: Added nautilus.c and nautilus.h to the build.

---
+++ libnautilus-private/nautilus-zoomable.h
@@ -0,0 +1,86 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: 8; c-basic-offset: 8 -*- */
+
+/*
+ *  libnautilus: A library for nautilus view implementations.
+ *
+ *  Copyright (C) 2000 Eazel, Inc.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Author: Maciej Stachowiak <mjs@eazel.com>
+ *
+ */
+
+/* nautilus-zoomable.h: Object for implementing the Zoomable CORBA interface. */
+
+#ifndef NAUTILUS_ZOOMABLE_H
+#define NAUTILUS_ZOOMABLE_H
+
+#include <libnautilus/nautilus.h>
+#include <bonobo/bonobo-object.h>
+#include <gtk/gtkwidget.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#define NAUTILUS_TYPE_ZOOMABLE			(nautilus_zoomable_get_type ())
+#define NAUTILUS_ZOOMABLE(obj)			(GTK_CHECK_CAST ((obj), NAUTILUS_TYPE_ZOOMABLE, NautilusZoomable))
+#define NAUTILUS_ZOOMABLE_CLASS(klass)		(GTK_CHECK_CLASS_CAST ((klass), NAUTILUS_TYPE_ZOOMABLE, NautilusZoomableClass))
+#define NAUTILUS_IS_ZOOMABLE(obj)		(GTK_CHECK_TYPE ((obj), NAUTILUS_TYPE_ZOOMABLE))
+#define NAUTILUS_IS_ZOOMABLE_CLASS(klass)	(GTK_CHECK_CLASS_TYPE ((obj), NAUTILUS_TYPE_ZOOMABLE))
+
+typedef struct _NautilusZoomable       NautilusZoomable;
+typedef struct _NautilusZoomableClass  NautilusZoomableClass;
+
+struct _NautilusZoomableClass
+{
+	BonoboObjectClass parent_spot;
+
+	void (*set_zoom_level)	        (NautilusZoomable *view,
+					 gdouble zoom_level);
+	void (*zoom_in)	                (NautilusZoomable *view);
+	void (*zoom_out)	        (NautilusZoomable *view);
+	void (*zoom_to_fit)	        (NautilusZoomable *view);
+
+	gpointer servant_init_func, servant_destroy_func, vepv;
+};
+
+typedef struct _NautilusZoomablePrivate NautilusZoomablePrivate;
+
+struct _NautilusZoomable
+{
+	BonoboObject parent;
+	NautilusZoomablePrivate *private;
+};
+
+GtkType            nautilus_zoomable_get_type                 (void);
+NautilusZoomable  *nautilus_zoomable_new                      (GtkWidget        *widget, 
+							       gdouble           min_zoom_level,
+							       gdouble           max_zoom_level,
+							       gboolean          is_continuous);
+NautilusZoomable  *nautilus_zoomable_new_from_bonobo_control  (BonoboObject     *bonobo_control, 
+							       gdouble           min_zoom_level,
+							       gdouble           max_zoom_level,
+							       gboolean          is_continuous);
+void               nautilus_zoomable_set_zoom_level           (NautilusZoomable *view,
+							       gdouble           zoom_level);
+BonoboObject      *nautilus_zoomable_get_bonobo_control       (NautilusZoomable *view);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
