aa399a9b1dde9b19e444c014846a4b8d235a116e
Maciej Stachowiak <mstachow@src.gnome.org>
2000-03-04 09:10:08 +0000

    Fix indentation on some of the lines.
            * components/sample/nautilus-sample-content-view.c: Fix
    	indentation on some of the lines.
            (nautilus_sample_content_view_load_uri): g_free() the old uri before
            g_strdup()ing a new one to avoid leaking memory.

--- components/sample/nautilus-sample-content-view.c
+++ components/sample/nautilus-sample-content-view.c
@@ -33,8 +33,8 @@
 #include <gtk/gtksignal.h>
 
 struct _NautilusSampleContentViewDetails {
-  gchar                     *uri;
-  NautilusContentViewFrame  *view_frame;
+	gchar                     *uri;
+	NautilusContentViewFrame  *view_frame;
 };
 
 
@@ -43,19 +43,19 @@ static void nautilus_sample_content_view_initialize       (NautilusSampleContent
 static void nautilus_sample_content_view_destroy          (GtkObject *object);
 
 NAUTILUS_DEFINE_CLASS_BOILERPLATE (NautilusSampleContentView, nautilus_sample_content_view, GTK_TYPE_LABEL)
-
+     
 static void sample_notify_location_change_cb              (NautilusContentViewFrame  *view, 
 							   Nautilus_NavigationInfo   *navinfo, 
 							   NautilusSampleContentView *sample);
-
-
+     
+     
 static void
 nautilus_sample_content_view_initialize_class (NautilusSampleContentViewClass *klass)
 {
 	GtkObjectClass *object_class;
-
+	
 	object_class = GTK_OBJECT_CLASS (klass);
-
+	
 	object_class->destroy = nautilus_sample_content_view_destroy;
 }
 
@@ -63,16 +63,16 @@ static void
 nautilus_sample_content_view_initialize (NautilusSampleContentView *view)
 {
 	view->details = g_new0 (NautilusSampleContentViewDetails, 1);
-
+	
 	gtk_label_set_text (GTK_LABEL (view), g_strdup ("(none)"));
-
+	
 	view->details->view_frame = nautilus_content_view_frame_new (GTK_WIDGET (view));
-
+	
 	gtk_signal_connect (GTK_OBJECT (view->details->view_frame), 
 			    "notify_location_change",
 			    GTK_SIGNAL_FUNC (sample_notify_location_change_cb), 
 			    view);
-
+	
 	gtk_widget_show (GTK_WIDGET (view));
 }
 
@@ -80,12 +80,12 @@ static void
 nautilus_sample_content_view_destroy (GtkObject *object)
 {
 	NautilusSampleContentView *view;
-
+	
 	view = NAUTILUS_SAMPLE_CONTENT_VIEW (object);
-
+	
 	g_free (view->details->uri);
 	g_free (view->details);
-
+	
 	NAUTILUS_CALL_PARENT_CLASS (GTK_OBJECT_CLASS, destroy, (object));
 }
 
@@ -94,7 +94,7 @@ nautilus_sample_content_view_destroy (GtkObject *object)
 NautilusContentViewFrame *
 nautilus_sample_content_view_get_view_frame (NautilusSampleContentView *view)
 {
-  return view->details->view_frame;
+	return view->details->view_frame;
 }
 
 /* URI handling */
@@ -102,8 +102,9 @@ void
 nautilus_sample_content_view_load_uri (NautilusSampleContentView *view,
 				       const gchar               *uri)
 {
-  view->details->uri = g_strdup (uri);
-  gtk_label_set_text (GTK_LABEL (view), uri);
+	g_free (view->details->uri);
+	view->details->uri = g_strdup (uri);
+	gtk_label_set_text (GTK_LABEL (view), uri);
 }
 
 static void
@@ -111,33 +112,32 @@ sample_notify_location_change_cb (NautilusContentViewFrame  *view,
 				  Nautilus_NavigationInfo   *navinfo, 
 				  NautilusSampleContentView *sample)
 {
-  Nautilus_ProgressRequestInfo pri;
+	Nautilus_ProgressRequestInfo pri;
+	
+	memset(&pri, 0, sizeof(pri));
+	
+	/* It's mandatory to send a PROGRESS_UNDERWAY message once the
+	   component starts loading, otherwise nautilus will assume it
+	   failed. In a real component, this will probably happen in some
+	   sort of callback from whatever loading mechanism it is using to
+	   load the data; this component loads no data, so it gives the
+	   progrss upodate here. */
+	
+	pri.type = Nautilus_PROGRESS_UNDERWAY;
+	pri.amount = 0.0;
+	nautilus_view_frame_request_progress_change (NAUTILUS_VIEW_FRAME (sample->details->view_frame), &pri);
+	
+	nautilus_sample_content_view_load_uri (sample, navinfo->actual_uri);
+	
+	/* It's mandatory to send a PROGRESS_DONE_OK message once the
+	   component is done loading successfully, or PROGRESS_DONE_ERROR if
+	   it completes unsuccessfully. In a real component, this will
+	   probably happen in some sort of callback from whatever loading
+	   mechanism it is using to load the data; this component loads no
+	   data, so it gives the progrss upodate here. */
 
-  memset(&pri, 0, sizeof(pri));
-
-  /* It's mandatory to send a PROGRESS_UNDERWAY message once the
-     component starts loading, otherwise nautilus will assume it
-     failed. In a real component, this will probably happen in some
-     sort of callback from whatever loading mechanism it is using to
-     load the data; this component loads no data, so it gives the
-     progrss upodate here. */
-
-  pri.type = Nautilus_PROGRESS_UNDERWAY;
-  pri.amount = 0.0;
-  nautilus_view_frame_request_progress_change (NAUTILUS_VIEW_FRAME (sample->details->view_frame), &pri);
-
-  nautilus_sample_content_view_load_uri (sample, navinfo->actual_uri);
-
-  /* It's mandatory to send a PROGRESS_DONE_OK message once the
-     component is done loading successfully, or PROGRESS_DONE_ERROR if
-     it completes unsuccessfully. In a real component, this will
-     probably happen in some sort of callback from whatever loading
-     mechanism it is using to load the data; this component loads no
-     data, so it gives the progrss upodate here. */
-
-  pri.type = Nautilus_PROGRESS_DONE_OK;
-  pri.amount = 100.0;
-  nautilus_view_frame_request_progress_change (NAUTILUS_VIEW_FRAME (sample->details->view_frame), &pri);
-
+	pri.type = Nautilus_PROGRESS_DONE_OK;
+	pri.amount = 100.0;
+	nautilus_view_frame_request_progress_change (NAUTILUS_VIEW_FRAME (sample->details->view_frame), &pri);
 }
 
