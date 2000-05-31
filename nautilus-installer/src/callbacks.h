c116005d372a36b3521e0dbadd4fae89f557536f
Eskil Heyn Olsen <eskil@src.gnome.org>
2000-05-31 05:13:50 +0000

    Added the support files
    	* components/services/install/lib/eazel-install-public.h:
    	* components/services/install/lib/eazel-install-types.h:
    	* nautilus-installer/src/callbacks.h:
    	* nautilus-installer/src/support.c: (lookup_widget),
    	(create_dummy_pixmap), (create_pixmap), (create_image):
    	* nautilus-installer/src/support.h:
    	Added the support files

---
+++ nautilus-installer/src/callbacks.h
@@ -0,0 +1,24 @@
+#include <gnome.h>
+
+
+void
+druid_cancel                           (GnomeDruid      *gnomedruid,
+                                        gpointer         user_data);
+
+void
+druid_finish                           (GnomeDruidPage  *gnomedruidpage,
+                                        gpointer         arg1,
+                                        gpointer         user_data);
+
+void
+begin_install                          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+prep_install                           (GnomeDruidPage  *gnomedruidpage,
+                                        gpointer         arg1,
+                                        gpointer         user_data);
+
+
+void set_images  (GtkWidget *window);
+
