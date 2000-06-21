c8556a87c4dd6487da4e9591b96e8f2a8aad9694
Rebecca Schulman <rebecka@eazel.com>
2000-06-21 02:59:53 +0000

    forgot to add this to the last commit
    2000-06-20  Rebecca Schulman  <rebecka@eazel.com>
    
    	* src/nautilus-search-bar-private.h:
    	forgot to add this to the last commit

---
+++ src/nautilus-search-bar-private.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
+
+/* nautilus-search-bar-criterion.c - Code to bring up
+   the various kinds of criterion supported in the nautilus search
+   bar 
+
+   Copyright (C) 2000 Eazel, Inc.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this program; see the file COPYING.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Author: Rebecca Schulman <rebecka@eazel.com>
+*/
+
+
+/* Functions to differentiate between using a complex search mode
+   and simple search mode */
+
+gboolean                   nautilus_search_bar_mode_is_useable_with_uri            (NautilusSearchBar *bar,
+										    const char *location,
+										    NautilusSearchBarMode mode);
+
+
+
