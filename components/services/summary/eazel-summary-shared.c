9b3632fad5daba7d8826a6e82ab0ea3d52a68ecc
J Shane Culpepper <pepper@eazel.com>
2000-09-01 03:11:13 +0000

    New snapshot of the summary xml parser. Getting closer to finished.
    2000-08-31  J Shane Culpepper  <pepper@eazel.com>
    
    	* components/services/summary/lib/eazel-summary-shared.c:
    	(summary_data_new), (services_data_new), (eazel_news_data_new),
    	(update_news_data_new), (parse_a_service),
    	(parse_a_eazel_news_item), (parse_a_update_news_item),
    	(build_services_glist_from_xml), (build_eazel_news_glist_from_xml),
    	(build_update_news_glist_from_xml), (parse_summary_xml_file):
    
    	New snapshot of the summary xml parser.  Getting closer to finished.

--- components/services/summary/eazel-summary-shared.c
+++ components/services/summary/eazel-summary-shared.c
@@ -40,6 +40,13 @@
 static GList * build_services_glist_from_xml (xmlNodePtr node);
 static GList * build_eazel_news_glist_from_xml (xmlNodePtr node);
 static GList * build_update_news_glist_from_xml (xmlNodePtr node);
+static SummaryData * summary_data_new (void);
+static ServicesData * services_data_new (void);
+static EazelNewsData * eazel_news_data_new (void);
+static UpdateNewsData * update_news_data_new (void);
+static ServicesData * parse_a_service (xmlNodePtr node);
+static EazelNewsData * parse_a_eazel_news_item (xmlNodePtr node);
+static UpdateNewsData * parse_a_update_news_item (xmlNodePtr node);
 
 
 gboolean
@@ -123,23 +130,166 @@ http_fetch_remote_file (char	*url, const char	*target_file)
 	}
 } /* end http_fetch_remote_file */
 
+static SummaryData *
+summary_data_new ()
+{
+	SummaryData	*return_value;
+	return_value = g_new0 (SummaryData, 1);
+
+	return_value->services_list = NULL;
+	return_value->eazel_news_list = NULL;
+	return_value->update_news_list = NULL;
+
+	return return_value;
+
+} /* end summary_data_new */
+
+static ServicesData *
+services_data_new ()
+{
+	ServicesData	*return_value;
+	return_value = g_new0 (ServicesData, 1);
+
+	return_value->name = NULL;
+	return_value->icon = NULL;
+	return_value->button_label = NULL;
+	return_value->description = NULL;
+
+	return return_value;
+
+} /* end services_data_new */
+
+static EazelNewsData *
+eazel_news_data_new ()
+{
+	EazelNewsData	*return_value;
+	return_value = g_new0 (EazelNewsData, 1);
+
+	return_value->name = NULL;
+	return_value->icon = NULL;
+	return_value->message = NULL;
+
+	return return_value;
+
+} /* end eazel_news_data_new */
+
+static UpdateNewsData *
+update_news_data_new ()
+{
+	UpdateNewsData	*return_value;
+	return_value = g_new0 (UpdateNewsData, 1);
+
+	return_value->name = NULL;
+	return_value->priority = NULL;
+	return_value->description = NULL;
+	return_value->icon = NULL;
+	return_value->install_uri = NULL;
+
+	return return_value;
+
+} /* end update_news_data_new */
+
+static ServicesData *
+parse_a_service (xmlNodePtr node)
+{
+	ServicesData *return_value;
+	return_value = services_data_new ();
+	return return_value;
+
+} /* end parse_a_service */
+
+static EazelNewsData *
+parse_a_eazel_news_item (xmlNodePtr node)
+{
+	EazelNewsData *return_value;
+	return_value = eazel_news_data_new ();
+	return return_value;
+
+} /* end parse_a_service */
+
+static UpdateNewsData *
+parse_a_update_news_item (xmlNodePtr node)
+{
+	UpdateNewsData *return_value;
+	return_value = update_news_data_new ();
+	return return_value;
+
+} /* end parse_a_service */
+
 static GList *
 build_services_glist_from_xml (xmlNodePtr node)
 {
-	return NULL;
-}
+	GList		*return_value;
+	xmlNodePtr	service;
+
+	return_value = NULL;
+	service = node->childs;
+	if (service == NULL) {
+		g_warning (_("There is no service data !\n"));
+		return NULL;
+	}
+
+	while (service) {
+		ServicesData	*sdata;
+
+		sdata = parse_a_service (service);
+		return_value = g_list_append (return_value, sdata);
+		service = service->next;
+	}
+
+	return return_value;
+
+} /* end build_services_glist_from_xml */
 
 static GList *
 build_eazel_news_glist_from_xml (xmlNodePtr node)
 {
-	return NULL;
-}
+	GList		*return_value;
+	xmlNodePtr	news_item;
+
+	return_value = NULL;
+	news_item = node->childs;
+	if (news_item == NULL) {
+		g_warning (_("There is no eazel news data !\n"));
+		return NULL;
+	}
+
+	while (news_item) {
+		EazelNewsData	*ndata;
+
+		ndata = parse_a_eazel_news_item (news_item);
+		return_value = g_list_append (return_value, ndata);
+		news_item = news_item->next;
+	}
+
+	return return_value;
+
+} /* end build_eazel_news_glist_from_xml */
 
 static GList *
 build_update_news_glist_from_xml (xmlNodePtr node)
 {
-	return NULL;
-}
+	GList		*return_value;
+	xmlNodePtr	news_item;
+
+	return_value = NULL;
+	news_item = node->childs;
+	if (news_item == NULL) {
+		g_warning (_("There is no eazel news data !\n"));
+		return NULL;
+	}
+
+	while (news_item) {
+		UpdateNewsData	*ndata;
+
+		ndata = parse_a_update_news_item (news_item);
+		return_value = g_list_append (return_value, ndata);
+		news_item = news_item->next;
+	}
+
+	return return_value;
+
+} /* end build_update_news_glist_from_xml */
 
 
 SummaryData *
@@ -149,13 +299,13 @@ parse_summary_xml_file (const char *summary_xml_file)
 	SummaryData	*return_value;
 	xmlDocPtr	doc;
 	xmlNodePtr	base;
-	xmlNodePtr	sections;
+	xmlNodePtr	child;
 
 	g_return_val_if_fail (summary_xml_file != NULL, NULL);
 
 	doc = xmlParseFile (summary_xml_file);
 
-	return_value = g_new0 (SummaryData, 1);
+	return_value = summary_data_new ();
 
 	base = doc->root;
 
@@ -172,19 +322,26 @@ parse_summary_xml_file (const char *summary_xml_file)
 		return NULL;
 	}
 
-	sections = doc->root->childs;
+	child = doc->root->childs;
 
-	if (sections == NULL) {
+	if (child == NULL) {
 		g_print (_("Could not find any summary configuration data!\n"));
 		xmlFreeDoc (doc);
 		g_warning (_("Bailing from summary configuration parse!\n"));
 		return NULL;
 	}
-	while (sections) {
 
-		return_value->services_list = build_services_glist_from_xml (sections);
-		return_value->eazel_news_list = build_eazel_news_glist_from_xml (sections);
-		return_value->update_news_list = build_update_news_glist_from_xml (sections);
+	while (child) {
+		if (g_strcasecmp (child->name, "SERVICES") == 0) {
+			return_value->services_list = build_services_glist_from_xml (child);
+		}
+		if (g_strcasecmp (child->name, "EAZEL_NEWS") == 0) {
+			return_value->eazel_news_list = build_eazel_news_glist_from_xml (child);
+		}
+		if (g_strcasecmp (child->name, "UPDATE_NEWS") == 0) {
+			return_value->update_news_list = build_update_news_glist_from_xml (child);
+		}
+		child = child->next;
 	}
 
 	return return_value;
