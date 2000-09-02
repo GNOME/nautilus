80eca943fa24a0f1e5ba82f6239250f9459e397e
Robey Pointer <robey@src.gnome.org>
2000-09-02 00:58:42 +0000

    fix the 'login view' so that it's really the change-password view; another bug (2710) has been filed on cleaning this up and possibly renaming it

--- components/services/login/nautilus-view/nautilus-login-view.c
+++ components/services/login/nautilus-view/nautilus-login-view.c
@@ -43,6 +43,21 @@
 #include <libnautilus-extensions/nautilus-gdk-extensions.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <libtrilobite/eazelproxy.h>
+#include <libtrilobite/libammonite.h>
+#include <liboaf/liboaf.h>
+#include <bonobo/bonobo-main.h>
+
+#if 0
+#define TEST_TARGET "http://tortoise.eazel.com:8080/protected/"
+#define TEST_LOGIN  "login.cgi"
+#endif
+
+
+typedef enum {
+	PENDING_NONE,
+	PENDING_LOGIN,
+} PendingOperation;
 
 /* A NautilusContentView's private information. */
 struct _NautilusLoginViewDetails {
@@ -51,10 +66,17 @@ struct _NautilusLoginViewDetails {
 	GtkWidget	*form;
 	GtkWidget	*form_title;
 	GtkWidget	*account_name;
-	GtkWidget	*account_password;
+	GtkWidget	*account_old_password;
+	GtkWidget	*account_new_password;
+	GtkWidget	*account_repeat_password;
 	GtkWidget	*login_button;
 	GtkWidget	*maintenance_button;
 	GtkWidget	*feedback_text;
+	PendingOperation pending;
+
+	/* for talking to the proxy (login/logout/pass-change) */
+	EazelProxy_UserControl user_control;
+	EazelProxy_AuthnCallback authn_callback;
 };
 
 #define SERVICE_SUMMARY_LOCATION                "eazel-summary:"
@@ -121,19 +143,44 @@ generate_login_form (NautilusLoginView	*view)
 	gtk_table_attach (table, view->details->account_name, 1, 2, 1, 2, GTK_FILL, GTK_FILL, 4, 4);
 	gtk_widget_show (view->details->account_name);
 
-	/* password */
-	temp_widget = gtk_label_new ("Password: ");
-
+	/* old password */
+	temp_widget = gtk_label_new ("Current password: ");
 	font = nautilus_font_factory_get_font_from_preferences (16);
 	nautilus_gtk_widget_set_font (temp_widget, font);
 	gdk_font_unref (font);
-
 	gtk_table_attach (table, temp_widget, 0, 2, 2, 3, GTK_FILL, GTK_FILL, 2, 2);
 	gtk_widget_show (temp_widget);
-	view->details->account_password = gtk_entry_new_with_max_length (36);
-	gtk_table_attach (table, view->details->account_password, 1, 2, 3, 4, GTK_FILL, GTK_FILL, 4, 4);
-	gtk_entry_set_visibility (GTK_ENTRY (view->details->account_password), FALSE);
-	gtk_widget_show (view->details->account_password);
+
+	view->details->account_old_password = gtk_entry_new_with_max_length (36);
+	gtk_table_attach (table, view->details->account_old_password, 1, 2, 3, 4, GTK_FILL, GTK_FILL, 4, 4);
+	gtk_entry_set_visibility (GTK_ENTRY (view->details->account_old_password), FALSE);
+	gtk_widget_show (view->details->account_old_password);
+
+	/* new password */
+	temp_widget = gtk_label_new ("New password: ");
+	font = nautilus_font_factory_get_font_from_preferences (16);
+	nautilus_gtk_widget_set_font (temp_widget, font);
+	gdk_font_unref (font);
+	gtk_table_attach (table, temp_widget, 0, 2, 4, 5, GTK_FILL, GTK_FILL, 2, 2);
+	gtk_widget_show (temp_widget);
+
+	view->details->account_new_password = gtk_entry_new_with_max_length (36);
+	gtk_table_attach (table, view->details->account_new_password, 1, 2, 5, 6, GTK_FILL, GTK_FILL, 4, 4);
+	gtk_entry_set_visibility (GTK_ENTRY (view->details->account_new_password), FALSE);
+	gtk_widget_show (view->details->account_new_password);
+
+	/* repeat password */
+	temp_widget = gtk_label_new ("New password (again): ");
+	font = nautilus_font_factory_get_font_from_preferences (16);
+	nautilus_gtk_widget_set_font (temp_widget, font);
+	gdk_font_unref (font);
+	gtk_table_attach (table, temp_widget, 0, 2, 6, 7, GTK_FILL, GTK_FILL, 2, 2);
+	gtk_widget_show (temp_widget);
+
+	view->details->account_repeat_password = gtk_entry_new_with_max_length (36);
+	gtk_table_attach (table, view->details->account_repeat_password, 1, 2, 7, 8, GTK_FILL, GTK_FILL, 4, 4);
+	gtk_entry_set_visibility (GTK_ENTRY (view->details->account_repeat_password), FALSE);
+	gtk_widget_show (view->details->account_repeat_password);
 
 	/* insert the table */
 	gtk_box_pack_start (GTK_BOX (view->details->form), GTK_WIDGET(table), 0, 0, 4);
@@ -141,7 +188,9 @@ generate_login_form (NautilusLoginView	*view)
 
 	/* attach a changed signal to the 2 entry fields, so we can enable the button when something is typed into both fields */
 	gtk_signal_connect (GTK_OBJECT (view->details->account_name),		"changed",	GTK_SIGNAL_FUNC (entry_changed_cb),	view);
-	gtk_signal_connect (GTK_OBJECT (view->details->account_password),	"changed",	GTK_SIGNAL_FUNC (entry_changed_cb),	view);
+	gtk_signal_connect (GTK_OBJECT (view->details->account_old_password),	"changed",	GTK_SIGNAL_FUNC (entry_changed_cb),	view);
+	gtk_signal_connect (GTK_OBJECT (view->details->account_new_password),	"changed",	GTK_SIGNAL_FUNC (entry_changed_cb),	view);
+	gtk_signal_connect (GTK_OBJECT (view->details->account_repeat_password),"changed",	GTK_SIGNAL_FUNC (entry_changed_cb),	view);
 
 	/* allocate the command buttons - first the login button */
 
@@ -186,15 +235,115 @@ entry_changed_cb (GtkWidget	*entry, NautilusLoginView	*view)
 {
 
 	char		*user_name;
-	char		*password;
+	char		*old_password;
+	char		*new_password;
+	char		*repeat_password;
 	gboolean	button_enabled;
 
 	user_name = gtk_entry_get_text (GTK_ENTRY (view->details->account_name));
-	password = gtk_entry_get_text (GTK_ENTRY (view->details->account_password));
+	old_password = gtk_entry_get_text (GTK_ENTRY (view->details->account_old_password));
+	new_password = gtk_entry_get_text (GTK_ENTRY (view->details->account_new_password));
+	repeat_password = gtk_entry_get_text (GTK_ENTRY (view->details->account_repeat_password));
 
-	button_enabled = user_name && strlen (user_name) && password && strlen (password);
+	button_enabled = user_name && strlen (user_name) && old_password && strlen (old_password) &&
+		new_password && strlen (new_password) && repeat_password && strlen (repeat_password) &&
+		(view->details->pending == PENDING_NONE);
 	gtk_widget_set_sensitive (view->details->login_button, button_enabled);
 }
+
+
+static void
+authn_succeeded (const EazelProxy_User *user, gpointer state, CORBA_Environment *ev)
+{
+	NautilusLoginView *view;
+
+	view = NAUTILUS_LOGIN_VIEW (state);
+	g_assert (view->details->pending == PENDING_LOGIN);
+
+	view->details->pending = PENDING_NONE;
+	g_message ("Login succeeeded!");
+
+	/* more ... */
+
+	ammonite_auth_callback_wrapper_free (bonobo_poa (), view->details->authn_callback);
+	bonobo_object_unref (BONOBO_OBJECT (view->details->nautilus_view));
+
+#if 0
+	if (registered_ok) {
+		go_to_uri (view->details->nautilus_view, SERVICE_SUMMARY_LOCATION);
+	}
+#endif
+}
+
+static void
+authn_failed (const EazelProxy_User *user, const EazelProxy_AuthnFailInfo *info, gpointer state,
+	      CORBA_Environment *ev)
+{
+	NautilusLoginView *view;
+
+	view = NAUTILUS_LOGIN_VIEW (state);
+	g_assert (view->details->pending == PENDING_LOGIN);
+
+	view->details->pending = PENDING_NONE;
+	gtk_widget_set_sensitive (view->details->login_button, TRUE);
+
+	show_feedback (view->details->feedback_text, "Incorrect current password.  Please try again.");
+	gtk_entry_set_text (GTK_ENTRY (view->details->account_old_password), "");
+	/* more? */
+
+	ammonite_auth_callback_wrapper_free (bonobo_poa (), view->details->authn_callback);
+	bonobo_object_unref (BONOBO_OBJECT (view->details->nautilus_view));
+}
+
+
+static void
+start_login (NautilusLoginView *view, const char *username, const char *password, const char *new_password)
+{
+	EazelProxy_AuthnInfo *authinfo;
+	CORBA_Environment ev;
+	CORBA_char *corba_new_password;
+
+	AmmoniteAuthCallbackWrapperFuncs callback = {
+		authn_succeeded,
+		authn_failed
+	};
+
+	CORBA_exception_init (&ev);
+	g_assert (view->details->pending == PENDING_NONE);
+
+	if (view->details->user_control == CORBA_OBJECT_NIL) {
+		/* can't do anything. */
+		return;
+	}
+
+	view->details->authn_callback = ammonite_auth_callback_wrapper_new (bonobo_poa (), &callback, view);
+	authinfo = EazelProxy_AuthnInfo__alloc ();
+	authinfo->username = CORBA_string_dup (username);
+	authinfo->password = CORBA_string_dup (password);
+#ifdef TEST_TARGET
+	authinfo->services_redirect_uri = CORBA_string_dup (TEST_TARGET);
+	authinfo->services_login_path = CORBA_string_dup (TEST_LOGIN);
+#else
+	authinfo->services_redirect_uri = CORBA_string_dup ("");
+	authinfo->services_login_path = CORBA_string_dup ("");
+#endif
+
+	/* ref myself until the callback returns */
+	bonobo_object_ref (BONOBO_OBJECT (view->details->nautilus_view));
+
+	view->details->pending = PENDING_LOGIN;
+	corba_new_password = CORBA_string_dup (new_password);
+	EazelProxy_UserControl_set_new_user_password (view->details->user_control, authinfo, corba_new_password,
+						      view->details->authn_callback, &ev);
+	if (ev._major != CORBA_NO_EXCEPTION) {
+		g_warning ("Exception during EazelProxy login: %s", CORBA_exception_id (&ev));
+		view->details->pending = PENDING_NONE;
+		bonobo_object_unref (BONOBO_OBJECT (view->details->nautilus_view));
+	}
+
+	CORBA_exception_free (&ev);
+}
+
 
 /* callback to handle the login button.  Right now only dumps a simple feedback message. */
 static void
@@ -202,25 +351,27 @@ login_button_cb (GtkWidget	*button, NautilusLoginView	*view)
 {
 
 	char		*user_name;
-	char		*password;
-	gboolean	registered_ok;
+	char		*old_password;
+	char		*new_password;
+	char		*repeat_password;
 
 	user_name = gtk_entry_get_text (GTK_ENTRY (view->details->account_name));
-	password = gtk_entry_get_text (GTK_ENTRY (view->details->account_password));
-	registered_ok = FALSE;
+	old_password = gtk_entry_get_text (GTK_ENTRY (view->details->account_old_password));
+	new_password = gtk_entry_get_text (GTK_ENTRY (view->details->account_new_password));
+	repeat_password = gtk_entry_get_text (GTK_ENTRY (view->details->account_repeat_password));
 
+	if (strcmp (new_password, repeat_password) != 0) {
+		show_feedback (view->details->feedback_text, "The new password fields don't match.  Please try again.");
+		gtk_entry_set_text (GTK_ENTRY (view->details->account_new_password), "");
+		gtk_entry_set_text (GTK_ENTRY (view->details->account_repeat_password), "");
+		gtk_widget_set_sensitive (view->details->login_button, FALSE);
+		return;
+	}
+
+	gtk_widget_set_sensitive (view->details->login_button, FALSE);
 	gtk_widget_hide (view->details->feedback_text);
 
-	/* Do Nothing right now just return an ok response */
-	registered_ok = TRUE;
-
-	if (registered_ok == FALSE) {
-		show_feedback (view->details->feedback_text, "Login Failed! Please try again.");
-	}
-
-	if (registered_ok) {
-		go_to_uri (view->details->nautilus_view, SERVICE_SUMMARY_LOCATION);
-	}
+	start_login (view, user_name, old_password, new_password);
 }
 
 /* callback to point account maintenance button to webpage */
@@ -250,11 +401,12 @@ nautilus_login_view_initialize_class (NautilusLoginViewClass *klass)
 static void
 nautilus_login_view_initialize (NautilusLoginView *view)
 {
-
-	NautilusBackground	*background;
+	NautilusBackground *background;
+	CORBA_Environment ev;
 
 	view->details = g_new0 (NautilusLoginViewDetails, 1);
 	view->details->nautilus_view = nautilus_view_new (GTK_WIDGET (view));
+	view->details->pending = PENDING_NONE;
 	gtk_signal_connect (GTK_OBJECT (view->details->nautilus_view), 
 			    "load_location",
 			    GTK_SIGNAL_FUNC (login_load_location_callback), 
@@ -263,24 +415,37 @@ nautilus_login_view_initialize (NautilusLoginView *view)
 	background = nautilus_get_widget_background (GTK_WIDGET (view));
 	nautilus_background_set_color (background, SERVICE_VIEW_DEFAULT_BACKGROUND_COLOR);
 
+	CORBA_exception_init (&ev);
+	view->details->user_control = (EazelProxy_UserControl) oaf_activate_from_id (IID_EAZELPROXY, 0, NULL, &ev);
+	if ( CORBA_NO_EXCEPTION != ev._major ) {
+		/* FIXME user should be warned that Ammonite may not be installed */
+		g_warning ("Couldn't instantiate eazel-proxy");
+		view->details->user_control = CORBA_OBJECT_NIL;
+	}
+	CORBA_exception_free (&ev);
+
 	gtk_widget_show (GTK_WIDGET (view));
 }
 
 static void
 nautilus_login_view_destroy (GtkObject *object)
 {
+	NautilusLoginView *view;
+	CORBA_Environment ev;
 
-	NautilusLoginView	*view;
-	
+	CORBA_exception_init (&ev);
+
 	view = NAUTILUS_LOGIN_VIEW (object);
 
 	if (view->details->uri) {
 		g_free (view->details->uri);
 	}
+	CORBA_Object_release (view->details->user_control, &ev);
 	g_free (view->details);
-	
-	NAUTILUS_CALL_PARENT_CLASS (GTK_OBJECT_CLASS, destroy, (object));
 
+	CORBA_exception_free (&ev);
+
+	NAUTILUS_CALL_PARENT_CLASS (GTK_OBJECT_CLASS, destroy, (object));
 }
 
 NautilusView *
