996e2bbb2410eb21ea455af59ae6b69dc5b52233
Ramiro Estrugo <ramiro@src.gnome.org>
2000-09-29 13:27:45 +0000

    Moved the leak-checker and profiler tools to their own gnome cvs module.
    	* Makefile.am:
    	* configure.in:
    	* HACKING:
    	* tools/.cvsignore:
    	* tools/Makefile.am:
    	* tools/README:
    	* tools/leak-checker/.cvsignore:
    	* tools/leak-checker/Makefile.am:
    	* tools/leak-checker/nautilus-leak-checker-stubs.h:
    	* tools/leak-checker/nautilus-leak-checker.c:
    	* tools/leak-checker/nautilus-leak-checker.h:
    	* tools/leak-checker/nautilus-leak-hash-table.c:
    	* tools/leak-checker/nautilus-leak-hash-table.h:
    	* tools/leak-checker/nautilus-leak-symbol-lookup.c:
    	* tools/leak-checker/nautilus-leak-symbol-lookup.h:
    	* tools/profiler/Makefile:
    	* tools/profiler/funcsummary.C:
    	* tools/profiler/funcsummary.h:
    	* tools/profiler/machine-profile.S:
    	* tools/profiler/machine-profile.h:
    	* tools/profiler/machine-profileP.h:
    	* tools/profiler/nautilus-leak-symbol-lookup.C:
    	* tools/profiler/nautilus-leak-symbol-lookup.h:
    	* tools/profiler/profile.C:
    	* tools/profiler/profile.h:
    	* tools/profiler/profileP.h:
    	* tools/profiler/profiledata.C:
    	* tools/profiler/profiledata.h:
    	* tools/profiler/symbol-table.h:
    	* tools/profiler/test.cpp:
    	* tools/profiler/totaltime.C:
    	* tools/profiler/totaltime.h:
    	Moved the leak-checker and profiler tools to their own gnome cvs
    	module.

--- tools/profiler/nautilus-leak-symbol-lookup.h
+++
@@ -1,9 +0,0 @@
-#ifndef NAUTILUS_LEAK_SYMBOL_LOOKUP__
-#define NAUTILUS_LEAK_SYMBOL_LOOKUP__
-
-#include <string>
-
-void nautilus_leak_print_symbol_address (const char *app_path, void *address);
-void get_function_at_address (const char *app_path, void *address, string &result);
-
-#endif
