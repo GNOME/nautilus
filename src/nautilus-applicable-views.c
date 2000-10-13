b82831fca0e5e1b264cb996681405820d65dd155
John Sullivan <sullivan@src.gnome.org>
2000-10-13 18:27:08 +0000

    added GNOME_VFS_ERROR_INTERNAL to the known list of errors that we can't
    	* src/nautilus-applicable-views.c:
    	(get_nautilus_navigation_result_from_gnome_vfs_result):
    	added GNOME_VFS_ERROR_INTERNAL to the known list of
    	errors that we can't say anything useful about, since
    	this is firing in some cases when Mozilla fails.

--- src/nautilus-applicable-views.c
+++ src/nautilus-applicable-views.c
@@ -80,7 +80,8 @@ get_nautilus_navigation_result_from_gnome_vfs_result (GnomeVFSResult gnome_vfs_r
 	case GNOME_VFS_ERROR_ACCESS_DENIED:	
 		return NAUTILUS_NAVIGATION_RESULT_ACCESS_DENIED;
         case GNOME_VFS_ERROR_GENERIC:
-                /* This one has occurred at least once in the web browser component */
+        case GNOME_VFS_ERROR_INTERNAL:
+                /* These two have occurred at least once in the web browser component */
                 return NAUTILUS_NAVIGATION_RESULT_UNSPECIFIC_ERROR;
         default:
                 /* Whenever this message fires, we should consider adding a specific case
