f84f0d231665608cb82379d2cbe07732f4d6b621
Ali Abdin <aliabdin@aucegypt.edu>
2000-10-21 13:18:32 +0000

    Add in 'break;' at the appropriate areas of the code. Should fix some
    2000-10-21  Ali Abdin  <aliabdin@aucegypt.edu>
    
            * components/help/converters/gnome-db2html2/sect-elements.c:
            (sect_sect_end_element): Add in 'break;' at the appropriate areas of
            the code. Should fix some issues. Fixes Bug #3863.

--- components/help/converters/gnome-db2html2/sect-elements.c
+++ components/help/converters/gnome-db2html2/sect-elements.c
@@ -478,19 +478,26 @@ sect_sect_end_element (Context *context,
 	switch (name[4]) {
 	case 'a':
 	        context->chapter = 0;
+		break;
 	case 't':
 		context->sect1 = 0;
+		break;
 	case '1':
 		context->sect2 = 0;
+		break;
 	case '2':
 		context->sect3 = 0;
+		break;
 	case '3':
 		context->sect4 = 0;
+		break;
 	case '4':
 		context->sect5 = 0;
+		break;
 	case 'n':
-		context->appendix = 0;
-		context->sect1 = 0;
+		/* Nothing should come after the appendix tag */
+		/* context->appendix = 0; */
+		break;
 	default:
 		break;
 	}
