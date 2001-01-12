f875eba7ba53730f4e5b075848e552def38db441
Rebecca Schulman <rebecka@eazel.com>
2001-01-12 02:03:13 +0000

    Change "index-only" to "index-if-available" in search uris, to take
    2001-01-12  Rebecca Schulman  <rebecka@eazel.com>
    
    	* src/nautilus-complex-search-bar.c:
    	(nautilus_complex_search_bar_get_location):
    	Change "index-only" to "index-if-available"
    	in search uris, to take advantage of the new
    	option in medusa to perform a slow search
    	only if an index is unavailable

--- src/nautilus-complex-search-bar.c
+++ src/nautilus-complex-search-bar.c
@@ -362,7 +362,7 @@ nautilus_complex_search_bar_get_location (NautilusNavigationBar *navigation_bar)
 	if (nautilus_preferences_get_boolean (NAUTILUS_PREFERENCES_SEARCH_METHOD, FALSE)) {
 		search_uri = g_strconcat ("search:index-with-backup", escaped_fragment, NULL);
 	} else {
-		search_uri = g_strconcat ("search:index-only", escaped_fragment, NULL);
+		search_uri = g_strconcat ("search:index-if-available", escaped_fragment, NULL);
 	}
 	g_free (escaped_fragment);
 
