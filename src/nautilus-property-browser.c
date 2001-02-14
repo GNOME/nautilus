46ce81a9a33c8f65061ade4df36902511b441b69
Andy Hertzfeld <andy@src.gnome.org>
2001-02-14 01:38:10 +0000

    fixed bug 6554, typing random text into the background selector crashes,
    	* src/nautilus-property-browser.c: (add_pattern_to_browser):
    	fixed bug 6554, typing random text into the background selector
    	crashes, by adding a check for a valid path and displaying an
    	error dialog if the path isn't valid.

--- src/nautilus-property-browser.c
+++ src/nautilus-property-browser.c
@@ -1029,63 +1029,67 @@ add_pattern_to_browser (const char *path_name, gpointer *data)
 	char *directory_uri;
 	GnomeVFSResult result;
 	
-	NautilusPropertyBrowser *property_browser = NAUTILUS_PROPERTY_BROWSER(data);
+	NautilusPropertyBrowser *property_browser = NAUTILUS_PROPERTY_BROWSER (data);
 
-	/* FIXME this is not a problem in nautilus but rather in the
-        gtk widget that selects the tiles. that would have to be fixed to
-        support tilde's. at the moment this is the best we can do. */
-	if (*path_name != '~') {
-		/* fetch the mime type and make sure that the file is an image */
-		path_uri = gnome_vfs_get_uri_from_local_path (path_name);	
-
-		/* don't allow the user to change the reset image */
-		basename = nautilus_uri_get_basename (path_uri);
-		if (basename && nautilus_strcmp (basename, RESET_IMAGE_NAME) == 0) {
-			nautilus_show_error_dialog (_("Sorry, but you can't replace the reset image."), _("Not an Image"), NULL);
-			g_free (path_uri);
-			g_free (basename);
-			return;
+	/* make sure that it's a valid path */
+	if (path_name == NULL || path_name[0] != '/') {
+		char *message;
+		if (path_name != NULL) {
+			message = g_strdup_printf (_("Sorry, but \"%s\" is not a valid file name."), path_name);
+		} else {
+			message = g_strdup (_("Sorry, but you did not supply a valid file name."));
 		}
-		
-		g_free (path_uri);	
+		nautilus_show_error_dialog (message, _("Couldn't install pattern"), GTK_WINDOW (property_browser));
+		g_free (message);
+		return;
+	}
+	
+	/* fetch the mime type and make sure that the file is an image */
+	path_uri = gnome_vfs_get_uri_from_local_path (path_name);	
+
+	/* don't allow the user to change the reset image */
+	basename = nautilus_uri_get_basename (path_uri);
+	if (basename && nautilus_strcmp (basename, RESET_IMAGE_NAME) == 0) {
+		nautilus_show_error_dialog (_("Sorry, but you can't replace the reset image."), _("Not an Image"), NULL);
+		g_free (path_uri);
 		g_free (basename);
+		return;
+	}
 		
-		user_directory = nautilus_get_user_directory ();
+	g_free (path_uri);	
+	g_free (basename);
 		
-		/* copy the image file to the patterns directory */
-		directory_path = nautilus_make_path (user_directory, property_browser->details->category);
-		g_free (user_directory);
-		source_file_name = strrchr (path_name, '/');
-		destination_name = nautilus_make_path (directory_path, source_file_name + 1);
+	user_directory = nautilus_get_user_directory ();
+		
+	/* copy the image file to the patterns directory */
+	directory_path = nautilus_make_path (user_directory, property_browser->details->category);
+	g_free (user_directory);
+	source_file_name = strrchr (path_name, '/');
+	destination_name = nautilus_make_path (directory_path, source_file_name + 1);
 
-		/* make the directory if it doesn't exist */
-		if (!g_file_exists(directory_path)) {
-			directory_uri = gnome_vfs_get_uri_from_local_path (directory_path);
-			gnome_vfs_make_directory (directory_uri,
-						  GNOME_VFS_PERM_USER_ALL
-						  | GNOME_VFS_PERM_GROUP_ALL
-						  | GNOME_VFS_PERM_OTHER_READ);
-			g_free (directory_uri);
-		}
+	/* make the directory if it doesn't exist */
+	if (!g_file_exists(directory_path)) {
+		directory_uri = gnome_vfs_get_uri_from_local_path (directory_path);
+		gnome_vfs_make_directory (directory_uri,
+						 GNOME_VFS_PERM_USER_ALL
+						 | GNOME_VFS_PERM_GROUP_ALL
+						 | GNOME_VFS_PERM_OTHER_READ);
+		g_free (directory_uri);
+	}
 		
-		g_free (directory_path);
+	g_free (directory_path);
 		
-		result = nautilus_copy_uri_simple (path_name, destination_name);		
-		if (result != GNOME_VFS_OK) {
-			char *message = g_strdup_printf (_("Sorry, but the pattern %s couldn't be installed."), path_name);
-			nautilus_show_error_dialog (message, _("Couldn't install pattern"), GTK_WINDOW (property_browser));
-			g_free (message);
-		}
-		
-		g_free (destination_name);
-		
-		/* update the property browser's contents to show the new one */
-		nautilus_property_browser_update_contents (property_browser);
-	} else {
-	        char *message = g_strdup_printf (_("Sorry, but ~ as a directory is not currently supported.\n Please Type the full path."));
+	result = nautilus_copy_uri_simple (path_name, destination_name);		
+	if (result != GNOME_VFS_OK) {
+		char *message = g_strdup_printf (_("Sorry, but the pattern %s couldn't be installed."), path_name);
 		nautilus_show_error_dialog (message, _("Couldn't install pattern"), GTK_WINDOW (property_browser));
 		g_free (message);
 	}
+		
+	g_free (destination_name);
+	
+	/* update the property browser's contents to show the new one */
+	nautilus_property_browser_update_contents (property_browser);
 }
 
 /* here's where we initiate adding a new pattern by putting up a file selector */
