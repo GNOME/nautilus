14eec077150442233a8aa79549bbfbb1781249cd
Ramiro Estrugo <ramiro@src.gnome.org>
2001-03-14 16:34:38 +0000

    Dont clobber any fonts that the user might have installed in ~/.nautilus.
    	* helper-scripts/nautilus-restore-settings-to-default.sh:
    	Dont clobber any fonts that the user might have installed in
    	~/.nautilus.  This is a bit of hack, but its very inconvenient to
    	have these fonts be trashed when testing Nautilus in a "from
    	scratch" environment.
    
    	* nautilus.spec.in:
    	Add the "restore settings to default" script to the package.

--- helper-scripts/nautilus-restore-settings-to-default.sh
+++ helper-scripts/nautilus-restore-settings-to-default.sh
@@ -3,6 +3,21 @@
 nautilus --quit
 gconftool --shutdown
 
-rm -rf $HOME/.nautilus
-rm -rf $HOME/.gconf
+nautilus_dir=$HOME/.nautilus
+fonts_tmp=$HOME/nautilus-fonts-$$
+
+if [ -d $nautilus_dir/fonts ]
+then
+    mv $nautilus_dir/fonts $fonts_tmp
+fi
+
+rm -rf $nautilus_dir/
+rm -rf $HOME/.gconf/apps/nautilus/
 rm -rf $HOME/.gconfd
+
+if [ -d $fonts_tmp ]
+then
+    mkdir -p $nautilus_dir/
+    mv $fonts_tmp $nautilus_dir/fonts
+fi
+
