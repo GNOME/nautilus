b4339ade14e7b8b8543cb4c2b0da335202636305
Jens Finke <jens@triq.net>
2002-02-20 18:20:42 +0000

    Call gnome2 background setting program.
    2002-02-20  Jens Finke <jens@triq.net>
    
    	* src/file-manager/fm-desktop-icon-view.c
    	(change_background_callback): Call gnome2 background setting
    	program.

--- src/file-manager/fm-desktop-icon-view.c
+++ src/file-manager/fm-desktop-icon-view.c
@@ -653,7 +653,7 @@ change_background_callback (BonoboUIComponent *component,
 {
 	nautilus_launch_application_from_command 
 		(_("Background"),
-		 "background-properties-capplet", NULL, FALSE);
+		 "gnome2-background-properties", NULL, FALSE);
 }
 
 static void
