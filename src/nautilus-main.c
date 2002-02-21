4c998001e3bfa8a87c646c596ca1354f82398149
Kjartan Maraas <kmaraas@gnome.org>
2002-02-21 08:19:00 +0000

    Use GETTEXT_PACKAGE, not just PACKAGE in the bindtextdomain functions.
    2002-02-21  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* src/nautilus-main.c: Use GETTEXT_PACKAGE, not just PACKAGE
    	in the bindtextdomain functions.

--- src/nautilus-main.c
+++ src/nautilus-main.c
@@ -171,9 +171,9 @@ main (int argc, char *argv[])
 	 * lines cause empty statement warnings.
 	 */
 #ifdef ENABLE_NLS
-	bindtextdomain (PACKAGE, GNOMELOCALEDIR);
-	bind_textdomain_codeset (PACKAGE, "UTF-8");
-	textdomain (PACKAGE);
+	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
 #endif
 
 	/* Get parameters. */
