c239d8439d9ac0fd3cf94f593b013588b331e041
Alex Larsson <alexl@redhat.com>
2002-03-28 23:09:19 +0000

    Temporary revert libjpeg patch to keep it building. Will fix for real
    2002-03-28  Alex Larsson  <alexl@redhat.com>
    
    	* libnautilus-private/nautilus-thumbnails-jpeg.c:
    	Temporary revert libjpeg patch to keep it building.
    	Will fix for real later.

--- libnautilus-private/nautilus-thumbnails-jpeg.c
+++ libnautilus-private/nautilus-thumbnails-jpeg.c
@@ -32,6 +32,8 @@
 #include <setjmp.h>
 
 #include <stdio.h>
+/* Some versions of libjpeg seem to have jconfig.h in the public header */
+#define JCONFIG_INCLUDED
 #include <jpeglib.h>
 
 #include <libgnomevfs/gnome-vfs-types.h>
