dc04f196917f6cc040bc659c02b5586c8b05943f
Narayana Pattipati <narayana.pattipati@wipro.com>
2002-10-01 07:37:04 +0000

    Unescape the media path so that nautilus link is removed properly when it
    2002-09-30  Narayana Pattipati  <narayana.pattipati@wipro.com>
    
            * src/file-manager/fm-desktop-icon-view.c: Unescape the media path so
            that nautilus link is removed properly when it has special characters
            like #.
    
            * libnautilus-private/nautilus-link-desktop-file.c: Escape the media
            path so that media name is created properly when it has special
            characters like #.

--- src/file-manager/fm-desktop-icon-view.c
+++ src/file-manager/fm-desktop-icon-view.c
@@ -1052,7 +1052,7 @@ volume_mounted_callback (NautilusVolumeMonitor *monitor,
 static void
 unlink_and_notify (const char *path)
 {
-	char *uri;
+	char *uri, *unescaped_uri;
 	GList one_item_list;
 
 	unlink (path);
@@ -1061,8 +1061,11 @@ unlink_and_notify (const char *path)
 	if (uri == NULL) {
 		return;
 	}
+ 
+	unescaped_uri = gnome_vfs_unescape_string (uri, NULL);
+	g_free (uri);
 
-	one_item_list.data = uri;
+	one_item_list.data = unescaped_uri;
 	one_item_list.next = NULL;
 	one_item_list.prev = NULL;
 	nautilus_directory_notify_files_removed (&one_item_list);
