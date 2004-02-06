75263adf72fa67bc25222f246801314040b9c293
Martin Wehner <martin.wehner@epost.de>
2004-02-06 21:28:42 +0000

    Don't crash when trying to rename empty selection. Fixes bug #131861.
    2004-02-06  Martin Wehner  <martin.wehner@epost.de>
    
    	* src/file-manager/fm-directory-view.c (rename_file_callback):
    	Don't crash when trying to rename empty selection. Fixes bug #131861.

--- src/file-manager/fm-directory-view.c
+++ src/file-manager/fm-directory-view.c
@@ -4584,11 +4584,11 @@ rename_file_callback (BonoboUIComponent *component, gpointer callback_data, cons
 	view = FM_DIRECTORY_VIEW (callback_data);
 	selection = fm_directory_view_get_selection (view);
 
-	file = NAUTILUS_FILE (selection->data);
-
-	EEL_CALL_METHOD (FM_DIRECTORY_VIEW_CLASS, view, start_renaming_file, (view, file));
-	
-	nautilus_file_list_free (selection);
+	if (selection_not_empty_in_menu_callback (view, selection)) {
+		file = NAUTILUS_FILE (selection->data);
+		EEL_CALL_METHOD (FM_DIRECTORY_VIEW_CLASS, view, start_renaming_file, (view, file));
+		nautilus_file_list_free (selection);
+    }
 }
 
 static void
