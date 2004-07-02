d3ff6083a113a8064a0627b7d6c987db8c598632
Bastien Nocera <hadess@hadess.net>
2004-07-02 11:37:38 +0000

    Don't warn about mime-type mismatches if the handler is the same for both
    2004-07-02  Bastien Nocera  <hadess@hadess.net>
    
    	* src/file-manager/fm-directory-view.c:
    	(activate_check_mime_types): Don't warn about mime-type mismatches
    	if the handler is the same for both mime-types (Closes: #145086)

--- src/file-manager/fm-directory-view.c
+++ src/file-manager/fm-directory-view.c
@@ -4022,17 +4022,27 @@ activate_check_mime_types (FMDirectoryView *view,
 	char *guessed_mime_type;
 	char *mime_type;
 	gboolean ret;
+	GnomeVFSMimeApplication *default_app;
+	GnomeVFSMimeApplication *guessed_default_app;
 	
 	g_return_val_if_fail (nautilus_file_check_if_ready (file, NAUTILUS_FILE_ATTRIBUTE_SLOW_MIME_TYPE), FALSE);
+
+	ret = TRUE;
 
 	guessed_mime_type = nautilus_file_get_guessed_mime_type (file);
 	mime_type = nautilus_file_get_mime_type (file);
 
 	if (strcmp (guessed_mime_type, mime_type) != 0) {
-		warn_mismatched_mime_types (view, file);
-		ret = FALSE;
-	} else {
-		ret = TRUE;
+		default_app = gnome_vfs_mime_get_default_application
+			(mime_type);
+		guessed_default_app = gnome_vfs_mime_get_default_application
+			(guessed_mime_type);
+		if (default_app->id == NULL ||
+		    guessed_default_app->id == NULL ||
+		    strcmp (default_app->id, guessed_default_app->id) != 0) {
+			warn_mismatched_mime_types (view, file);
+			ret = FALSE;
+		}
 	}
 
 	g_free (guessed_mime_type);
