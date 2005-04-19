93c7f2deef476379c5e1363e9907d2fa86b14e02
Christian Neumair <cneumair@src.gnome.org>
2005-04-19 12:05:54 +0000

    Eeek! Revert bogus autogen/configure.in commit.

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,6 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="nautilus"
-REQUIRED_AUTOMAKE_VERSION=1.7
 
 (test -f $srcdir/configure.in \
   && test -f $srcdir/README \
@@ -19,4 +18,4 @@ which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME CVS"
     exit 1
 }
-. gnome-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.7 . gnome-autogen.sh
