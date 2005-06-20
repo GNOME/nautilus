2fdbb10670fb606a27470af90c35ca5fa3147a1d
Martin Wehner <martin.wehner@gmail.com>
2005-06-20 07:43:48 +0000

    Make mime sniffing a high priority job. Prevents activation from getting
    2005-06-20  Martin Wehner  <martin.wehner@gmail.com>
    
    	* libnautilus-private/nautilus-directory-async.c:
    	(start_or_stop_io), (file_needs_high_priority_work_done),
    	(file_needs_low_priority_work_done):
    	Make mime sniffing a high priority job. Prevents activation
    	from getting blocked by deep count I/O. (#145560)

--- libnautilus-private/nautilus-directory-async.c
+++ libnautilus-private/nautilus-directory-async.c
@@ -3345,7 +3345,6 @@ start_or_stop_io (NautilusDirectory *directory)
 
 		if (file_needs_low_priority_work_done (directory, file)) {
 			/* Start getting attributes if possible */
-			file_info_start (directory, file); /* for slow mime type */
 			directory_count_start (directory, file);
 			deep_count_start (directory, file);
 			mime_list_start (directory, file);
@@ -3574,6 +3573,10 @@ file_needs_high_priority_work_done (NautilusDirectory *directory,
 		return TRUE;
 	}
 
+	if (is_needy (file, lacks_slow_mime_type, wants_slow_mime_type)) {
+		return TRUE;
+	}
+
 	return FALSE;
 }
 
@@ -3590,10 +3593,6 @@ file_needs_low_priority_work_done (NautilusDirectory *directory,
 	}
 
 	if (is_needy (file, lacks_mime_list, wants_mime_list)) {
-		return TRUE;
-	}
-
-	if (is_needy (file, lacks_slow_mime_type, wants_slow_mime_type)) {
 		return TRUE;
 	}
 
