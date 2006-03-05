e8415e35901bc9080090f46d40a7f312e60d9403
Martin Wehner <martin.wehner@gmail.com>
2006-03-05 16:19:20 +0000

    Add file to work queue of enclosing directory when recomputing deep count
    2006-03-05  Martin Wehner  <martin.wehner@gmail.com>
    
    	* libnautilus-private/nautilus-file.c:
    	(nautilus_file_recompute_deep_counts):
    	Add file to work queue of enclosing directory when recomputing
    	deep count for the properties dialog. Fixes #169342.
    
    	Patch from Christian Neumair

--- libnautilus-private/nautilus-file.c
+++ libnautilus-private/nautilus-file.c
@@ -3338,6 +3338,7 @@ nautilus_file_recompute_deep_counts (NautilusFile *file)
 	if (file->details->deep_counts_status != NAUTILUS_REQUEST_IN_PROGRESS) {
 		file->details->deep_counts_status = NAUTILUS_REQUEST_NOT_STARTED;
 		if (file->details->directory != NULL) {
+			nautilus_directory_add_file_to_work_queue (file->details->directory, file);
 			nautilus_directory_async_state_changed (file->details->directory);
 		}
 	}
