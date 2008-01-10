0f05bd800502d2280389c1803288514f2ba6d07b
Alexander Larsson <alexl@redhat.com>
2008-01-10 13:11:28 +0000

    Fix assertion crash on cancel.
    2008-01-10  Alexander Larsson  <alexl@redhat.com>
    
            * libnautilus-private/nautilus-mime-actions.c:
            (cancel_activate_callback):
    	Fix assertion crash on cancel.
    
    
    svn path=/trunk/; revision=13582

--- libnautilus-private/nautilus-mime-actions.c
+++ libnautilus-private/nautilus-mime-actions.c
@@ -905,6 +905,7 @@ cancel_activate_callback (gpointer callback_data)
 
 	if (parameters->files_handle) {
 		nautilus_file_list_cancel_call_when_ready (parameters->files_handle);
+		parameters->files_handle = NULL;
 		activation_parameters_free (parameters);
 	}
 }
