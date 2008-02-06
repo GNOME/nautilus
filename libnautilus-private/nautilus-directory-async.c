fff1fc0665ae2b95943a93a05d3864b296e42039
Alexander Larsson <alexl@redhat.com>
2008-02-06 15:34:28 +0000

    Avoid crashing if gvfs backend gives NULL as name for a new file.
    2008-02-06  Alexander Larsson  <alexl@redhat.com>
    
            * libnautilus-private/nautilus-directory-async.c:
            (directory_load_one):
    	Avoid crashing if gvfs backend gives NULL as name for
    	a new file. (#511601)
    
    
    svn path=/trunk/; revision=13699

--- libnautilus-private/nautilus-directory-async.c
+++ libnautilus-private/nautilus-directory-async.c
@@ -957,6 +957,16 @@ directory_load_one (NautilusDirectory *directory,
 		return;
 	}
 
+	if (g_file_info_get_name (info) == NULL) {
+		char *uri;
+
+		uri = nautilus_directory_get_uri (directory);
+		g_warning ("Got GFileInfo with NULL name in %s, ignoring. This shouldn't happen unless the gvfs backend is broken.\n", uri);
+		g_free (uri);
+		
+		return;
+	}
+	
 	/* Arrange for the "loading" part of the work. */
 	g_object_ref (info);
 	directory->details->pending_file_info
