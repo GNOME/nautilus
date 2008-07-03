494fd79322ebd1851c92ad7bec17647d3a1ebe3b
Wouter Bolsterlee <wbolster@svn.gnome.org>
2008-07-03 13:33:17 +0000

    Include <glib/gstdio.h> to fix the build. Some cleanup in header imports
    2008-07-03  Wouter Bolsterlee  <wbolster@svn.gnome.org>
    
    	* libnautilus-private/nautilus-emblem-utils.c:
    	Include <glib/gstdio.h> to fix the build. Some cleanup
    	in header imports to improve code legibility.
    
    	* libnautilus-private/nautilus-file.c:
    	Include <glib/gstdio.h> here as well.
    
    svn path=/trunk/; revision=14319

--- libnautilus-private/nautilus-emblem-utils.c
+++ libnautilus-private/nautilus-emblem-utils.c
@@ -30,12 +30,17 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include "nautilus-file.h"
+
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+
 #include <eel/eel-glib-extensions.h>
 #include <eel/eel-gdk-pixbuf-extensions.h>
 #include <eel/eel-stock-dialogs.h>
-#include <glib/gi18n.h>
-#include <gtk/gtk.h>
+
+#include "nautilus-file.h"
 #include "nautilus-emblem-utils.h"
 
 #define EMBLEM_NAME_TRASH   "emblem-trash"
--- libnautilus-private/nautilus-file.c
+++ libnautilus-private/nautilus-file.c
@@ -56,6 +56,7 @@
 #include <grp.h>
 #include <gtk/gtk.h>
 #include <glib/gi18n.h>
+#include <glib/gstdio.h>
 #include <gio/gio.h>
 #include <glib/gurifuncs.h>
 #include <libgnome/gnome-macros.h>
