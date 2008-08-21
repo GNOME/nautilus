07c1d8321e89e58b61253e125487b68b73630a20
Cosimo Cecchi <cosimoc@gnome.org>
2008-08-21 10:27:05 +0000

    Don't show "Browse Folders" in spatial mode if no folders are selected.
    2008-08-21  Cosimo Cecchi  <cosimoc@gnome.org>
    
    	* src/file-manager/fm-directory-view.c: (real_update_menus):
    	Don't show "Browse Folders" in spatial mode if no folders are
    	selected.
    	Patch by Paweł Paprota (#351431).
    
    svn path=/trunk/; revision=14513

--- src/file-manager/fm-directory-view.c
+++ src/file-manager/fm-directory-view.c
@@ -7787,7 +7787,7 @@ real_update_menus (FMDirectoryView *view)
 	
 	g_free (label_with_underscore);
 
-	show_open_alternate = file_list_all_are_folders (selection);
+	show_open_alternate = file_list_all_are_folders (selection) && selection_count > 0;
 	show_open_folder_window = FALSE;
 	if (nautilus_window_info_get_window_type (view->details->window) == NAUTILUS_WINDOW_NAVIGATION) {
 		if (eel_preferences_get_boolean (NAUTILUS_PREFERENCES_ALWAYS_USE_BROWSER)) {
