fd2e7311edd0d673adc21efdd7d4c6585723c730
Alexander Larsson <alexl@redhat.com>
2009-12-04 11:48:54 +0100

    Revert commit 7b86b78e2ff6a0f1b45ac8f8a9c30cf3d0c6ae96
    This is not the right solution

--- eel/eel-background.c
+++ eel/eel-background.c
@@ -563,8 +563,6 @@ eel_background_set_color (EelBackground *background,
 		background->details->color = g_strdup (color);
 		
 		set_image_properties (background);
-
-		g_signal_emit (background, signals[APPEARANCE_CHANGED], 0);
 	}
 }
 
