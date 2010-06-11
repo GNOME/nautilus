33a1a775679f68684e124fba9965d62a8cd11565
Cosimo Cecchi <cosimoc@gnome.org>
2010-06-11 13:23:58 +0200

    [src] GSEAL nautilus-window.

--- src/nautilus-window.c
+++ src/nautilus-window.c
@@ -333,10 +333,10 @@ update_cursor (NautilusWindow *window)
 
 	if (slot->allow_stop) {
 		cursor = gdk_cursor_new (GDK_WATCH);
-		gdk_window_set_cursor (GTK_WIDGET (window)->window, cursor);
+		gdk_window_set_cursor (gtk_widget_get_window (GTK_WIDGET (window)), cursor);
 		gdk_cursor_unref (cursor);
 	} else {
-		gdk_window_set_cursor (GTK_WIDGET (window)->window, NULL);
+		gdk_window_set_cursor (gtk_widget_get_window (GTK_WIDGET (window)), NULL);
 	}
 }
 
