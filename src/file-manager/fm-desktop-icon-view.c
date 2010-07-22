831787a7460655ebbac3499d399d72d45b1cdc60
Alexander Larsson <alexl@redhat.com>
2010-07-22 21:50:06 +0200

    Fix desktop-font gsettings conversion

--- src/file-manager/fm-desktop-icon-view.c
+++ src/file-manager/fm-desktop-icon-view.c
@@ -90,6 +90,8 @@ static void     real_merge_menus                                  (FMDirectoryVi
 static void     real_update_menus                                 (FMDirectoryView        *view);
 static gboolean real_supports_zooming                             (FMDirectoryView        *view);
 static void     fm_desktop_icon_view_update_icon_container_fonts  (FMDesktopIconView      *view);
+static void     font_changed_callback                             (gpointer                callback_data);
+
 
 EEL_CLASS_BOILERPLATE (FMDesktopIconView,
 		       fm_desktop_icon_view,
@@ -292,6 +294,9 @@ fm_desktop_icon_view_finalize (GObject *object)
 
 	g_signal_handlers_disconnect_by_func (nautilus_icon_view_preferences,
 					      default_zoom_level_changed,
+					      icon_view);
+	g_signal_handlers_disconnect_by_func (nautilus_preferences,
+					      font_changed_callback,
 					      icon_view);
 
 	eel_preferences_remove_callback (NAUTILUS_PREFERENCES_LOCKDOWN_COMMAND_LINE,
@@ -601,10 +606,11 @@ fm_desktop_icon_view_init (FMDesktopIconView *desktop_icon_view)
 				  G_CALLBACK (default_zoom_level_changed),
 				  desktop_icon_view);
 
-	eel_preferences_add_callback_while_alive (NAUTILUS_PREFERENCES_DESKTOP_FONT,
-						  font_changed_callback, 
-						  desktop_icon_view, G_OBJECT (desktop_icon_view));
-	
+	g_signal_connect_swapped (nautilus_preferences,
+				  "changed::" NAUTILUS_PREFERENCES_DESKTOP_FONT,
+				  G_CALLBACK (font_changed_callback),
+				  desktop_icon_view);
+
 	default_zoom_level_changed (desktop_icon_view);
 	fm_desktop_icon_view_update_icon_container_fonts (desktop_icon_view);
 
