13bfe3958472ae96e1c6c7b1bd597ae4bfbd1380
Cosimo Cecchi <cosimoc@gnome.org>
2010-08-03 01:47:57 +0200

    Don't unref the DBus proxy if it's NULL

--- libnautilus-private/nautilus-mime-actions.c
+++ libnautilus-private/nautilus-mime-actions.c
@@ -1152,11 +1152,15 @@ activate_parameters_install_free (ActivateParametersInstall *parameters_install)
 	if (parameters_install->parent_window) {
 		g_object_remove_weak_pointer (G_OBJECT (parameters_install->parent_window), (gpointer *)&parameters_install->parent_window);
 	}
+
+	if (parameters_install->proxy != NULL) {
+	        g_object_unref (parameters_install->proxy);
+	}
+
 	nautilus_file_unref (parameters_install->file);
 	nautilus_file_list_free (parameters_install->files);
 	g_free (parameters_install->activation_directory);
 	g_free (parameters_install->uri);
-        g_object_unref (parameters_install->proxy);
 	g_free (parameters_install);
 }
 
