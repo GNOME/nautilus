d2fb7d661a639633473c9339ea2004f18ad3fcf2
Cosimo Cecchi <cosimoc@gnome.org>
2010-08-19 16:03:12 +0200

    [eel] remove eel-art-gtk-extensions

--- eel/eel.h
+++ eel/eel.h
@@ -26,7 +26,6 @@
 #define EEL_H
 
 #include <eel/eel-art-extensions.h>
-#include <eel/eel-art-gtk-extensions.h>
 #include <eel/eel-background.h>
 #include <eel/eel-gdk-extensions.h>
 #include <eel/eel-gdk-pixbuf-extensions.h>
