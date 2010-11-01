802e1967cdf301a50b8403cfe820097561c60033
Cosimo Cecchi <cosimoc@gnome.org>
2010-11-01 03:35:02 +0100

    [eel] remove unused eel_gradient code

--- eel/eel-gdk-extensions.h
+++ eel/eel-gdk-extensions.h
@@ -69,31 +69,6 @@ typedef enum {
 	EEL_GDK_Y_NEGATIVE   = 0x20
 } EelGdkGeometryFlags;
 
-/* A gradient spec. is a string that contains a specifier for either a
-   color or a gradient. If the string has a "-" in it, then it's a gradient.
-   The gradient is vertical by default and the spec. can end with ":v" to indicate that.
-   If the gradient ends with ":h", the gradient is horizontal.
-*/
-char *              eel_gradient_new                       (const char          *start_color,
-							    const char          *end_color,
-							    gboolean             is_horizontal);
-char *              eel_gradient_parse_one_color_spec      (const char          *spec,
-							    int                 *percent,
-							    const char         **next_spec);
-gboolean            eel_gradient_is_gradient               (const char          *gradient_spec);
-char *              eel_gradient_get_start_color_spec      (const char          *gradient_spec);
-char *              eel_gradient_get_end_color_spec        (const char          *gradient_spec);
-gboolean            eel_gradient_is_horizontal             (const char          *gradient_spec);
-char *              eel_gradient_set_left_color_spec       (const char          *gradient_spec,
-							    const char          *left_color);
-char *              eel_gradient_set_top_color_spec        (const char          *gradient_spec,
-							    const char          *top_color);
-char *              eel_gradient_set_right_color_spec      (const char          *gradient_spec,
-							    const char          *right_color);
-char *              eel_gradient_set_bottom_color_spec     (const char          *gradient_spec,
-							    const char          *bottom_color);
-
-
 /* A version of parse_color that substitutes a default color instead of returning
    a boolean to indicate it cannot be parsed.
 */
