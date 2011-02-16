610b14e6c85d16d519a0c9665ada09b289d21de7
Cosimo Cecchi <cosimoc@gnome.org>
2011-02-16 09:51:47 -0500

    window-pane: move code around
    Makes things more readable.

--- src/nautilus-window-pane.c
+++ src/nautilus-window-pane.c
@@ -28,15 +28,24 @@
 #include "nautilus-window-manage-views.h"
 #include <eel/eel-gtk-macros.h>
 
-static void nautilus_window_pane_init       (NautilusWindowPane *pane);
-static void nautilus_window_pane_class_init (NautilusWindowPaneClass *class);
-static void nautilus_window_pane_dispose    (GObject *object);
-
-G_DEFINE_TYPE (NautilusWindowPane,
-	       nautilus_window_pane,
+G_DEFINE_TYPE (NautilusWindowPane, nautilus_window_pane,
 	       G_TYPE_OBJECT)
 #define parent_class nautilus_window_pane_parent_class
 
+static void
+real_sync_location_widgets (NautilusWindowPane *pane)
+{
+	NautilusWindowSlot *slot;
+
+	/* TODO: Would be nice with a real subclass for spatial panes */
+	g_assert (NAUTILUS_IS_SPATIAL_WINDOW (pane->window));
+
+	slot = pane->active_slot;
+
+	/* Change the location button to match the current location. */
+	nautilus_spatial_window_set_location_button (NAUTILUS_SPATIAL_WINDOW (pane->window),
+						     slot->location);
+}
 
 static inline NautilusWindowSlot *
 get_first_inactive_slot (NautilusWindowPane *pane)
@@ -54,126 +63,6 @@ get_first_inactive_slot (NautilusWindowPane *pane)
 	return NULL;
 }
 
-void
-nautilus_window_pane_show (NautilusWindowPane *pane)
-{
-	pane->visible = TRUE;
-	EEL_CALL_METHOD (NAUTILUS_WINDOW_PANE_CLASS, pane,
-			 show, (pane));
-}
-
-void
-nautilus_window_pane_slot_close (NautilusWindowPane *pane, NautilusWindowSlot *slot)
-{
-	NautilusWindowSlot *next_slot;
-
-	if (pane->window) {
-		NautilusWindow *window;
-		window = pane->window;
-		if (pane->active_slot == slot) {
-			next_slot = get_first_inactive_slot (NAUTILUS_WINDOW_PANE (pane));
-			nautilus_window_set_active_slot (window, next_slot);
-		}
-		nautilus_window_close_slot (slot);
-
-		/* If that was the last slot in the active pane, close the pane or even the whole window. */
-		if (window->details->active_pane->slots == NULL) {
-			NautilusWindowPane *next_pane;
-			next_pane = nautilus_window_get_next_pane (window);
-			
-			/* If next_pane is non-NULL, we have more than one pane available. In this
-			 * case, close the current pane and switch to the next one. If there is
-			 * no next pane, close the window. */
-			if(next_pane) {
-				nautilus_window_close_pane (pane);
-				nautilus_window_pane_switch_to (next_pane);
-				if (NAUTILUS_IS_NAVIGATION_WINDOW (window)) {
-					nautilus_navigation_window_update_show_hide_menu_items (NAUTILUS_NAVIGATION_WINDOW (window));
-				}
-			} else {
-				nautilus_window_close (window);
-			}
-		}
-	}
-}
-
-static void
-real_sync_location_widgets (NautilusWindowPane *pane)
-{
-	NautilusWindowSlot *slot;
-
-	/* TODO: Would be nice with a real subclass for spatial panes */
-	g_assert (NAUTILUS_IS_SPATIAL_WINDOW (pane->window));
-
-	slot = pane->active_slot;
-
-	/* Change the location button to match the current location. */
-	nautilus_spatial_window_set_location_button (NAUTILUS_SPATIAL_WINDOW (pane->window),
-						     slot->location);
-}
-
-
-void
-nautilus_window_pane_sync_location_widgets (NautilusWindowPane *pane)
-{
-	EEL_CALL_METHOD (NAUTILUS_WINDOW_PANE_CLASS, pane,
-			 sync_location_widgets, (pane));
-}
-
-void
-nautilus_window_pane_sync_search_widgets (NautilusWindowPane *pane)
-{
-	g_assert (NAUTILUS_IS_WINDOW_PANE (pane));
-
-	EEL_CALL_METHOD (NAUTILUS_WINDOW_PANE_CLASS, pane,
-			 sync_search_widgets, (pane));
-}
-
-void
-nautilus_window_pane_grab_focus (NautilusWindowPane *pane)
-{
-	if (NAUTILUS_IS_WINDOW_PANE (pane) && pane->active_slot) {
-		nautilus_view_grab_focus (pane->active_slot->content_view);
-	}	
-}
-
-void
-nautilus_window_pane_switch_to (NautilusWindowPane *pane)
-{
-	nautilus_window_pane_grab_focus (pane);
-}
-
-static void
-nautilus_window_pane_init (NautilusWindowPane *pane)
-{
-	pane->slots = NULL;
-	pane->active_slot = NULL;
-	pane->is_active = FALSE;
-}
-
-void
-nautilus_window_pane_set_active (NautilusWindowPane *pane, gboolean is_active)
-{
-	if (is_active == pane->is_active) {
-		return;
-	}
-
-	pane->is_active = is_active;
-
-	/* notify the current slot about its activity state (so that it can e.g. modify the bg color) */
-	nautilus_window_slot_is_in_active_pane (pane->active_slot, is_active);
-
-	EEL_CALL_METHOD (NAUTILUS_WINDOW_PANE_CLASS, pane,
-			 set_active, (pane, is_active));
-}
-
-static void
-nautilus_window_pane_class_init (NautilusWindowPaneClass *class)
-{
-	G_OBJECT_CLASS (class)->dispose = nautilus_window_pane_dispose;
-	NAUTILUS_WINDOW_PANE_CLASS (class)->sync_location_widgets = real_sync_location_widgets;
-}
-
 static void
 nautilus_window_pane_dispose (GObject *object)
 {
@@ -183,6 +72,23 @@ nautilus_window_pane_dispose (GObject *object)
 
 	pane->window = NULL;
 	G_OBJECT_CLASS (parent_class)->dispose (object);
+}
+
+static void
+nautilus_window_pane_class_init (NautilusWindowPaneClass *klass)
+{
+	GObjectClass *oclass = G_OBJECT_CLASS (klass);
+
+	oclass->dispose = nautilus_window_pane_dispose;
+	klass->sync_location_widgets = real_sync_location_widgets;
+}
+
+static void
+nautilus_window_pane_init (NautilusWindowPane *pane)
+{
+	pane->slots = NULL;
+	pane->active_slot = NULL;
+	pane->is_active = FALSE;
 }
 
 NautilusWindowPane *
@@ -210,4 +116,85 @@ nautilus_window_pane_get_slot_for_content_box (NautilusWindowPane *pane,
 		}
 	}
 	return NULL;
+}
+
+void
+nautilus_window_pane_set_active (NautilusWindowPane *pane, gboolean is_active)
+{
+	if (is_active == pane->is_active) {
+		return;
+	}
+
+	pane->is_active = is_active;
+
+	/* notify the current slot about its activity state (so that it can e.g. modify the bg color) */
+	nautilus_window_slot_is_in_active_pane (pane->active_slot, is_active);
+
+	EEL_CALL_METHOD (NAUTILUS_WINDOW_PANE_CLASS, pane,
+			 set_active, (pane, is_active));
+}
+
+void
+nautilus_window_pane_show (NautilusWindowPane *pane)
+{
+	pane->visible = TRUE;
+	EEL_CALL_METHOD (NAUTILUS_WINDOW_PANE_CLASS, pane,
+			 show, (pane));
+}
+
+
+void
+nautilus_window_pane_sync_location_widgets (NautilusWindowPane *pane)
+{
+	EEL_CALL_METHOD (NAUTILUS_WINDOW_PANE_CLASS, pane,
+			 sync_location_widgets, (pane));
+}
+
+void
+nautilus_window_pane_sync_search_widgets (NautilusWindowPane *pane)
+{
+	EEL_CALL_METHOD (NAUTILUS_WINDOW_PANE_CLASS, pane,
+			 sync_search_widgets, (pane));
+}
+
+void
+nautilus_window_pane_slot_close (NautilusWindowPane *pane, NautilusWindowSlot *slot)
+{
+	NautilusWindowSlot *next_slot;
+
+	if (pane->window) {
+		NautilusWindow *window;
+		window = pane->window;
+		if (pane->active_slot == slot) {
+			next_slot = get_first_inactive_slot (NAUTILUS_WINDOW_PANE (pane));
+			nautilus_window_set_active_slot (window, next_slot);
+		}
+		nautilus_window_close_slot (slot);
+
+		/* If that was the last slot in the active pane, close the pane or even the whole window. */
+		if (window->details->active_pane->slots == NULL) {
+			NautilusWindowPane *next_pane;
+			next_pane = nautilus_window_get_next_pane (window);
+			
+			/* If next_pane is non-NULL, we have more than one pane available. In this
+			 * case, close the current pane and switch to the next one. If there is
+			 * no next pane, close the window. */
+			if(next_pane) {
+				nautilus_window_close_pane (pane);
+				if (NAUTILUS_IS_NAVIGATION_WINDOW (window)) {
+					nautilus_navigation_window_update_show_hide_menu_items (NAUTILUS_NAVIGATION_WINDOW (window));
+				}
+			} else {
+				nautilus_window_close (window);
+			}
+		}
+	}
+}
+
+void
+nautilus_window_pane_grab_focus (NautilusWindowPane *pane)
+{
+	if (NAUTILUS_IS_WINDOW_PANE (pane) && pane->active_slot) {
+		nautilus_view_grab_focus (pane->active_slot->content_view);
+	}	
 }
