GNOME/nautilus

commit b9c51fd4eb84a9f52ee4be1bf183fc516984130b

view: show applications for non launcher desktop files
https://bugzilla.gnome.org/show_bug.cgi?id=319981

src/nautilus-view.c
@@ -4491,9 +4491,8 @@ reset_open_with_menu (NautilusView *view, GList *selection)
 
 		file = NAUTILUS_FILE (node->data);
 
-		other_applications_visible &=
-			(!nautilus_mime_file_opens_in_view (file) ||
-			 nautilus_file_is_directory (file));
+		other_applications_visible &= (!nautilus_mime_file_opens_in_view (file) &&
+					       !nautilus_file_is_nautilus_link (file));
 	}
 
 	default_app = NULL;

src/nautilus-mime-actions.c
@@ -759,8 +759,7 @@ gboolean
 nautilus_mime_file_opens_in_view (NautilusFile *file)
 {
   return (nautilus_file_is_directory (file) ||
-	  NAUTILUS_IS_DESKTOP_ICON_FILE (file) ||
-	  nautilus_file_is_nautilus_link (file));
+	  NAUTILUS_IS_DESKTOP_ICON_FILE (file));
 }
 
 static ActivationAction
@@ -769,7 +768,7 @@ get_activation_action (NautilusFile *file)
 	ActivationAction action;
 	char *activation_uri;
 
-	if (nautilus_file_is_launcher (file)) {
+	if (nautilus_file_is_nautilus_link (file)) {
 		return ACTIVATION_ACTION_LAUNCH_DESKTOP_FILE;
 	}
 	

commit c1551dfcfa9c92bf43c57b219535f63a9fecdad4

view: ensure we show "Open with..." entries for folders
This is a regression from commit
b9c51fd4eb84a9f52ee4be1bf183fc516984130b

https://bugzilla.gnome.org/show_bug.cgi?id=672809

src/nautilus-view.c
@@ -4491,8 +4491,9 @@ reset_open_with_menu (NautilusView *view, GList *selection)
 
 		file = NAUTILUS_FILE (node->data);
 
-		other_applications_visible &= (!nautilus_mime_file_opens_in_view (file) &&
-					       !nautilus_file_is_nautilus_link (file));
+		other_applications_visible &= ((!nautilus_mime_file_opens_in_view (file) &&
+						!nautilus_file_is_nautilus_link (file)) ||
+					       nautilus_file_is_directory (file));
 	}
 
 	default_app = NULL;