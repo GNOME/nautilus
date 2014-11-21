GNOME/nautilus

commit 37be3809bc0d25d28298b38bc0b893a6fd3a665f

Remove all traces of former nautilus metadata code

libnautilus-private/nautilus-file.c
@@ -25,7 +25,6 @@
 #include <config.h>
 #include "nautilus-file.h"
 
-#include "nautilus-directory-metafile.h"
 #include "nautilus-directory-notify.h"
 #include "nautilus-directory-private.h"
 #include "nautilus-signaller.h"
@@ -90,9 +89,6 @@
 /* Files that start with these characters sort after files that don't. */
 #define SORT_LAST_CHAR1 '.'
 #define SORT_LAST_CHAR2 '#'
-
-/* Name to use to tag metadata for the directory itself. */
-#define FILE_NAME_FOR_DIRECTORY_METADATA "."
 
 /* Name of Nautilus trash directories */
 #define TRASH_DIRECTORY_NAME ".Trash"
@@ -1451,15 +1447,6 @@ rename_get_info_callback (GObject *source_object,
 		old_name = g_strdup (eel_ref_str_peek (op->file->details->name));
 		
 		update_info_and_name (op->file, new_info);
-		
-		/* Self-owned files store their metadata under the
-		 * hard-code name "."  so there's no need to rename
-		 * their metadata when they are renamed.
-		 */
-		if (!nautilus_file_is_self_owned (op->file)) {
-			nautilus_directory_rename_file_metadata
-				(directory, old_name, eel_ref_str_peek (op->file->details->name));
-		}
 		
 		g_free (old_name);
 		
@@ -3241,7 +3228,9 @@ set_metadata_get_info_callback (GObject *source_object,
 	error = NULL;
 	new_info = g_file_query_info_finish (G_FILE (source_object), res, &error);
 	if (new_info != NULL) {
-		nautilus_file_update_info (file, new_info);
+		if (nautilus_file_update_info (file, new_info)) {
+			nautilus_file_changed (file);
+		}
 		g_object_unref (new_info);
 	}
 	nautilus_file_unref (file);
@@ -3307,7 +3296,6 @@ nautilus_file_set_metadata (NautilusFile *file,
 		val = "<null>";
 	}
 
-	g_print ("setting key %s to %s\n", key, val);
 	gio_key = g_strconcat ("metadata::", key, NULL);
 	g_file_info_set_attribute_string (info, gio_key, val);
 	g_free (gio_key);
@@ -3349,7 +3337,6 @@ nautilus_file_set_metadata_list (NautilusFile *file,
 	}
 	val[i] = NULL;
 
-	g_print ("setting list key %s to %d items\n", key, len);
 	gio_key = g_strconcat ("metadata::", key, NULL);
 	g_file_info_set_attribute_stringv (info, gio_key, val);
 	g_free (gio_key);
@@ -4681,7 +4668,9 @@ set_attributes_get_info_callback (GObject *source_object,
 	error = NULL;
 	new_info = g_file_query_info_finish (G_FILE (source_object), res, &error);
 	if (new_info != NULL) {
-		nautilus_file_update_info (op->file, new_info);
+		if (nautilus_file_update_info (op->file, new_info)) {
+			nautilus_file_changed (op->file);
+		}
 		g_object_unref (new_info);
 	}
 	nautilus_file_operation_complete (op, NULL, error);
@@ -7208,7 +7197,6 @@ nautilus_file_get_all_attributes (void)
 		NAUTILUS_FILE_ATTRIBUTE_DEEP_COUNTS |
 		NAUTILUS_FILE_ATTRIBUTE_DIRECTORY_ITEM_COUNT | 
 		NAUTILUS_FILE_ATTRIBUTE_DIRECTORY_ITEM_MIME_TYPES | 
-		NAUTILUS_FILE_ATTRIBUTE_METADATA | 
 		NAUTILUS_FILE_ATTRIBUTE_TOP_LEFT_TEXT | 
 		NAUTILUS_FILE_ATTRIBUTE_LARGE_TOP_LEFT_TEXT |
 		NAUTILUS_FILE_ATTRIBUTE_EXTENSION_INFO |

libnautilus-private/Makefile.am
@@ -75,8 +75,6 @@ libnautilus_private_la_SOURCES = \
 	nautilus-directory-async.c \
 	nautilus-directory-background.c \
 	nautilus-directory-background.h \
-	nautilus-directory-metafile.c \
-	nautilus-directory-metafile.h \
 	nautilus-directory-notify.h \
 	nautilus-directory-private.h \
 	nautilus-directory.c \
@@ -130,8 +128,6 @@ libnautilus_private_la_SOURCES = \
 	nautilus-merged-directory.h \
 	nautilus-metadata.h \
 	nautilus-metadata.c \
-	nautilus-metafile.c \
-	nautilus-metafile.h \
 	nautilus-mime-actions.c \
 	nautilus-mime-actions.h \
 	nautilus-mime-application-chooser.c \

libnautilus-private/nautilus-directory-async.c
@@ -24,7 +24,6 @@
 
 #include <config.h>
 
-#include "nautilus-directory-metafile.h"
 #include "nautilus-directory-notify.h"
 #include "nautilus-directory-private.h"
 #include "nautilus-file-attributes.h"
@@ -34,7 +33,6 @@
 #include "nautilus-global-preferences.h"
 #include "nautilus-link.h"
 #include "nautilus-marshal.h"
-#include "nautilus-metafile.h"
 #include <eel/eel-glib-extensions.h>
 #include <eel/eel-string.h>
 #include <gtk/gtk.h>
@@ -663,10 +661,6 @@ nautilus_directory_set_up_request (NautilusFileAttributes file_attributes)
 		REQUEST_SET_TYPE (request, REQUEST_FILE_INFO);
 	}
 
-	if ((file_attributes & NAUTILUS_FILE_ATTRIBUTE_METADATA) != 0) {
-		REQUEST_SET_TYPE (request, REQUEST_METAFILE);
-	}
-	
 	if ((file_attributes & NAUTILUS_FILE_ATTRIBUTE_EXTENSION_INFO) != 0) {
 		REQUEST_SET_TYPE (request, REQUEST_EXTENSION_INFO);
 	}
@@ -698,7 +692,6 @@ mime_db_changed_callback (GObject *ignore, NautilusDirectory *dir)
 
 	attrs = NAUTILUS_FILE_ATTRIBUTE_INFO |
 		NAUTILUS_FILE_ATTRIBUTE_LINK_INFO |
-		NAUTILUS_FILE_ATTRIBUTE_METADATA |
 		NAUTILUS_FILE_ATTRIBUTE_DIRECTORY_ITEM_MIME_TYPES;
 
 	nautilus_directory_force_reload_internal (dir, attrs);
@@ -753,13 +746,6 @@ nautilus_directory_monitor_add_internal (NautilusDirectory *directory,
 		directory->details->monitor = nautilus_monitor_directory (directory->details->location);
 	}
 	
-	/* We could just call update_metadata_monitors here, but we can be smarter
-	 * since we know what monitor was just added.
-	 */
-	if (REQUEST_WANTS_TYPE (monitor->request, REQUEST_METAFILE) &&
-	    !directory->details->metafile_monitored) {
-		nautilus_directory_register_metadata_monitor (directory);	
-	}
 
 	if (REQUEST_WANTS_TYPE (monitor->request, REQUEST_FILE_INFO) &&
 	    directory->details->mime_db_monitor == 0) {
@@ -1119,39 +1105,6 @@ directory_load_done (NautilusDirectory *directory,
 	directory_load_cancel (directory);
 }
 
-/* This checks if there's a request for the metafile contents. */
-static gboolean
-is_anyone_waiting_for_metafile (NautilusDirectory *directory)
-{
-	if (directory->details->call_when_ready_counters[REQUEST_METAFILE] > 0) {
-		return TRUE;
-	}
-
-	if (directory->details->monitor_counters[REQUEST_METAFILE] > 0) {
-		return TRUE;
-	}
-
-	return FALSE;
-}
-
-static void
-update_metadata_monitors (NautilusDirectory *directory)
-{
-	gboolean is_metadata_monitored;
-	
-	is_metadata_monitored = is_anyone_waiting_for_metafile (directory);
-	
-	if (!directory->details->metafile_monitored) {
-		if (is_metadata_monitored) {
-			nautilus_directory_register_metadata_monitor (directory);
-		}
-	} else {
-		if (!is_metadata_monitored) {
-			nautilus_directory_unregister_metadata_monitor (directory);
-		}
-	}
-}
-
 void
 nautilus_directory_monitor_remove_internal (NautilusDirectory *directory,
 					    NautilusFile *file,
@@ -1168,8 +1121,6 @@ nautilus_directory_monitor_remove_internal (NautilusDirectory *directory,
 		nautilus_monitor_cancel (directory->details->monitor);
 		directory->details->monitor = NULL;
 	}
-
-	update_metadata_monitors (directory);
 
 	/* XXX - do we need to remove anything from the work queue? */
 
@@ -1200,8 +1151,6 @@ nautilus_directory_remove_file_monitors (NautilusDirectory *directory,
 		}
 	}
 
-	update_metadata_monitors (directory);
-
 	/* XXX - do we need to remove anything from the work queue? */
 
 	nautilus_directory_async_state_changed (directory);
@@ -1229,7 +1178,6 @@ nautilus_directory_add_file_monitors (NautilusDirectory *directory,
 
 	nautilus_directory_add_file_to_work_queue (directory, file);
 
-	update_metadata_monitors (directory);
 	nautilus_directory_async_state_changed (directory);
 }
 
@@ -1374,15 +1322,6 @@ nautilus_directory_call_when_ready_internal (NautilusDirectory *directory,
 	request_counter_add_request (directory->details->call_when_ready_counters,
 				     callback.request);
 
-	/* When we change the ready list we need to sync up metadata monitors.
-	 * We could just call update_metadata_monitors here, but we can be smarter
-	 * since we know what was just added.
-	 */
-	if (REQUEST_WANTS_TYPE (callback.request, REQUEST_METAFILE) &&
-	    !directory->details->metafile_monitored) {
-		nautilus_directory_register_metadata_monitor (directory);	
-	}
-
 	/* Put the callback file or all the files on the work queue. */
 	if (file != NULL) {
 		nautilus_directory_add_file_to_work_queue (directory, file);
@@ -1468,8 +1407,6 @@ nautilus_directory_cancel_callback_internal (NautilusDirectory *directory,
 					   ready_callback_key_compare);
 		if (node != NULL) {
 			remove_callback_link (directory, node);
-			/* When we change the ready list we need to sync up metadata monitors. */
-			update_metadata_monitors (directory);
 			
 			nautilus_directory_async_state_changed (directory);
 		}
@@ -1597,11 +1534,6 @@ nautilus_async_destroying_file (NautilusFile *file)
 			remove_monitor_link (directory, node);
 			changed = TRUE;
 		}
-	}
-
-	/* When we change the monitor or ready list we need to sync up metadata monitors */
-	if (changed) {
-		update_metadata_monitors (directory);
 	}
 
 	/* Check if it's a file that's currently being worked on.
@@ -1799,11 +1731,6 @@ request_is_satisfied (NautilusDirectory *directory,
 		      NautilusFile *file,
 		      Request request)
 {
-	if (REQUEST_WANTS_TYPE (request, REQUEST_METAFILE) &&
-	    !nautilus_directory_is_metadata_read (directory)) {
-		return FALSE;
-	}
-
 	if (REQUEST_WANTS_TYPE (request, REQUEST_FILE_LIST) &&
 	    !(directory->details->directory_loaded &&
 				    directory->details->directory_loaded_sent_notification)) {
@@ -1908,9 +1835,6 @@ call_ready_callbacks_at_idle (gpointer callback_data)
 		ready_callback_call (directory, callback);
 		g_free (callback);
 	}
-
-	/* When we change the ready list we need to sync up metadata monitors. */
-	update_metadata_monitors (directory);
 
 	nautilus_directory_async_state_changed (directory);
 
@@ -4822,9 +4746,6 @@ cancel_loading_attributes (NautilusDirectory *directory,
 		mount_cancel (directory);
 	}
 	
-	/* FIXME bugzilla.gnome.org 45064: implement cancelling metadata when we
-	   implement invalidating metadata */
-
 	nautilus_directory_async_state_changed (directory);
 }
 
@@ -4866,9 +4787,6 @@ nautilus_directory_cancel_loading_file_attributes (NautilusDirectory      *direc
 	if (REQUEST_WANTS_TYPE (request, REQUEST_MOUNT)) {
 		cancel_mount_for_file (directory, file);
 	}
-
-	/* FIXME bugzilla.gnome.org 45064: implement cancelling metadata when we
-	   implement invalidating metadata */
 
 	nautilus_directory_async_state_changed (directory);
 }

libnautilus-private/nautilus-directory-background.c
@@ -653,7 +653,7 @@ nautilus_connect_background_to_file_metadata (GtkWidget    *widget,
 		/* arrange to receive file metadata */
 		nautilus_file_monitor_add (file,
                                            background,
-                                           NAUTILUS_FILE_ATTRIBUTE_METADATA);
+                                           NAUTILUS_FILE_ATTRIBUTE_INFO);
 
 		/* arrange for notification when the theme changes */
 		eel_preferences_add_callback (NAUTILUS_PREFERENCES_THEME,

libnautilus-private/nautilus-directory-metafile.c
@@ -1,363 +1,0 @@
-/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*-
-
-   nautilus-directory-metafile.c: Nautilus directory model.
- 
-   Copyright (C) 2000, 2001 Eazel, Inc.
-  
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
-  
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-  
-   You should have received a copy of the GNU General Public
-   License along with this program; if not, write to the
-   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
-  
-   Authors: Darin Adler <darin@bentspoon.com>,
-            Mike Engber <engber@eazel.com>
-*/
-
-#include <config.h>
-#include "nautilus-directory-metafile.h"
-
-#include "nautilus-directory-private.h"
-#include "nautilus-file-private.h"
-#include "nautilus-metafile.h"
-#include <eel/eel-debug.h>
-#include <eel/eel-string.h>
-#include <stdio.h>
-
-
-static NautilusMetafile *
-get_metafile (NautilusDirectory *directory)
-{
-	char *uri;
-
-	if (directory->details->metafile == NULL) {
-		uri = nautilus_directory_get_uri (directory);
-		directory->details->metafile = nautilus_metafile_get_for_uri (uri);
-		g_free (uri);
-	}
-
-	return directory->details->metafile;	
-}
-
-gboolean
-nautilus_directory_is_metadata_read (NautilusDirectory *directory)
-{
-	NautilusMetafile *metafile;
-
-	g_return_val_if_fail (NAUTILUS_IS_DIRECTORY (directory), FALSE);
-	
-	metafile = get_metafile (directory);
-
-	if (metafile == NULL) {
-		return TRUE;
-	}
-		
-	return nautilus_metafile_is_read (metafile);
-}
-
-char *
-nautilus_directory_get_file_metadata (NautilusDirectory *directory,
-				      const char *file_name,
-				      const char *key,
-				      const char *default_metadata)
-{
-	NautilusMetafile *metafile;
-
-	g_return_val_if_fail (NAUTILUS_IS_DIRECTORY (directory), g_strdup (default_metadata));
-	g_return_val_if_fail (!eel_str_is_empty (file_name), g_strdup (default_metadata));
-	g_return_val_if_fail (!eel_str_is_empty (key), g_strdup (default_metadata));
-	
-	metafile = get_metafile (directory);
-
-	return nautilus_metafile_get (metafile, file_name, key, default_metadata);
-}
-
-GList *
-nautilus_directory_get_file_metadata_list (NautilusDirectory *directory,
-					   const char *file_name,
-					   const char *list_key,
-					   const char *list_subkey)
-{
-	NautilusMetafile *     metafile;
-
-	g_return_val_if_fail (NAUTILUS_IS_DIRECTORY (directory), NULL);
-	g_return_val_if_fail (!eel_str_is_empty (file_name), NULL);
-	g_return_val_if_fail (!eel_str_is_empty (list_key), NULL);
-	g_return_val_if_fail (!eel_str_is_empty (list_subkey), NULL);
-	
-	metafile = get_metafile (directory);
-	return nautilus_metafile_get_list (metafile, file_name, list_key, list_subkey);
-}
-
-void
-nautilus_directory_set_file_metadata (NautilusDirectory *directory,
-				      const char *file_name,
-				      const char *key,
-				      const char *default_metadata,
-				      const char *metadata)
-{
-	NautilusMetafile *metafile;
-
-	g_return_if_fail (NAUTILUS_IS_DIRECTORY (directory));
-	g_return_if_fail (!eel_str_is_empty (file_name));
-	g_return_if_fail (!eel_str_is_empty (key));
-	
-	metafile = get_metafile (directory);
-	nautilus_metafile_set (metafile, file_name, key, default_metadata, metadata);
-}
-
-void
-nautilus_directory_set_file_metadata_list (NautilusDirectory *directory,
-					   const char *file_name,
-					   const char *list_key,
-					   const char *list_subkey,
-					   GList *list)
-{
-	NautilusMetafile *metafile;
-
-	g_return_if_fail (NAUTILUS_IS_DIRECTORY (directory));
-	g_return_if_fail (!eel_str_is_empty (file_name));
-	g_return_if_fail (!eel_str_is_empty (list_key));
-	g_return_if_fail (!eel_str_is_empty (list_subkey));
-
-	metafile = get_metafile (directory);
-	nautilus_metafile_set_list (metafile,
-				    file_name,
-				    list_key,
-				    list_subkey,
-				    list);
-}
-
-gboolean 
-nautilus_directory_get_boolean_file_metadata (NautilusDirectory *directory,
-					      const char *file_name,
-					      const char *key,
-					      gboolean default_metadata)
-{
-	char *result_as_string;
-	gboolean result;
-
-	result_as_string = nautilus_directory_get_file_metadata
-		(directory, file_name, key,
-		 default_metadata ? "true" : "false");
-	g_assert (result_as_string != NULL);
-	
-	if (g_ascii_strcasecmp (result_as_string, "true") == 0) {
-		result = TRUE;
-	} else if (g_ascii_strcasecmp (result_as_string, "false") == 0) {
-		result = FALSE;
-	} else {
-		g_error ("boolean metadata with value other than true or false");
-		result = default_metadata;
-	}
-
-	g_free (result_as_string);
-	return result;
-}
-
-void
-nautilus_directory_set_boolean_file_metadata (NautilusDirectory *directory,
-					      const char *file_name,
-					      const char *key,
-					      gboolean default_metadata,
-					      gboolean metadata)
-{
-	nautilus_directory_set_file_metadata
-		(directory, file_name, key,
-		 default_metadata ? "true" : "false",
-		 metadata ? "true" : "false");
-}
-
-int 
-nautilus_directory_get_integer_file_metadata (NautilusDirectory *directory,
-					      const char *file_name,
-					      const char *key,
-					      int default_metadata)
-{
-	char *result_as_string;
-	char default_as_string[32];
-	int result;
-	char c;
-
-	g_snprintf (default_as_string, sizeof (default_as_string), "%d", default_metadata);
-	result_as_string = nautilus_directory_get_file_metadata
-		(directory, file_name, key, default_as_string);
-
-	/* Normally we can't get a a NULL, but we check for it here to
-	 * handle the oddball case of a non-existent directory.
-	 */
-	if (result_as_string == NULL) {
-		result = default_metadata;
-	} else {
-		if (sscanf (result_as_string, " %d %c", &result, &c) != 1) {
-			result = default_metadata;
-		}
-		g_free (result_as_string);
-	}
-
-	return result;
-}
-
-void
-nautilus_directory_set_integer_file_metadata (NautilusDirectory *directory,
-					      const char *file_name,
-					      const char *key,
-					      int default_metadata,
-					      int metadata)
-{
-	char value_as_string[32];
-	char default_as_string[32];
-
-	g_snprintf (value_as_string, sizeof (value_as_string), "%d", metadata);
-	g_snprintf (default_as_string, sizeof (default_as_string), "%d", default_metadata);
-
-	nautilus_directory_set_file_metadata
-		(directory, file_name, key,
-		 default_as_string, value_as_string);
-}
-
-void
-nautilus_directory_copy_file_metadata (NautilusDirectory *source_directory,
-				       const char *source_file_name,
-				       NautilusDirectory *destination_directory,
-				       const char *destination_file_name)
-{
-	char *destination_uri;
-	NautilusMetafile *metafile;
-
-	g_return_if_fail (NAUTILUS_IS_DIRECTORY (source_directory));
-	g_return_if_fail (source_file_name != NULL);
-	g_return_if_fail (NAUTILUS_IS_DIRECTORY (destination_directory));
-	g_return_if_fail (destination_file_name != NULL);
-
-	metafile = get_metafile (source_directory);
-	destination_uri = nautilus_directory_get_uri (destination_directory);
-
-	nautilus_metafile_copy (metafile, source_file_name,
-				destination_uri, destination_file_name);
-
-	g_free (destination_uri);
-}
-
-void
-nautilus_directory_remove_file_metadata (NautilusDirectory *directory,
-					 const char *file_name)
-{
-	NautilusMetafile *metafile;
-
-	g_return_if_fail (NAUTILUS_IS_DIRECTORY (directory));
-	g_return_if_fail (file_name != NULL);
-
-	metafile = get_metafile (directory);
-	nautilus_metafile_remove (metafile, file_name);
-}
-
-void
-nautilus_directory_rename_file_metadata (NautilusDirectory *directory,
-					 const char *old_file_name,
-					 const char *new_file_name)
-{
-	NautilusMetafile *metafile;
-
-	g_return_if_fail (NAUTILUS_IS_DIRECTORY (directory));
-	g_return_if_fail (old_file_name != NULL);
-	g_return_if_fail (new_file_name != NULL);
-
-	metafile = get_metafile (directory);
-	nautilus_metafile_rename (metafile, old_file_name,new_file_name);
-}
-
-void
-nautilus_directory_rename_directory_metadata (NautilusDirectory *directory,
-					      const char *new_directory_uri)
-{
-	NautilusMetafile *metafile;
-
-	g_return_if_fail (NAUTILUS_IS_DIRECTORY (directory));
-	g_return_if_fail (new_directory_uri != NULL);
-
-	metafile = get_metafile (directory);
-	nautilus_metafile_rename_directory (metafile, new_directory_uri);
-}
-
-static void
-metafile_changed_cb (NautilusMetafile *metafile,
-		     GList *file_names,
-		     NautilusDirectory *directory)
-{
-	GList                   *file_list;
-	NautilusFile		*file;
-	
-	file_list = NULL;
-	while (file_names != NULL) {
-		file = nautilus_directory_find_file_by_internal_filename
-			(directory, file_names->data);
-
-		if (file != NULL) {
-			if (nautilus_file_is_self_owned (file)) {
-				nautilus_file_emit_changed (file);
-			} else {
-				file_list = g_list_prepend (file_list, file);
-			}
-		}
-		file_names = file_names->next;
-	}
-	
-	if (file_list != NULL) {
-		file_list = g_list_reverse (file_list);
-		nautilus_directory_emit_change_signals (directory, file_list);
-		g_list_free (file_list);
-	}
-}
-
-static void
-metafile_ready_cb (NautilusMetafile *metafile,
-		   NautilusDirectory *directory)
-{
-	emit_change_signals_for_all_files (directory);
-	nautilus_directory_async_state_changed (directory);
-}
-
-
-void
-nautilus_directory_register_metadata_monitor (NautilusDirectory *directory)
-{
-	NautilusMetafile *metafile;
-
-	g_return_if_fail (NAUTILUS_IS_DIRECTORY (directory));
-
-	if (directory->details->metafile_monitored) {
-		return;
-	}
-
-	metafile = get_metafile (directory);
-	directory->details->metafile_monitored = TRUE;
-
-	g_signal_connect (metafile, "changed", (GCallback)metafile_changed_cb, directory);
-	g_signal_connect (metafile, "ready", (GCallback)metafile_ready_cb, directory);
-	
-	nautilus_metafile_load (metafile);
-}
-
-void
-nautilus_directory_unregister_metadata_monitor (NautilusDirectory *directory)
-{
-	NautilusMetafile *metafile;
-
-	g_return_if_fail (NAUTILUS_IS_DIRECTORY (directory));	
-
-	directory->details->metafile_monitored = FALSE;
-
-	metafile = get_metafile (directory);
-	g_signal_handlers_disconnect_by_func (metafile, metafile_changed_cb, directory);
-	g_signal_handlers_disconnect_by_func (metafile, metafile_ready_cb, directory);
-}

libnautilus-private/nautilus-directory-metafile.h
@@ -1,81 +1,0 @@
-/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*-
-
-   nautilus-directory-metafile.h: Nautilus directory model.
- 
-   Copyright (C) 2000 Eazel, Inc.
-  
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
-  
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-  
-   You should have received a copy of the GNU General Public
-   License along with this program; if not, write to the
-   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
-  
-   Author: Darin Adler <darin@bentspoon.com>
-*/
-
-#include <libnautilus-private/nautilus-directory.h>
-
-/* Interface for file metadata. */
-gboolean nautilus_directory_is_metadata_read	      (NautilusDirectory *directory);
-
-char *   nautilus_directory_get_file_metadata         (NautilusDirectory *directory,
-						       const char        *file_name,
-						       const char        *key,
-						       const char        *default_metadata);
-GList *  nautilus_directory_get_file_metadata_list    (NautilusDirectory *directory,
-						       const char        *file_name,
-						       const char        *list_key,
-						       const char        *list_subkey);
-gboolean nautilus_directory_get_boolean_file_metadata (NautilusDirectory *directory,
-						       const char        *file_name,
-						       const char        *key,
-						       gboolean           default_metadata);
-int      nautilus_directory_get_integer_file_metadata (NautilusDirectory *directory,
-						       const char        *file_name,
-						       const char        *key,
-						       int                default_metadata);
-
-void nautilus_directory_set_file_metadata         (NautilusDirectory *directory,
-						   const char        *file_name,
-						   const char        *key,
-						   const char        *default_metadata,
-						   const char        *metadata);
-void nautilus_directory_set_file_metadata_list    (NautilusDirectory *directory,
-						   const char        *file_name,
-						   const char        *list_key,
-						   const char        *list_subkey,
-						   GList             *list);
-void nautilus_directory_set_boolean_file_metadata (NautilusDirectory *directory,
-						   const char        *file_name,
-						   const char        *key,
-						   gboolean           default_metadata,
-						   gboolean           metadata);
-void nautilus_directory_set_integer_file_metadata (NautilusDirectory *directory,
-						   const char        *file_name,
-						   const char        *key,
-						   int                default_metadata,
-						   int                metadata);
-
-void     nautilus_directory_copy_file_metadata             (NautilusDirectory *source_directory,
-							    const char        *source_file_name,
-							    NautilusDirectory *destination_directory,
-							    const char        *destination_file_name);
-void     nautilus_directory_remove_file_metadata           (NautilusDirectory *directory,
-							    const char        *file_name);
-void     nautilus_directory_rename_file_metadata           (NautilusDirectory *directory,
-							    const char        *old_file_name,
-							    const char        *new_file_name);
-void	 nautilus_directory_rename_directory_metadata      (NautilusDirectory *directory,
-							    const char        *new_directory_uri);
-
-void nautilus_directory_register_metadata_monitor   (NautilusDirectory *directory);
-void nautilus_directory_unregister_metadata_monitor (NautilusDirectory *directory);

libnautilus-private/nautilus-directory-private.h
@@ -27,7 +27,6 @@
 #include <libnautilus-private/nautilus-directory.h>
 #include <libnautilus-private/nautilus-file-queue.h>
 #include <libnautilus-private/nautilus-file.h>
-#include <libnautilus-private/nautilus-metafile.h>
 #include <libnautilus-private/nautilus-monitor.h>
 #include <libnautilus-private/nautilus-idle-queue.h>
 #include <libnautilus-extension/nautilus-info-provider.h>
@@ -52,7 +51,6 @@ typedef enum {
 	REQUEST_DIRECTORY_COUNT,
 	REQUEST_FILE_INFO,
 	REQUEST_FILE_LIST, /* always FALSE if file != NULL */
-	REQUEST_METAFILE,
 	REQUEST_MIME_LIST,
 	REQUEST_TOP_LEFT_TEXT,
 	REQUEST_LARGE_TOP_LEFT_TEXT,
@@ -96,10 +94,6 @@ struct NautilusDirectoryDetails
 
 	NautilusMonitor *monitor;
 	gulong 		 mime_db_monitor;
-
-	NautilusIdleQueue *idle_queue;
-	gboolean metafile_monitored;
-	NautilusMetafile *metafile;
 
 	gboolean in_async_service_loop;
 	gboolean state_changed;

commit 9992b0917263e47aff9c0182f965ae93c1a9ac63

file: Remove unused variable
Fallout from 37be3809bc0d25d28298b38bc0b893a6fd3a665f

https://bugzilla.gnome.org/show_bug.cgi?id=740383

libnautilus-private/nautilus-file.c
@@ -1715,7 +1715,6 @@ rename_get_info_callback (GObject *source_object,
 	NautilusFileOperation *op;
 	NautilusDirectory *directory;
 	NautilusFile *existing_file;
-	char *old_name;
 	char *old_uri;
 	char *new_uri;
 	const char *new_name;
@@ -1742,11 +1741,8 @@ rename_get_info_callback (GObject *source_object,
 		}
 		
 		old_uri = nautilus_file_get_uri (op->file);
-		old_name = g_strdup (eel_ref_str_peek (op->file->details->name));
 		
 		update_info_and_name (op->file, new_info);
-		
-		g_free (old_name);
 		
 		new_uri = nautilus_file_get_uri (op->file);
 		nautilus_directory_moved (old_uri, new_uri);