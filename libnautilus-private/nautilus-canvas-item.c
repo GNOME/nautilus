8296f8c3a57340a86d1de754e5d381678b1627e4
Cosimo Cecchi <cosimoc@gnome.org>
2015-01-19 11:33:10 -0800

    canvas-item: add a missing gtk_style_context_restore()

--- libnautilus-private/nautilus-canvas-item.c
+++ libnautilus-private/nautilus-canvas-item.c
@@ -984,6 +984,8 @@ draw_label_text (NautilusCanvasItem *item,
 		gtk_render_layout (context, cr,
 				   x, text_rect.y0 + details->editable_text_height + LABEL_LINE_SPACING + TEXT_BACK_PADDING_Y,
 				   additional_layout);
+
+		gtk_style_context_restore (context);
 	}
 
 	if (item->details->is_highlighted_as_keyboard_focus) {
