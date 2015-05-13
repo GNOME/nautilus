5b197d1bf8123d4dac2be5f24b6f3fd168f50853
Carlos Soriano <csoriano@gnome.org>
2015-05-13 13:14:16 +0200

    nautilus-view: allow folders to be opened in other app
    So the user can open a folder in a different app.
    This is useful in cases like IDEs or text editors
    for entire projects.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=691479

--- src/nautilus-view.c
+++ src/nautilus-view.c
@@ -6207,7 +6207,9 @@ real_update_actions_state (NautilusView *view)
 	action = g_action_map_lookup_action (G_ACTION_MAP (view_action_group),
 					     "open-with-other-application");
 	g_simple_action_set_enabled (G_SIMPLE_ACTION (action),
-				     app != NULL);
+				     app != NULL ||
+                                     (selection_count > 0 &&
+                                      nautilus_file_is_directory (NAUTILUS_FILE (selection->data))));
 
 	action = g_action_map_lookup_action (G_ACTION_MAP (view_action_group),
 					     "open-item-new-tab");
