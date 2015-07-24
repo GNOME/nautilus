bd3b921347503cfe55d4c06eebba25e2939a7d96
Carlos Soriano <csoriano@gnome.org>
2015-07-24 16:30:31 +0200

    window: use watch cursor with pointer

--- src/nautilus-window.c
+++ src/nautilus-window.c
@@ -764,7 +764,7 @@ update_cursor (NautilusWindow *window)
                 GdkDisplay *display;
 
                 display = gtk_widget_get_display (GTK_WIDGET (window));
-		cursor = gdk_cursor_new_for_display (display, GDK_WATCH);
+                cursor = gdk_cursor_new_from_name (display, "left_ptr_watch");
 		gdk_window_set_cursor (gtk_widget_get_window (GTK_WIDGET (window)), cursor);
 		g_object_unref (cursor);
 	} else {
