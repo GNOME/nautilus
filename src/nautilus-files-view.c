180c26f98889e4d1762fad2f03a33253a0e9fd9e
Carlos Soriano <csoriano@gnome.org>
2016-01-13 18:44:04 +0100

    files-view: use finalize instead of dispose
    Dispose is intended to let the object still valid for external
    calls.
    Freeing the action group on dispose makes the view somehow invalid,
    so some calls were made at that time and were making the view to crash.
    
    This change to dispose was made in commit 347369d18eb6 to fix a crash as
    well. However, an upcoming patch fixed it as well as noted in the
    commit message.
    So although dispose seemed good, it's not if it makes the object
    invalid.
    
    On the other hand, instead of dispose we should use destroy, since we
    are already using it, and implementing dispose was kind of an error.
    
    In upcoming patches we will move some disconnect_signal calls from
    finalized to destroy.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=759717

--- src/nautilus-files-view.c
+++ src/nautilus-files-view.c
@@ -2909,7 +2909,7 @@ nautilus_files_view_destroy (GtkWidget *object)
 }
 
 static void
-nautilus_files_view_dispose (GObject *object)
+nautilus_files_view_finalize (GObject *object)
 {
         NautilusFilesView *view;
 
@@ -2937,16 +2937,6 @@ nautilus_files_view_dispose (GObject *object)
                 gtk_popover_set_relative_to (GTK_POPOVER (view->details->rename_file_popover),
                                              NULL);
         }
-
-        G_OBJECT_CLASS (nautilus_files_view_parent_class)->dispose (object);
-}
-
-static void
-nautilus_files_view_finalize (GObject *object)
-{
-        NautilusFilesView *view;
-
-        view = NAUTILUS_FILES_VIEW (object);
 
         g_hash_table_destroy (view->details->non_ready_files);
 
@@ -7958,7 +7948,6 @@ nautilus_files_view_class_init (NautilusFilesViewClass *klass)
         oclass = G_OBJECT_CLASS (klass);
 
         oclass->finalize = nautilus_files_view_finalize;
-        oclass->dispose = nautilus_files_view_dispose;
         oclass->get_property = nautilus_files_view_get_property;
         oclass->set_property = nautilus_files_view_set_property;
 
