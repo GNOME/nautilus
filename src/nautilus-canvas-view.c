8eac8e7718b4190b0f6be52606ad1542d1f6e040
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2016-03-02 00:59:06 -0300

    action-bar: implement NautilusActionBar
    The new NautilusActionBar class handles everything
    needed to be an usable actionbar as envisioned by
    the available mockups.

--- src/nautilus-canvas-view.c
+++ src/nautilus-canvas-view.c
@@ -23,6 +23,7 @@
 
 #include "nautilus-canvas-view.h"
 
+#include "nautilus-action-bar.h"
 #include "nautilus-canvas-view-container.h"
 #include "nautilus-error-reporting.h"
 #include "nautilus-files-view-dnd.h"
@@ -1607,6 +1608,7 @@ nautilus_canvas_view_init (NautilusCanvasView *canvas_view)
     NautilusCanvasContainer *canvas_container;
     GActionGroup *view_action_group;
     GtkClipboard *clipboard;
+    NautilusView *view;
 
     canvas_view->sort = &sort_criteria[0];
     canvas_view->destroyed = FALSE;
@@ -1656,6 +1658,11 @@ nautilus_canvas_view_init (NautilusCanvasView *canvas_view)
     /* Keep the action synced with the actual value, so the toolbar can poll it */
     g_action_group_change_action_state (nautilus_files_view_get_action_group (NAUTILUS_FILES_VIEW (canvas_view)),
                                         "zoom-to-level", g_variant_new_int32 (get_default_zoom_level (canvas_view)));
+
+    /* Don't show thumbnails */
+    view = NAUTILUS_VIEW (canvas_view);
+    nautilus_action_bar_set_show_thumbnail (NAUTILUS_ACTION_BAR (nautilus_view_get_action_bar (view)),
+                                            FALSE);
 }
 
 NautilusFilesView *
