1faac40b52992f1e8a4756f6006e9a94c5179733
Carlos Soriano <csoriano@gnome.org>
2016-09-06 15:28:22 +0200

    new-folder-dialog-controller: add header

--- src/nautilus-new-folder-dialog-controller.c
+++ src/nautilus-new-folder-dialog-controller.c
@@ -1,3 +1,22 @@
+/* nautilus-new-folder-dialog-controller.c
+ *
+ * Copyright (C) 2016 the Nautilus developers
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <glib/gi18n.h>
 
 #include <eel/eel-vfs-extensions.h>
