576607829f709faf5a71833c6bc33b16c9f37d45
Carlos Soriano <csoriano@gnome.org>
2016-09-06 15:27:08 +0200

    file-name-widget-controller: add header
    Copyright and licence were missing

--- src/nautilus-file-name-widget-controller.h
+++ src/nautilus-file-name-widget-controller.h
@@ -1,3 +1,22 @@
+/* nautilus-file-name-widget-controller.h
+ *
+ * Copyright (C) 2016 the Nautilus developers
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef NAUTILUS_FILE_NAME_WIDGET_CONTROLLER_H
 #define NAUTILUS_FILE_NAME_WIDGET_CONTROLLER_H
 
