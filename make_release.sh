ed5652c89ac0654df2e82b54b00b27d51c825465
Ernestas Kulik <ernestask@src.gnome.org>
2016-09-28 09:16:11 +0300

    general: add support for Meson
    Since it’s 2017 already, Nautilus should use a build system that doesn’t
    take longer to set up the build than it takes to actually build. An
    observed build time using Ninja of roughly one-fifth of what it took
    Autotools is more than reason enough to add support for Meson. Along
    with that, this commit adds a convenience script to generate a tarball
    for releases, since we use libgd as a submodule and Meson does not
    handle source distributions.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=778167

---
+++ make_release.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
+
+cd $srcdir
+
+VERSION=$(git describe --abbrev=0)
+NAME="nautilus-$VERSION"
+
+echo "Updating submodules…"
+git submodule update --init
+
+echo "Creating git tree archive…"
+git archive --prefix="${NAME}/" --format=tar HEAD > nautilus.tar
+
+cd libgd
+
+git archive --prefix="${NAME}/libgd/" --format=tar HEAD > libgd.tar
+
+cd ..
+
+rm -f "${NAME}.tar"
+
+tar -Af "${NAME}.tar" nautilus.tar
+tar -Af "${NAME}.tar" libgd/libgd.tar
+
+rm -f nautilus.tar
+rm -f libgd/libgd.tar
+
+echo "Compressing archive…"
+xz -f "${NAME}.tar"
