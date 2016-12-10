acff418810418e594a094dfee045300320f69ff4
Neil Herald <neil.herald@gmail.com>
2016-12-10 14:28:24 +0000

    window-slot: prevent menu disappearing when Hidden Files toggled
    When the user toggled the Show Hidden Files option on the toolbar menu,
    previously the menu disappeared - which isn't the correct behaviour. This
    commit changes it so that the menu doesn't disappear.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=766952

--- src/nautilus-toolbar.h
+++ src/nautilus-toolbar.h
@@ -45,6 +45,8 @@ void       nautilus_toolbar_set_show_location_entry (NautilusToolbar *self,
 void       nautilus_toolbar_set_active_slot    (NautilusToolbar    *toolbar,
                                                 NautilusWindowSlot *slot);
 
+gboolean   nautilus_toolbar_is_menu_visible    (NautilusToolbar *toolbar);
+
 gboolean   nautilus_toolbar_is_operations_button_active (NautilusToolbar *toolbar);
 
 void       nautilus_toolbar_on_window_constructed       (NautilusToolbar *toolbar);
