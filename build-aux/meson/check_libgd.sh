9948131521ea3162234fb0024ba0ccdb491c7f2f
Ernestas Kulik <ernestask@gnome.org>
2017-04-23 11:34:25 +0300

    meson: delegate submodule initialization to Meson
    As of 0.40.0, Meson has the ability to initialize subprojects that are
    git submodules. This removes the need for a separate script to achieve
    that.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=781653

--- build-aux/meson/check_libgd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -f "${MESON_SOURCE_ROOT}/subprojects/libgd/meson.build" ]
-then
-    git \
-        --git-dir="${MESON_SOURCE_ROOT}/.git" \
-        submodule update --init subprojects/libgd
-fi
