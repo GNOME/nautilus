8480203d1a9a58b48322f031ec9a0c7fab73454b
Piotr Drąg <piotrdrag@gmail.com>
2017-05-13 19:45:26 +0200

    batch-rename-dialog: fix grammar in translatable strings
    https://bugzilla.gnome.org/show_bug.cgi?id=782601

--- src/nautilus-batch-rename-dialog.c
+++ src/nautilus-batch-rename-dialog.c
@@ -1423,25 +1423,25 @@ have_unallowed_character (NautilusBatchRenameDialog *dialog)
     if (have_empty_name)
     {
         gtk_label_set_label (GTK_LABEL (dialog->conflict_label),
-                             _("A file must have a name"));
+                             _("A file must have a name."));
     }
 
     if (have_unallowed_character_slash)
     {
         gtk_label_set_label (GTK_LABEL (dialog->conflict_label),
-                             _("“/” is an unallowed character"));
+                             _("File names cannot contain “/”."));
     }
 
     if (have_unallowed_character_dot)
     {
         gtk_label_set_label (GTK_LABEL (dialog->conflict_label),
-                             _("“.” is an unallowed file name"));
+                             _("A file cannot be called “.”."));
     }
 
     if (have_unallowed_character_dotdot)
     {
         gtk_label_set_label (GTK_LABEL (dialog->conflict_label),
-                             _("“..” is an unallowed file name"));
+                             _("A file cannot be called “..”."));
     }
 
     if (have_unallowed_character_slash || have_unallowed_character_dot || have_unallowed_character_dotdot
