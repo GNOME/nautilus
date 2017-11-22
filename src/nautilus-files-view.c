d3bb4b05befe360c9d84d2e76497a6bef2b9d8d3
António Fernandes <antoniojpfernandes@gmail.com>
2017-11-22 15:20:04 +0000

    Revert "files-view: Ignore "sort-directories-first" for search results"
    This reverts commit eb1a956c262bcd77290def0fd5fd6b7d169a9d09

--- src/nautilus-files-view.c
+++ src/nautilus-files-view.c
@@ -2768,12 +2768,10 @@ gboolean
 nautilus_files_view_should_sort_directories_first (NautilusFilesView *view)
 {
     NautilusFilesViewPrivate *priv;
-    gboolean is_search;
 
     priv = nautilus_files_view_get_instance_private (view);
-    is_search = nautilus_view_is_searching (NAUTILUS_VIEW (view));
 
-    return priv->sort_directories_first && !is_search;
+    return priv->sort_directories_first;
 }
 
 static void
