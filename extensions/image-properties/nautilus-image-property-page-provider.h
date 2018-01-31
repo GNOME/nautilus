13fe5e1edcc178a4a366178b7d3bdd7e6617c96a
Ernestas Kulik <ernestask@gnome.org>
2018-01-31 10:24:07 +0200

    general: reimplement image properties as an extension
    The image property page is already implemented using the extension API,
    but it’s not an extension and relies on a library that is not essential
    to Nautilus. This commit builds the image properties as an extension and
    introduces some minor refactoring to the code.

---
+++ extensions/image-properties/nautilus-image-property-page-provider.h
@@ -0,0 +1,33 @@
+/* Copyright (C) 2018 Ernestas Kulik <ernestask@gnome.org>
+ *
+ * This file is part of Nautilus.
+ *
+ * Nautilus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Nautilus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nautilus.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef NAUTILUS_IMAGE_PROPERTY_PAGE_PROVIDER_H
+#define NAUTILUS_IMAGE_PROPERTY_PAGE_PROVIDER_H
+
+#include <glib-object.h>
+
+#define NAUTILUS_TYPE_IMAGE_PROPERTY_PAGE_PROVIDER (nautilus_image_property_page_provider_get_type ())
+
+G_DECLARE_FINAL_TYPE (NautilusImagePropertyPageProvider,
+                      nautilus_image_property_page_provider,
+                      NAUTILUS, IMAGE_PROPERTY_PAGE_PROVIDER,
+                      GObject)
+
+void nautilus_image_property_page_provider_load (GTypeModule *module);
+
+#endif
