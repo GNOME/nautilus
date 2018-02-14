d7e30914b9c01807aef2ea2da0ceb9cd9c5c703b
Ernestas Kulik <ernestask@gnome.org>
2018-02-14 11:16:38 +0200

    run-uncrustify.sh: invert lineup-parameters check
    Inverting the checks and exiting instead will help keep indentation
    levels in check.

--- data/run-uncrustify.sh
+++ data/run-uncrustify.sh
@@ -8,18 +8,19 @@ then
     exit 1
 fi
 
-if [ -x "$DATA/lineup-parameters" ];
+if [ ! -x "$DATA/lineup-parameters" ];
 then
-    for DIR in "$DATA/../"{src,test,libnautilus-extension,eel,extensions}
-    do
-        for FILE in $(find "$DIR" -name "*.c")
-        do
-            # Aligning prototypes is not working yet, so avoid headers
-            "$UNCRUSTIFY" -c "$DATA/uncrustify.cfg" --no-backup "$FILE"
-            "$DATA/lineup-parameters" "$FILE" > "$FILE.temp" && mv "$FILE.temp" "$FILE"
-       done
-    done
-else
     echo "Script lineup-parameters does not exists here in (source directory)/data, probably because Nautilus was built in a different directory than the source directory.
 Copy the program in the (build directory)/data/lineup-parameters here in (source directory)/data and run again run-uncrustify.sh."
+    exit 1
 fi
+
+for DIR in "$DATA/../"{src,test,libnautilus-extension,eel,extensions}
+do
+    for FILE in $(find "$DIR" -name "*.c")
+    do
+        # Aligning prototypes is not working yet, so avoid headers
+        "$UNCRUSTIFY" -c "$DATA/uncrustify.cfg" --no-backup "$FILE"
+        "$DATA/lineup-parameters" "$FILE" > "$FILE.temp" && mv "$FILE.temp" "$FILE"
+   done
+done
