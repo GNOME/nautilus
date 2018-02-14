c64756756910789c8cf4f0523a2fe45e0ac79682
Ernestas Kulik <ernestask@gnome.org>
2018-02-14 11:13:04 +0200

    run-uncrustify.sh: detect if uncrustify is not installed
    Otherwise it will keep spamming stderr about the command not being
    found.

--- data/run-uncrustify.sh
+++ data/run-uncrustify.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 DATA=$(dirname "$BASH_SOURCE")
+UNCRUSTIFY=$(command -v uncrustify)
+
+if [ -z "$UNCRUSTIFY" ];
+then
+    echo "Uncrustify is not installed on your system."
+    exit 1
+fi
+
 if [ -x "$DATA/lineup-parameters" ];
 then
     for DIR in "$DATA/../"{src,test,libnautilus-extension,eel,extensions}
@@ -7,7 +15,7 @@ then
         for FILE in $(find "$DIR" -name "*.c")
         do
             # Aligning prototypes is not working yet, so avoid headers
-            uncrustify -c "$DATA/uncrustify.cfg" --no-backup "$FILE"
+            "$UNCRUSTIFY" -c "$DATA/uncrustify.cfg" --no-backup "$FILE"
             "$DATA/lineup-parameters" "$FILE" > "$FILE.temp" && mv "$FILE.temp" "$FILE"
        done
     done
