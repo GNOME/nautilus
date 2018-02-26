86e394bef80cfcf04aa8a1def449cac52aa97a4b
Rahul Verma <rv404674@gmail.com>
2018-02-26 14:25:25 +0530

    nautilus-properties-window: Add a button to open gnome-disks
    The volume label is visible in properties, so to edit it we need to open
    gnome-disks.
    
    Closes #20

--- src/nautilus-properties-window.c
+++ src/nautilus-properties-window.c
@@ -3105,12 +3105,41 @@ create_volume_usage_widget (NautilusPropertiesWindow *window)
 }
 
 static void
+open_disk (GtkButton                *button,
+           NautilusPropertiesWindow *self)
+{
+    char *message;
+    GError *err = NULL;
+    g_autoptr (GAppInfo) app_info = NULL;
+
+    app_info = g_app_info_create_from_commandline ("gnome-disks", NULL, G_APP_INFO_CREATE_NONE, NULL);
+    g_app_info_launch (app_info, NULL, NULL, &err);
+
+    if (err != NULL)
+    {
+        message = g_strconcat (_("Details: "), err->message, NULL);
+        eel_show_error_dialog
+            (_("There was an error launching the application."),
+            message,
+            self);
+
+        g_error_free (err);
+        g_free (message);
+    }
+
+}
+
+static void
 create_basic_page (NautilusPropertiesWindow *window)
 {
     GtkGrid *grid;
     GtkWidget *icon_pixmap_widget;
     GtkWidget *volume_usage;
     GtkWidget *hbox, *vbox;
+    GtkWidget *button;
+    GMount *mount;
+    GDrive *drive;
+    NautilusFile *file;
 
     hbox = create_page_with_box (window->details->notebook,
                                  GTK_ORIENTATION_HORIZONTAL,
@@ -3133,6 +3162,13 @@ create_basic_page (NautilusPropertiesWindow *window)
     vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
     gtk_widget_show (vbox);
     gtk_container_add (GTK_CONTAINER (hbox), vbox);
+
+    file = get_original_file (window);
+
+    if (file == NULL)
+    {
+        return;
+    }
 
     grid = GTK_GRID (create_grid_with_standard_properties ());
     gtk_box_pack_start (GTK_BOX (vbox), GTK_WIDGET (grid), FALSE, FALSE, 0);
@@ -3253,6 +3289,21 @@ create_basic_page (NautilusPropertiesWindow *window)
                                                NULL);
         }
     }
+
+    mount = nautilus_file_get_mount (file);
+    drive = g_mount_get_drive (mount);
+
+    if (drive != NULL)
+    {
+       button = gtk_button_new_with_label ("Open in Disks");
+       g_signal_connect (button, "clicked", G_CALLBACK (open_disk), NULL);
+       gtk_container_add_with_properties (GTK_CONTAINER (grid),
+                                               button,
+                                               "width", 3,
+                                               NULL);
+       gtk_widget_show_all (GTK_WIDGET (grid));
+    }
+
 }
 
 static gboolean
