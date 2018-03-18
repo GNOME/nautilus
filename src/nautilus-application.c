ac49a439d9b086e832ee97e8cbbf2c5b752cff75
Rahul Verma <rv404674@gmail.com>
2018-03-18 10:53:15 +0530

    general: Error out if unhandled protocol is entered from CLI or pathbar
    The solution is to explicitly check for unhandled protocol( including x-nautilus-search) in both cli and pathbar, and error out before even opening the uri.
    
    Closes #237

--- src/nautilus-application.c
+++ src/nautilus-application.c
@@ -984,7 +984,15 @@ nautilus_application_handle_file_args (NautilusApplication *self,
                 file = g_file_new_for_commandline_arg_and_cwd (remaining[idx], cwd);
                 g_free (cwd);
             }
-            g_ptr_array_add (file_array, file);
+
+            if (nautilus_is_search_directory(file))
+            {
+                g_printf(_("error: x-nautilus-search is no longer supported"));
+            }
+            else
+            {
+                g_ptr_array_add (file_array, file);
+            }
         }
     }
     else if (g_variant_dict_contains (options, "new-window"))
--- src/nautilus-window.c
+++ src/nautilus-window.c
@@ -40,6 +40,7 @@
 #include "nautilus-other-locations-window-slot.h"
 
 #include <eel/eel-debug.h>
+#include <eel/eel-stock-dialogs.h>
 #include <eel/eel-gtk-extensions.h>
 #include <eel/eel-vfs-extensions.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
@@ -742,6 +743,17 @@ location_entry_location_changed_callback (GtkWidget      *widget,
                                           NautilusWindow *window)
 {
     NautilusWindowPrivate *priv;
+    g_autofree gchar *uri = NULL;
+
+    if (location != NULL)
+    {
+        uri = g_file_get_uri (location);
+        if (eel_uri_is_search (uri))
+        {
+            eel_show_error_dialog (_("This command is no longer supported"),"", window);
+        }
+        g_object_unref (location);
+    }
 
     priv = nautilus_window_get_instance_private (window);
 
