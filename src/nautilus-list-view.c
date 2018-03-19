e25132e3051f5a9eb3b0f1d3b0c773bff45628a1
Alexandru Fazakas <alex.fazakas97@yahoo.com>
2018-03-19 14:34:39 +0200

    list-view: Fix right click interactions
    Left click and right click are not consistent when interacting
    with CTRL and SHIFT combinations. Additionally, right clicking
    an expander is not working properly.
    
    We want consistency between these behaviours and right clicking
    expander support.
    
    This patch solves this issue by allowing the parent class to
    handle the CTRL+SHIFT cases similar to left click. The expander
    handling is resolved by selecting the clicked row.
    
    Closes https://gitlab.gnome.org/GNOME/nautilus/issues/286

--- src/nautilus-list-view.c
+++ src/nautilus-list-view.c
@@ -678,15 +678,64 @@ button_press_callback (GtkWidget      *widget,
     }
     else
     {
+        GtkTreePath *cursor;
+
+        GList *selected_rows = NULL;
         /* We're going to filter out some situations where
          * we can't let the default code run because all
          * but one row would be would be deselected. We don't
          * want that; we want the right click menu or single
          * click to apply to everything that's currently selected.
          */
-        if (event->button == GDK_BUTTON_SECONDARY && path_selected)
+        if (event->button == GDK_BUTTON_SECONDARY)
         {
-            call_parent = FALSE;
+            if (!path_selected)
+            {
+                if (!on_expander)
+                {
+                    if ((event->state & GDK_CONTROL_MASK) != 0)
+                    {
+                        GList *l;
+
+                        call_parent = FALSE;
+                        if ((event->state & GDK_SHIFT_MASK) != 0)
+                        {
+                            gtk_tree_view_get_cursor (tree_view, &cursor, NULL);
+                            if (cursor != NULL)
+                            {
+                                gtk_tree_selection_select_range (selection, cursor, path);
+                            }
+                            else
+                            {
+                                gtk_tree_selection_select_path (selection, path);
+                            }
+                        }
+                        else
+                        {
+                            gtk_tree_selection_select_path (selection, path);
+                        }
+                        selected_rows = gtk_tree_selection_get_selected_rows (selection, NULL);
+
+                        /* This unselects everything */
+                        gtk_tree_view_set_cursor (tree_view, path, NULL, FALSE);
+
+                        /* So select it again */
+                        for (l = selected_rows; l != NULL; l = l->next)
+                        {
+                            gtk_tree_selection_select_path (selection, l->data);
+                        }
+                        g_list_free_full (selected_rows, (GDestroyNotify) gtk_tree_path_free);
+                    }
+                }
+                else
+                {
+                    gtk_tree_view_set_cursor (tree_view, path, NULL, FALSE);
+                }
+            }
+            else
+            {
+                call_parent = FALSE;
+            }
         }
 
         if ((event->button == GDK_BUTTON_PRIMARY || event->button == GDK_BUTTON_MIDDLE) &&
@@ -701,12 +750,11 @@ button_press_callback (GtkWidget      *widget,
             }
             else if ((event->state & GDK_CONTROL_MASK) != 0)
             {
-                GList *selected_rows, *l;
+                GList *l;
 
                 call_parent = FALSE;
                 if ((event->state & GDK_SHIFT_MASK) != 0)
                 {
-                    GtkTreePath *cursor;
                     gtk_tree_view_get_cursor (tree_view, &cursor, NULL);
                     if (cursor != NULL)
                     {
