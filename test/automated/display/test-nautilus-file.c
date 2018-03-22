9951b06a07c52d2906c71f01ee22c27109ab4e58
Rahul Verma <rv404674@gmail.com>
2018-03-22 18:28:50 +0530

    test-nautilus-file: Added new tests for file renaming using glib

--- test/automated/display/test-nautilus-file.c
+++ test/automated/display/test-nautilus-file.c
@@ -1,67 +1,48 @@
-#include <gtk/gtk.h>
+#include <glib.h>
+#include <glib/gprintf.h>
 #include <src/nautilus-directory.h>
 #include <src/nautilus-file-utilities.h>
 #include <src/nautilus-search-directory.h>
 #include <src/nautilus-file.h>
+#include <src/nautlius-file.c>
 #include <unistd.h>
+#include "eel/eel-string.h"
 
-void *client1;
 
-/*
 static void
-file_renamed (NautilusFile *file)
+test_check_for_wrong_filename (void)
 {
-  g_print (" file is being renamed\n");
+    char *new_name;
+    new_name = "ab/";
 
-    g_print ("file old name: %s \n",
-             nautilus_file_get_name (name));
+    g_assert (new_name != NULL);
 
-    g_print ("file uri: %s \n",
-             nautilus_file_get_uri (file));
+    g_assert (new_name[0] != '\0');
+
+    /*check incoming file names for path separators*/
+    g_assert ((strstr (new_name, "/") != NULL));
 }
-*/
 
-int
-main (int    argc,
-      char **argv)
+static void
+test_check_file_is_renamed_correctly (void)
 {
+
     NautilusFile *file;
-    NautilusFileAttributes attributes;
+    NautilusDirectory *directory;
     const char *uri;
     const char *new_name, *initial_name;
-    const char *path;
-    GFile *location;
 
-    client1 = g_new0 (int, 1);
-    gtk_init (&argc, &argv);
-
-    nautilus_ensure_extension_points ();
+    uri = "file:///tmp";
     initial_name = "abc";
-    if (argv[1] == NULL)
-    {
-        uri = "file:///tmp";
-    }
-    else
-    {
-        uri = argv[1];
-    }
-    g_print ("loading %s", uri);
-    directory = nautilus_directory_get_by_uri (uri);
-    file = nautilus_file_new_from_filename (directory, initial_name, FALSE);
 
-    /*location = g_file_new_for_path (path);
-    file = nautilus_file_get (location);*/
+    directory = nautilus_directory_get_by_uri (uri);
+    g_assert (NAUTILUS_IS_DIRECTORY (directory));
+
+    file = nautilus_file_new_from_filename (directory, initial_name, FALSE);
+    g_assert (NAUTILUS_IS_FILE (file));
 
     new_name = "xyz";
 
-    g_assert (NAUTILUS_IS_FILE (file));
-    g_assert (new_name != NULL);
-    g_assert (new_name[0] != '\0');
-
-//    g_signal_connect (file, "changed", G_CALLBACK (file_renamed), NULL);
-
-    /*check incoming file names for path separators*/
-    g_assert ((strstr (new_name, "/") != NULL);
     /*cant rename a file that is already gone*/
     g_assert (!nautilus_file_is_gone (file));
 
@@ -69,25 +50,32 @@ main (int    argc,
     nautilus_file_rename (file, new_name, rename_callback, NULL);
 
     while (nautilus_file_rename_in_progress (file))
-        {
-            gtk_main_iteration();
-        }
+          {
+              /*keep looping till file hasn't been renamed*/
+          }
 
     /*get the newname of file using its uri and check whether it is same as the new name we gave it to*/
     g_assert (g_strcmp0 (nautilus_file_get_name (file), new_name) == 0);
 
-/*
-     attributes =
-      NAUTILUS_FILE_ATTRIBUTES_FOR_ICON |
-      NAUTILUS_FILE_ATTRIBUTE_DIRECTORY_ITEM_COUNT |
-      NAUTILUS_FILE_ATTRIBUTE_INFO |
-      NAUTILUS_FILE_ATTRIBUTE_LINK_INFO |
-      NAUTILUS_FILE_ATTRIBUTE_MOUNT |
-      NAUTILUS_FILE_ATTRIBUTE_EXTENSION_INFO;
+}
 
-     nautilus_file_monitor_add (file, client1,
-                                         attributes);
-*/
-    gtk_main ();
-    return 0;
+static void
+setup_test_suite (void)
+{
+    g_test_add_func ("/renaming-operation/1.0",
+                     test_check_for_wrong_filename);
+    g_test_add_func ("/renaming-operation/1.1",
+                     test_check_file_is_renamed_correctly);
+}
+
+int
+main (int   argc,
+      char *argv[])
+{
+    g_test_init (&argc, &argv, NULL);
+    g_test_set_nonfatal_assertions ();
+
+    setup_test_suite ();
+
+    return g_test_run ();
 }
