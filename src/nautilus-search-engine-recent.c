9383613d32b22c71b3ae2906bdf39947fa5b6478
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-04-11 13:59:47 -0400

    search-engine-recent: remove unused include

--- src/nautilus-search-engine-recent.c
+++ src/nautilus-search-engine-recent.c
@@ -23,7 +23,6 @@
 #include "nautilus-search-provider.h"
 #include "nautilus-search-engine-recent.h"
 #include "nautilus-ui-utilities.h"
-#include "nautilus-tag-manager.h"
 #define DEBUG_FLAG NAUTILUS_DEBUG_SEARCH
 #include "nautilus-debug.h"
 
