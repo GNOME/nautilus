cff4ca30b9fc5fe8ae5466e64093fc269367ed65
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-04-12 13:21:50 -0400

    recent: add function to update manager on file rename or move
    When files have been moved or renamed nautilus should inform
    recent manager about this change for the files that
    are listed there.

--- src/nautilus-recent.h
+++ src/nautilus-recent.h
@@ -4,5 +4,9 @@
 #include "nautilus-file.h"
 #include <gio/gio.h>
 
-void nautilus_recent_add_file (NautilusFile *file,
-			       GAppInfo *application);
+void nautilus_recent_add_file          (NautilusFile *file,
+                                        GAppInfo *application);
+void nautilus_recent_update_file_moved (const char *old_uri,
+                                        const char *new_uri,
+                                        const char *old_display_name,
+                                        const char *new_dispaly_name);
