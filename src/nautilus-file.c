ff882de72d2e96bf183f2b715f9c12aca3e1537f
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-04-12 12:22:24 -0500

    file: Update recent files manager on file rename or move

--- src/nautilus-file.c
+++ src/nautilus-file.c
@@ -1861,6 +1861,8 @@ rename_get_info_callback (GObject      *source_object,
     {
         g_autofree char *old_uri = NULL;
         g_autofree char *new_uri = NULL;
+        g_autofree char *new_display_name = NULL;
+        g_autofree char *old_display_name = NULL;
 
         directory = op->file->details->directory;
         new_name = g_file_info_get_name (new_info);
@@ -1877,11 +1879,16 @@ rename_get_info_callback (GObject      *source_object,
         }
 
         old_uri = nautilus_file_get_uri (op->file);
+        old_display_name = nautilus_file_get_display_name (op->file);
 
         update_info_and_name (op->file, new_info);
 
         new_uri = nautilus_file_get_uri (op->file);
+        new_display_name = nautilus_file_get_display_name (op->file);
+
         nautilus_directory_moved (old_uri, new_uri);
+        nautilus_file_moved_update_recent (old_uri, new_uri,
+                                           old_display_name, new_display_name);
     }
     nautilus_file_operation_complete (op, NULL, error);
 }
