bf4220eae20596828cd6d2388232190e631c4a5b
Alexandru Fazakas <alex.fazakas97@gmail.com>
2018-05-20 23:41:57 +0300

    file-operations: Add copy_file sync alternative
    In this patch we added a synchronous alternative to the
    file_operations_copy_file function for testing purposes.

--- src/nautilus-file-operations.h
+++ src/nautilus-file-operations.h
@@ -56,13 +56,21 @@ void nautilus_file_operations_copy_move   (const GList               *item_uris,
 					   GtkWidget                 *parent_view,
 					   NautilusCopyCallback       done_callback,
 					   gpointer                   done_callback_data);
-void nautilus_file_operations_copy_file (GFile *source_file,
-					 GFile *target_dir,
-					 const gchar *source_display_name,
-					 const gchar *new_name,
-					 GtkWindow *parent_window,
-					 NautilusCopyCallback done_callback,
-					 gpointer done_callback_data);
+void nautilus_file_operations_copy_file_async (GFile *source_file,
+					        GFile *target_dir,
+					        const gchar *source_display_name,
+                                               const gchar *new_name,
+					        GtkWindow *parent_window,
+					        NautilusCopyCallback done_callback,
+					        gpointer done_callback_data);
+void
+nautilus_file_operations_copy_file_sync (GFile                *source_file,
+                                         GFile                *target_dir,
+                                         const gchar          *source_display_name,
+                                         const gchar          *new_name,
+                                         GtkWindow            *parent_window,
+                                         NautilusCopyCallback  done_callback,
+                                         gpointer              done_callback_data);
 void nautilus_file_operations_empty_trash (GtkWidget                 *parent_view);
 void nautilus_file_operations_new_folder  (GtkWidget                 *parent_view,
 					   const char                *parent_dir_uri,
