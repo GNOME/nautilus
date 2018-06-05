4680e33c4df9d3996c0c2e127a73be5258c84068
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-06-06 01:54:17 +0200

    shell-search-provider: enable recursive search on indexed engines
    Allow to retrieve results from search engines which are only searching on
    over indexed results (tracker, recent in future), while disable them otherwise
    (simple search)

--- src/nautilus-shell-search-provider.c
+++ src/nautilus-shell-search-provider.c
@@ -451,6 +451,7 @@ execute_search (NautilusShellSearchProvider  *self,
 
     query = nautilus_query_new ();
     nautilus_query_set_show_hidden_files (query, FALSE);
+    nautilus_query_set_recursive (query, NAUTILUS_QUERY_RECURSIVE_IF_INDEXED);
     nautilus_query_set_text (query, terms_joined);
     nautilus_query_set_location (query, home);
 
