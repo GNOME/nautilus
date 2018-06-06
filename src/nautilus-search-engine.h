6308d4af1f6809ffc2623768f2c0010b9be0208a
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-06-06 16:06:40 +0200

    search-engine: remove getter for simple provider
    No need for special case for this anymore

--- src/nautilus-search-engine.h
+++ src/nautilus-search-engine.h
@@ -41,7 +41,6 @@ struct _NautilusSearchEngineClass
 NautilusSearchEngine *nautilus_search_engine_new                (void);
 NautilusSearchEngineModel *
                       nautilus_search_engine_get_model_provider (NautilusSearchEngine *engine);
-NautilusSearchEngineSimple *
-                      nautilus_search_engine_get_simple_provider (NautilusSearchEngine *engine);
+
 
 G_END_DECLS
