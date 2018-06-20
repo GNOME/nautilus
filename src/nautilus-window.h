b8ab8514c33f6e01465d37ebde4097461fc42d4d
Ernestas Kulik <ernestask@gnome.org>
2018-06-20 16:59:35 +0300

    window-slot: Stop using flags parameter for history navigation
    The feature that allows opening a new window from the history popup menu
    by middle-clicking hasn’t been working for three years, since the flags
    parameter in nautilus_window_back_or_forward() is not honored. In some
    places it’s used inappropriately, so it’s better to just drop the code
    and rethink the approach if such a feature is desirable.

--- src/nautilus-window.h
+++ src/nautilus-window.h
@@ -80,13 +80,11 @@ void     nautilus_window_hide_sidebar         (NautilusWindow *window);
 void     nautilus_window_show_sidebar         (NautilusWindow *window);
 void     nautilus_window_back_or_forward      (NautilusWindow *window,
                                                gboolean        back,
-                                               guint           distance,
-                                               NautilusWindowOpenFlags flags);
+                                               guint           distance);
 void nautilus_window_reset_menus (NautilusWindow *window);
 
 GtkWidget *         nautilus_window_get_notebook (NautilusWindow *window);
 
-NautilusWindowOpenFlags nautilus_event_get_window_open_flags   (void);
 void     nautilus_window_show_about_dialog    (NautilusWindow *window);
 
 GtkWidget *nautilus_window_get_toolbar (NautilusWindow *window);
