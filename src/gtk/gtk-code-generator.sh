db62d55be498798b8fce1fed71cc79ed7488d834
Ernestas Kulik <ernestask@gnome.org>
2018-07-02 13:11:25 +0300

    gtk-code-generator.sh: Update for master
    This commit adds the sources for GtkPlacesSidebar and their
    dependencies, since it’s become private API.

--- src/gtk/gtk-code-generator.sh
+++ src/gtk/gtk-code-generator.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+set -e
+
 # Fetch the GtkPlacesView files but rename the symbols to avoid symbol clashes
 # when using the file chooser inside nautilus i.e. when activating the "move to"
 # action.
 # Also remove/add the neccesary bits to make it work inside nautilus
 
-URL=https://gitlab.gnome.org/GNOME/gtk/raw/gtk-3-22/gtk/
-URLUI=https://gitlab.gnome.org/GNOME/gtk/raw/gtk-3-22/gtk/ui/
-SUFIX=?h=gtk-3-22
+URL=https://gitlab.gnome.org/GNOME/gtk/raw/master/gtk/
+URLUI=https://gitlab.gnome.org/GNOME/gtk/raw/master/gtk/ui/
+SUFFIX=
 
 # Since comments are not allowed inside the sed line, this is what it will do
 # by order:
@@ -44,12 +46,15 @@ update_file () {
         -e '/gtkbox.h/d' \
         -e '/#error/d' \
         -e 's/gtk\/libgtk/gnome\/nautilus\/gtk/g' \
+        -e '/#include "gtk.*\.h"/d' \
+        -e 's|<gtk/gtkplacessidebarprivate.h>|"nautilusgtkplacessidebarprivate.h"|' \
+        -e 's/GTK_PARAM/G_PARAM/g' \
         > "${_dest}"
 }
 
-update_file "${URL}/gtkplacesview.c${SUFIX}" "nautilusgtkplacesview.c"
-update_file "${URL}/gtkplacesviewprivate.h${SUFIX}" "nautilusgtkplacesviewprivate.h"
-update_file "${URLUI}/gtkplacesview.ui${SUFIX}" "nautilusgtkplacesview.ui"
+update_file "${URL}/gtkplacesview.c${SUFFIX}" "nautilusgtkplacesview.c"
+update_file "${URL}/gtkplacesviewprivate.h${SUFFIX}" "nautilusgtkplacesviewprivate.h"
+update_file "${URLUI}/gtkplacesview.ui${SUFFIX}" "nautilusgtkplacesview.ui"
 
 # Since comments are not allowed inside the sed line, this is what it will do
 # by order:
@@ -86,6 +91,43 @@ update_file () {
  > "${_dest}"
 }
 
-update_file "${URL}/gtkplacesviewrow.c${SUFIX}" "nautilusgtkplacesviewrow.c"
-update_file "${URL}/gtkplacesviewrowprivate.h${SUFIX}" "nautilusgtkplacesviewrowprivate.h"
-update_file "${URLUI}/gtkplacesviewrow.ui${SUFIX}" "nautilusgtkplacesviewrow.ui"
+update_file "${URL}/gtkplacesviewrow.c${SUFFIX}" "nautilusgtkplacesviewrow.c"
+update_file "${URL}/gtkplacesviewrowprivate.h${SUFFIX}" "nautilusgtkplacesviewrowprivate.h"
+update_file "${URLUI}/gtkplacesviewrow.ui${SUFFIX}" "nautilusgtkplacesviewrow.ui"
+
+update_file () {
+    _source="$1"
+    _dest="$2"
+
+    curl "${_source}" | sed                                                  \
+        -e 's|<gtk/.*\.h>|<gtk/gtk.h>|g'                                     \
+        -e '/#include <glib\/gi18n-lib\.h>/d'                                \
+        -e '/"config.h"/a #include <glib\/gi18n.h>'                          \
+        -e '/"config.h"/a #include <gtk\/gtk.h>'                             \
+        -e 's/gtkbookmarksmanager\.h/nautilusgtkbookmarksmanager.h/'         \
+        -e 's/gtkfilesystem\.h/nautilusgtkfilesystem.h/'                     \
+        -e 's/gtkplacessidebarprivate\.h/nautilusgtkplacessidebarprivate.h/' \
+        -e 's/gtksidebarrowprivate\.h/nautilusgtksidebarrowprivate.h/'       \
+        -e 's/gtktrashmonitor\.h/nautilusgtktrashmonitor.h/'                 \
+        -e 's|#include "gtklistbox\.h"|#include <gtk/gtk.h>|'                \
+        -e '/#include "g[dt]k.*\.h"/d'                                       \
+        -e 's/_gtk_marshal_.*,/NULL,/g'                                      \
+        -e "s/I_(\(.*\))/\1/"                                                \
+        -e "s/P_(\(.*\))/\1/"                                                \
+        -e 's/GTK_PARAM/G_PARAM/g'                                           \
+    > "${_dest}"
+}
+update_file "${URL}/gtkplacessidebar.c${SUFFIX}" "nautilusgtkplacessidebar.c"
+update_file "${URL}/gtkplacessidebarprivate.h${SUFFIX}" "nautilusgtkplacessidebarprivate.h"
+
+update_file "${URL}/gtksidebarrow.c${SUFFIX}" "nautilusgtksidebarrow.c"
+update_file "${URL}/gtksidebarrowprivate.h${SUFFIX}" "nautilusgtksidebarrowprivate.h"
+
+update_file "${URL}/gtkbookmarksmanager.c${SUFFIX}" "nautilusgtkbookmarksmanager.c"
+update_file "${URL}/gtkbookmarksmanager.h${SUFFIX}" "nautilusgtkbookmarksmanager.h"
+
+update_file "${URL}/gtktrashmonitor.c${SUFFIX}" "nautilusgtktrashmonitor.c"
+update_file "${URL}/gtktrashmonitor.h${SUFFIX}" "nautilusgtktrashmonitor.h"
+
+update_file "${URL}/gtkfilesystem.c${SUFFIX}" "nautilusgtkfilesystem.c"
+update_file "${URL}/gtkfilesystem.h${SUFFIX}" "nautilusgtkfilesystem.h"
