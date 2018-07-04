ba6a68dc6527e18fd78ac095bebbbea49104bcb8
Ernestas Kulik <ernestask@gnome.org>
2018-07-04 11:46:57 +0300

    general: Rework clipboard handling
    GtkClipboard has been removed in favor of GdkClipboard. This commit
    takes advantage of new API, such as GdkContentProvider, to reimplement
    clipboard handling. A feature added is pasting files from outside
    sources.
    
    Fixes https://gitlab.gnome.org/GNOME/nautilus/issues/540

--- src/nautilus-clipboard.h
+++ src/nautilus-clipboard.h
@@ -1,4 +1,3 @@
-
 /* fm-directory-view.h
  *
  * Copyright (C) 1999, 2000  Free Software Foundaton
@@ -24,12 +23,5 @@
 
 #include <gtk/gtk.h>
 
-void nautilus_clipboard_clear_if_colliding_uris    (GtkWidget          *widget,
-                                                    const GList        *item_uris);
-GtkClipboard* nautilus_clipboard_get               (GtkWidget          *widget);
-GList* nautilus_clipboard_get_uri_list_from_selection_data (GtkSelectionData   *selection_data);
-gboolean nautilus_clipboard_is_cut_from_selection_data (GtkSelectionData *selection_data);
-void nautilus_clipboard_prepare_for_files (GtkClipboard *clipboard,
-                                           GList        *files,
-                                           gboolean      cut);
-GdkAtom nautilus_clipboard_get_atom (void);
+void nautilus_clipboard_clear_if_colliding_uris (GtkWidget *widget,
+                                                 GList     *uris);
