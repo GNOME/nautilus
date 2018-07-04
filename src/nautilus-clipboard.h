b2039422d75ef3e2818a68d0bd2528eb40f94521
Ernestas Kulik <ernestask@gnome.org>
2018-07-04 11:46:57 +0300

    general: Rework clipboard handling
    GtkClipboard has been removed in favor of GdkClipboard. This means doing
    equivalent things with new API, or dropping some things that have been
    removed, such as listening to owner changes.

--- src/nautilus-clipboard.h
+++ src/nautilus-clipboard.h
@@ -24,12 +24,15 @@
 
 #include <gtk/gtk.h>
 
-void nautilus_clipboard_clear_if_colliding_uris    (GtkWidget          *widget,
-                                                    const GList        *item_uris);
-GtkClipboard* nautilus_clipboard_get               (GtkWidget          *widget);
-GList* nautilus_clipboard_get_uri_list_from_selection_data (GtkSelectionData   *selection_data);
-gboolean nautilus_clipboard_is_cut_from_selection_data (GtkSelectionData *selection_data);
-void nautilus_clipboard_prepare_for_files (GtkClipboard *clipboard,
-                                           GList        *files,
-                                           gboolean      cut);
-GdkAtom nautilus_clipboard_get_atom (void);
+void      nautilus_clipboard_clear                     (GtkWidget   *widget);
+void      nautilus_clipboard_clear_if_colliding_uris   (GtkWidget   *widget,
+                                                        const GList *item_uris);
+
+GList    *nautilus_clipboard_get_files                 (GtkWidget   *widget,
+                                                        gboolean    *cut);
+
+void      nautilus_clipboard_prepare_for_files         (GtkWidget   *widget,
+                                                        GList       *files,
+                                                        gboolean     cut);
+
+void      nautilus_clipboard_init                      (void);
