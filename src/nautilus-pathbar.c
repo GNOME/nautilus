e37100534e31e8eece5597b1cae177630d006e86
Ernestas Kulik <ernestask@gnome.org>
2018-07-06 10:13:07 +0300

    general: Use gtk_widget_get_preferred_size()
    The functions for getting the width and height separately are now gone.

--- src/nautilus-pathbar.c
+++ src/nautilus-pathbar.c
@@ -508,7 +508,6 @@ nautilus_path_bar_size_allocate (GtkWidget     *widget,
         return;
     }
     direction = gtk_widget_get_direction (widget);
-
     width = 0;
 
     gtk_widget_get_preferred_size (BUTTON_DATA (self->button_list->data)->container,
@@ -593,6 +592,7 @@ nautilus_path_bar_size_allocate (GtkWidget     *widget,
 
     /* Determine the largest possible allocation size */
     largest_width = allocation->width;
+
     for (list = first_button; list; list = list->prev)
     {
         child = BUTTON_DATA (list->data)->container;
@@ -603,7 +603,6 @@ nautilus_path_bar_size_allocate (GtkWidget     *widget,
         {
             child_allocation.x -= child_allocation.width;
         }
-        /* Check to see if we've don't have any more space to allocate buttons */
 
         gtk_widget_set_child_visible (child, TRUE);
         gtk_widget_size_allocate (child, &child_allocation);
