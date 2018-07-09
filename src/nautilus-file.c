2f8b609d1b25a1fb62798244d80ee8b73e4c0b15
Ernestas Kulik <ernestask@gnome.org>
2018-07-09 12:38:41 +0300

    eel: canvas: Use snapshot API for drawing
    Additionally, this replaces the canvas item thumbnail pixbuf with a GdkTexture,
    which breaks prelighting and selection coloring. Clips are no longer
    checked for using Cairo, which should be investigated later, as that
    might be a performance issue later on.

--- src/nautilus-file.c
+++ src/nautilus-file.c
@@ -5170,6 +5170,7 @@ nautilus_file_get_thumbnail_icon (NautilusFile          *file,
                                                                  NAUTILUS_PREFERENCES_USE_EXPERIMENTAL_VIEWS);
                 if (!use_experimental_views)
                 {
+#if 0
                     if (nautilus_is_video_file (file))
                     {
                         nautilus_ui_frame_video (&pixbuf);
@@ -5178,6 +5179,7 @@ nautilus_file_get_thumbnail_icon (NautilusFile          *file,
                     {
                         nautilus_ui_frame_image (&pixbuf);
                     }
+#endif
                 }
             }
 
