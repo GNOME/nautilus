0bed86cd38e4d9d3c418e8cf42c0aea713345d33
Ernestas Kulik <ernestask@gnome.org>
2018-07-12 09:30:43 +0300

    file: Don’t frame videos or images
    Video framing is no longer desirable from a design standpoint, and
    framing images does not align with what’s done in the flow box view.

--- src/nautilus-ui-utilities.h
+++ src/nautilus-ui-utilities.h
@@ -37,9 +37,6 @@ void        nautilus_pop_up_context_menu_at_pointer (GtkWidget         *parent,
                                                      GMenu             *menu,
                                                      const GdkEvent    *event);
 
-void        nautilus_ui_frame_image                 (GdkPixbuf        **pixbuf);
-void        nautilus_ui_frame_video                 (GdkPixbuf        **pixbuf);
-
 gboolean    nautilus_file_date_in_between           (guint64            file_unix_time,
                                                      GDateTime         *initial_date,
                                                      GDateTime         *end_date);
