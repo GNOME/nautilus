3f80f57cce1e2c23f93f6c1ab4d319bb77d380ba
Ernestas Kulik <ernestask@gnome.org>
2018-07-13 13:27:50 +0300

    general: Replace GtkWidget::delete-event with GtkWindow::close-request

--- src/nautilus-window.c
+++ src/nautilus-window.c
@@ -2568,10 +2568,9 @@ on_is_maximized_changed (GObject    *object,
 }
 
 static gboolean
-nautilus_window_delete_event (GtkWidget   *widget,
-                              GdkEventAny *event)
+nautilus_window_close_request (GtkWindow *window)
 {
-    nautilus_window_close (NAUTILUS_WINDOW (widget));
+    nautilus_window_close (NAUTILUS_WINDOW (window));
     return FALSE;
 }
 
@@ -2704,6 +2703,7 @@ nautilus_window_class_init (NautilusWindowClass *class)
 {
     GObjectClass *oclass = G_OBJECT_CLASS (class);
     GtkWidgetClass *wclass = GTK_WIDGET_CLASS (class);
+    GtkWindowClass *window_class = GTK_WINDOW_CLASS (class);
 
     oclass->finalize = nautilus_window_finalize;
     oclass->constructed = nautilus_window_constructed;
@@ -2711,8 +2711,9 @@ nautilus_window_class_init (NautilusWindowClass *class)
     wclass->destroy = nautilus_window_destroy;
     wclass->show = nautilus_window_show;
     wclass->realize = nautilus_window_realize;
-    wclass->delete_event = nautilus_window_delete_event;
     wclass->grab_focus = nautilus_window_grab_focus;
+
+    window_class->close_request = nautilus_window_close_request;
 
     gtk_widget_class_set_template_from_resource (wclass,
                                                  "/org/gnome/nautilus/ui/nautilus-window.ui");
