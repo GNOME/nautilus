226dec75ddde837e13caa1c4a755f00756108b34
Ernestas Kulik <ernestask@gnome.org>
2018-07-26 15:05:46 +0300

    pathbar: Remove event GdkWindow

--- src/nautilus-pathbar.c
+++ src/nautilus-pathbar.c
@@ -84,8 +84,6 @@ typedef struct
 
 typedef struct
 {
-    GdkWindow *event_window;
-
     GFile *current_path;
     gpointer current_button_data;
 
@@ -435,30 +433,6 @@ nautilus_path_bar_measure (GtkWidget      *widget,
     }
 }
 
-static void
-nautilus_path_bar_unmap (GtkWidget *widget)
-{
-    NautilusPathBarPrivate *priv;
-
-    priv = nautilus_path_bar_get_instance_private (NAUTILUS_PATH_BAR (widget));
-
-    gdk_window_hide (priv->event_window);
-
-    GTK_WIDGET_CLASS (nautilus_path_bar_parent_class)->unmap (widget);
-}
-
-static void
-nautilus_path_bar_map (GtkWidget *widget)
-{
-    NautilusPathBarPrivate *priv;
-
-    priv = nautilus_path_bar_get_instance_private (NAUTILUS_PATH_BAR (widget));
-
-    gdk_window_show (priv->event_window);
-
-    GTK_WIDGET_CLASS (nautilus_path_bar_parent_class)->map (widget);
-}
-
 #define BUTTON_BOTTOM_SHADOW 1
 
 /* This is a tad complicated */
@@ -627,61 +601,6 @@ nautilus_path_bar_display_changed (GtkWidget  *widget,
 }
 
 static void
-nautilus_path_bar_realize (GtkWidget *widget)
-{
-    NautilusPathBar *self;
-    NautilusPathBarPrivate *priv;
-    GtkAllocation allocation;
-    GdkWindow *window;
-    GdkWindowAttr attributes;
-    gint attributes_mask;
-
-    gtk_widget_set_realized (widget, TRUE);
-
-    self = NAUTILUS_PATH_BAR (widget);
-    priv = nautilus_path_bar_get_instance_private (self);
-
-    window = gtk_widget_get_parent_window (widget);
-    gtk_widget_set_window (widget, window);
-    g_object_ref (window);
-
-    gtk_widget_get_allocation (widget, &allocation);
-
-    attributes.window_type = GDK_WINDOW_CHILD;
-    attributes.x = allocation.x;
-    attributes.y = allocation.y;
-    attributes.width = allocation.width;
-    attributes.height = allocation.height;
-    attributes.wclass = GDK_INPUT_ONLY;
-    attributes.event_mask = gtk_widget_get_events (widget);
-    attributes.event_mask |=
-        GDK_BUTTON_PRESS_MASK |
-        GDK_BUTTON_RELEASE_MASK |
-        GDK_POINTER_MOTION_MASK;
-    attributes_mask = GDK_WA_X | GDK_WA_Y;
-
-    priv->event_window = gdk_window_new (gtk_widget_get_parent_window (widget),
-                                         &attributes, attributes_mask);
-    gdk_window_set_user_data (priv->event_window, widget);
-}
-
-static void
-nautilus_path_bar_unrealize (GtkWidget *widget)
-{
-    NautilusPathBar *self;
-    NautilusPathBarPrivate *priv;
-
-    self = NAUTILUS_PATH_BAR (widget);
-    priv = nautilus_path_bar_get_instance_private (self);
-
-    gdk_window_set_user_data (priv->event_window, NULL);
-    gdk_window_destroy (priv->event_window);
-    priv->event_window = NULL;
-
-    GTK_WIDGET_CLASS (nautilus_path_bar_parent_class)->unrealize (widget);
-}
-
-static void
 nautilus_path_bar_add (GtkContainer *container,
                        GtkWidget    *widget)
 {
@@ -844,10 +763,6 @@ nautilus_path_bar_class_init (NautilusPathBarClass *path_bar_class)
     gobject_class->dispose = nautilus_path_bar_dispose;
 
     widget_class->measure = nautilus_path_bar_measure;
-    widget_class->realize = nautilus_path_bar_realize;
-    widget_class->unrealize = nautilus_path_bar_unrealize;
-    widget_class->unmap = nautilus_path_bar_unmap;
-    widget_class->map = nautilus_path_bar_map;
     widget_class->size_allocate = nautilus_path_bar_size_allocate;
     widget_class->style_updated = nautilus_path_bar_style_updated;
     widget_class->display_changed = nautilus_path_bar_display_changed;
