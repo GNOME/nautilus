203a0464357f14bf89e1bf80214760985e2bbff5
Carlos Soriano <csoriano@redhat.com>
2018-07-26 13:42:15 +0200

    toolbar: Adjust width requirements
    So we allow half sized windows in 1360px screens. This is delicatily
    adjusted with the default GNOME set up: Adwaita theme, only close
    button as window controls and Cantarell as font.
    
    Closes https://gitlab.gnome.org/GNOME/nautilus/issues/425

--- src/nautilus-pathbar.c
+++ src/nautilus-pathbar.c
@@ -57,7 +57,7 @@ typedef enum
 static guint path_bar_signals [LAST_SIGNAL] = { 0 };
 
 #define NAUTILUS_PATH_BAR_ICON_SIZE 16
-#define NAUTILUS_PATH_BAR_BUTTON_MAX_WIDTH 250
+#define NAUTILUS_PATH_BAR_BUTTON_MAX_WIDTH 175
 
 typedef struct
 {
