aca72d28691de1164f69aad6fc8de438ddd72c4d
António Fernandes <antoniof@gnome.org>
2018-07-31 18:39:02 +0100

    file-name-widget: Warn when name exceeds size limit
    An warning for names exceeding the size limit has been introduced in
    122f201dcf356460a30a7b260730bcfc7246a614 for the rename popover.
    
    This would be useful for the compress and new folder dialogs as well.
    
    So, make all file name widget derivatives share this feature.

--- src/nautilus-file-name-widget-controller.h
+++ src/nautilus-file-name-widget-controller.h
@@ -48,3 +48,5 @@ gchar * nautilus_file_name_widget_controller_get_new_name (NautilusFileNameWidge
 
 void    nautilus_file_name_widget_controller_set_containing_directory (NautilusFileNameWidgetController *controller,
                                                                        NautilusDirectory                *directory);
+gboolean nautilus_file_name_widget_controller_is_name_too_long (NautilusFileNameWidgetController  *self,
+                                                                gchar                             *name);
