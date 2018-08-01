6c19153b4bdaad655e43aa9146e14bc2c8b5577e
Alexandru Fazakas <alex.fazakas97@gmail.com>
2018-08-01 17:26:39 +0300

    test: Add nautilus-search-engine test
    This patch updates the nautilus-search-engine test to work properly
    while running all engines.

--- test/automated/displayless/test-utilities.h
+++ test/automated/displayless/test-utilities.h
@@ -1,4 +1,9 @@
 #include <gio/gio.h>
+#include <src/nautilus-file-utilities.h>
+#include <src/nautilus-global-preferences.h>
+#include <src/nautilus-search-engine.h>
+#include <gtk/gtk.h>
+#include <locale.h>
 
 #pragma once
 
