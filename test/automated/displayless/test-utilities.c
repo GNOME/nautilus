ce12da68c464b0a7535b4bd2d95802df4e280b06
Alexandru Fazakas <alex.fazakas97@gmail.com>
2018-08-01 17:15:30 +0300

    test: Add test utilities library
    This library includes a few functions for creating and deleting
    different file hierarchies which should make working with tests
    easier and reduce the amount of repeated code.

---
+++ test/automated/displayless/test-utilities.c
@@ -0,0 +1,100 @@
+#include "test-utilities.h"
+
+void
+create_search_file_hierarchy (gchar *search_engine)
+{
+    g_autoptr (GFile) location = NULL;
+    g_autoptr (GFile) file = NULL;
+    GFileOutputStream *out;
+    g_autoptr (GError) error = NULL;
+    g_autofree gchar *file_name = NULL;
+
+    location = g_file_new_for_path (g_get_tmp_dir ());
+
+    file_name = g_strdup_printf ("engine_%s", search_engine);
+    file = g_file_get_child (location, file_name);
+    out = g_file_create (file, G_FILE_CREATE_NONE, NULL, NULL);
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_EXISTS))
+    {
+        g_object_unref (out);
+    }
+
+
+    file_name = g_strdup_printf ("engine_%s_directory", search_engine);
+    file = g_file_get_child (location, file_name);
+    g_file_make_directory (file, NULL, NULL);
+
+    file_name = g_strdup_printf ("%s_child", search_engine);
+    file = g_file_get_child (file, file_name);
+    out = g_file_create (file, G_FILE_CREATE_NONE, NULL, NULL);
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_EXISTS))
+    {
+        g_object_unref (out);
+    }
+
+    file_name = g_strdup_printf ("engine_%s_second_directory", search_engine);
+    file = g_file_get_child (location, file_name);
+    g_file_make_directory (file, NULL, NULL);
+
+    file_name = g_strdup_printf ("engine_%s_child", search_engine);
+    file = g_file_get_child (file, file_name);
+    out = g_file_create (file, G_FILE_CREATE_NONE, NULL, NULL);
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_EXISTS))
+    {
+        g_object_unref (out);
+    }
+
+    file_name = g_strdup_printf ("%s_directory", search_engine);
+    file = g_file_get_child (location, file_name);
+    g_file_make_directory (file, NULL, NULL);
+
+    file_name = g_strdup_printf ("engine_%s_child", search_engine);
+    file = g_file_get_child (file, file_name);
+    out = g_file_create (file, G_FILE_CREATE_NONE, NULL, NULL);
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_EXISTS))
+    {
+        g_object_unref (out);
+    }
+}
+
+void
+delete_search_file_hierarchy (gchar *search_engine)
+{
+    g_autoptr (GFile) location = NULL;
+    g_autoptr (GFile) file = NULL;
+    g_autoptr (GError) error = NULL;
+    g_autofree gchar *file_name = NULL;
+
+    location = g_file_new_for_path (g_get_tmp_dir ()); 
+
+    file_name = g_strdup_printf ("engine_%s", search_engine);
+    file = g_file_get_child (location, file_name);
+    g_file_delete (file, NULL, NULL);
+
+    file_name = g_strdup_printf ("engine_%s_directory", search_engine);
+    file = g_file_get_child (location, file_name);
+    file_name = g_strdup_printf ("%s_child", search_engine);
+    file = g_file_get_child (file, file_name);
+    g_file_delete (file, NULL, NULL);
+    file_name = g_strdup_printf ("engine_%s_directory", search_engine);
+    file = g_file_get_child (location, file_name);
+    g_file_delete (file, NULL, NULL);
+
+    file_name = g_strdup_printf ("engine_%s_second_directory", search_engine);
+    file = g_file_get_child (location, file_name);
+    file_name = g_strdup_printf ("engine_%s_child", search_engine);
+    file = g_file_get_child (file, file_name);
+    g_file_delete (file, NULL, NULL);
+    file_name = g_strdup_printf ("engine_%s_second_directory", search_engine);
+    file = g_file_get_child (location, file_name);
+    g_file_delete (file, NULL, NULL);
+
+    file_name = g_strdup_printf ("%s_directory", search_engine);
+    file = g_file_get_child (location, file_name);
+    file_name = g_strdup_printf ("engine_%s_child", search_engine);
+    file = g_file_get_child (file, file_name);
+    g_file_delete (file, NULL, NULL);
+    file_name = g_strdup_printf ("%s_directory", search_engine);
+    file = g_file_get_child (location, file_name);
+    g_file_delete (file, NULL, NULL);
+}
