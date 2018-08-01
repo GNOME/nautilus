b53f36cb9abee95cec4213218e06189072351f20
Alexandru Fazakas <alex.fazakas97@gmail.com>
2018-08-01 17:26:39 +0300

    test: Add nautilus-search-engine test
    This patch updates the nautilus-search-engine test to work properly
    while running all engines.

--- test/automated/displayless/test-nautilus-search-engine.c
+++ test/automated/displayless/test-nautilus-search-engine.c
@@ -1,18 +1,21 @@
 #include <src/nautilus-file-utilities.h>
 #include <src/nautilus-global-preferences.h>
 #include <src/nautilus-search-provider.h>
-#include <src/nautilus-search-engine.h>
+#include <src/nautilus-search-engine.c>
 #include <gtk/gtk.h>
+#include <locale.h>
+#include "test-nautilus-search-utilities.h"
 
 static void
 hits_added_cb (NautilusSearchEngine *engine,
                GSList               *hits)
 {
-    g_print ("hits added\n");
-    while (hits)
+    gint hit_number = 0;
+
+    g_print ("Hits added for search engine!\n");
+    for (; hits != NULL; hits = hits->next, hit_number++)
     {
-        g_print (" - %s\n", (char *) hits->data);
-        hits = hits->next;
+        g_print ("Hit %i: %s\n", hit_number, nautilus_search_hit_get_uri (hits->data));
     }
 }
 
@@ -21,7 +24,12 @@ finished_cb (NautilusSearchEngine         *engine,
              NautilusSearchProviderStatus  status,
              gpointer                      user_data)
 {
-    g_print ("finished!\n");
+    g_print ("\nNautilus search engine finished!\n");
+
+    nautilus_search_provider_stop (NAUTILUS_SEARCH_PROVIDER (engine));
+
+    delete_search_file_hierarchy ("all_engines");
+
     g_main_loop_quit (user_data);
 }
 
@@ -29,12 +37,15 @@ int
 main (int   argc,
       char *argv[])
 {
-    GMainLoop *loop;
+    g_autoptr (GMainLoop) loop = NULL;
     NautilusSearchEngine *engine;
     NautilusSearchEngineModel *model;
-    NautilusDirectory *directory;
-    NautilusQuery *query;
-    GFile *location;
+    g_autoptr (NautilusDirectory) directory = NULL;
+    g_autoptr (NautilusQuery) query = NULL;
+    g_autoptr (GFile) location = NULL;
+    g_autoptr (GFile) file = NULL;
+    GFileOutputStream *out;
+    g_autoptr (GError) error = NULL;
 
     loop = g_main_loop_new (NULL, TRUE);
 
@@ -49,21 +60,26 @@ main (int   argc,
                       G_CALLBACK (finished_cb), loop);
 
     query = nautilus_query_new ();
-    nautilus_query_set_text (query, "richard hult");
+    nautilus_query_set_text (query, "engine_all_engines");
     nautilus_search_provider_set_query (NAUTILUS_SEARCH_PROVIDER (engine), query);
-    g_object_unref (query);
 
-    location = g_file_new_for_path (g_get_home_dir ());
+    location = g_file_new_for_path (g_get_tmp_dir ());
     directory = nautilus_directory_get (location);
-    g_object_unref (location);
+    nautilus_query_set_location (query, location);
 
     model = nautilus_search_engine_get_model_provider (engine);
     nautilus_search_engine_model_set_model (model, directory);
-    g_object_unref (directory);
+
+    create_search_file_hierarchy ("all_engines");
+
+    priv->providers_running = 0;
+    priv->providers_finished = 0;
+    priv->providers_error = 0;
+
+    priv->restart = FALSE;
+    priv->running++;
 
     nautilus_search_provider_start (NAUTILUS_SEARCH_PROVIDER (engine));
-    nautilus_search_provider_stop (NAUTILUS_SEARCH_PROVIDER (engine));
-    g_object_unref (engine);
 
     g_main_loop_run (loop);
     return 0;
