7d249989710c15f2cd6494d65c6c6966a58e6905
Alexandru Fazakas <alex.fazakas97@gmail.com>
2018-08-17 12:39:20 +0300

    test: Implement all file hierarchies in library
    This patch adds functions for creating and deleting all of the file
    hierarchies we use across all tests to the testing library.
    
    Using the library should reduce the amount of unnecessary code in
    all implemented tests.

--- test/automated/displayless/test-utilities.h
+++ test/automated/displayless/test-utilities.h
@@ -3,12 +3,32 @@
 #include <src/nautilus-global-preferences.h>
 #include <src/nautilus-search-engine.h>
 #include <tracker-sparql.h>
-#include <gtk/gtk.h>
-
 #include <locale.h>
+#include <src/nautilus-file-undo-manager.h>
+#include <src/nautilus-file-operations.h>
+#include <gio/gio.h>
 
 #pragma once
 
-void create_search_file_hierarchy (gchar *search_engine);
+void empty_directory (GFile *parent,
+                      gchar *prefix);
 
+void create_search_file_hierarchy (gchar *search_engine);
 void delete_search_file_hierarchy (gchar *search_engine);
+
+void quit_loop_callback (NautilusFileUndoManager *undo_manager,
+                         GMainLoop               *loop);
+void test_operation_undo_redo (void);
+void test_operation_undo (void);
+
+void create_one_file (gchar *prefix);
+void create_one_empty_directory (gchar *prefix);
+
+void create_multiple_files (gchar *prefix, gint number_of_files);
+void create_multiple_directories (gchar *prefix, gint number_of_directories);
+void create_multiple_full_directories (gchar *prefix, gint number_of_directories);
+
+void create_first_hierarchy (gchar *prefix);
+void create_second_hierarchy (gchar *prefix);
+void create_third_hierarchy (gchar *prefix);
+void create_fourth_hierarchy (gchar *prefix);
