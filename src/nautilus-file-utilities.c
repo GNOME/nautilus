GNOME/nautilus

commit a9c446afe262b7989bc17ef1f9ab19931db8dede

Add favorite files
Add option to make files Favorite, by either toggling a star in the
list view, or from the context menu.

https://bugzilla.gnome.org/show_bug.cgi?id=786039

src/nautilus-file-utilities.c
@@ -322,7 +322,10 @@ nautilus_compute_title_for_location (GFile *location)
         {
             title = g_strdup (_("Other Locations"));
         }
-        else
+        else if (nautilus_file_is_favorite_location (file))
+        {
+            title = g_strdup (_("Starred"));
+        }
         {
             title = nautilus_file_get_description (file);
 
@@ -590,6 +593,19 @@ nautilus_is_search_directory (GFile *dir)
 
     uri = g_file_get_uri (dir);
     return eel_uri_is_search (uri);
+}
+
+gboolean
+nautilus_is_favorite_directory (GFile *dir)
+{
+    g_autofree gchar *uri = NULL;
+
+    uri = g_file_get_uri (dir);
+
+    if (eel_uri_is_favorites (uri))
+        return TRUE;
+
+    return FALSE;
 }
 
 gboolean

data/org.gnome.nautilus.gschema.xml
@@ -255,11 +255,11 @@
       <summary>Default list view zoom level</summary>
     </key>
     <key type="as" name="default-visible-columns">
-      <default>[ 'name', 'size', 'date_modified' ]</default>
+      <default>[ 'name', 'size', 'date_modified', 'favorite' ]</default>
       <summary>Columns visible in list view</summary>
     </key>
     <key type="as" name="default-column-order">
-      <default>[ 'name', 'size', 'type', 'owner', 'group', 'permissions', 'mime_type', 'where', 'date_modified', 'date_modified_with_time', 'date_accessed' ]</default>
+      <default>[ 'name', 'size', 'type', 'owner', 'group', 'permissions', 'mime_type', 'where', 'date_modified', 'date_modified_with_time', 'date_accessed', 'recency', 'favorite' ]</default>
       <summary>Column order in list view</summary>
     </key>
     <key type="b" name="use-tree-view">

eel/eel-vfs-extensions.c
@@ -38,6 +38,12 @@
 #include <stdlib.h>
 
 gboolean
+eel_uri_is_favorites (const gchar *uri)
+{
+    return g_str_has_prefix (uri, "favorites:");
+}
+
+gboolean
 eel_uri_is_trash (const char *uri)
 {
     return g_str_has_prefix (uri, "trash:");

eel/eel-vfs-extensions.h
@@ -35,6 +35,7 @@ G_BEGIN_DECLS
 #define EEL_DESKTOP_URI "x-nautilus-desktop:"
 #define EEL_SEARCH_URI "x-nautilus-search:"
 
+gboolean           eel_uri_is_favorites                  (const char           *uri);
 gboolean           eel_uri_is_trash                      (const char           *uri);
 gboolean           eel_uri_is_trash_folder               (const char           *uri);
 gboolean           eel_uri_is_in_trash                   (const char           *uri);

src/meson.build
@@ -259,7 +259,11 @@ libnautilus_sources = [
     'nautilus-batch-rename-utilities.c',
     'nautilus-batch-rename-utilities.h',
     'nautilus-search-engine-tracker.c',
-    'nautilus-search-engine-tracker.h'
+    'nautilus-search-engine-tracker.h',
+    'nautilus-tag-manager.c',
+    'nautilus-tag-manager.h',
+    'nautilus-favorite-directory.c',
+    'nautilus-favorite-directory.h'
 ]
 
 nautilus_deps = [glib,

src/nautilus-application.c
@@ -39,6 +39,7 @@
 #include "nautilus-window.h"
 #include "nautilus-window-slot.h"
 #include "nautilus-preferences-window.h"
+#include "nautilus-tag-manager.h"
 
 #include "nautilus-directory-private.h"
 #include "nautilus-file-utilities.h"
@@ -80,6 +81,9 @@ typedef struct
     GHashTable *notifications;
 
     NautilusFileUndoManager *undo_manager;
+
+    NautilusTagManager *tag_manager;
+    GCancellable *tag_manager_cancellable;
 } NautilusApplicationPrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE (NautilusApplication, nautilus_application, GTK_TYPE_APPLICATION);
@@ -612,6 +616,11 @@ nautilus_application_finalize (GObject *object)
 
     g_clear_object (&priv->undo_manager);
 
+    g_clear_object (&priv->tag_manager);
+
+    g_cancellable_cancel (priv->tag_manager_cancellable);
+    g_clear_object (&priv->tag_manager_cancellable);
+
     G_OBJECT_CLASS (nautilus_application_parent_class)->finalize (object);
 }
 
@@ -1102,6 +1111,11 @@ nautilus_application_init (NautilusApplication *self)
                                                  NULL);
 
     priv->undo_manager = nautilus_file_undo_manager_new ();
+
+    priv->tag_manager_cancellable = g_cancellable_new ();
+    priv->tag_manager = nautilus_tag_manager_get ();
+    nautilus_tag_manager_set_cancellable (priv->tag_manager,
+                                          priv->tag_manager_cancellable);
 
     g_application_add_main_option_entries (G_APPLICATION (self), options);
 

src/nautilus-bookmark-list.c
@@ -643,6 +643,11 @@ nautilus_bookmark_list_can_bookmark_location (NautilusBookmarkList *list,
         return FALSE;
     }
 
+    if (nautilus_is_favorite_directory (location))
+    {
+        return FALSE;
+    }
+
     bookmark = nautilus_bookmark_new (location, NULL);
     is_builtin = nautilus_bookmark_get_is_builtin (bookmark);
     g_object_unref (bookmark);

src/nautilus-column-utilities.c
@@ -42,6 +42,7 @@ static const char *default_column_order[] =
     "date_modified",
     "date_accessed",
     "recency",
+    "favorite",
     NULL
 };
 
@@ -147,6 +148,16 @@ get_builtin_columns (void)
                                            "description", _("The date the file was accessed by the user."),
                                            "default-sort-order", GTK_SORT_DESCENDING,
                                            "xalign", 1.0,
+                                           NULL));
+
+    columns = g_list_append (columns,
+                             g_object_new (NAUTILUS_TYPE_COLUMN,
+                                           "name", "favorite",
+                                           "attribute", "favorite",
+                                           "label", _("Star"),
+                                           "description", _("Shows if file is favorite."),
+                                           "default-sort-order", GTK_SORT_DESCENDING,
+                                           "xalign", 0.5,
                                            NULL));
 
     return columns;

commit 64d47557e03ffa1e29b49ec19d139dcf93dc8c97

file-utilities: Add missing else statement
a9c446afe262b7989bc17ef1f9ab19931db8dede inadvertently removed it and
that is causing leaks that don’t make sense at first.

src/nautilus-file-utilities.c
@@ -326,6 +326,7 @@ nautilus_compute_title_for_location (GFile *location)
         {
             title = g_strdup (_("Starred"));
         }
+        else
         {
             title = nautilus_file_get_description (file);
 