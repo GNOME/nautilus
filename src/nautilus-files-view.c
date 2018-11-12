dc936b7e022b0522886261df91ed4bfe318cea3d
Ernestas Kulik <ekulik@redhat.com>
2018-11-12 12:51:58 +0100

    files-view: Adapt to changes in GtkEventControllerScroll
    The ::scroll handler now returns a boolean value to indicate whether the
    event was handled.

--- src/nautilus-files-view.c
+++ src/nautilus-files-view.c
@@ -8952,7 +8952,7 @@ nautilus_files_view_set_property (GObject      *object,
     }
 }
 
-static void
+static gboolean
 on_event_controller_scroll_scroll (GtkEventControllerScroll *controller,
                                    double                    dx,
                                    double                    dy,
@@ -8965,12 +8965,12 @@ on_event_controller_scroll_scroll (GtkEventControllerScroll *controller,
 
     if (!gtk_get_current_event_state (&state))
     {
-        return;
+        return GDK_EVENT_PROPAGATE;
     }
 
     if (!(state & GDK_CONTROL_MASK))
     {
-        return;
+        return GDK_EVENT_PROPAGATE;
     }
 
     if (dy < 0)
@@ -8983,6 +8983,8 @@ on_event_controller_scroll_scroll (GtkEventControllerScroll *controller,
         /* Zoom Out */
         nautilus_files_view_bump_zoom_level (directory_view, -1);
     }
+
+    return GDK_EVENT_STOP;
 }
 
 static void
