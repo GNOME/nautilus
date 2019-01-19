4757a79ac01fedfc6b6d47b95ded0ab60c6f10b2
Timothy OBrien <obrien.timothy.a@gmail.com>
2019-01-19 11:39:38 +1100

    nautilus-batch-rename: batch rename with long file names
    The batch rename dialog (NautilusBatchRenameDialog) is a fixed-size window. Therefore, ellipsized names and changes may become ambiguous to the user.
    
    This patch removes the ellipsize so that the user can scroll vertically or horizontally to reveal all file names and changes.
    
    Fixes #819

--- src/nautilus-batch-rename-dialog.c
+++ src/nautilus-batch-rename-dialog.c
@@ -590,7 +590,6 @@ create_original_name_row_for_label (NautilusBatchRenameDialog *dialog,
     gtk_label_set_xalign (GTK_LABEL (label_old), 0.0);
     gtk_widget_set_hexpand (label_old, TRUE);
     gtk_widget_set_margin_start (label_old, ROW_MARGIN_START);
-    gtk_label_set_ellipsize (GTK_LABEL (label_old), PANGO_ELLIPSIZE_END);
 
     dialog->listbox_labels_old = g_list_prepend (dialog->listbox_labels_old, label_old);
 
@@ -616,7 +615,6 @@ create_result_row_for_label (NautilusBatchRenameDialog *dialog,
     gtk_label_set_xalign (GTK_LABEL (label_new), 0.0);
     gtk_widget_set_hexpand (label_new, TRUE);
     gtk_widget_set_margin_start (label_new, ROW_MARGIN_START);
-    gtk_label_set_ellipsize (GTK_LABEL (label_new), PANGO_ELLIPSIZE_END);
 
     dialog->listbox_labels_new = g_list_prepend (dialog->listbox_labels_new, label_new);
 
