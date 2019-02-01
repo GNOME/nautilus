905faaa2d6493cb535c72f9639911d4c5f126836
Sumaid <sumaidsyed@gmail.com>
2019-02-01 15:56:26 +0530

    nautilus-canvas-container.c : Shorten Long Names in Last Row of Canvas View
    Files in last row in canvas view have thier full names names,
    even if name is very long.
    
    Attribute 'whole-text' is set to FALSE for last lines also.
    
    Follows setting text-ellipsis-limit.
    
    https://gitlab.gnome.org/GNOME/nautilus/issues/843

--- src/nautilus-canvas-container.c
+++ src/nautilus-canvas-container.c
@@ -1306,7 +1306,7 @@ lay_down_icons_horizontal (NautilusCanvasContainer *container,
         /* Advance to the baseline. */
         y += ICON_PAD_TOP + max_height_above;
 
-        lay_down_one_line (container, line_start, NULL, y, max_height_above, positions, TRUE);
+        lay_down_one_line (container, line_start, NULL, y, max_height_above, positions, FALSE);
     }
 
     g_array_free (positions, TRUE);
