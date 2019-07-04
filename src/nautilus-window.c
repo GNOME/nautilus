aa73ee0d59ba3e7fd155894b56a395322d923815
Ondrej Holy <oholy@redhat.com>
2019-07-04 08:31:23 +0000

    window: Add website link to About dialog
    Add the missing website link in the About dialog.
    
    This fixes the following downstream bug:
    https://bugzilla.redhat.com/show_bug.cgi?id=1725101

--- src/nautilus-window.c
+++ src/nautilus-window.c
@@ -2864,6 +2864,7 @@ nautilus_window_show_about_dialog (NautilusWindow *window)
                            "program-name", program_name,
                            "version", VERSION,
                            "comments", _("Access and organize your files"),
+                           "website", "https://wiki.gnome.org/action/show/Apps/Files",
                            "copyright", "© 1999–2018 The Files Authors",
                            "license-type", GTK_LICENSE_GPL_3_0,
                            "artists", artists,
