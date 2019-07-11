f99977dd150c84659950bacff36727adc7c05f49
António Fernandes <antoniof@gnome.org>
2019-07-12 00:20:13 +0100

    application: Hide --no-default-window option
    It's a dead option since commit b80390d0c486a2c19ba7a8005e038c809a45763d
    
    Hide it and drop it form the man page.
    
    While we are at it, rearrange the code to group dead options together,
    and elaborate on the code comment. Also, drop their descriptions, which
    are useless for hidden options and may be wasting translators time.

--- src/nautilus-application.c
+++ src/nautilus-application.c
@@ -1065,23 +1065,25 @@ nautilus_application_init (NautilusApplication *self)
         { "check", 'c', 0, G_OPTION_ARG_NONE, NULL,
           N_("Perform a quick set of self-check tests."), NULL },
 #endif
-        /* dummy, only for compatibility reasons */
-        { "browser", '\0', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, NULL,
-          NULL, NULL },
-        /* ditto */
-        { "geometry", 'g', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_STRING, NULL,
-          N_("Create the initial window with the given geometry."), N_("GEOMETRY") },
         { "version", '\0', 0, G_OPTION_ARG_NONE, NULL,
           N_("Show the version of the program."), NULL },
         { "new-window", 'w', 0, G_OPTION_ARG_NONE, NULL,
           N_("Always open a new window for browsing specified URIs"), NULL },
-        { "no-default-window", 'n', 0, G_OPTION_ARG_NONE, NULL,
-          N_("Only create windows for explicitly specified URIs."), NULL },
         { "quit", 'q', 0, G_OPTION_ARG_NONE, NULL,
           N_("Quit Nautilus."), NULL },
         { "select", 's', 0, G_OPTION_ARG_NONE, NULL,
           N_("Select specified URI in parent folder."), NULL },
         { G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, NULL, NULL, N_("[URI…]") },
+
+        /* The following are old options which have no effect anymore. We keep
+         * them around for compatibility reasons, e.g. not breaking old scripts.
+         */
+        { "browser", '\0', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, NULL,
+          NULL, NULL },
+        { "geometry", 'g', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_STRING, NULL,
+          NULL, NULL },
+        { "no-default-window", 'n', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, NULL,
+          NULL, NULL },
 
         { NULL }
     };
