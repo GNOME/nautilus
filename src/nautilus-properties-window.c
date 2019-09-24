7032469bfd25f9a3ae22c3f737c1ec250b12c3cc
Ondrej Holy <oholy@redhat.com>
2019-09-24 18:25:42 +0200

    properties-window: Remove unused parameter
    The cancel_destroy_handler parameter is not used by remove_pending()
    function. Let's remove it to make the code simpler.

--- src/nautilus-properties-window.c
+++ src/nautilus-properties-window.c
@@ -200,8 +200,7 @@ static void set_icon (const char               *icon_path,
                       NautilusPropertiesWindow *properties_window);
 static void remove_pending (StartupData *data,
                             gboolean     cancel_call_when_ready,
-                            gboolean     cancel_timed_wait,
-                            gboolean     cancel_destroy_handler);
+                            gboolean     cancel_timed_wait);
 static void append_extension_pages (NautilusPropertiesWindow *window);
 
 static void name_field_focus_changed (GObject    *object,
@@ -5241,7 +5240,7 @@ properties_window_finish (StartupData *data)
                                               data);
     }
 
-    remove_pending (data, TRUE, (data->window == NULL && !data->cancelled), FALSE);
+    remove_pending (data, TRUE, (data->window == NULL && !data->cancelled));
     startup_data_free (data);
 }
 
@@ -5277,8 +5276,7 @@ cancel_call_when_ready_callback (gpointer key,
 static void
 remove_pending (StartupData *startup_data,
                 gboolean     cancel_call_when_ready,
-                gboolean     cancel_timed_wait,
-                gboolean     cancel_destroy_handler)
+                gboolean     cancel_timed_wait)
 {
     if (cancel_call_when_ready)
     {
@@ -5333,7 +5331,7 @@ is_directory_ready_callback (NautilusFile *file,
         add_window (new_window);
         startup_data->window = new_window;
 
-        remove_pending (startup_data, FALSE, TRUE, TRUE);
+        remove_pending (startup_data, FALSE, TRUE);
 
         gtk_window_present (GTK_WINDOW (new_window));
         g_signal_connect(GTK_WIDGET (new_window), "destroy",
