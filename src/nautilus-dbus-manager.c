f74d8fd37de6167b325aa0e2985488db60740f82
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2020-01-31 00:01:25 +0200

    dbus-manager: add DeleteFiles method
    Add DeleteFiles method to make it possible to permanently delete
    files avoiding trash.
    
    https://gitlab.gnome.org/GNOME/nautilus/merge_requests/504

--- src/nautilus-dbus-manager.c
+++ src/nautilus-dbus-manager.c
@@ -405,6 +405,49 @@ handle_trash_files2 (NautilusDBusFileOperations2  *object,
 }
 
 static void
+delete_on_finished (GHashTable *debutting_uris,
+                    gboolean    user_cancel,
+                    gpointer    callback_data)
+{
+    g_application_release (g_application_get_default ());
+}
+
+static void
+handle_delete_files_internal (const char                     **sources,
+                              NautilusFileOperationsDBusData  *dbus_data)
+{
+    g_autolist (GFile) source_files = NULL;
+    gint idx;
+
+    for (idx = 0; sources[idx] != NULL; idx++)
+    {
+        source_files = g_list_prepend (source_files,
+                                       g_file_new_for_uri (sources[idx]));
+    }
+
+    g_application_hold (g_application_get_default ());
+    nautilus_file_operations_delete_async (source_files, NULL,
+                                           dbus_data,
+                                           delete_on_finished, NULL);
+}
+
+static gboolean
+handle_delete_files2 (NautilusDBusFileOperations2  *object,
+                      GDBusMethodInvocation        *invocation,
+                      const gchar                 **sources,
+                      GVariant                     *platform_data)
+{
+    g_autoptr (NautilusFileOperationsDBusData) dbus_data = NULL;
+
+    dbus_data = nautilus_file_operations_dbus_data_new (platform_data);
+
+    handle_delete_files_internal (sources, dbus_data);
+
+    nautilus_dbus_file_operations2_complete_delete_files (object, invocation);
+    return TRUE; /* invocation was handled */
+}
+
+static void
 rename_file_on_finished (NautilusFile *file,
                          GFile        *result_location,
                          GError       *error,
@@ -507,6 +550,10 @@ nautilus_dbus_manager_init (NautilusDBusManager *self)
     g_signal_connect (self->file_operations2,
                       "handle-trash-files",
                       G_CALLBACK (handle_trash_files2),
+                      self);
+    g_signal_connect (self->file_operations2,
+                      "handle-delete-files",
+                      G_CALLBACK (handle_delete_files2),
                       self);
     g_signal_connect (self->file_operations,
                       "handle-create-folder",
