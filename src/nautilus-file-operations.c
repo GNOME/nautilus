70c0f5b15b06142c5ed21520e39dfc8fa048cde6
António Fernandes <antoniof@gnome.org>
2021-10-15 23:44:33 +0100

    file-operations: Move job structs to private header
    To be usable from a separate file for admin operations.

--- src/nautilus-file-operations.c
+++ src/nautilus-file-operations.c
@@ -33,6 +33,7 @@
 #include <stdlib.h>
 
 #include "nautilus-file-operations.h"
+#include "nautilus-file-operations-private.h"
 
 #include "nautilus-file-changes-queue.h"
 #include "nautilus-lib-self-check-functions.h"
@@ -70,92 +71,6 @@
 
 typedef struct
 {
-    GTimer *time;
-    GtkWindow *parent_window;
-    NautilusFileOperationsDBusData *dbus_data;
-    guint inhibit_cookie;
-    NautilusProgressInfo *progress;
-    GCancellable *cancellable;
-    GHashTable *skip_files;
-    GHashTable *skip_readdir_error;
-    NautilusFileUndoInfo *undo_info;
-    gboolean skip_all_error;
-    gboolean skip_all_conflict;
-    gboolean merge_all;
-    gboolean replace_all;
-    gboolean delete_all;
-} CommonJob;
-
-typedef struct
-{
-    CommonJob common;
-    gboolean is_move;
-    GList *files;
-    GFile *destination;
-    GFile *fake_display_source;
-    GHashTable *debuting_files;
-    gchar *target_name;
-    NautilusCopyCallback done_callback;
-    gpointer done_callback_data;
-} CopyMoveJob;
-
-typedef struct
-{
-    CommonJob common;
-    GList *files;
-    gboolean try_trash;
-    gboolean user_cancel;
-    NautilusDeleteCallback done_callback;
-    gpointer done_callback_data;
-} DeleteJob;
-
-typedef struct
-{
-    CommonJob common;
-    GFile *dest_dir;
-    char *filename;
-    gboolean make_dir;
-    GFile *src;
-    char *src_data;
-    int length;
-    GFile *created_file;
-    NautilusCreateCallback done_callback;
-    gpointer done_callback_data;
-} CreateJob;
-
-
-typedef struct
-{
-    CommonJob common;
-    GList *trash_dirs;
-    gboolean should_confirm;
-    NautilusOpCallback done_callback;
-    gpointer done_callback_data;
-} EmptyTrashJob;
-
-typedef struct
-{
-    CommonJob common;
-    GFile *file;
-    NautilusOpCallback done_callback;
-    gpointer done_callback_data;
-    guint32 file_permissions;
-    guint32 file_mask;
-    guint32 dir_permissions;
-    guint32 dir_mask;
-} SetPermissionsJob;
-
-typedef enum
-{
-    OP_KIND_COPY,
-    OP_KIND_MOVE,
-    OP_KIND_DELETE,
-    OP_KIND_TRASH,
-    OP_KIND_COMPRESS
-} OpKind;
-
-typedef struct
-{
     int num_files_children;
     goffset num_bytes_children;
 } SourceDirInfo;
@@ -186,44 +101,6 @@ typedef struct
      */
     gboolean partial_progress;
 } TransferInfo;
-
-typedef struct
-{
-    CommonJob common;
-    GList *source_files;
-    GFile *destination_directory;
-    GList *output_files;
-    gboolean destination_decided;
-    gboolean extraction_failed;
-
-    gdouble base_progress;
-
-    guint64 archive_compressed_size;
-    guint64 total_compressed_size;
-    gint total_files;
-
-    NautilusExtractCallback done_callback;
-    gpointer done_callback_data;
-} ExtractJob;
-
-typedef struct
-{
-    CommonJob common;
-    GList *source_files;
-    GFile *output_file;
-
-    AutoarFormat format;
-    AutoarFilter filter;
-    gchar *passphrase;
-
-    guint64 total_size;
-    guint total_files;
-
-    gboolean success;
-
-    NautilusCreateCallback done_callback;
-    gpointer done_callback_data;
-} CompressJob;
 
 static void
 source_info_clear (SourceInfo *source_info)
