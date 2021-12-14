GNOME/nautilus

commit e9ae1add9cccee61d10e724b3d7f3526df8ad32a

general: Stop using gtk_container_add()
GtkContainer is gone in GTK4.

Use per-class child-adding methods. For the cases where such methods
are unavailable in GTK3, introduce small wrapper functions with the
same signature as the GTK4 methods, to help with the transition.

src/nautilus-places-view.c
@@ -397,7 +397,7 @@ nautilus_places_view_init (NautilusPlacesView *self)
     gtk_widget_set_hexpand (priv->places_view, TRUE);
     gtk_widget_set_vexpand (priv->places_view, TRUE);
     gtk_widget_show (priv->places_view);
-    gtk_container_add (GTK_CONTAINER (self), priv->places_view);
+    gtk_grid_attach_next_to (GTK_GRID (self), priv->places_view, NULL, GTK_POS_BOTTOM, 1, 1);
 
     g_signal_connect_swapped (priv->places_view,
                               "notify::loading",

src/meson.build
@@ -65,6 +65,8 @@ libnautilus_sources = [
   'gtk/nautilusgtkplacesviewprivate.h',
   'gtk/nautilusgtkplacesviewrow.c',
   'gtk/nautilusgtkplacesviewrowprivate.h',
+  'nautilus-gtk4-helpers.c',
+  'nautilus-gtk4-helpers.h',
   'nautilus-application.c',
   'nautilus-application.h',
   'nautilus-bookmark-list.c',

src/nautilus-canvas-view.c
@@ -44,6 +44,7 @@
 #include "nautilus-canvas-dnd.h"
 #include "nautilus-metadata.h"
 #include "nautilus-clipboard.h"
+#include "nautilus-gtk4-helpers.h"
 
 #define DEBUG_FLAG NAUTILUS_DEBUG_CANVAS_VIEW
 #include "nautilus-debug.h"
@@ -1413,8 +1414,8 @@ initialize_canvas_container (NautilusCanvasView      *canvas_view,
     g_signal_connect_object (canvas_container, "get-container-uri",
                              G_CALLBACK (canvas_view_get_container_uri), canvas_view, 0);
 
-    gtk_container_add (GTK_CONTAINER (content_widget),
-                       GTK_WIDGET (canvas_container));
+    gtk_scrolled_window_set_child (GTK_SCROLLED_WINDOW (content_widget),
+                                   GTK_WIDGET (canvas_container));
 
     nautilus_canvas_view_update_click_mode (canvas_view);
     nautilus_canvas_container_set_zoom_level (canvas_container,

src/nautilus-file-operations.c
@@ -59,6 +59,7 @@
 #include "nautilus-file-undo-operations.h"
 #include "nautilus-file-undo-manager.h"
 #include "nautilus-ui-utilities.h"
+#include "nautilus-gtk4-helpers.h"
 
 #ifdef GDK_WINDOWING_X11
 #include <gdk/gdkx.h>
@@ -1362,7 +1363,7 @@ do_run_simple_dialog (gpointer _data)
         gtk_label_set_max_width_chars (GTK_LABEL (label),
                                        MAXIMUM_DISPLAYED_ERROR_MESSAGE_LENGTH);
 
-        gtk_container_add (GTK_CONTAINER (content_area), label);
+        gtk_box_append (GTK_BOX (content_area), label);
 
         gtk_widget_show (label);
     }

src/nautilus-files-view.c
@@ -89,6 +89,7 @@
 #include "nautilus-view-icon-controller.h"
 #include "nautilus-window.h"
 #include "nautilus-tracker-utilities.h"
+#include "nautilus-gtk4-helpers.h"
 
 #ifdef HAVE_LIBPORTAL
 #include <libportal/portal.h>
@@ -9870,7 +9871,7 @@ nautilus_files_view_init (NautilusFilesView *view)
     priv->overlay = gtk_overlay_new ();
     gtk_widget_set_vexpand (priv->overlay, TRUE);
     gtk_widget_set_hexpand (priv->overlay, TRUE);
-    gtk_container_add (GTK_CONTAINER (view), priv->overlay);
+    gtk_grid_attach_next_to (GTK_GRID (view), priv->overlay, NULL, GTK_POS_BOTTOM, 1, 1);
     gtk_widget_show (priv->overlay);
 
     /* NautilusFloatingBar listen to its parent's 'event' signal
@@ -9896,7 +9897,7 @@ nautilus_files_view_init (NautilusFilesView *view)
                               G_CALLBACK (popup_menu_callback),
                               view);
 
-    gtk_container_add (GTK_CONTAINER (priv->overlay), priv->scrolled_window);
+    gtk_overlay_set_child (GTK_OVERLAY (priv->overlay), priv->scrolled_window);
 
     /* Empty states */
     builder = gtk_builder_new_from_resource ("/org/gnome/nautilus/ui/nautilus-no-search-results.ui");

src/nautilus-floating-bar.c
@@ -24,6 +24,7 @@
 #include <string.h>
 
 #include "nautilus-floating-bar.h"
+#include "nautilus-gtk4-helpers.h"
 
 #define HOVER_HIDE_TIMEOUT_INTERVAL 100
 
@@ -435,13 +436,13 @@ nautilus_floating_bar_constructed (GObject *obj)
     w = gtk_label_new (NULL);
     gtk_label_set_ellipsize (GTK_LABEL (w), PANGO_ELLIPSIZE_MIDDLE);
     gtk_label_set_single_line_mode (GTK_LABEL (w), TRUE);
-    gtk_container_add (GTK_CONTAINER (labels_box), w);
+    gtk_box_append (GTK_BOX (labels_box), w);
     self->primary_label_widget = w;
     gtk_widget_show (w);
 
     w = gtk_label_new (NULL);
     gtk_label_set_single_line_mode (GTK_LABEL (w), TRUE);
-    gtk_container_add (GTK_CONTAINER (labels_box), w);
+    gtk_box_append (GTK_BOX (labels_box), w);
     self->details_label_widget = w;
     gtk_widget_show (w);
 
@@ -450,7 +451,7 @@ nautilus_floating_bar_constructed (GObject *obj)
     gtk_style_context_add_class (context, "circular");
     gtk_style_context_add_class (context, "flat");
     gtk_widget_set_valign (w, GTK_ALIGN_CENTER);
-    gtk_container_add (GTK_CONTAINER (self), w);
+    gtk_box_append (GTK_BOX (self), w);
     self->stop_button = w;
     gtk_widget_set_visible (w, FALSE);
     gtk_widget_set_no_show_all (w, TRUE);

src/nautilus-gtk4-helpers.c
@@ -1,0 +1,65 @@
+#include "nautilus-gtk4-helpers.h"
+
+void
+gtk_button_set_child (GtkButton *button,
+                      GtkWidget *child)
+{
+    g_assert (GTK_IS_BUTTON (button));
+
+    gtk_container_add (GTK_CONTAINER (button), child);
+}
+
+void
+gtk_box_append (GtkBox    *box,
+                GtkWidget *child)
+{
+    g_assert (GTK_IS_BOX (box));
+
+    gtk_container_add (GTK_CONTAINER (box), child);
+}
+
+void
+gtk_overlay_set_child (GtkOverlay *overlay,
+                       GtkWidget  *child)
+{
+    g_assert (GTK_IS_OVERLAY (overlay));
+
+    gtk_container_add (GTK_CONTAINER (overlay), child);
+}
+
+void
+gtk_scrolled_window_set_child (GtkScrolledWindow *scrolled_window,
+                               GtkWidget         *child)
+{
+    g_assert (GTK_IS_SCROLLED_WINDOW (scrolled_window));
+
+    gtk_container_add (GTK_CONTAINER (scrolled_window), child);
+}
+
+void
+gtk_list_box_row_set_child (GtkListBoxRow *row,
+                            GtkWidget     *child)
+{
+    g_assert (GTK_IS_LIST_BOX_ROW (row));
+
+    gtk_container_add (GTK_CONTAINER (row), child);
+}
+
+void
+gtk_info_bar_add_child (GtkInfoBar *info_bar,
+                        GtkWidget  *widget)
+{
+    g_assert (GTK_IS_INFO_BAR (info_bar));
+
+    gtk_container_add (GTK_CONTAINER (gtk_info_bar_get_content_area (info_bar)),
+                       widget);
+}
+
+void
+gtk_revealer_set_child (GtkRevealer *revealer,
+                        GtkWidget   *child)
+{
+    g_assert (GTK_IS_REVEALER (revealer));
+
+    gtk_container_add (GTK_CONTAINER (revealer), child);
+}

src/nautilus-gtk4-helpers.h
@@ -1,0 +1,26 @@
+#pragma once
+
+#include <glib.h>
+#include <gio/gio.h>
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+#if GTK_MAJOR_VERSION < 4
+
+void gtk_button_set_child          (GtkButton         *button,
+                                    GtkWidget         *child);
+void gtk_box_append                (GtkBox            *box,
+                                    GtkWidget         *child);
+void gtk_overlay_set_child         (GtkOverlay        *overlay,
+                                    GtkWidget         *child);
+void gtk_scrolled_window_set_child (GtkScrolledWindow *scrolled_window,
+                                    GtkWidget         *child);
+void gtk_list_box_row_set_child    (GtkListBoxRow     *row,
+                                    GtkWidget         *child);
+void gtk_info_bar_add_child        (GtkInfoBar        *info_bar,
+                                    GtkWidget         *widget);
+void gtk_revealer_set_child        (GtkRevealer       *revealer,
+                                    GtkWidget         *child);
+
+#endif
+G_END_DECLS

commit c687f8632c774dc239cd2dccb260f96732c5540a

places-view: Fix regression
Unlike NautilusFilesView, NautilusPlacesView is not a subclass of
GtkGrid, but instead it's a subclass of GtkBox.

Casting it to GTK_GRID fails at runtime, showing the Other Locations
view as empty.
This is a regression caused by e9ae1add9cccee61d10e724b3d7f3526df8ad32a

Fix it by treating it as a GtkBox.

src/nautilus-places-view.c
@@ -26,6 +26,7 @@
 #include "nautilus-toolbar-menu-sections.h"
 #include "nautilus-view.h"
 #include "nautilus-window-slot.h"
+#include "nautilus-gtk4-helpers.h"
 
 typedef struct
 {
@@ -397,7 +398,7 @@ nautilus_places_view_init (NautilusPlacesView *self)
     gtk_widget_set_hexpand (priv->places_view, TRUE);
     gtk_widget_set_vexpand (priv->places_view, TRUE);
     gtk_widget_show (priv->places_view);
-    gtk_grid_attach_next_to (GTK_GRID (self), priv->places_view, NULL, GTK_POS_BOTTOM, 1, 1);
+    gtk_box_append (GTK_BOX (self), priv->places_view);
 
     g_signal_connect_swapped (priv->places_view,
                               "notify::loading",