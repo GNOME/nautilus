9b1c1825f537f270428c121f61bc4ab9dad24cd4
António Fernandes <antoniof@gnome.org>
2022-02-11 19:01:46 +0000

    Revert "files-view: Stop using gtk_widget_get_action_group()"
    This reverts commit e004836ccfa6d7d4b430c465360217547eca32e2.

--- src/nautilus-files-view.c
+++ src/nautilus-files-view.c
@@ -9256,11 +9256,13 @@ on_parent_changed (GObject    *object,
     else
     {
         remove_update_context_menus_timeout_callback (view);
-        /* Only remove the action group if this is still the active view.
-         * Otherwise we might be removing an action group set by a different
-         * view i.e. if slot_active_changed() is called before this one.
+        /* Only remove the action group if it matchs the current view
+         * action group. If not, we can remove an action group set by
+         * a different view i.e. if the slot_active function is called
+         * before this one
          */
-        if (priv->active)
+        if (gtk_widget_get_action_group (GTK_WIDGET (window), "view") ==
+            priv->view_action_group)
         {
             gtk_widget_insert_action_group (GTK_WIDGET (nautilus_files_view_get_window (view)),
                                             "view",
