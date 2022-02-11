3158681a189ce54cc7fe1eef988107be081765c3
Ondrej Holy <oholy@redhat.com>
2022-02-11 14:25:26 +0100

    Revert "general: Stop using GEmblemedIcon"
    This reverts commit 3c977dcf51bdaaa23258a5da8f19a9c1a9ca2d6d.

--- src/nautilus-file.c
+++ src/nautilus-file.c
@@ -4967,7 +4967,6 @@ nautilus_file_get_keywords (NautilusFile *file)
     return sort_keyword_list_and_remove_duplicates (keywords);
 }
 
-#if 0 && EMBLEMS_NEEDS_GTK4_REIMPLEMENTATION
 /**
  * nautilus_file_get_emblem_icons
  *
@@ -5020,7 +5019,6 @@ nautilus_file_get_emblem_icons (NautilusFile *file)
 
     return icons;
 }
-#endif
 
 static void
 prepend_icon_name (const char  *name,
@@ -5034,9 +5032,7 @@ apply_emblems_to_icon (NautilusFile           *file,
                        GIcon                 **icon,
                        NautilusFileIconFlags   flags)
 {
-#if 0 && EMBLEMS_NEEDS_GTK4_REIMPLEMENTATION
     GIcon *emblemed_icon = NULL;
-
     g_autolist (GIcon) emblems = NULL;
 
     emblems = nautilus_file_get_emblem_icons (file);
@@ -5073,7 +5069,6 @@ apply_emblems_to_icon (NautilusFile           *file,
         g_object_unref (*icon);
         *icon = emblemed_icon;
     }
-#endif
 }
 
 GIcon *
