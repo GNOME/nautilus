viaitlinux/viait-files

nautilus-directory: handle change events
for files that were still not known to nautilus because they were in
the midst of being added to Nautilus. This happens when eg. inotify
fires two consecutive NEW + CHANGE events for a new file, and it's
important to handle the CHANGE event as the file may have changed
its properties with respect the ones reported at the time the NEW
event fired.

A case this happens is for mountpoint directories of
removable devices, as seen in the referenced bug below.

We now queue CHANGE events received for files unknown to Nautilus only
when the parent folder is currently adding new files. When the folder
finish adding the new files, we then process the list of queued files.
If there are still files unknown to Nautilus in this list, they will
be ignored as before.

https://bugzilla.gnome.org/show_bug.cgi?id=703179
Issue #1576

src/nautilus-directory-private.h
@@ -100,6 +100,12 @@ struct NautilusDirectoryDetails
 
 	GList *new_files_in_progress; /* list of NewFilesState * */
 
+	/* List of GFile's that received CHANGE events while new files were being added in
+	 * that same folder. We will process this CHANGE events after new_files_in_progress
+	 * list is finished. See Bug 703179 and issue #1576 for a case when this happens.
+	 */
+	GList *new_files_in_progress_changes;
+
 	DirectoryCountState *count_in_progress;
 
 	NautilusFile *deep_count_file;

GNOME/nautilus

src/nautilus-directory-private.h
@@ -100,6 +100,12 @@ struct NautilusDirectoryDetails
 
 	GList *new_files_in_progress; /* list of NewFilesState * */
 
+	/* List of GFile's that received CHANGE events while new files were being added in
+	 * that same folder. We will process this CHANGE events after new_files_in_progress
+	 * list is finished. See Bug 703179 and issue #1576 for a case when this happens.
+	 */
+	GList *files_changed_while_adding;
+
 	DirectoryCountState *count_in_progress;
 
 	NautilusFile *deep_count_file;