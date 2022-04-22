5896e6af41b1fe89c9c5aef12793224d7492b482
António Fernandes <antoniojpfernandes@gmail.com>
2022-04-22 21:58:45 +0000

    Revert "thumbnail: update for gnome-desktop thumbnail API changes"
    This reverts commit fbc9bd6ff4ef32951391d4cdf89c27f58632f492

--- src/nautilus-thumbnails.c
+++ src/nautilus-thumbnails.c
@@ -447,7 +447,6 @@ thumbnail_thread_func (GTask        *task,
     time_t current_orig_mtime = 0;
     time_t current_time;
     GList *node;
-    GError *error = NULL;
 
     thumbnail_factory = get_thumbnail_factory ();
 
@@ -530,9 +529,7 @@ thumbnail_thread_func (GTask        *task,
 
         pixbuf = gnome_desktop_thumbnail_factory_generate_thumbnail (thumbnail_factory,
                                                                      info->image_uri,
-                                                                     info->mime_type,
-                                                                     NULL,
-                                                                     &error);
+                                                                     info->mime_type);
 
         if (pixbuf)
         {
@@ -542,27 +539,17 @@ thumbnail_thread_func (GTask        *task,
             gnome_desktop_thumbnail_factory_save_thumbnail (thumbnail_factory,
                                                             pixbuf,
                                                             info->image_uri,
-                                                            current_orig_mtime,
-                                                            NULL,
-                                                            &error);
-            if (error)
-            {
-                DEBUG ("(Thumbnail Thread) Saving thumbnail failed: %s (%s)\n",
-                       info->image_uri, error->message);
-                g_clear_error (&error);
-            }
+                                                            current_orig_mtime);
             g_object_unref (pixbuf);
         }
         else
         {
-            DEBUG ("(Thumbnail Thread) Thumbnail failed: %s (%s)\n",
-                   info->image_uri, error->message);
-            g_clear_error (&error);
+            DEBUG ("(Thumbnail Thread) Thumbnail failed: %s\n",
+                   info->image_uri);
 
             gnome_desktop_thumbnail_factory_create_failed_thumbnail (thumbnail_factory,
                                                                      info->image_uri,
-                                                                     current_orig_mtime,
-                                                                     NULL, NULL);
+                                                                     current_orig_mtime);
         }
         /* We need to call nautilus_file_changed(), but I don't think that is
          *  thread safe. So add an idle handler and do it from the main loop. */
