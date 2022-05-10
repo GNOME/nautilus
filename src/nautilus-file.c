GNOME/nautilus

commit 99f55748ef69a68df86b9a039998ee576d59b898

file: Also use FS remote attribute in is_remote
Gio can provide us this info, so we can use this together with the checks
nautilus already does.

However, since there are many false-positives cases (like when using a fuse
filesystem that is actually a remote mount, like sshfs or similar), we can't
take this information as granted.

src/nautilus-file.c
@@ -4689,6 +4689,29 @@ nautilus_file_get_filesystem_type (NautilusFile *file)
 }
 
 gboolean
+nautilus_file_get_filesystem_remote (NautilusFile *file)
+{
+    NautilusFile *parent;
+
+    g_assert (NAUTILUS_IS_FILE (file));
+
+    if (nautilus_file_is_directory (file))
+    {
+        return file->details->filesystem_remote;
+    }
+    else
+    {
+        parent = nautilus_file_get_parent (file);
+        if (parent != NULL)
+        {
+            return parent->details->filesystem_remote;
+        }
+    }
+
+    return FALSE;
+}
+
+gboolean
 nautilus_file_should_show_thumbnail (NautilusFile *file)
 {
     const char *mime_type;
@@ -8069,6 +8092,11 @@ nautilus_file_is_remote (NautilusFile *file)
     g_autofree char *filesystem_type = NULL;
 
     g_assert (NAUTILUS_IS_FILE (file));
+
+    if (nautilus_file_get_filesystem_remote (file))
+    {
+        return TRUE;
+    }
 
     filesystem_type = nautilus_file_get_filesystem_type (file);
 

src/nautilus-directory-async.c
@@ -4009,6 +4009,7 @@ got_filesystem_info (FilesystemInfoState *state,
         file->details->filesystem_readonly =
             g_file_info_get_attribute_boolean (info, G_FILE_ATTRIBUTE_FILESYSTEM_READONLY);
         filesystem_type = g_file_info_get_attribute_string (info, G_FILE_ATTRIBUTE_FILESYSTEM_TYPE);
+        file->details->filesystem_remote = g_file_info_get_attribute_boolean (info, G_FILE_ATTRIBUTE_FILESYSTEM_REMOTE);
         if (g_strcmp0 (eel_ref_str_peek (file->details->filesystem_type), filesystem_type) != 0)
         {
             eel_ref_str_unref (file->details->filesystem_type);
@@ -4091,7 +4092,8 @@ filesystem_info_start (NautilusDirectory *directory,
     g_file_query_filesystem_info_async (location,
                                         G_FILE_ATTRIBUTE_FILESYSTEM_READONLY ","
                                         G_FILE_ATTRIBUTE_FILESYSTEM_USE_PREVIEW ","
-                                        G_FILE_ATTRIBUTE_FILESYSTEM_TYPE,
+                                        G_FILE_ATTRIBUTE_FILESYSTEM_TYPE ","
+                                        G_FILE_ATTRIBUTE_FILESYSTEM_REMOTE,
                                         G_PRIORITY_DEFAULT,
                                         state->cancellable,
                                         query_filesystem_info_callback,

src/nautilus-file-private.h
@@ -194,7 +194,8 @@ struct NautilusFileDetails
 	eel_boolean_bit filesystem_readonly           : 1;
 	eel_boolean_bit filesystem_use_preview        : 2; /* GFilesystemPreviewType */
 	eel_boolean_bit filesystem_info_is_up_to_date : 1;
-        eel_ref_str     filesystem_type;
+	eel_boolean_bit filesystem_remote             : 1;
+	eel_ref_str     filesystem_type;
 
 	time_t trash_time; /* 0 is unknown */
 	time_t recency; /* 0 is unknown */

src/nautilus-file.h
@@ -248,6 +248,8 @@ char *                  nautilus_file_get_filesystem_id                 (Nautilu
 
 char *                  nautilus_file_get_filesystem_type               (NautilusFile                   *file);
 
+gboolean                nautilus_file_get_filesystem_remote             (NautilusFile                   *file);
+
 NautilusFile *          nautilus_file_get_trash_original_file           (NautilusFile                   *file);
 
 /* Permissions. */

commit 2ab0fcbdad71b6a9ebf94e0f65b1cb53f6219840

file: Fix remote filesystem check
Files are flagged as remote in file->details->filesystem_remote
through directory-async as folders are browsed.
nautilus_file_get_filesystem_remote() incorrectly returns FALSE for
child directories that have not been processed by directory-async
on a remote filesystem.  This causes the "Count Number of Files in
Folders" options to not work correctly when set to "On This Computer
Only".  Rather than unnecessarily process all of the child folders,
check whether filesystem_info_is_up_to_date==TRUE.  If not,
check whether the parent is remote instead.

This issue was introduced in 99f55748ef69a68df86b9a039998ee576d59b898.
Previously the prefix of the URI was checked to match a hardcoded list.

Fixes: https://gitlab.gnome.org/GNOME/nautilus/-/issues/2035

src/nautilus-file.c
@@ -4737,7 +4737,7 @@ nautilus_file_get_filesystem_remote (NautilusFile *file)
 {
     g_assert (NAUTILUS_IS_FILE (file));
 
-    if (nautilus_file_is_directory (file))
+    if (nautilus_file_is_directory (file) && file->details->filesystem_info_is_up_to_date)
     {
         return file->details->filesystem_remote;
     }