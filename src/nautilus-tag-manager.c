GNOME/nautilus

commit df78c517516cec20892f2a108df47613153ddf5b

tag-manager: Use regular initialization
Somehow we have ended up in a situation where the singleton object
is not initialized on creation but instead when requires the
NautilusApplication code to pass a GCancellable instance in order to
complete the initialization.

The passed cancellable pointer is stored by the application object
in order to cancel it on finalization, but there is no other usage
for this cancellabled from outside of the NautilusTagManager code.

So, we can just cancel it in NautilusTagManager::finalize, such that
there is no need for NautilusApplication to pass or store it.

This way, we can move the initalization from ::set_cancellable()
into ::init instead.

src/nautilus-tag-manager.c
@@ -300,8 +300,6 @@ nautilus_tag_manager_query_starred_files (NautilusTagManager *self,
         return;
     }
 
-    self->cancellable = cancellable;
-
     tracker_sparql_statement_execute_async (self->query_starred_files,
                                             cancellable,
                                             on_get_starred_files_query_callback,
@@ -533,6 +531,8 @@ nautilus_tag_manager_finalize (GObject *object)
                                               self);
     }
 
+    g_cancellable_cancel (self->cancellable);
+    g_clear_object (&self->cancellable);
     g_clear_object (&self->notifier);
     g_clear_object (&self->db);
     g_clear_object (&self->query_file_is_starred);
@@ -647,15 +647,20 @@ setup_database (NautilusTagManager  *self,
     return TRUE;
 }
 
-/* Initialize the tag mananger. */
-void
-nautilus_tag_manager_set_cancellable (NautilusTagManager *self,
-                                      GCancellable       *cancellable)
+static void
+nautilus_tag_manager_init (NautilusTagManager *self)
 {
     g_autoptr (GError) error = NULL;
 
-    self->database_ok = setup_database (self, cancellable, &error);
+    self->starred_file_uris = g_hash_table_new_full (g_str_hash,
+                                                     g_str_equal,
+                                                     (GDestroyNotify) g_free,
+                                                     /* values are keys */
+                                                     NULL);
+    self->home = g_file_new_for_path (g_get_home_dir ());
 
+    self->cancellable = g_cancellable_new ();
+    self->database_ok = setup_database (self, self->cancellable, &error);
     if (error)
     {
         g_warning ("Unable to initialize tag manager: %s", error->message);
@@ -664,7 +669,7 @@ nautilus_tag_manager_set_cancellable (NautilusTagManager *self,
 
     self->notifier = tracker_sparql_connection_create_notifier (self->db);
 
-    nautilus_tag_manager_query_starred_files (self, cancellable);
+    nautilus_tag_manager_query_starred_files (self, self->cancellable);
 
     g_signal_connect (self->notifier,
                       "events",
@@ -672,26 +677,15 @@ nautilus_tag_manager_set_cancellable (NautilusTagManager *self,
                       self);
 }
 
-static void
-nautilus_tag_manager_init (NautilusTagManager *self)
-{
-    self->starred_file_uris = g_hash_table_new_full (g_str_hash,
-                                                     g_str_equal,
-                                                     (GDestroyNotify) g_free,
-                                                     /* values are keys */
-                                                     NULL);
-    self->home = g_file_new_for_path (g_get_home_dir ());
-}
-
 gboolean
-nautilus_tag_manager_can_star_contents (NautilusTagManager *tag_manager,
+nautilus_tag_manager_can_star_contents (NautilusTagManager *self,
                                         GFile              *directory)
 {
     /* We only allow files to be starred inside the home directory for now.
      * This avoids the starred files database growing too big.
      * See https://gitlab.gnome.org/GNOME/nautilus/-/merge_requests/553#note_903108
      */
-    return g_file_has_prefix (directory, tag_manager->home) || g_file_equal (directory, tag_manager->home);
+    return g_file_has_prefix (directory, self->home) || g_file_equal (directory, self->home);
 }
 
 static void

src/nautilus-tag-manager.h
@@ -30,9 +30,6 @@ G_DECLARE_FINAL_TYPE (NautilusTagManager, nautilus_tag_manager, NAUTILUS, TAG_MA
 
 NautilusTagManager* nautilus_tag_manager_get                (void);
 
-void                nautilus_tag_manager_set_cancellable    (NautilusTagManager *tag_manager,
-                                                             GCancellable *cancellable);
-
 GList*              nautilus_tag_manager_get_starred_files (NautilusTagManager *self);
 
 void                nautilus_tag_manager_star_files         (NautilusTagManager  *self,

src/nautilus-application.c
@@ -84,7 +84,6 @@ typedef struct
     NautilusFileUndoManager *undo_manager;
 
     NautilusTagManager *tag_manager;
-    GCancellable *tag_manager_cancellable;
 
     guint previewer_selection_id;
 } NautilusApplicationPrivate;
@@ -586,9 +585,6 @@ nautilus_application_finalize (GObject *object)
     g_clear_object (&priv->undo_manager);
 
     g_clear_object (&priv->tag_manager);
-
-    g_cancellable_cancel (priv->tag_manager_cancellable);
-    g_clear_object (&priv->tag_manager_cancellable);
 
     G_OBJECT_CLASS (nautilus_application_parent_class)->finalize (object);
 }
@@ -1101,11 +1097,7 @@ nautilus_application_init (NautilusApplication *self)
                                                  NULL);
 
     priv->undo_manager = nautilus_file_undo_manager_new ();
-
-    priv->tag_manager_cancellable = g_cancellable_new ();
     priv->tag_manager = nautilus_tag_manager_get ();
-    nautilus_tag_manager_set_cancellable (priv->tag_manager,
-                                          priv->tag_manager_cancellable);
 
     g_application_add_main_option_entries (G_APPLICATION (self), options);
 

commit 7a2b3e21419533c6e87799bf84e2c3d6b6376b11

tag-manager: Skip tracker initialization in tests
Fixes test failures introduced by df78c517516cec20892f2a108df47613153ddf5b

Before that commit, we used to fully initiate the tag manager only from
NautilusApplication, because the tests didn't call set_cancellable().

Now that initialization is completed on construction, add a constructor
which skips the part of initialization that the tests don't need.

src/nautilus-tag-manager.c
@@ -51,6 +51,9 @@ G_DEFINE_TYPE (NautilusTagManager, nautilus_tag_manager, G_TYPE_OBJECT);
 
 static NautilusTagManager *tag_manager = NULL;
 
+/* See nautilus_tag_manager_new_dummy() documentation for details. */
+static gboolean make_dummy_instance = FALSE;
+
 typedef struct
 {
     NautilusTagManager *tag_manager;
@@ -587,6 +590,23 @@ nautilus_tag_manager_new (void)
 }
 
 /**
+ * nautilus_tag_manager_new_dummy:
+ *
+ * Creates a dummy tag manager without database.
+ *
+ * Useful only for tests where the tag manager is needed but not being tested
+ * and we don't want to fail the tests due to irrelevant D-Bus failures.
+ *
+ * Returns: (transfer full): the #NautilusTagManager singleton object.
+ */
+NautilusTagManager *
+nautilus_tag_manager_new_dummy (void)
+{
+    make_dummy_instance = TRUE;
+    return nautilus_tag_manager_new ();
+}
+
+/**
  * nautilus_tag_manager_get:
  *
  * Returns: (transfer none): the #NautilusTagManager singleton object.
@@ -664,6 +684,12 @@ nautilus_tag_manager_init (NautilusTagManager *self)
                                                      /* values are keys */
                                                      NULL);
     self->home = g_file_new_for_path (g_get_home_dir ());
+
+    if (make_dummy_instance)
+    {
+        /* Skip database initiation for nautilus_tag_manager_new_dummy(). */
+        return;
+    }
 
     self->cancellable = g_cancellable_new ();
     self->database_ok = setup_database (self, self->cancellable, &error);

src/nautilus-tag-manager.h
@@ -29,6 +29,7 @@ G_BEGIN_DECLS
 G_DECLARE_FINAL_TYPE (NautilusTagManager, nautilus_tag_manager, NAUTILUS, TAG_MANAGER, GObject);
 
 NautilusTagManager* nautilus_tag_manager_new                (void);
+NautilusTagManager* nautilus_tag_manager_new_dummy          (void);
 NautilusTagManager* nautilus_tag_manager_get                (void);
 
 GList*              nautilus_tag_manager_get_starred_files (NautilusTagManager *self);

test/automated/displayless/test-file-operations-copy-files.c
@@ -1309,7 +1309,7 @@ main (int   argc,
     int ret;
 
     undo_manager = nautilus_file_undo_manager_new ();
-    tag_manager = nautilus_tag_manager_new ();
+    tag_manager = nautilus_tag_manager_new_dummy ();
     g_test_init (&argc, &argv, NULL);
     g_test_set_nonfatal_assertions ();
     nautilus_ensure_extension_points ();

test/automated/displayless/test-file-operations-move-files.c
@@ -1940,7 +1940,7 @@ main (int   argc,
     int ret;
 
     undo_manager = nautilus_file_undo_manager_new ();
-    tag_manager = nautilus_tag_manager_new ();
+    tag_manager = nautilus_tag_manager_new_dummy ();
     g_test_init (&argc, &argv, NULL);
     g_test_set_nonfatal_assertions ();
     nautilus_ensure_extension_points ();

test/automated/displayless/test-file-operations-trash-or-delete.c
@@ -578,7 +578,7 @@ main (int   argc,
     g_test_set_nonfatal_assertions ();
     nautilus_ensure_extension_points ();
     undo_manager = nautilus_file_undo_manager_new ();
-    tag_manager = nautilus_tag_manager_new ();
+    tag_manager = nautilus_tag_manager_new_dummy ();
 
     setup_test_suite ();
 