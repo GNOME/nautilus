GNOME/nautilus

commit 7e2605c681d065e6b0a3d779c30b892932597991

general: refactor extension library
The changes include:

  * adding a single-include header and deprecating
    nautilus-extension-types.h and direct inclusions of individual
    headers;

  * type definition simplifications - this causes some breakages in
    nautilus-file, because NautilusFile used to be typedefed to
    NautilusFileInfo, and that is no longer possible, so the interface
    implementation was moved to static functions and the public
    NautilusFile API provides thin wrappers for them to maintain
    compatibility;

  * documentation cleanups and reorganization;

  * general build rule and code cleanups: mostly g_auto* sprinkled
    around and style changes (sorry)

libnautilus-extension/nautilus-column-provider.h
@@ -30,37 +30,40 @@
 #ifndef NAUTILUS_COLUMN_PROVIDER_H
 #define NAUTILUS_COLUMN_PROVIDER_H
 
+#if !defined (NAUTILUS_EXTENSION_H) && !defined (NAUTILUS_COMPILATION)
+#warning "Only <nautilus-extension.h> should be included directly."
+#endif
+
 #include <glib-object.h>
-#include "nautilus-extension-types.h"
-#include "nautilus-column.h"
 
 G_BEGIN_DECLS
 
-#define NAUTILUS_TYPE_COLUMN_PROVIDER           (nautilus_column_provider_get_type ())
-#define NAUTILUS_COLUMN_PROVIDER(obj)           (G_TYPE_CHECK_INSTANCE_CAST ((obj), NAUTILUS_TYPE_COLUMN_PROVIDER, NautilusColumnProvider))
-#define NAUTILUS_IS_COLUMN_PROVIDER(obj)        (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NAUTILUS_TYPE_COLUMN_PROVIDER))
-#define NAUTILUS_COLUMN_PROVIDER_GET_IFACE(obj) (G_TYPE_INSTANCE_GET_INTERFACE ((obj), NAUTILUS_TYPE_COLUMN_PROVIDER, NautilusColumnProviderIface))
+#define NAUTILUS_TYPE_COLUMN_PROVIDER (nautilus_column_provider_get_type ())
 
-typedef struct _NautilusColumnProvider       NautilusColumnProvider;
-typedef struct _NautilusColumnProviderIface  NautilusColumnProviderIface;
+G_DECLARE_INTERFACE (NautilusColumnProvider, nautilus_column_provider,
+                     NAUTILUS, COLUMN_PROVIDER,
+                     GObject)
+
+/* For compatibility reasons, remove this once you start introducing breaking changes. */
+typedef NautilusColumnProviderInterface NautilusColumnProviderIface;
 
 /**
- * NautilusColumnProviderIface:
+ * NautilusColumnProviderInterface:
  * @g_iface: The parent interface.
  * @get_columns: Returns a #GList of #NautilusColumn.
  *   See nautilus_column_provider_get_columns() for details.
  *
  * Interface for extensions to provide additional list view columns.
  */
-struct _NautilusColumnProviderIface {
-	GTypeInterface g_iface;
+struct _NautilusColumnProviderInterface
+{
+    GTypeInterface g_iface;
 
-	GList *(*get_columns) (NautilusColumnProvider *provider);
+    GList *(*get_columns) (NautilusColumnProvider *provider);
 };
 
 /* Interface Functions */
-GType                   nautilus_column_provider_get_type       (void);
-GList                  *nautilus_column_provider_get_columns    (NautilusColumnProvider *provider);
+GList *nautilus_column_provider_get_columns (NautilusColumnProvider *provider);
 
 G_END_DECLS
 

libnautilus-extension/nautilus-file-info.h
@@ -25,28 +25,29 @@
 #ifndef NAUTILUS_FILE_INFO_H
 #define NAUTILUS_FILE_INFO_H
 
+#if !defined (NAUTILUS_EXTENSION_H) && !defined (NAUTILUS_COMPILATION)
+#warning "Only <nautilus-extension.h> should be included directly."
+#endif
+
 #include <glib-object.h>
 #include <gio/gio.h>
 
 G_BEGIN_DECLS
 
-#define NAUTILUS_TYPE_FILE_INFO           (nautilus_file_info_get_type ())
-#define NAUTILUS_FILE_INFO(obj)           (G_TYPE_CHECK_INSTANCE_CAST ((obj), NAUTILUS_TYPE_FILE_INFO, NautilusFileInfo))
-#define NAUTILUS_IS_FILE_INFO(obj)        (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NAUTILUS_TYPE_FILE_INFO))
-#define NAUTILUS_FILE_INFO_GET_IFACE(obj) (G_TYPE_INSTANCE_GET_INTERFACE ((obj), NAUTILUS_TYPE_FILE_INFO, NautilusFileInfoIface))
+#define NAUTILUS_TYPE_FILE_INFO (nautilus_file_info_get_type ())
 
-#ifndef NAUTILUS_FILE_DEFINED
-#define NAUTILUS_FILE_DEFINED
-/* Using NautilusFile for the vtable to make implementing this in 
- * NautilusFile easier */
-typedef struct NautilusFile          NautilusFile;
-#endif
+G_DECLARE_INTERFACE (NautilusFileInfo, nautilus_file_info, NAUTILUS, FILE_INFO, GObject)
 
-typedef NautilusFile                  NautilusFileInfo;
-typedef struct _NautilusFileInfoIface NautilusFileInfoIface;
+/* For compatibility reasons, remove this once you start introducing breaking changes.
+ *
+ * I’ve only noticed nautilus-python depending on this, but the context is that
+ * NautilusFileInfo used to be an alias for NautilusFile to make it easier to
+ * implement the interface,
+ */
+typedef struct NautilusFile NautilusFile;
 
 /**
- * NautilusFileInfoIface:
+ * NautilusFileInfoInterface:
  * @g_iface: The parent interface.
  * @is_gone: Returns whether the file info is gone.
  *   See nautilus_file_info_is_gone() for details.
@@ -89,83 +90,81 @@ typedef struct _NautilusFileInfoIface NautilusFileInfoIface;
  *
  * Interface for extensions to provide additional menu items.
  */
-struct _NautilusFileInfoIface 
+struct _NautilusFileInfoInterface
 {
-	GTypeInterface g_iface;
+    GTypeInterface g_iface;
 
-	gboolean          (*is_gone)              (NautilusFileInfo *file);
-	
-	char *            (*get_name)             (NautilusFileInfo *file);
-	char *            (*get_uri)              (NautilusFileInfo *file);
-	char *            (*get_parent_uri)       (NautilusFileInfo *file);
-	char *            (*get_uri_scheme)       (NautilusFileInfo *file);
-	
-	char *            (*get_mime_type)        (NautilusFileInfo *file);
-	gboolean          (*is_mime_type)         (NautilusFileInfo *file,
-						   const char       *mime_Type);
-	gboolean          (*is_directory)         (NautilusFileInfo *file);
-	
-	void              (*add_emblem)           (NautilusFileInfo *file,
-						   const char       *emblem_name);
-	char *            (*get_string_attribute) (NautilusFileInfo *file,
-						   const char       *attribute_name);
-	void              (*add_string_attribute) (NautilusFileInfo *file,
-						   const char       *attribute_name,
-						   const char       *value);
-	void              (*invalidate_extension_info) (NautilusFileInfo *file);
-	
-	char *            (*get_activation_uri)   (NautilusFileInfo *file);
+    gboolean          (*is_gone)                   (NautilusFileInfo *file_info);
 
-	GFileType         (*get_file_type)        (NautilusFileInfo *file);
-	GFile *           (*get_location)         (NautilusFileInfo *file);
-	GFile *           (*get_parent_location)  (NautilusFileInfo *file);
-	NautilusFileInfo* (*get_parent_info)      (NautilusFileInfo *file);
-	GMount *          (*get_mount)            (NautilusFileInfo *file);
-	gboolean          (*can_write)            (NautilusFileInfo *file);
-  
+    char             *(*get_name)                  (NautilusFileInfo *file_info);
+    char             *(*get_uri)                   (NautilusFileInfo *file_info);
+    char             *(*get_parent_uri)            (NautilusFileInfo *file_info);
+    char             *(*get_uri_scheme)            (NautilusFileInfo *file_info);
+
+    char             *(*get_mime_type)             (NautilusFileInfo *file_info);
+    gboolean          (*is_mime_type)              (NautilusFileInfo *file_info,
+                                                    const char       *mime_type);
+    gboolean          (*is_directory)              (NautilusFileInfo *file_info);
+
+    void              (*add_emblem)                (NautilusFileInfo *file_info,
+                                                    const char       *emblem_name);
+    char             *(*get_string_attribute)      (NautilusFileInfo *file_info,
+                                                    const char       *attribute_name);
+    void              (*add_string_attribute)      (NautilusFileInfo *file_info,
+                                                    const char       *attribute_name,
+                                                    const char       *value);
+    void              (*invalidate_extension_info) (NautilusFileInfo *file_info);
+
+    char             *(*get_activation_uri)        (NautilusFileInfo *file_info);
+
+    GFileType         (*get_file_type)             (NautilusFileInfo *file_info);
+    GFile            *(*get_location)              (NautilusFileInfo *file_info);
+    GFile            *(*get_parent_location)       (NautilusFileInfo *file_info);
+    NautilusFileInfo *(*get_parent_info)           (NautilusFileInfo *file_info);
+    GMount           *(*get_mount)                 (NautilusFileInfo *file_info);
+    gboolean          (*can_write)                 (NautilusFileInfo *file_info);
 };
 
 GList            *nautilus_file_info_list_copy            (GList            *files);
 void              nautilus_file_info_list_free            (GList            *files);
-GType             nautilus_file_info_get_type             (void);
 
 /* Return true if the file has been deleted */
-gboolean          nautilus_file_info_is_gone              (NautilusFileInfo *file);
+gboolean          nautilus_file_info_is_gone              (NautilusFileInfo *file_info);
 
 /* Name and Location */
-GFileType         nautilus_file_info_get_file_type        (NautilusFileInfo *file);
-GFile *           nautilus_file_info_get_location         (NautilusFileInfo *file);
-char *            nautilus_file_info_get_name             (NautilusFileInfo *file);
-char *            nautilus_file_info_get_uri              (NautilusFileInfo *file);
-char *            nautilus_file_info_get_activation_uri   (NautilusFileInfo *file);
-GFile *           nautilus_file_info_get_parent_location  (NautilusFileInfo *file);
-char *            nautilus_file_info_get_parent_uri       (NautilusFileInfo *file);
-GMount *          nautilus_file_info_get_mount            (NautilusFileInfo *file);
-char *            nautilus_file_info_get_uri_scheme       (NautilusFileInfo *file);
+GFileType         nautilus_file_info_get_file_type        (NautilusFileInfo *file_info);
+GFile            *nautilus_file_info_get_location         (NautilusFileInfo *file_info);
+char             *nautilus_file_info_get_name             (NautilusFileInfo *file_info);
+char             *nautilus_file_info_get_uri              (NautilusFileInfo *file_info);
+char             *nautilus_file_info_get_activation_uri   (NautilusFileInfo *file_info);
+GFile            *nautilus_file_info_get_parent_location  (NautilusFileInfo *file_info);
+char             *nautilus_file_info_get_parent_uri       (NautilusFileInfo *file_info);
+GMount           *nautilus_file_info_get_mount            (NautilusFileInfo *file_info);
+char             *nautilus_file_info_get_uri_scheme       (NautilusFileInfo *file_info);
 /* It's not safe to call this recursively multiple times, as it works
  * only for files already cached by Nautilus.
  */
-NautilusFileInfo* nautilus_file_info_get_parent_info      (NautilusFileInfo *file);
+NautilusFileInfo *nautilus_file_info_get_parent_info      (NautilusFileInfo *file_info);
 
 /* File Type */
-char *            nautilus_file_info_get_mime_type        (NautilusFileInfo *file);
-gboolean          nautilus_file_info_is_mime_type         (NautilusFileInfo *file,
-							   const char       *mime_type);
-gboolean          nautilus_file_info_is_directory         (NautilusFileInfo *file);
-gboolean          nautilus_file_info_can_write            (NautilusFileInfo *file);
+char *            nautilus_file_info_get_mime_type        (NautilusFileInfo *file_info);
+gboolean          nautilus_file_info_is_mime_type         (NautilusFileInfo *file_info,
+                                                           const char       *mime_type);
+gboolean          nautilus_file_info_is_directory         (NautilusFileInfo *file_info);
+gboolean          nautilus_file_info_can_write            (NautilusFileInfo *file_info);
 
 
 /* Modifying the NautilusFileInfo */
-void              nautilus_file_info_add_emblem           (NautilusFileInfo *file,
-							   const char       *emblem_name);
-char *            nautilus_file_info_get_string_attribute (NautilusFileInfo *file,
-							   const char       *attribute_name);
-void              nautilus_file_info_add_string_attribute (NautilusFileInfo *file,
-							   const char       *attribute_name,
-							   const char       *value);
+void              nautilus_file_info_add_emblem           (NautilusFileInfo *file_info,
+                                                           const char       *emblem_name);
+char             *nautilus_file_info_get_string_attribute (NautilusFileInfo *file_info,
+                                                           const char       *attribute_name);
+void              nautilus_file_info_add_string_attribute (NautilusFileInfo *file_info,
+                                                           const char       *attribute_name,
+                                                           const char       *value);
 
 /* Invalidating file info */
-void              nautilus_file_info_invalidate_extension_info (NautilusFileInfo *file);
+void              nautilus_file_info_invalidate_extension_info (NautilusFileInfo *file_info);
 
 NautilusFileInfo *nautilus_file_info_lookup                (GFile *location);
 NautilusFileInfo *nautilus_file_info_create                (GFile *location);

libnautilus-extension/nautilus-info-provider.h
@@ -29,26 +29,63 @@
 #ifndef NAUTILUS_INFO_PROVIDER_H
 #define NAUTILUS_INFO_PROVIDER_H
 
+#if !defined (NAUTILUS_EXTENSION_H) && !defined (NAUTILUS_COMPILATION)
+#warning "Only <nautilus-extension.h> should be included directly."
+#endif
+
 #include <glib-object.h>
-#include "nautilus-extension-types.h"
 #include "nautilus-file-info.h"
 
 G_BEGIN_DECLS
 
-#define NAUTILUS_TYPE_INFO_PROVIDER           (nautilus_info_provider_get_type ())
-#define NAUTILUS_INFO_PROVIDER(obj)           (G_TYPE_CHECK_INSTANCE_CAST ((obj), NAUTILUS_TYPE_INFO_PROVIDER, NautilusInfoProvider))
-#define NAUTILUS_IS_INFO_PROVIDER(obj)        (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NAUTILUS_TYPE_INFO_PROVIDER))
-#define NAUTILUS_INFO_PROVIDER_GET_IFACE(obj) (G_TYPE_INSTANCE_GET_INTERFACE ((obj), NAUTILUS_TYPE_INFO_PROVIDER, NautilusInfoProviderIface))
+#define NAUTILUS_TYPE_INFO_PROVIDER (nautilus_info_provider_get_type ())
 
-typedef struct _NautilusInfoProvider       NautilusInfoProvider;
-typedef struct _NautilusInfoProviderIface  NautilusInfoProviderIface;
+G_DECLARE_INTERFACE (NautilusInfoProvider, nautilus_info_provider,
+                     NAUTILUS, INFO_PROVIDER,
+                     GObject)
 
-typedef void (*NautilusInfoProviderUpdateComplete) (NautilusInfoProvider    *provider,
-						    NautilusOperationHandle *handle,
-						    NautilusOperationResult  result,
-						    gpointer                 user_data);
+/* For compatibility reasons, remove this once you start introducing breaking changes. */
+typedef NautilusInfoProviderInterface NautilusInfoProviderIface;
+
 /**
- * NautilusInfoProviderIface:
+ * NautilusOperationHandle:
+ *
+ * Handle for asynchronous interfaces. These are opaque handles that must
+ * be unique within an extension object. These are returned by operations
+ * that return #NAUTILUS_OPERATION_IN_PROGRESS.
+ */
+typedef struct _NautilusOperationHandle NautilusOperationHandle;
+
+/**
+ * NautilusOperationResult:
+ * @NAUTILUS_OPERATION_COMPLETE: the operation succeeded, and the extension
+ *  is done with the request.
+ * @NAUTILUS_OPERATION_FAILED: the operation failed.
+ * @NAUTILUS_OPERATION_IN_PROGRESS: the extension has begin an async operation.
+ *  When this value is returned, the extension must set the handle parameter
+ *  and call the callback closure when the operation is complete.
+ *
+ * Return values for asynchronous operations performed by the extension.
+ * See nautilus_info_provider_update_file_info().
+ */
+typedef enum
+{
+    /* Returned if the call succeeded, and the extension is done 
+     * with the request */
+    NAUTILUS_OPERATION_COMPLETE,
+
+    /* Returned if the call failed */
+    NAUTILUS_OPERATION_FAILED,
+
+    /* Returned if the extension has begun an async operation. 
+     * If this is returned, the extension must set the handle 
+     * parameter and call the callback closure when the 
+     * operation is complete. */
+    NAUTILUS_OPERATION_IN_PROGRESS
+} NautilusOperationResult;
+
+/**
+ * NautilusInfoProviderInterface:
  * @g_iface: The parent interface.
  * @update_file_info: Returns a #NautilusOperationResult.
  *   See nautilus_info_provider_update_file_info() for details.
@@ -57,33 +94,33 @@ typedef void (*NautilusInfoProviderUpdateComplete) (NautilusInfoProvider    *pro
  *
  * Interface for extensions to provide additional information about files.
  */
-struct _NautilusInfoProviderIface {
-	GTypeInterface g_iface;
+struct _NautilusInfoProviderInterface
+{
+    GTypeInterface g_iface;
 
-	NautilusOperationResult (*update_file_info) (NautilusInfoProvider     *provider,
-						     NautilusFileInfo         *file,
-						     GClosure                 *update_complete,
-						     NautilusOperationHandle **handle);
-	void                    (*cancel_update)    (NautilusInfoProvider     *provider,
-						     NautilusOperationHandle  *handle);
+    NautilusOperationResult (*update_file_info) (NautilusInfoProvider     *provider,
+                                                 NautilusFileInfo         *file,
+                                                 GClosure                 *update_complete,
+                                                 NautilusOperationHandle **handle);
+    void                    (*cancel_update)    (NautilusInfoProvider     *provider,
+                                                 NautilusOperationHandle  *handle);
 };
 
 /* Interface Functions */
-GType                   nautilus_info_provider_get_type               (void);
 NautilusOperationResult nautilus_info_provider_update_file_info       (NautilusInfoProvider     *provider,
-								       NautilusFileInfo         *file,
-								       GClosure                 *update_complete,
-								       NautilusOperationHandle **handle);
+                                                                       NautilusFileInfo         *file,
+                                                                       GClosure                 *update_complete,
+                                                                       NautilusOperationHandle **handle);
 void                    nautilus_info_provider_cancel_update          (NautilusInfoProvider     *provider,
-								       NautilusOperationHandle  *handle);
+                                                                       NautilusOperationHandle  *handle);
 
 
 
 /* Helper functions for implementations */
 void                    nautilus_info_provider_update_complete_invoke (GClosure                 *update_complete,
-								       NautilusInfoProvider     *provider,
-								       NautilusOperationHandle  *handle,
-								       NautilusOperationResult   result);
+                                                                       NautilusInfoProvider     *provider,
+                                                                       NautilusOperationHandle  *handle,
+                                                                       NautilusOperationResult   result);
 
 G_END_DECLS
 

libnautilus-extension/nautilus-location-widget-provider.h
@@ -31,41 +31,46 @@
 #ifndef NAUTILUS_LOCATION_WIDGET_PROVIDER_H
 #define NAUTILUS_LOCATION_WIDGET_PROVIDER_H
 
+#if !defined (NAUTILUS_EXTENSION_H) && !defined (NAUTILUS_COMPILATION)
+#warning "Only <nautilus-extension.h> should be included directly."
+#endif
+
 #include <glib-object.h>
 #include <gtk/gtk.h>
-#include "nautilus-extension-types.h"
 
 G_BEGIN_DECLS
 
-#define NAUTILUS_TYPE_LOCATION_WIDGET_PROVIDER           (nautilus_location_widget_provider_get_type ())
-#define NAUTILUS_LOCATION_WIDGET_PROVIDER(obj)           (G_TYPE_CHECK_INSTANCE_CAST ((obj), NAUTILUS_TYPE_LOCATION_WIDGET_PROVIDER, NautilusLocationWidgetProvider))
-#define NAUTILUS_IS_LOCATION_WIDGET_PROVIDER(obj)        (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NAUTILUS_TYPE_LOCATION_WIDGET_PROVIDER))
-#define NAUTILUS_LOCATION_WIDGET_PROVIDER_GET_IFACE(obj) (G_TYPE_INSTANCE_GET_INTERFACE ((obj), NAUTILUS_TYPE_LOCATION_WIDGET_PROVIDER, NautilusLocationWidgetProviderIface))
+#define NAUTILUS_TYPE_LOCATION_WIDGET_PROVIDER (nautilus_location_widget_provider_get_type ())
 
-typedef struct _NautilusLocationWidgetProvider       NautilusLocationWidgetProvider;
-typedef struct _NautilusLocationWidgetProviderIface  NautilusLocationWidgetProviderIface;
+G_DECLARE_INTERFACE (NautilusLocationWidgetProvider, nautilus_location_widget_provider,
+                     NAUTILUS, LOCATION_WIDGET_PROVIDER,
+                     GObject)
+
+/* For compatibility reasons, remove this once you start introducing breaking changes. */
+typedef NautilusLocationWidgetProviderInterface NautilusLocationWidgetProviderIface;
 
 /**
- * NautilusLocationWidgetProviderIface:
+ * NautilusLocationWidgetProviderInterface:
  * @g_iface: The parent interface.
  * @get_widget: Returns a #GtkWidget.
  *   See nautilus_location_widget_provider_get_widget() for details.
  *
  * Interface for extensions to provide additional location widgets.
  */
-struct _NautilusLocationWidgetProviderIface {
-	GTypeInterface g_iface;
+struct _NautilusLocationWidgetProviderInterface
+{
+    GTypeInterface g_iface;
 
-	GtkWidget * (*get_widget) (NautilusLocationWidgetProvider *provider,
-				   const char                     *uri,
-				   GtkWidget                      *window);
+    GtkWidget *(*get_widget) (NautilusLocationWidgetProvider *provider,
+                              const char                     *uri,
+                              GtkWidget                      *window);
 };
 
 /* Interface Functions */
-GType       nautilus_location_widget_provider_get_type      (void);
-GtkWidget * nautilus_location_widget_provider_get_widget    (NautilusLocationWidgetProvider     *provider,
-							     const char                         *uri,
-							     GtkWidget                          *window);
+GtkWidget *nautilus_location_widget_provider_get_widget (NautilusLocationWidgetProvider *provider,
+                                                         const char                     *uri,
+                                                         GtkWidget                      *window);
+
 G_END_DECLS
 
 #endif

commit a9bc154e19ea79c29a982585094498a44a8c2bd3

libnautilus-extension: Remove compatibility typedefs
Now that we have broken the API, remove compatibility typedefs
introduced in 7e2605c681d065e6b0a3d779c30b892932597991

Also update audio-video-properies to use the new symbols.

extensions/audio-video-properties/totem-properties-main.c
@@ -34,7 +34,7 @@
 
 static GType tpp_type = 0;
 static void property_page_provider_iface_init
-	(NautilusPropertyPageProviderIface *iface);
+	(NautilusPropertyPageProviderInterface *iface);
 static GList *totem_properties_get_pages
 	(NautilusPropertyPageProvider *provider, GList *files);
 
@@ -68,7 +68,7 @@ totem_properties_plugin_register_type (GTypeModule *module)
 }
 
 static void
-property_page_provider_iface_init (NautilusPropertyPageProviderIface *iface)
+property_page_provider_iface_init (NautilusPropertyPageProviderInterface *iface)
 {
 	iface->get_pages = totem_properties_get_pages;
 }

libnautilus-extension/nautilus-column-provider.h
@@ -46,12 +46,6 @@ G_DECLARE_INTERFACE (NautilusColumnProvider, nautilus_column_provider,
                      NAUTILUS, COLUMN_PROVIDER,
                      GObject)
 
-/* For compatibility reasons, remove this once you start introducing breaking changes. */
-/**
- * NautilusColumnProviderIface: (skip)
- */
-typedef NautilusColumnProviderInterface NautilusColumnProviderIface;
-
 /**
  * SECTION:nautilus-column-provider
  * @title: NautilusColumnProvider

libnautilus-extension/nautilus-file-info.h
@@ -37,17 +37,6 @@ G_BEGIN_DECLS
 
 G_DECLARE_INTERFACE (NautilusFileInfo, nautilus_file_info, NAUTILUS, FILE_INFO, GObject)
 
-/* For compatibility reasons; remove this once you start introducing breaking changes.
- *
- * I’ve only noticed nautilus-python depending on this, but the context is that
- * NautilusFileInfo used to be an alias for NautilusFile to make it easier to
- * implement the interface,
- */
-/**
- * NautilusFile: (skip)
- */
-typedef struct NautilusFile NautilusFile;
-
 /**
  * SECTION:nautilus-file-info
  * @title: NautilusFileInfo

libnautilus-extension/nautilus-info-provider.h
@@ -45,12 +45,6 @@ G_DECLARE_INTERFACE (NautilusInfoProvider, nautilus_info_provider,
                      NAUTILUS, INFO_PROVIDER,
                      GObject)
 
-/* For compatibility reasons, remove this once you start introducing breaking changes. */
-/**
- * NautilusInfoProviderIface: (skip)
- */
-typedef NautilusInfoProviderInterface NautilusInfoProviderIface;
-
 /**
  * SECTION:nautilus-info-provider
  * @title: NautilusInfoProvider

libnautilus-extension/nautilus-location-widget-provider.h
@@ -47,12 +47,6 @@ G_DECLARE_INTERFACE (NautilusLocationWidgetProvider, nautilus_location_widget_pr
                      NAUTILUS, LOCATION_WIDGET_PROVIDER,
                      GObject)
 
-/* For compatibility reasons, remove this once you start introducing breaking changes. */
-/**
- * NautilusLocationWidgetProviderIface: (skip)
- */
-typedef NautilusLocationWidgetProviderInterface NautilusLocationWidgetProviderIface;
-
 /**
  * SECTION:nautilus-location-widget-provider
  * @title: NautilusLocationWidgetProvider

libnautilus-extension/nautilus-menu-provider.h
@@ -47,12 +47,6 @@ G_DECLARE_INTERFACE (NautilusMenuProvider, nautilus_menu_provider,
                      NAUTILUS, MENU_PROVIDER,
                      GObject)
 
-/* For compatibility reasons, remove this once you start introducing breaking changes. */
-/**
- * NautilusMenuProviderIface: (skip)
- */
-typedef NautilusMenuProviderInterface NautilusMenuProviderIface;
-
 /**
  * SECTION:nautilus-menu-provider
  * @title: NautilusMenuProvider

libnautilus-extension/nautilus-property-page-provider.h
@@ -48,12 +48,6 @@ G_DECLARE_INTERFACE (NautilusPropertyPageProvider, nautilus_property_page_provid
                      NAUTILUS, PROPERTY_PAGE_PROVIDER,
                      GObject)
 
-/* For compatibility reasons, remove this once you start introducing breaking changes. */
-/**
- * NautilusPropertyPageProviderIface: (skip)
- */
-typedef NautilusPropertyPageProviderInterface NautilusPropertyPageProviderIface;
-
 /**
  * SECTION:nautilus-property-page-provider
  * @title: NautilusPropertyPageProvider