d47403bcede0f331c04c7fb28cec2583fed16724
Jan Tojnar <jtojnar@gmail.com>
2022-08-10 21:15:44 +0200

    docs: Switch to gi-docgen

---
+++ docs/reference/urlmap.js
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2021 GNOME Foundation
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+// A map between namespaces and base URLs for their online documentation
+baseURLs = [
+  ["GLib", "https://docs.gtk.org/glib/"],
+  ["Gio", "https://docs.gtk.org/gio/"],
+  ["GObject", "https://docs.gtk.org/gobject/"],
+];
