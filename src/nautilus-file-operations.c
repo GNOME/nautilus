b2cdc179853738c68488600c2c9e412f1b4e6c56
António Fernandes <antoniof@gnome.org>
2022-12-18 19:53:53 +0000

    file-operations: Always unlock worker thread on GtkDialog::respose
    The worker thread is blocked while waiting for user response on the
    main thread, through a GtkDialog. It gets unblocked upon response.
    
    Except... we have an early return on our ::response hanlder, which
    causes the worker thread never to be unblocked. This comes from
    38a04943f9029ed4e8c7481b05502454a82a2811
    The apparent intention was to presenve the behaviour from when we were
    would calling gtk_dialog_run() in a loop. Maybe it worked under GTK3,
    but it definitely doesn't under GTK4.
    
    Fortunately, this should be dead code, because data->ignore_close_box
    is always FALSE. Yet, under irregular situations, it may be reached,
    as the following report suggests. So, it's best to remove this code
    which is both wrong and unused.
    
    Closes: https://gitlab.gnome.org/GNOME/nautilus/-/issues/2604

--- src/nautilus-file-operations.c
+++ src/nautilus-file-operations.c
@@ -1326,11 +1326,6 @@ simple_dialog_cb (GtkDialog *dialog,
 {
     RunSimpleDialogData *data = user_data;
 
-    if ((response_id == GTK_RESPONSE_NONE || response_id == GTK_RESPONSE_DELETE_EVENT) && data->ignore_close_box)
-    {
-        return;
-    }
-
     gtk_window_destroy (GTK_WINDOW (dialog));
 
     data->result = response_id;
