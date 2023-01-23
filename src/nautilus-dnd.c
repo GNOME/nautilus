GNOME/nautilus

commit 3d17ed252960cbc9848aea0b398a4a74ef05623c

dnd: Don't allow dropping an archive on itself
In `nautilus_dnd_get_preferred_action()` we check if the dropped file
and target file are equal, but only after the check if we are dropping
onto an archive.  The `g_file_equal()` check if wrapped in a
`dropped != NULL` check which turns out to be unnecessary because
we return in the very beginning when `dropped == NULL`

Move around the drop target action checks to simplify and
prevent dropping anything on itself.

Fixes: https://gitlab.gnome.org/GNOME/nautilus/-/issues/2737

src/nautilus-dnd.c
@@ -167,9 +167,19 @@ nautilus_dnd_get_preferred_action (NautilusFile *target_file,
                                    GFile        *dropped)
 {
     g_autoptr (NautilusDirectory) directory = NULL;
+    g_autoptr (GFile) target_location = NULL;
+    g_autoptr (NautilusFile) dropped_file = NULL;
+    gboolean same_fs;
+    gboolean source_deletable;
 
     g_return_val_if_fail (NAUTILUS_IS_FILE (target_file), 0);
     g_return_val_if_fail (dropped == NULL || G_IS_FILE (dropped), 0);
+
+    target_location = nautilus_file_get_location (target_file);
+    if (g_file_equal (target_location, dropped))
+    {
+        return 0;
+    }
 
     /* First check target imperatives */
     directory = nautilus_directory_get_for_file (target_file);
@@ -198,37 +208,22 @@ nautilus_dnd_get_preferred_action (NautilusFile *target_file,
          * accepts drops */
         return 0;
     }
-
-    if (nautilus_file_is_in_trash (target_file))
+    else if (nautilus_file_is_in_trash (target_file))
     {
         return GDK_ACTION_MOVE;
     }
 
-    if (dropped != NULL)
+    if (g_file_has_uri_scheme (dropped, "trash"))
     {
-        g_autoptr (GFile) target_location = NULL;
-        g_autoptr (NautilusFile) dropped_file = NULL;
-        gboolean same_fs;
-        gboolean source_deletable;
+        return GDK_ACTION_MOVE;
+    }
 
-        if (g_file_has_uri_scheme (dropped, "trash"))
-        {
-            return GDK_ACTION_MOVE;
-        }
-
-        target_location = nautilus_file_get_location (target_file);
-        if (g_file_equal (target_location, dropped))
-        {
-            return 0;
-        }
-
-        dropped_file = nautilus_file_get (dropped);
-        same_fs = check_same_fs (target_file, dropped_file);
-        source_deletable = source_is_deletable (dropped);
-        if (same_fs && source_deletable)
-        {
-            return GDK_ACTION_MOVE;
-        }
+    dropped_file = nautilus_file_get (dropped);
+    same_fs = check_same_fs (target_file, dropped_file);
+    source_deletable = source_is_deletable (dropped);
+    if (same_fs && source_deletable)
+    {
+        return GDK_ACTION_MOVE;
     }
 
     return GDK_ACTION_COPY;

commit a72031b2e73dd8512b3f295e8575d633dd69dcfd

dnd: Fix dnd_get_preferred_action for non-local drops
With non-local drops, when the value is initially NULL, we get
warnings because of some of the checks done on a NULL value.  This was
a regression from 3d17ed252960cbc9848aea0b398a4a74ef05623c.  Return
action copy before any check dependent on a dropped value.

src/nautilus-dnd.c
@@ -174,10 +174,11 @@ nautilus_dnd_get_preferred_action (NautilusFile *target_file,
     gboolean source_deletable;
 
     g_return_val_if_fail (NAUTILUS_IS_FILE (target_file), 0);
+    /* With non-local drops, it's normal for dropped to initially be NULL */
     g_return_val_if_fail (dropped == NULL || G_IS_FILE (dropped), 0);
 
     target_location = nautilus_file_get_location (target_file);
-    if (g_file_equal (target_location, dropped))
+    if (dropped != NULL && g_file_equal (target_location, dropped))
     {
         return 0;
     }
@@ -212,6 +213,11 @@ nautilus_dnd_get_preferred_action (NautilusFile *target_file,
     else if (nautilus_file_is_in_trash (target_file))
     {
         return GDK_ACTION_MOVE;
+    }
+
+    if (dropped == NULL)
+    {
+        return GDK_ACTION_COPY;
     }
 
     if (g_file_has_uri_scheme (dropped, "trash"))