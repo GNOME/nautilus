GNOME/nautilus

commit e2969a9c29d0d171038fcb35d8baa5a7df372191

file-operation: Return early when basename is NULL
Calling g_utf8_validate() on a NULL string crashes Nautilus.  This
can occur if performing an operation on an invalid file. If
basename is NULL, there's nothing we would do anyways, so just
return early and prevent the crash.

There's a similar issue with get_unique_target_file(), but
if a bad uri was passed to this function it should fail much
earlier, so lets just add an assertion.

Fixes: https://gitlab.gnome.org/GNOME/nautilus/-/issues/2376

src/nautilus-file-operations.c
@@ -1022,6 +1022,11 @@ get_basename (GFile *file)
     if (name == NULL)
     {
         basename = g_file_get_basename (file);
+        if (basename == NULL)
+        {
+            return g_strdup (_("unknown"));
+        }
+
         if (g_utf8_validate (basename, -1, NULL))
         {
             name = basename;
@@ -4384,6 +4389,7 @@ get_unique_target_file (GFile      *src,
     if (dest == NULL)
     {
         basename = g_file_get_basename (src);
+        g_assert (basename == NULL);
 
         if (g_utf8_validate (basename, -1, NULL))
         {

commit fcf8f9de8336549f6395c440a9be983849254b31

file-operations: Fix inverted g_assert condition
The commit e2969a9c29d0d171038fcb35d8baa5a7df372191 introduced an assertion
statement to ensure that the `g_file_get_basename` returned non-NULL string.
Unfortunately, the assertion has an inverted condition. This leads to crashes
in the `get_unique_target_file` function for files that don't have the
`edit-name` attribute set (i.e just a few GVfs backends). Let's fix the
inverted condition.

Fixes: https://gitlab.gnome.org/GNOME/nautilus/-/issues/2825

src/nautilus-file-operations.c
@@ -4383,7 +4383,7 @@ get_unique_target_file (GFile      *src,
     if (dest == NULL)
     {
         basename = g_file_get_basename (src);
-        g_assert (basename == NULL);
+        g_assert (basename != NULL);
 
         if (g_utf8_validate (basename, -1, NULL))
         {