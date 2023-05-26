e193b2ce5d5cb4bec92e6e71aa8889ed569e5538
António Fernandes <antoniof@gnome.org>
2023-05-26 16:53:59 +0100

    style: Add real neutral spacing between items
    We've been adding margins to items to keep them appart.
    
    But the rubberbanding algorith doesn't account for these margins, so,
    e.g., when drawing a selection rectangle in the list view by starting
    from the space between rows, the closest row is included in the
    selection even if the selection extends in the opposite direction.
    
    Now that GTK has gained support for the border-spacing CSS property in
    GtkListBase subclasses, which the rubberbanding algorithm accounts for,
    we can finally fix this bug in list view. For grid view this is not
    a complete fix, because items have variable sizes. But at least there
    is a safe space.
    
    Resolves a large part of https://gitlab.gnome.org/GNOME/nautilus/-/issues/2974

--- src/nautilus-grid-view.c
+++ src/nautilus-grid-view.c
@@ -459,10 +459,6 @@ bind_cell (GtkSignalListItemFactory *factory,
         parent = gtk_widget_get_parent (cell);
         gtk_widget_set_halign (parent, GTK_ALIGN_CENTER);
         gtk_widget_set_valign (parent, GTK_ALIGN_START);
-        gtk_widget_set_margin_top (parent, 3);
-        gtk_widget_set_margin_bottom (parent, 3);
-        gtk_widget_set_margin_start (parent, 3);
-        gtk_widget_set_margin_end (parent, 3);
 
         gtk_accessible_update_relation (GTK_ACCESSIBLE (parent),
                                         GTK_ACCESSIBLE_RELATION_LABELLED_BY, cell, NULL,
