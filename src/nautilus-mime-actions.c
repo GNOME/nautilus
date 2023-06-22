GNOME/nautilus

commit 24bb7b818a7458501afeddef26336e4b1e023dfb

mime-action: Use GtkFileLauncher
While sandboxed, we should use a portal to open files with another app.

But the portal is not getting used for archive file types or any of
their subtypes. Instead, nautilus itself tries to handle them. Such is
the case for Open Document file types, which are subtypes of ZIP.

This happens because GAppInfo uses the portal only as a fallback for
when it doesn't find any handler in the sandbox. But it finds nautilus
itself inside of the sandbox, which claims to handle archives, so it
picks it.

Instead of using GAppInfo, use the new GtkFileInfo API which uses the
portal directly. Also, remove the now unused hack, because GtkFileInfo
does everything we need while running as a flatpak.

Fixes https://gitlab.gnome.org/GNOME/nautilus/-/issues/2841

src/nautilus-mime-actions.c
@@ -1407,9 +1407,9 @@ out:
 }
 
 static void
-launch_default_for_uris_callback (GObject      *source_object,
-                                  GAsyncResult *res,
-                                  gpointer      user_data)
+sandboxed_launcher_callback (GObject      *source_object,
+                             GAsyncResult *res,
+                             gpointer      user_data)
 {
     ActivateParameters *activation_params;
     char *uri;
@@ -1418,7 +1418,7 @@ launch_default_for_uris_callback (GObject      *source_object,
     activation_params = user_data;
     uri = g_queue_pop_head (activation_params->open_in_app_uris);
 
-    nautilus_launch_default_for_uri_finish (res, &error);
+    gtk_file_launcher_launch_finish (GTK_FILE_LAUNCHER (source_object), res, &error);
     if (error == NULL)
     {
         gtk_recent_manager_add_item (gtk_recent_manager_get_default (), uri);
@@ -1426,11 +1426,14 @@ launch_default_for_uris_callback (GObject      *source_object,
 
     if (!g_queue_is_empty (activation_params->open_in_app_uris))
     {
-        nautilus_launch_default_for_uri_async (g_queue_peek_head (activation_params->open_in_app_uris),
-                                               activation_params->parent_window,
-                                               activation_params->cancellable,
-                                               launch_default_for_uris_callback,
-                                               g_steal_pointer (&activation_params));
+        g_autoptr (GFile) location = g_file_new_for_uri (g_queue_peek_head (activation_params->open_in_app_uris));
+        GtkFileLauncher *launcher = gtk_file_launcher_new (location);
+
+        gtk_file_launcher_launch (launcher,
+                                  activation_params->parent_window,
+                                  activation_params->cancellable,
+                                  sandboxed_launcher_callback,
+                                  g_steal_pointer (&activation_params));
     }
     else
     {
@@ -1657,14 +1660,18 @@ activate_files_internal (ActivateParameters *parameters)
     if (!g_queue_is_empty (parameters->open_in_app_uris) && nautilus_application_is_sandboxed ())
     {
         const char *uri;
+        g_autoptr (GFile) location = NULL;
+        g_autoptr (GtkFileLauncher) launcher = NULL;
 
         uri = g_queue_peek_head (parameters->open_in_app_uris);
+        location = g_file_new_for_uri (uri);
+        launcher = gtk_file_launcher_new (location);
 
-        nautilus_launch_default_for_uri_async (uri,
-                                               parameters->parent_window,
-                                               parameters->cancellable,
-                                               launch_default_for_uris_callback,
-                                               g_steal_pointer (&parameters));
+        gtk_file_launcher_launch (launcher,
+                                  parameters->parent_window,
+                                  parameters->cancellable,
+                                  sandboxed_launcher_callback,
+                                  g_steal_pointer (&parameters));
         return;
     }
 

src/nautilus-program-choosing.c
@@ -467,145 +467,3 @@ nautilus_launch_desktop_file (const char  *desktop_file_uri,
     g_object_unref (context);
     g_object_unref (app_info);
 }
-
-/* HAX
- *
- * TODO: remove everything below once it’s doable from GTK+.
- *
- * Context: https://bugzilla.gnome.org/show_bug.cgi?id=781132 and
- *          https://bugzilla.gnome.org/show_bug.cgi?id=779312
- *
- * In a sandboxed environment, this is needed to able to get the actual
- * result of the operation, since gtk_show_uri_on_window () neither blocks
- * nor returns a useful value.
- */
-
-static void
-on_launch_default_for_uri (GObject      *source,
-                           GAsyncResult *result,
-                           gpointer      data)
-{
-    GTask *task;
-    NautilusWindow *window;
-    gboolean success;
-    GError *error = NULL;
-
-    task = data;
-    window = g_task_get_source_object (task);
-
-    success = g_app_info_launch_default_for_uri_finish (result, &error);
-
-    if (window)
-    {
-        nautilus_window_unexport_handle (window);
-    }
-
-    if (success)
-    {
-        g_task_return_boolean (task, success);
-    }
-    else
-    {
-        g_task_return_error (task, error);
-    }
-
-    /* Reffed in the call to nautilus_window_export_handle */
-    g_object_unref (task);
-}
-
-static void
-on_window_handle_export (NautilusWindow *window,
-                         const char     *handle_str,
-                         guint           xid,
-                         gpointer        user_data)
-{
-    GTask *task = user_data;
-    GAppLaunchContext *context = g_task_get_task_data (task);
-    const char *uri;
-
-    uri = g_object_get_data (G_OBJECT (context), "uri");
-
-    g_app_launch_context_setenv (context, "PARENT_WINDOW_ID", handle_str);
-
-    g_app_info_launch_default_for_uri_async (uri,
-                                             context,
-                                             g_task_get_cancellable (task),
-                                             on_launch_default_for_uri,
-                                             task);
-}
-
-static void
-launch_default_for_uri_thread_func (GTask        *task,
-                                    gpointer      source_object,
-                                    gpointer      task_data,
-                                    GCancellable *cancellable)
-{
-    GAppLaunchContext *launch_context;
-    const char *uri;
-    gboolean success;
-    GError *error = NULL;
-
-    launch_context = task_data;
-    uri = g_object_get_data (G_OBJECT (launch_context), "uri");
-    success = g_app_info_launch_default_for_uri (uri, launch_context, &error);
-
-    if (success)
-    {
-        g_task_return_boolean (task, success);
-    }
-    else
-    {
-        g_task_return_error (task, error);
-    }
-}
-
-void
-nautilus_launch_default_for_uri_async  (const char         *uri,
-                                        GtkWindow          *parent_window,
-                                        GCancellable       *cancellable,
-                                        GAsyncReadyCallback callback,
-                                        gpointer            callback_data)
-{
-    g_autoptr (GdkAppLaunchContext) launch_context = NULL;
-    g_autoptr (GTask) task = NULL;
-
-    g_return_if_fail (uri != NULL);
-
-    launch_context = get_launch_context (parent_window);
-    task = g_task_new (parent_window, cancellable, callback, callback_data);
-
-    gdk_app_launch_context_set_timestamp (launch_context, GDK_CURRENT_TIME);
-
-    g_object_set_data_full (G_OBJECT (launch_context),
-                            "uri", g_strdup (uri), g_free);
-    g_task_set_task_data (task,
-                          g_object_ref (launch_context), g_object_unref);
-
-    if (parent_window != NULL)
-    {
-        gboolean handle_exported;
-
-        handle_exported = nautilus_window_export_handle (NAUTILUS_WINDOW (parent_window),
-                                                         on_window_handle_export,
-                                                         g_object_ref (task));
-
-        if (handle_exported)
-        {
-            /* Launching will now be handled from the callback */
-            return;
-        }
-    }
-
-    g_task_run_in_thread (task, launch_default_for_uri_thread_func);
-}
-
-gboolean
-nautilus_launch_default_for_uri_finish (GAsyncResult  *result,
-                                        GError       **error)
-{
-    g_return_val_if_fail (G_IS_ASYNC_RESULT (result), FALSE);
-
-    return g_task_propagate_boolean (G_TASK (result), error);
-}
-
-/* END OF HAX */

src/nautilus-program-choosing.h
@@ -50,10 +50,3 @@ void nautilus_launch_application_from_command_array (GdkDisplay                 
 void nautilus_launch_desktop_file                   (const char                        *desktop_file_uri,
                                                      const GList                       *parameter_uris,
                                                      GtkWindow                         *parent_window);
-void nautilus_launch_default_for_uri_async          (const char                        *uri,
-                                                     GtkWindow                         *parent_window,
-                                                     GCancellable                      *cancellable,
-                                                     GAsyncReadyCallback                callback,
-                                                     gpointer                           callback_data);
-gboolean nautilus_launch_default_for_uri_finish     (GAsyncResult                      *result,
-                                                     GError                           **error);

commit 9154c3f53d42bc9ad209a3acdfbfc9e28ee8545b

mime-actions: Don't crash on sandboxed launch
I've added g_steal_pointer() to clearly indicate that the callback
takes ownership. However, the same call where this pointer is stolen
also dereferences it for other 2 parameters (parent window and the
cancellable).

So, use a comment instead and pass the pointer directly without
clearing the local variable.

Fixes bug introduced by commit 24bb7b818a7458501afeddef26336e4b1e023dfb

src/nautilus-mime-actions.c
@@ -1439,7 +1439,7 @@ sandboxed_launcher_callback (GObject      *source_object,
                                   activation_params->parent_window,
                                   activation_params->cancellable,
                                   sandboxed_launcher_callback,
-                                  g_steal_pointer (&activation_params));
+                                  activation_params /* callback takes ownership */);
     }
     else
     {
@@ -1681,7 +1681,7 @@ activate_files_internal (ActivateParameters *parameters)
                                   parameters->parent_window,
                                   parameters->cancellable,
                                   sandboxed_launcher_callback,
-                                  g_steal_pointer (&parameters));
+                                  parameters /* callback takes ownership */);
         return;
     }
 