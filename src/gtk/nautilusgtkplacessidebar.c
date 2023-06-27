GNOME/nautilus

commit ab1d3019edfcf12411537d2bf716db5f187627f9

sidebar: Hide format command for mtp devices
Format option is displayed for mtp devices even though gnome-disks
doesn't support them.  Hide format option if uri starts with mtp://

This applies to MTP and PTP (gphoto2)

Fixes: https://gitlab.gnome.org/GNOME/nautilus/-/issues/1358

src/gtk/nautilusgtkplacessidebar.c
@@ -3160,12 +3160,13 @@ static GActionEntry entries[] = {
 };
 
 static gboolean
-should_show_format_command (GVolume *volume)
+should_show_format_command (GVolume *volume,
+                            gchar   *uri)
 {
     g_autofree gchar *unix_device_id = NULL;
     gboolean disks_available;
 
-    if (volume == NULL || !G_IS_VOLUME (volume))
+    if (volume == NULL || !G_IS_VOLUME (volume) || g_str_has_prefix (uri, "mtp://"))
       return FALSE;
 
     unix_device_id = g_volume_get_identifier (volume, G_VOLUME_IDENTIFIER_KIND_UNIX_DEVICE);
@@ -3437,7 +3438,7 @@ create_row_popover (NautilusGtkPlacesSidebar *sidebar,
       g_object_unref (item);
     }
 
-  if (should_show_format_command (volume))
+  if (should_show_format_command (volume, uri))
     {
       item = g_menu_item_new (_("Format…"), "row.format");
       g_menu_append_item (section, item);

commit dde4ec1883c7922aa28f861882fece83774d1d0c

sidebar: Hide format for unmounted MTP/GPhoto2 volumes
Currently, the Format action is offered also for the MTP volumes when
they are unmounted. This is a regression from the commit ab1d3019. This
is because the `uri` variable is `NULL` for unmounted volumes. This
amoung other causes critical warnings when opening popover menu for
unmounted volumes. Let's check that the activation root is native file
instead to avoid the mention issue. This will also correctly hide the
Format action also for GPhoto2 devices that are mentioned in the original
commit, but unhandled currently.

src/gtk/nautilusgtkplacessidebar.c
@@ -3187,13 +3187,18 @@ static GActionEntry entries[] = {
 };
 
 static gboolean
-should_show_format_command (GVolume *volume,
-                            gchar   *uri)
+should_show_format_command (GVolume *volume)
 {
     g_autofree gchar *unix_device_id = NULL;
     gboolean disks_available;
+    g_autoptr (GFile) activation_root = NULL;
 
-    if (volume == NULL || !G_IS_VOLUME (volume) || g_str_has_prefix (uri, "mtp://"))
+    if (volume == NULL || !G_IS_VOLUME (volume))
+      return FALSE;
+
+    /* Don't show format command for MTP and GPhoto2 devices. */
+    activation_root = g_volume_get_activation_root (volume);
+    if (activation_root != NULL && !g_file_is_native (activation_root))
       return FALSE;
 
     unix_device_id = g_volume_get_identifier (volume, G_VOLUME_IDENTIFIER_KIND_UNIX_DEVICE);
@@ -3471,7 +3476,7 @@ create_row_popover (NautilusGtkPlacesSidebar *sidebar,
       g_object_unref (item);
     }
 
-  if (should_show_format_command (volume, uri))
+  if (should_show_format_command (volume))
     {
       item = g_menu_item_new (_("Format…"), "row.format");
       g_menu_append_item (section, item);