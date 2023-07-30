05a7c18473f6ebcd8c954c544397ac4aafa14926
Peter Eisenmann <p3732@getgoogleoff.me>
2023-07-30 21:21:34 +0200

    nautilus-scheme: Introduce new header
    A simple header that contains all schemes known to nautilus.

---
+++ src/nautilus-scheme.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023 Peter Eisenmann <p3732@getgoogleoff.me>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#define SCHEME_ADMIN "admin"
+#define SCHEME_BURN "burn"
+#define SCHEME_NAUTILUS_SEARCH "x-nautilus-search"
+#define	SCHEME_OTHER_LOCATIONS "other-locations"
+#define	SCHEME_NETWORK "network"
+#define	SCHEME_RECENT "recent"
+#define	SCHEME_STARRED "starred"
+#define	SCHEME_TRASH "trash"
