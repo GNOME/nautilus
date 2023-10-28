2b6a4906b8a02294d7c0a4a7aeac859c731f6d38
António Fernandes <antoniof@gnome.org>
2023-10-28 14:32:51 +0000

    remove_iterations

--- test/automated/displayless/test-file-metadata.c
+++ test/automated/displayless/test-file-metadata.c
@@ -13,12 +13,6 @@ test_file_metadata_bool_set_true (void)
 {
     g_autoptr (NautilusFile) file = nautilus_file_get_by_uri (TEST_FILE);
     nautilus_file_set_boolean_metadata (file, KEY_BOOL, TRUE);
-    for (guint i = 0;
-         i < 10 && nautilus_file_get_boolean_metadata (file, KEY_BOOL, FALSE) != TRUE;
-         i += 1)
-    {
-        g_main_context_iteration (NULL, TRUE);
-    }
     g_assert_true (nautilus_file_get_boolean_metadata (file, KEY_BOOL, FALSE));
 }
 
@@ -27,12 +21,6 @@ test_file_metadata_bool_set_false (void)
 {
     g_autoptr (NautilusFile) file = nautilus_file_get_by_uri (TEST_FILE);
     nautilus_file_set_boolean_metadata (file, KEY_BOOL, FALSE);
-    for (guint i = 0;
-         i < 10 && nautilus_file_get_boolean_metadata (file, KEY_BOOL, TRUE) != FALSE;
-         i++)
-    {
-        g_main_context_iteration (NULL, TRUE);
-    }
     g_assert_false (nautilus_file_get_boolean_metadata (file, KEY_BOOL, TRUE));
 }
 
@@ -48,19 +36,12 @@ static void
 test_file_metadata_str_set (void)
 {
     g_autoptr (NautilusFile) file = nautilus_file_get_by_uri (TEST_FILE);
-    gboolean metadata_was_set = FALSE;
+    g_autofree char *metadata = NULL;
+
     nautilus_file_set_metadata (file, KEY_STR, "default", "value");
-    for (guint i = 0; i < 10; i += 1)
-    {
-        g_autofree char *metadata = nautilus_file_get_metadata (file, KEY_STR, "default");
-        if (strcmp (metadata, "value") == 0)
-        {
-            metadata_was_set = TRUE;
-            break;
-        }
-        g_main_context_iteration (NULL, TRUE);
-    }
-    g_assert_true (metadata_was_set);
+    metadata = nautilus_file_get_metadata (file, KEY_STR, "default");
+
+    g_assert_true (strcmp (metadata, "value") == 0);
 }
 
 static void
