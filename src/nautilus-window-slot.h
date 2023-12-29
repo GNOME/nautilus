bd2e942e0b6e5b96331bcaa64da2ef2e64b18e55
Khalid Abu Shawarib <khalid.shawarib@gmail.com>
2023-12-29 03:54:09 +0300

    window-slot: Remove unused property

--- src/nautilus-window-slot.h
+++ src/nautilus-window-slot.h
@@ -1,21 +1,21 @@
 /*
    nautilus-window-slot.h: Nautilus window slot
- 
+
    Copyright (C) 2008 Free Software Foundation, Inc.
-  
+
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
    published by the Free Software Foundation; either version 2 of the
    License, or (at your option) any later version.
-  
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    General Public License for more details.
-  
+
    You should have received a copy of the GNU General Public
    License along with this program; if not, see <http://www.gnu.org/licenses/>.
-  
+
    Author: Christian Neumair <cneumair@gnome.org>
 */
 
@@ -99,8 +99,6 @@ gboolean nautilus_window_slot_get_loading                  (NautilusWindowSlot *
 gboolean nautilus_window_slot_get_search_visible           (NautilusWindowSlot *slot);
 
 gboolean nautilus_window_slot_get_search_global            (NautilusWindowSlot *self);
-
-GList* nautilus_window_slot_get_selection                  (NautilusWindowSlot *slot);
 
 void     nautilus_window_slot_search                       (NautilusWindowSlot *slot,
                                                             NautilusQuery      *query);
