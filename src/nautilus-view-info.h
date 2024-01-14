c79810dff75527e6a51dec3bd7a7ce2dd93d18d8
António Fernandes <antoniof@gnome.org>
2024-01-14 21:28:45 +0000

    view-info: Introduce shareable header
    NautilusListBase and its subclasses need these ids, but will soon
    stop including nautilus-view.h (through nautilus-files-view.h), when
    they become child widgets instead of subclasses of NautilusFilesView

---
+++ src/nautilus-view-info.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2024 The GNOME project contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+/* Keep values in sync with the org.gnome.nautilus.FolderView schema enums: */
+#define NAUTILUS_VIEW_LIST_ID            1
+#define NAUTILUS_VIEW_GRID_ID            2
+/* Special ids, not used by GSettings schemas: */
+#define NAUTILUS_VIEW_INVALID_ID         0
+#define NAUTILUS_VIEW_OTHER_LOCATIONS_ID 3
+
+typedef struct
+{
+    unsigned int view_id;
+    int zoom_level_min;
+    int zoom_level_max;
+    int zoom_level_standard;
+} NautilusViewInfo;
