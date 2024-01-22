5cf429f962d7653abfb231e42790a8d37a29cf4b
Demigod345 <divyanshjain.2206@gmail.com>
2024-01-22 15:29:45 +0530

    window: Change issue-url for "Report An Issue"
    "Report An Issue" redirects to https://gitlab.gnome.org/GNOME/nautilus/-/issues/new directly.
    However, the users that are not logged in, would be greeted by a login screen.
    
    This fix would redirect all users to see all the existing issues, irrespective of whether they are logged in or not.
    
    Fixes: #3220

--- src/nautilus-window.c
+++ src/nautilus-window.c
@@ -2494,7 +2494,7 @@ nautilus_window_show_about_dialog (NautilusWindow *window)
                            "developer-name", _("The GNOME Project"),
                            "version", VERSION,
                            "website", "https://wiki.gnome.org/action/show/Apps/Files",
-                           "issue-url", "https://gitlab.gnome.org/GNOME/nautilus/-/issues/new",
+                           "issue-url", "https://gitlab.gnome.org/GNOME/nautilus/-/issues",
                            "support-url", "https://discourse.gnome.org/tag/nautilus",
                            "debug-info", debug_info,
                            "copyright", "© 1999 The Files Authors",
