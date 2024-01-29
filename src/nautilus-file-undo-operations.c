868dc666dfda1ef6d30d2bc63b4fe1dabc6055d4
Khalid Abu Shawarib <khalid.shawarib@gmail.com>
2024-01-29 21:10:25 +0300

    file-undo-operation: Fix batch rename list leak

--- src/nautilus-file-undo-operations.c
+++ src/nautilus-file-undo-operations.c
@@ -1201,7 +1201,8 @@ batch_rename_redo_func (NautilusFileUndoInfo           *info,
 {
     NautilusFileUndoInfoBatchRename *self = NAUTILUS_FILE_UNDO_INFO_BATCH_RENAME (info);
 
-    GList *l, *files;
+    GList *l;
+    g_autolist (NautilusFile) files = NULL;
     NautilusFile *file;
     GFile *old_file;
 
@@ -1234,7 +1235,8 @@ batch_rename_undo_func (NautilusFileUndoInfo           *info,
 {
     NautilusFileUndoInfoBatchRename *self = NAUTILUS_FILE_UNDO_INFO_BATCH_RENAME (info);
 
-    GList *l, *files;
+    GList *l;
+    g_autolist (NautilusFile) files = NULL;
     NautilusFile *file;
     GFile *new_file;
 
