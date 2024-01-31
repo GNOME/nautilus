GNOME/nautilus

commit d4526131c17ee138a853ebd70e63ea9c994bc80d

filename-util: add conflict name creation helper
Add a helper function that appends a bracketed number to the end of a
file name, and can also detect previous numbers. Adjust functions in
file-operations and file-util to use it for handling conflicts. Also,
add unittests for it.

src/nautilus-file-operations.c
@@ -7070,7 +7070,6 @@ create_task_thread_func (GTask        *task,
     g_autoptr (GFile) dest = NULL;
     g_autofree gchar *dest_uri = NULL;
     g_autofree char *filename = NULL;
-    char *filename_base;
     g_autofree char *dest_fs_type = NULL;
     GError *error;
     gboolean res;
@@ -7081,7 +7080,7 @@ create_task_thread_func (GTask        *task,
     gsize length;
     GFileOutputStream *out;
     gboolean handled_invalid_filename;
-    int max_length, offset;
+    int max_length;
 
     job = task_data;
     common = &job->common;
@@ -7299,27 +7298,8 @@ retry:
             }
             else
             {
-                g_autofree char *suffix = NULL;
-
-                filename_base = filename;
-                if (job->src != NULL)
-                {
-                    GFileType file_type;
-                    file_type = g_file_query_file_type (job->src,
-                                                        G_FILE_QUERY_INFO_NONE,
-                                                        common->cancellable);
-                    if (file_type != G_FILE_TYPE_DIRECTORY)
-                    {
-                        filename_base = eel_filename_strip_extension (filename);
-                    }
-                }
-
-                offset = strlen (filename_base);
-                suffix = g_strdup (filename + offset);
-
-                new_filename = g_strdup_printf ("%s %d%s", filename_base, count, suffix);
-
-                nautilus_filename_shorten_base (&new_filename, filename_base, max_length);
+                gboolean use_extension = job->src != NULL && !is_dir (job->src, common->cancellable);
+                new_filename = nautilus_filename_for_conflict (filename, count, max_length, use_extension);
             }
 
             if (make_file_name_valid_for_dest_fs (new_filename, dest_fs_type))
@@ -7342,32 +7322,11 @@ retry:
 
         if (IS_IO_ERROR (error, EXISTS))
         {
-            g_autofree char *suffix = NULL;
-            g_autofree gchar *filename2 = NULL;
-
-            g_clear_object (&dest);
-
-            filename_base = filename;
-            if (job->src != NULL)
-            {
-                GFileType file_type;
-                file_type = g_file_query_file_type (job->src,
-                                                    G_FILE_QUERY_INFO_NONE,
-                                                    common->cancellable);
-                if (file_type != G_FILE_TYPE_DIRECTORY)
-                {
-                    filename_base = eel_filename_strip_extension (filename);
-                }
-            }
-
-
-            offset = strlen (filename_base);
-            suffix = g_strdup (filename + offset);
-
-            filename2 = g_strdup_printf ("%s %d%s", filename_base, ++count, suffix);
-            nautilus_filename_shorten_base (&filename2, filename_base, max_length);
+            gboolean use_extension = job->src != NULL && !is_dir (job->src, common->cancellable);
+            g_autofree gchar *filename2 = nautilus_filename_for_conflict (filename, count, max_length, use_extension);
 
             make_file_name_valid_for_dest_fs (filename2, dest_fs_type);
+            g_clear_object (&dest);
             if (filename_is_utf8)
             {
                 dest = g_file_get_child_for_display_name (job->dest_dir, filename2, NULL);

src/nautilus-file-utilities.c
@@ -268,34 +268,18 @@ GFile *
 nautilus_generate_unique_file_in_directory (GFile      *directory,
                                             const char *basename)
 {
-    g_autofree char *basename_without_extension = NULL;
-    const char *extension;
-    GFile *child;
-    int copy;
-
     g_return_val_if_fail (directory != NULL, NULL);
     g_return_val_if_fail (basename != NULL, NULL);
     g_return_val_if_fail (g_file_query_exists (directory, NULL), NULL);
 
-    basename_without_extension = eel_filename_strip_extension (basename);
-    extension = nautilus_filename_get_extension (basename);
+    GFile *child = g_file_get_child (directory, basename);
 
-    child = g_file_get_child (directory, basename);
-
-    copy = 1;
-    while (g_file_query_exists (child, NULL))
+    for (size_t counter = 1; g_file_query_exists (child, NULL); counter += 1)
     {
-        g_autofree char *filename = NULL;
+        g_autofree char *filename = nautilus_filename_for_conflict (basename, counter, -1, FALSE);
 
         g_object_unref (child);
-
-        filename = g_strdup_printf ("%s (%d)%s",
-                                    basename_without_extension,
-                                    copy,
-                                    extension);
         child = g_file_get_child (directory, filename);
-
-        copy++;
     }
 
     return child;

src/nautilus-filename-utilities.c
@@ -114,6 +114,55 @@ create_appendix_name (const char      *name,
     return result;
 }
 
+static gboolean
+parse_previous_conflict_name (const char *appendix,
+                              size_t      appendix_len,
+                              size_t     *count)
+{
+    if (sscanf (appendix, "%zu)", count) == 1)
+    {
+        g_autofree char *appendix_check = g_strdup_printf ("%zu", *count);
+
+        if (strlen (appendix_check) == appendix_len)
+        {
+            return TRUE;
+        }
+    }
+
+    *count = 0;
+    return FALSE;
+}
+
+static char *
+get_conflict_appendix (size_t count)
+{
+    /* First appendix should be (2), not (1) */
+    count = MAX (count, 2);
+    return g_strdup_printf ("%zu", count);
+}
+
+/**
+ * nautilus_filename_for_conflict:
+ * @name: Name of the original file
+ * @count_increment: By how much to increase the detected copy number of @name
+ * @max_length: Maximum length that resulting file name can have
+ * @ignore_extension: Whether to ignore file extensions (should be FALSE for directories)
+ *
+ * Creates a new name for a copy of @name, that is no longer than @max_length
+ * bytes long.
+ *
+ * Returns: (transfer full): A file name for a copy of @name.
+ */
+char *
+nautilus_filename_for_conflict (const char *name,
+                                int         count_increment,
+                                int         max_length,
+                                gboolean    ignore_extension)
+{
+    return create_appendix_name (name, count_increment, max_length, ignore_extension,
+                                 parse_previous_conflict_name, get_conflict_appendix);
+}
+
 /* Translators: This is appended to a file name when a copy of a file is created. */
 #define COPY_APPENDIX_FIRST_COPY _("Copy")
 /* Translators: This is appended to a file name when a copy of an already copied file is created.

src/nautilus-filename-utilities.h
@@ -8,6 +8,11 @@
 
 #include <glib.h>
 
+char *
+nautilus_filename_for_conflict (const char *name,
+                                int         count_increment,
+                                int         max_length,
+                                gboolean    ignore_extension);
 
 char *
 nautilus_filename_for_copy (const char *name,

test/automated/displayless/test-filename-utilities.c
@@ -88,6 +88,38 @@ test_filename_create_dir_copy (void)
 #undef ASSERT_DUPLICATION_NAME
 }
 
+static void
+test_filename_create_conflict_name (void)
+{
+#define ASSERT_DUPLICATION_NAME(ORIGINAL, DUPLICATE) \
+        { \
+            g_autofree char *duplicated = nautilus_filename_for_conflict (ORIGINAL, 1, -1, FALSE); \
+            g_assert_cmpstr (duplicated, ==, DUPLICATE); \
+        }
+
+    ASSERT_DUPLICATION_NAME ("foo", "foo (2)");
+    ASSERT_DUPLICATION_NAME (".bashrc", ".bashrc (2)");
+    ASSERT_DUPLICATION_NAME (".foo.txt", ".foo (2).txt");
+    ASSERT_DUPLICATION_NAME ("foo foo", "foo foo (2)");
+    ASSERT_DUPLICATION_NAME ("foo.txt", "foo (2).txt");
+    ASSERT_DUPLICATION_NAME ("foo foo.txt", "foo foo (2).txt");
+    ASSERT_DUPLICATION_NAME ("foo foo.txt txt", "foo foo.txt txt (2)");
+    ASSERT_DUPLICATION_NAME ("foo...txt", "foo.. (2).txt");
+    ASSERT_DUPLICATION_NAME ("foo...", "foo... (2)");
+    ASSERT_DUPLICATION_NAME ("foo. (2)", "foo. (3)");
+    ASSERT_DUPLICATION_NAME ("foo (2)", "foo (3)");
+    ASSERT_DUPLICATION_NAME ("foo (2a)", "foo (2a) (2)");
+    ASSERT_DUPLICATION_NAME ("foo (b2)", "foo (b2) (2)");
+    ASSERT_DUPLICATION_NAME ("foo (2).txt", "foo (3).txt");
+    ASSERT_DUPLICATION_NAME ("foo (2)", "foo (3)");
+    ASSERT_DUPLICATION_NAME ("foo (2).txt", "foo (3).txt");
+    ASSERT_DUPLICATION_NAME ("foo foo (2).txt", "foo foo (3).txt");
+    ASSERT_DUPLICATION_NAME ("foo (13)", "foo (14)");
+    ASSERT_DUPLICATION_NAME ("foo foo (123345678901).txt", "foo foo (123345678902).txt");
+
+#undef ASSERT_DUPLICATION_NAME
+}
+
 static const char *long_base = "great-text-but-sadly-too-long";
 static const char *short_base = "great-text";
 static const char *suffix = "-123456789";
@@ -149,6 +181,8 @@ main (int   argc,
                      test_filename_shortening_with_base);
     g_test_add_func ("/file-name-shortening-with-base/not-needed",
                      test_filename_shortening_with_base_not_needed);
+    g_test_add_func ("/filename-create-conflict/file",
+                     test_filename_create_conflict_name);
 
     return g_test_run ();
 }

commit 02866479395e0c2179f759c0f63cf9d9e960fe92

file-operations: Increment unique filename count
A regression in commit d4526131c1 ("filename-util: add conflict
name creation helper") causes an infinite loop when creating a
file (i.e. from template) where the suffix needs to increment.
This also led to an unexpected behavior of causing the first
duplicate to be suffixed with (1) instead of (2).

Pre-increment the counter to avoid the infinite loop and
start a (2).

src/nautilus-file-operations.c
@@ -7363,7 +7363,7 @@ retry:
         if (IS_IO_ERROR (error, EXISTS))
         {
             gboolean use_extension = job->src != NULL && !is_dir (job->src, common->cancellable);
-            g_autofree gchar *filename2 = nautilus_filename_for_conflict (filename, count, max_length, use_extension);
+            g_autofree gchar *filename2 = nautilus_filename_for_conflict (filename, ++count, max_length, use_extension);
 
             make_file_name_valid_for_dest_fs (filename2, dest_fs_type);
             g_clear_object (&dest);