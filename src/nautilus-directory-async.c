4366099f3ccc9d5e54f50e2c5dc59e5ae66364ab
Peter Eisenmann <p3732@getgoogleoff.me>
2024-02-22 13:04:38 +0100

    directory-async: simplify destroying function
    Inline some variables and prevent log spam when multiple pending
    call-when-ready callbacks are still around.

--- src/nautilus-directory-async.c
+++ src/nautilus-directory-async.c
@@ -1519,31 +1519,30 @@ nautilus_directory_get_info_for_new_files (NautilusDirectory *directory,
 void
 nautilus_async_destroying_file (NautilusFile *file)
 {
-    NautilusDirectory *directory;
-    gboolean changed;
-    GList *node, *next;
-    Monitor *monitor;
-
-    directory = file->details->directory;
-    changed = FALSE;
+    NautilusDirectory *directory = file->details->directory;
+    gboolean changed = FALSE;
+    GList *node;
 
     /* Check for callbacks. */
     node = g_hash_table_lookup (directory->details->call_when_ready_hash.unsatisfied, file);
 
-    /* Client should have cancelled callback. */
-    for (; node != NULL; node = next)
+    if (node != NULL)
     {
+        /* Client should have cancelled callback. */
         g_warning ("destroyed file has call_when_ready pending");
-
-        next = remove_callback_link (directory, node, FALSE);
         changed = TRUE;
+
+        while (node != NULL)
+        {
+            node = remove_callback_link (directory, node, FALSE);
+        }
     }
 
     node = g_hash_table_lookup (directory->details->call_when_ready_hash.ready, file);
 
-    for (; node != NULL; node = next)
+    while (node != NULL)
     {
-        next = remove_callback_link (directory, node, TRUE);
+        node = remove_callback_link (directory, node, TRUE);
         changed = TRUE;
     }
 
@@ -1553,11 +1552,11 @@ nautilus_async_destroying_file (NautilusFile *file)
     {
         /* Client should have removed monitor earlier. */
         g_warning ("destroyed file still being monitored");
-        for (; node; node = next)
-        {
-            next = node->next;
-            monitor = node->data;
 
+        while (node != NULL)
+        {
+            Monitor *monitor = node->data;
+            node = node->next;
             remove_monitor (directory, monitor->file, monitor->client);
         }
         changed = TRUE;
