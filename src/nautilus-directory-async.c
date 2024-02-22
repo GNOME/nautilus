21b30ce5d8935abb2ba3df2635f521bbfd454b01
Peter Eisenmann <p3732@getgoogleoff.me>
2024-02-22 13:02:31 +0100

    directory-async: inline unused preserve data variant

--- src/nautilus-directory-async.c
+++ src/nautilus-directory-async.c
@@ -1335,9 +1335,9 @@ nautilus_directory_check_if_ready_internal (NautilusDirectory      *directory,
 }
 
 static GList *
-remove_callback_link_keep_data (NautilusDirectory *directory,
-                                GList             *link,
-                                gboolean           ready)
+remove_callback_link (NautilusDirectory *directory,
+                      GList             *link,
+                      gboolean           ready)
 {
     ReadyCallback *callback = link->data;
     GList *list = g_list_first (link);
@@ -1363,21 +1363,9 @@ remove_callback_link_keep_data (NautilusDirectory *directory,
     request_counter_remove_request (directory->details->call_when_ready_counters,
                                     callback->request);
 
-    return list;
-}
-
-static GList *
-remove_callback_link (NautilusDirectory *directory,
-                      GList             *link,
-                      gboolean           ready)
-{
-    ReadyCallback *callback;
-
-    callback = link->data;
-    link = remove_callback_link_keep_data (directory, link, ready);
     g_free (callback);
 
-    return link;
+    return list;
 }
 
 static void
