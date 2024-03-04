GNOME/nautilus

commit c653f8809092223dc3ae0366bfd6f63ca85e9c3c

list-base: Don't inherit from NautilusFilesView
Become a child of NautilusFilesView's internal GtkStack instead, in
place of the "content_widget".

As planned in https://gitlab.gnome.org/GNOME/nautilus/-/issues/3042

src/nautilus-files-view.c
@@ -236,8 +236,6 @@ typedef struct
 
     GtkWidget *stack;
 
-    GtkWidget *content_widget;
-
     GtkWidget *extend_search_revealer;
 
     /* Empty states */
@@ -3189,7 +3187,7 @@ slot_active_changed (NautilusWindowSlot *slot,
 static gboolean
 nautilus_files_view_grab_focus (GtkWidget *widget)
 {
-    /* focus the child view if it exists */
+    /* focus the inner view if it exists */
     NautilusFilesView *view = NAUTILUS_FILES_VIEW (widget);
     NautilusFilesViewPrivate *priv = nautilus_files_view_get_instance_private (view);
 
@@ -3345,14 +3343,9 @@ update_extend_search_revealer (NautilusFilesView *self)
 }
 
 static void
-nautilus_files_view_constructed (GObject *object)
+connect_inner_view (NautilusFilesView *self)
 {
-    NautilusFilesView *self = NAUTILUS_FILES_VIEW (object);
     NautilusFilesViewPrivate *priv = nautilus_files_view_get_instance_private (self);
-
-    G_OBJECT_CLASS (nautilus_files_view_parent_class)->constructed (object);
-
-    priv->list_base = NAUTILUS_LIST_BASE (self);
 
     /* Model must be set before the sort-state is bound, for a new sorter to be
      * set on the model. */
@@ -3882,7 +3875,7 @@ real_check_empty_states (NautilusFilesView *view)
     }
     else
     {
-        gtk_stack_set_visible_child (GTK_STACK (priv->stack), priv->content_widget);
+        gtk_stack_set_visible_child (GTK_STACK (priv->stack), GTK_WIDGET (priv->list_base));
     }
 }
 
@@ -4970,18 +4963,6 @@ nautilus_files_view_get_loading (NautilusFilesView *view)
     priv = nautilus_files_view_get_instance_private (view);
 
     return priv->loading;
-}
-
-GtkWidget *
-nautilus_files_view_get_content_widget (NautilusFilesView *view)
-{
-    NautilusFilesViewPrivate *priv;
-
-    g_return_val_if_fail (NAUTILUS_IS_FILES_VIEW (view), NULL);
-
-    priv = nautilus_files_view_get_instance_private (view);
-
-    return priv->content_widget;
 }
 
 /* home_dir_in_selection()
@@ -9434,7 +9415,6 @@ nautilus_files_view_class_init (NautilusFilesViewClass *klass)
     widget_class = GTK_WIDGET_CLASS (klass);
     oclass = G_OBJECT_CLASS (klass);
 
-    oclass->constructed = nautilus_files_view_constructed;
     oclass->dispose = nautilus_files_view_dispose;
     oclass->finalize = nautilus_files_view_finalize;
     oclass->get_property = nautilus_files_view_get_property;
@@ -9551,7 +9531,6 @@ nautilus_files_view_class_init (NautilusFilesViewClass *klass)
     gtk_widget_class_bind_template_child_private (widget_class, NautilusFilesView, overlay);
     gtk_widget_class_bind_template_child_private (widget_class, NautilusFilesView, stack);
     gtk_widget_class_bind_template_child_private (widget_class, NautilusFilesView, empty_view_page);
-    gtk_widget_class_bind_template_child_private (widget_class, NautilusFilesView, content_widget);
     gtk_widget_class_bind_template_child_private (widget_class, NautilusFilesView, floating_bar);
     gtk_widget_class_bind_template_child_private (widget_class, NautilusFilesView, extend_search_revealer);
     gtk_widget_class_bind_template_callback (widget_class, globalize_search);
@@ -9786,32 +9765,44 @@ nautilus_files_view_init (NautilusFilesView *view)
     gtk_widget_set_parent (priv->rename_file_popover, GTK_WIDGET (view));
 }
 
-NautilusFilesView *
-nautilus_files_view_new (guint               id,
-                         NautilusWindowSlot *slot)
+static void
+create_inner_view (NautilusFilesView *self,
+                   guint              id)
 {
-    NautilusFilesView *view = NULL;
-
+    NautilusFilesViewPrivate *priv = nautilus_files_view_get_instance_private (self);
     switch (id)
     {
         case NAUTILUS_VIEW_GRID_ID:
         {
-            view = NAUTILUS_FILES_VIEW (nautilus_grid_view_new (slot));
+            priv->list_base = NAUTILUS_LIST_BASE (nautilus_grid_view_new ());
         }
         break;
 
         case NAUTILUS_VIEW_LIST_ID:
         {
-            view = NAUTILUS_FILES_VIEW (nautilus_list_view_new (slot));
+            priv->list_base = NAUTILUS_LIST_BASE (nautilus_list_view_new ());
         }
         break;
 
         default:
         {
             g_critical ("Unknown view type ID: %d. Falling back to list.", id);
-            view = NAUTILUS_FILES_VIEW (nautilus_list_view_new (slot));
+            priv->list_base = NAUTILUS_LIST_BASE (nautilus_list_view_new ());
         }
     }
+
+    gtk_stack_add_child (GTK_STACK (priv->stack), GTK_WIDGET (priv->list_base));
+}
+
+NautilusFilesView *
+nautilus_files_view_new (guint               id,
+                         NautilusWindowSlot *slot)
+{
+    NautilusFilesView *view = NULL;
+
+    view = NAUTILUS_FILES_VIEW (g_object_new (NAUTILUS_TYPE_FILES_VIEW,
+                                              "window-slot", slot,
+                                              NULL));
 
     if (view == NULL)
     {
@@ -9821,6 +9812,9 @@ nautilus_files_view_new (guint               id,
     {
         g_object_ref_sink (view);
     }
+
+    create_inner_view (view, id);
+    connect_inner_view (view);
 
     return view;
 }

src/nautilus-list-base.c
@@ -12,7 +12,6 @@
 #include "nautilus-view-item.h"
 #include "nautilus-view-model.h"
 #include "nautilus-enum-types.h"
-#include "nautilus-files-view.h"
 #include "nautilus-file.h"
 #include "nautilus-file-operations.h"
 #include "nautilus-metadata.h"
@@ -55,7 +54,7 @@ struct _NautilusListBasePrivate
     GtkDropTarget *view_drop_target;
 };
 
-G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE (NautilusListBase, nautilus_list_base, NAUTILUS_TYPE_FILES_VIEW)
+G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE (NautilusListBase, nautilus_list_base, ADW_TYPE_BIN)
 
 enum
 {
@@ -1266,12 +1265,10 @@ static void
 nautilus_list_base_init (NautilusListBase *self)
 {
     NautilusListBasePrivate *priv = nautilus_list_base_get_instance_private (self);
-    GtkWidget *content_widget;
     GtkEventController *controller;
 
-    content_widget = nautilus_files_view_get_content_widget (NAUTILUS_FILES_VIEW (self));
     priv->scrolled_window = gtk_scrolled_window_new ();
-    adw_bin_set_child (ADW_BIN (content_widget), priv->scrolled_window);
+    adw_bin_set_child (ADW_BIN (self), priv->scrolled_window);
 
     controller = gtk_event_controller_scroll_new (GTK_EVENT_CONTROLLER_SCROLL_VERTICAL);
     gtk_widget_add_controller (priv->scrolled_window, controller);
@@ -1301,6 +1298,14 @@ nautilus_list_base_get_model (NautilusListBase *self)
     NautilusListBasePrivate *priv = nautilus_list_base_get_instance_private (self);
 
     return priv->model;
+}
+
+GtkWidget *
+nautilus_list_base_get_scrolled_window (NautilusListBase *self)
+{
+    NautilusListBasePrivate *priv = nautilus_list_base_get_instance_private (self);
+
+    return priv->scrolled_window;
 }
 
 void

src/nautilus-list-base.h
@@ -6,7 +6,8 @@
 
 #pragma once
 
-#include "nautilus-files-view.h"
+#include <adwaita.h>
+
 #include "nautilus-view-info.h"
 #include "nautilus-view-item.h"
 #include "nautilus-view-model.h"
@@ -15,11 +16,11 @@ G_BEGIN_DECLS
 
 #define NAUTILUS_TYPE_LIST_BASE (nautilus_list_base_get_type())
 
-G_DECLARE_DERIVABLE_TYPE (NautilusListBase, nautilus_list_base, NAUTILUS, LIST_BASE, NautilusFilesView)
+G_DECLARE_DERIVABLE_TYPE (NautilusListBase, nautilus_list_base, NAUTILUS, LIST_BASE, AdwBin)
 
 struct _NautilusListBaseClass
 {
-        NautilusFilesViewClass parent_class;
+        AdwBinClass parent_class;
 
         /* Subclasses must provide implementation. */
         NautilusViewInfo (*get_view_info)     (NautilusListBase *self);

src/resources/ui/nautilus-files-view.ui
@@ -20,13 +20,6 @@
                 </property>
               </object>
             </child>
-            <child>
-              <object class="GtkStackPage">
-                <property name="child">
-                  <object class="AdwBin" id="content_widget"/>
-                </property>
-              </object>
-            </child>
           </object>
         </child>
         <child type="overlay">

src/nautilus-files-view.h
@@ -161,6 +161,4 @@ void              nautilus_files_view_update_context_menus       (NautilusFilesV
 void              nautilus_files_view_update_toolbar_menus       (NautilusFilesView      *view);
 void              nautilus_files_view_update_actions_state       (NautilusFilesView      *view);
 
-GtkWidget*        nautilus_files_view_get_content_widget         (NautilusFilesView      *view);
-
 G_END_DECLS

src/nautilus-grid-view.c
@@ -541,7 +541,7 @@ nautilus_grid_view_class_init (NautilusGridViewClass *klass)
 static void
 nautilus_grid_view_init (NautilusGridView *self)
 {
-    GtkWidget *content_widget;
+    GtkWidget *scrolled_window = nautilus_list_base_get_scrolled_window (NAUTILUS_LIST_BASE (self));
 
     gtk_widget_add_css_class (GTK_WIDGET (self), "nautilus-grid-view");
 
@@ -552,12 +552,11 @@ nautilus_grid_view_init (NautilusGridView *self)
                              self,
                              G_CONNECT_SWAPPED);
 
-    content_widget = nautilus_files_view_get_content_widget (NAUTILUS_FILES_VIEW (self));
 
     self->view_ui = create_view_ui (self);
     nautilus_list_base_setup_gestures (NAUTILUS_LIST_BASE (self));
 
-    gtk_scrolled_window_set_child (GTK_SCROLLED_WINDOW (adw_bin_get_child (ADW_BIN (content_widget))),
+    gtk_scrolled_window_set_child (GTK_SCROLLED_WINDOW (scrolled_window),
                                    GTK_WIDGET (self->view_ui));
 
     g_signal_connect_object (gtk_filechooser_preferences,
@@ -570,9 +569,7 @@ nautilus_grid_view_init (NautilusGridView *self)
 }
 
 NautilusGridView *
-nautilus_grid_view_new (NautilusWindowSlot *slot)
+nautilus_grid_view_new (void)
 {
-    return g_object_new (NAUTILUS_TYPE_GRID_VIEW,
-                         "window-slot", slot,
-                         NULL);
+    return g_object_new (NAUTILUS_TYPE_GRID_VIEW, NULL);
 }

src/nautilus-grid-view.h
@@ -7,7 +7,6 @@
 #pragma once
 
 #include "nautilus-list-base.h"
-#include "nautilus-window-slot.h"
 
 G_BEGIN_DECLS
 
@@ -15,6 +14,6 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE (NautilusGridView, nautilus_grid_view, NAUTILUS, GRID_VIEW, NautilusListBase)
 
-NautilusGridView *nautilus_grid_view_new (NautilusWindowSlot *slot);
+NautilusGridView *nautilus_grid_view_new (void);
 
 G_END_DECLS

src/nautilus-list-base-private.h
@@ -21,6 +21,7 @@ void               nautilus_list_base_activate_selection (NautilusListBase *self
                                                           gboolean          open_in_new_tab);
 NautilusFile      *nautilus_list_base_get_directory_as_file (NautilusListBase *self);
 NautilusViewModel *nautilus_list_base_get_model     (NautilusListBase *self);
+GtkWidget         *nautilus_list_base_get_scrolled_window (NautilusListBase *self);
 void               nautilus_list_base_setup_gestures (NautilusListBase *self);
 
 /* Shareable helpers */

commit 7b17260676068cbfe50035535650efe2f8c1ac02

list-base: Handle events on empty page
The changes in commit c653f880 ("list-base: Don't inherit
from NautilusFilesView"), broke our gestures on the empty status page
which is still a child of files-view. Rather than attempt to fix this
through files view, as done in commit 95f3db44 ("files-view: Add
gestures to open background menu in empty view"), which fixed the click
gestures, but not the dnd gestures, let's make the more appropriate
change of making the empty status page a child of list-base.

Having the empty status page as a child of the list-base is more logical
because it represents the view, and ultimately our goal is to remove
all view components for files-view (despiste its name).

The empty page is still managed externally, to make it reusable accross
view mode changes. Being an overlay also allows its visibility to be
externally controlled.

Fixes: https://gitlab.gnome.org/GNOME/nautilus/-/issues/3309

(Commit message originally by Corey Berla)

src/nautilus-files-view.c
@@ -3330,6 +3330,8 @@ connect_inner_view (NautilusFilesView *self)
                                  G_CALLBACK (on_unload_subdirectory), self,
                                  G_CONNECT_DEFAULT);
     }
+
+    nautilus_list_base_add_overlay (priv->list_base, priv->empty_view_page);
 }
 
 static void
@@ -3443,6 +3445,7 @@ nautilus_files_view_finalize (GObject *object)
     view = NAUTILUS_FILES_VIEW (object);
     priv = nautilus_files_view_get_instance_private (view);
 
+    g_clear_object (&priv->empty_view_page);
     g_clear_object (&priv->view_action_group);
     g_clear_object (&priv->background_menu_model);
     g_clear_object (&priv->selection_menu_model);
@@ -3829,12 +3832,13 @@ real_check_empty_states (NautilusFilesView *view)
             adw_status_page_set_description (status_page, NULL);
         }
 
-        gtk_stack_set_visible_child (GTK_STACK (priv->stack), priv->empty_view_page);
+        gtk_widget_set_visible (priv->empty_view_page, TRUE);
+
         nautilus_files_view_display_selection_info (view);
     }
     else
     {
-        gtk_stack_set_visible_child (GTK_STACK (priv->stack), GTK_WIDGET (priv->list_base));
+        gtk_widget_set_visible (priv->empty_view_page, FALSE);
     }
 }
 
@@ -9492,7 +9496,6 @@ nautilus_files_view_class_init (NautilusFilesViewClass *klass)
 
     gtk_widget_class_bind_template_child_private (widget_class, NautilusFilesView, overlay);
     gtk_widget_class_bind_template_child_private (widget_class, NautilusFilesView, stack);
-    gtk_widget_class_bind_template_child_private (widget_class, NautilusFilesView, empty_view_page);
     gtk_widget_class_bind_template_child_private (widget_class, NautilusFilesView, floating_bar);
 
     /* See also the global accelerators in init() in addition to all the local
@@ -9576,6 +9579,12 @@ nautilus_files_view_init (NautilusFilesView *view)
     };
 
     priv = nautilus_files_view_get_instance_private (view);
+
+    /* Own a reference to keep the status page alive even if reparented while
+     * switching view modes. */
+    priv->empty_view_page = g_object_ref_sink (adw_status_page_new ());
+    /* Ensure opaque background, to hide the view underneath it. */
+    gtk_widget_add_css_class (priv->empty_view_page, "view");
 
     priv->model = nautilus_view_model_new ();
     g_signal_connect_object (GTK_SELECTION_MODEL (priv->model),
@@ -9759,6 +9768,9 @@ nautilus_files_view_change (NautilusFilesView *self,
                             guint              id)
 {
     NautilusFilesViewPrivate *priv = nautilus_files_view_get_instance_private (self);
+
+    /* Prepare empty page for reuse. It's not destroyed because we own it. */
+    gtk_widget_unparent (priv->empty_view_page);
 
     /* Destroy existing inner view (which is owned by the stack) */
     gtk_stack_remove (GTK_STACK (priv->stack), GTK_WIDGET (priv->list_base));

src/nautilus-list-base.c
@@ -41,6 +41,7 @@ struct _NautilusListBasePrivate
     NautilusViewModel *model;
     NautilusFile *directory_as_file;
 
+    GtkWidget *overlay;
     GtkWidget *scrolled_window;
 
     gboolean single_click_mode;
@@ -213,6 +214,26 @@ rubberband_set_state (NautilusListBase *self,
     {
         gtk_column_view_set_enable_rubberband (GTK_COLUMN_VIEW (view), enabled);
     }
+}
+
+/**
+ * nautilus_list_base_add_overlay:
+ * @self: a `NautilusListBase` instance
+ * @widget: a `GtkWidget` to be added
+ *
+ * Adds @widget as an overlay to the view. This allows the view event handling,
+ * such as secondary clicks and drops, to work as expected even if the pointer
+ * is on the overlay.
+ *
+ * The primary use case is the empty status page.
+ */
+void
+nautilus_list_base_add_overlay (NautilusListBase *self,
+                                GtkWidget        *widget)
+{
+    NautilusListBasePrivate *priv = nautilus_list_base_get_instance_private (self);
+
+    gtk_overlay_add_overlay (GTK_OVERLAY (priv->overlay), widget);
 }
 
 static void
@@ -1268,7 +1289,10 @@ nautilus_list_base_init (NautilusListBase *self)
     GtkEventController *controller;
 
     priv->scrolled_window = gtk_scrolled_window_new ();
-    adw_bin_set_child (ADW_BIN (self), priv->scrolled_window);
+    priv->overlay = gtk_overlay_new ();
+
+    gtk_overlay_set_child (GTK_OVERLAY (priv->overlay), priv->scrolled_window);
+    adw_bin_set_child (ADW_BIN (self), priv->overlay);
 
     controller = gtk_event_controller_scroll_new (GTK_EVENT_CONTROLLER_SCROLL_VERTICAL);
     gtk_widget_add_controller (priv->scrolled_window, controller);

src/nautilus-list-base.h
@@ -60,6 +60,8 @@ void nautilus_list_base_set_cursor (NautilusListBase *self,
                                     gboolean          scroll_to);
 void    nautilus_list_base_set_model               (NautilusListBase  *self,
                                                     NautilusViewModel *model);
+void    nautilus_list_base_add_overlay             (NautilusListBase  *self,
+                                                    GtkWidget         *widget);
 void    nautilus_list_base_set_sort_state          (NautilusListBase  *self,
                                                     GVariant          *sort_state);
 void    nautilus_list_base_set_zoom_level          (NautilusListBase  *self,

src/resources/ui/nautilus-files-view.ui
@@ -13,13 +13,6 @@
         <child>
           <object class="GtkStack" id="stack">
             <property name="hexpand">True</property>
-            <child>
-              <object class="GtkStackPage">
-                <property name="child">
-                  <object class="AdwStatusPage" id="empty_view_page"/>
-                </property>
-              </object>
-            </child>
           </object>
         </child>
         <child type="overlay">