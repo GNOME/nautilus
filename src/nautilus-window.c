GNOME/nautilus

commit ad865de6181e9e5110988bc92859572fb3160126

window: Replace deprecated toplevel wayland function
Use gdk_wayland_toplevel_drop_exported_handle instead of the
deprecated no-op gdk_wayland_toplevel_unexport_handle. Also
bump the gtk dependency.

src/nautilus-window.c
@@ -2023,7 +2023,8 @@ nautilus_window_unexport_handle (NautilusWindow *window)
         GdkSurface *gdk_surface = gtk_native_get_surface (GTK_NATIVE (window));
         if (GDK_IS_WAYLAND_TOPLEVEL (gdk_surface))
         {
-            gdk_wayland_toplevel_unexport_handle (GDK_WAYLAND_TOPLEVEL (gdk_surface));
+            gdk_wayland_toplevel_drop_exported_handle (GDK_WAYLAND_TOPLEVEL (gdk_surface),
+                                                       window->export_handle);
         }
     }
 #endif

meson.build
@@ -108,7 +108,7 @@ glib = dependency('glib-2.0', version: glib_ver)
 gmodule = dependency('gmodule-no-export-2.0', version: glib_ver)
 gnome_autoar = dependency('gnome-autoar-0', version: '>= 0.4.4')
 gnome_desktop = dependency('gnome-desktop-4', version: '>= 43')
-gtk = dependency('gtk4', version: '>= 4.11.2')
+gtk = dependency('gtk4', version: '>= 4.12.0')
 libadwaita = dependency('libadwaita-1', version: '>= 1.4.alpha')
 libportal = dependency('libportal', version: '>= 0.5')
 libportal_gtk4 = dependency('libportal-gtk4', version: '>= 0.5')

commit af9cda927761987208d1877e9ffd6f8bea8e88aa

window: Drop actual wayland export handle
We've not been dropping handles properly, and we've been getting
warnings from gdk_wayland_toplevel_real_unexport_handle() as such.

This is because we are passing the "wayland:"-prefixed string, but
the actual wayland export handle is only the part after the colon.

Fixes a regression from commit ad865de6181e9e5110988bc92859572fb3160126


(cherry picked from commit 4fc8812040520f40ef01f8b016c02018077e4c23)

src/nautilus-window.c
@@ -2091,10 +2091,11 @@ nautilus_window_unexport_handle (NautilusWindow *window)
     if (GDK_IS_WAYLAND_DISPLAY (gtk_widget_get_display (GTK_WIDGET (window))))
     {
         GdkSurface *gdk_surface = gtk_native_get_surface (GTK_NATIVE (window));
-        if (GDK_IS_WAYLAND_TOPLEVEL (gdk_surface))
+        if (GDK_IS_WAYLAND_TOPLEVEL (gdk_surface) &&
+            g_str_has_prefix (window->export_handle, "wayland:"))
         {
             gdk_wayland_toplevel_drop_exported_handle (GDK_WAYLAND_TOPLEVEL (gdk_surface),
-                                                       window->export_handle);
+                                                       window->export_handle + strlen ("wayland:"));
         }
     }
 #endif