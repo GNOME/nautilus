79463b5fcca5c8a3e525cd12990bbe3206d9a5da
Khalid Abu Shawarib <khalid.shawarib@gmail.com>
2024-10-11 21:39:37 +0300

    pathbar: Remove dir separator in front of some schemes
    And add network icon to network scheme

--- src/nautilus-pathbar.c
+++ src/nautilus-pathbar.c
@@ -57,6 +57,8 @@ typedef enum
     MOUNT_BUTTON,
     TRASH_BUTTON,
     NETWORK_BUTTON,
+    COMPUTER_BUTTON,
+    BURN_BUTTON,
 } ButtonType;
 
 #define BUTTON_DATA(x) ((ButtonData *) (x))
@@ -1074,9 +1076,20 @@ setup_button_type (ButtonData      *button_data,
         button_data->type = TRASH_BUTTON;
         button_data->is_root = TRUE;
     }
-    else if (nautilus_is_root_for_scheme (location, SCHEME_NETWORK_VIEW))
+    else if (nautilus_is_root_for_scheme (location, SCHEME_NETWORK_VIEW) ||
+             nautilus_is_root_for_scheme (location, SCHEME_NETWORK))
     {
         button_data->type = NETWORK_BUTTON;
+        button_data->is_root = TRUE;
+    }
+    else if (nautilus_is_root_for_scheme (location, SCHEME_COMPUTER))
+    {
+        button_data->type = COMPUTER_BUTTON;
+        button_data->is_root = TRUE;
+    }
+    else if (nautilus_is_root_for_scheme (location, SCHEME_BURN))
+    {
+        button_data->type = BURN_BUTTON;
         button_data->is_root = TRUE;
     }
     else
@@ -1217,6 +1230,8 @@ make_button_data (NautilusPathBar *self,
         case RECENT_BUTTON:
         case STARRED_BUTTON:
         case NETWORK_BUTTON:
+        case COMPUTER_BUTTON:
+        case BURN_BUTTON:
         {
             button_data->label = gtk_label_new (NULL);
             child = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 6);
