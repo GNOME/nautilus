0df09cc9861fc6cd89e96bf147f78dc9ae410796
Automeris naranja <104251-automerisnaranja@users.noreply.gitlab.gnome.org>
2024-10-28 22:36:12 -0300

    nautilus-network-address-bar: Port AdwMessageDialog to AdwAlertDialog

--- src/nautilus-network-address-bar.c
+++ src/nautilus-network-address-bar.c
@@ -41,10 +41,12 @@ show_error_message (NautilusNetworkAddressBar *self,
                     const gchar               *primary,
                     const gchar               *secondary)
 {
-    GtkRoot *window = gtk_widget_get_root (GTK_WIDGET (self));
-    GtkWidget *dialog = adw_message_dialog_new (GTK_WINDOW (window), primary, secondary);
-    adw_message_dialog_add_response (ADW_MESSAGE_DIALOG (dialog), "close", _("_Close"));
-    gtk_window_present (GTK_WINDOW (dialog));
+    AdwDialog *dialog;
+
+    dialog = adw_alert_dialog_new (primary, secondary);
+    adw_alert_dialog_add_response (ADW_ALERT_DIALOG (dialog), "close", _("_Close"));
+
+    adw_dialog_present (dialog, GTK_WIDGET (self));
 }
 
 static void
