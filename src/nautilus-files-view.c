e145a4cffd8e46596bb620861e71d6f0df7f2c09
Khalid Abu Shawarib <khalid.shawarib@gmail.com>
2024-11-20 23:10:56 +0300

    new-file-dialog: Add basic dialog structure
    Related: https://gitlab.gnome.org/GNOME/nautilus/-/issues/2205

--- src/nautilus-files-view.c
+++ src/nautilus-files-view.c
@@ -69,6 +69,7 @@
 #include "nautilus-mime-actions.h"
 #include "nautilus-network-view.h"
 #include "nautilus-module.h"
+#include "nautilus-new-file-dialog.h"
 #include "nautilus-new-folder-dialog.h"
 #include "nautilus-previewer.h"
 #include "nautilus-program-choosing.h"
