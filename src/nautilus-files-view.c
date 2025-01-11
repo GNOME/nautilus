df9f141483ea2acf3b8d063a33070510291b7d62
Khalid Abu Shawarib <khalid.shawarib@gmail.com>
2025-01-12 01:19:18 +0300

    files-view: Try activation uri to get a path
    This gives scripts a path for recent files.
    
    (cherry picked from commit 86fa31468381322e72374110207a7156d62b637b)

--- src/nautilus-files-view.c
+++ src/nautilus-files-view.c
@@ -5025,6 +5025,14 @@ get_file_paths_or_uris_as_newline_delimited_string (NautilusFileList *selection,
             }
 
             path = g_filename_from_uri (uri, NULL, NULL);
+
+            if (path == NULL)
+            {
+                g_autofree gchar *activation_uri = nautilus_file_get_activation_uri (file);
+
+                path = g_filename_from_uri (uri, NULL, NULL);
+            }
+
             if (path != NULL)
             {
                 g_string_append (expanding_string, path);
