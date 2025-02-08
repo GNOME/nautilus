GNOME/nautilus

commit 5ebaed57cba99ccab7ad51273d6be16b43df0a95

properties-window: Don't set "Owner" row as insensitive
When they are no options to be selected, the Owner row
is insensitive. However, this makes the row to not be
focusable and Orca can't announce it.

Also, when there are no options, AdwComboRow already
becomes non-activatable.

src/nautilus-properties-window.c
@@ -1854,8 +1854,6 @@ update_owner_row (AdwComboRow     *row,
                                  && nautilus_file_can_set_owner (get_file (self)));
     gboolean had_dropdown = gtk_widget_is_sensitive (GTK_WIDGET (row));
 
-    gtk_widget_set_sensitive (GTK_WIDGET (row), provide_dropdown);
-
     /* check if should provide dropdown */
     if (provide_dropdown)
     {

src/resources/ui/nautilus-properties-window.ui
@@ -559,7 +559,6 @@
                                       <object class="AdwPreferencesGroup">
                                         <child>
                                           <object class="AdwComboRow" id="owner_row">
-                                            <property name="sensitive">False</property>
                                             <property name="activatable">False</property>
                                             <property name="title" translatable="yes">_Owner</property>
                                             <property name="use-underline">True</property>

commit 45852dcbf1b79fabbc4c7132fa2e21c859fd73b0

properties-window: Fix owner change callback
This fixes a regression from 5ebaed57cba99ccab7ad51273d6be16b43df0a95

src/nautilus-properties-window.c
@@ -1795,18 +1795,16 @@ unschedule_or_cancel_owner_change (NautilusPropertiesWindow *self)
 }
 
 static void
-changed_owner_callback (AdwComboRow              *row,
-                        GParamSpec               *pspec,
-                        NautilusPropertiesWindow *self)
+changed_owner_callback (NautilusPropertiesWindow *self)
 {
-    guint selected_pos = adw_combo_row_get_selected (row);
+    guint selected_pos = adw_combo_row_get_selected (self->owner_row);
     g_assert (NAUTILUS_IS_PROPERTIES_WINDOW (self));
 
     if (selected_pos != GTK_INVALID_LIST_POSITION)
     {
         NautilusFile *file = get_file (self);
 
-        GListModel *list = adw_combo_row_get_model (row);
+        GListModel *list = adw_combo_row_get_model (self->owner_row);
         const gchar *selected_owner_str = gtk_string_list_get_string (GTK_STRING_LIST (list), selected_pos);
         gsize owner_name_length = get_first_word_length (selected_owner_str);
         g_autofree gchar *new_owner_name = g_strndup (selected_owner_str, owner_name_length);
@@ -1852,7 +1850,6 @@ update_owner_row (AdwComboRow     *row,
     NautilusPropertiesWindow *self = permissions_info->window;
     gboolean provide_dropdown = (!permissions_info->is_multi_file_window
                                  && nautilus_file_can_set_owner (get_file (self)));
-    gboolean had_dropdown = gtk_widget_is_sensitive (GTK_WIDGET (row));
 
     /* check if should provide dropdown */
     if (provide_dropdown)
@@ -1866,14 +1863,6 @@ update_owner_row (AdwComboRow     *row,
 
         /* display current owner */
         select_ownership_row_entry (row, owner_name, string_list_item_starts_with_word);
-
-        if (!had_dropdown)
-        {
-            /* Update file when selection changes. */
-            g_signal_connect (row, "notify::selected",
-                              G_CALLBACK (changed_owner_callback),
-                              self);
-        }
     }
     else
     {
@@ -1883,8 +1872,6 @@ update_owner_row (AdwComboRow     *row,
         {
             owner_name = g_strdup (_("Multiple"));
         }
-
-        g_signal_handlers_disconnect_by_func (row, G_CALLBACK (changed_owner_callback), self);
 
         ownership_row_set_single_entry (row, owner_name, string_list_item_starts_with_word);
     }
@@ -4170,6 +4157,7 @@ nautilus_properties_window_class_init (NautilusPropertiesWindowClass *klass)
     gtk_widget_class_bind_template_callback (widget_class, navigate_permissions_page);
     gtk_widget_class_bind_template_callback (widget_class, reset_icon);
     gtk_widget_class_bind_template_callback (widget_class, select_image_button_callback);
+    gtk_widget_class_bind_template_callback (widget_class, changed_owner_callback);
     gtk_widget_class_bind_template_callback (widget_class, on_change_permissions_clicked);
 }
 

src/resources/ui/nautilus-properties-window.ui
@@ -559,13 +559,13 @@
                                       <object class="AdwPreferencesGroup">
                                         <child>
                                           <object class="AdwComboRow" id="owner_row">
-                                            <property name="activatable">False</property>
                                             <property name="title" translatable="yes">_Owner</property>
                                             <property name="use-underline">True</property>
                                             <property name="use-markup">False</property>
                                             <property name="expression">
                                               <lookup type="GtkStringObject" name="string"/>
                                             </property>
+                                            <signal name="notify::selected" handler="changed_owner_callback" object="NautilusPropertiesWindow" swapped="yes"/>
                                           </object>
                                         </child>
                                         <child>