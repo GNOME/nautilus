77af98ed10ece438964fe630f72148d588cb32f7
Peter Eisenmann <p3732@getgoogleoff.me>
2025-06-30 15:06:15 +0700

    search-engine: Check providers directly after start
    Some providers finish synchronously, check after starting of all
    providers, whether all providers finished already.

--- src/nautilus-search-engine.c
+++ src/nautilus-search-engine.c
@@ -48,6 +48,7 @@ struct _NautilusSearchEngine
 
     NautilusQuery *query;
     gboolean running;
+    gboolean starting;
     gboolean restart;
 };
 
@@ -70,6 +71,9 @@ enum
 static guint signals[LAST_SIGNAL];
 
 G_DEFINE_FINAL_TYPE (NautilusSearchEngine, nautilus_search_engine, G_TYPE_OBJECT)
+
+static void
+check_providers_status (NautilusSearchEngine *self);
 
 static void
 search_engine_start_provider (NautilusSearchProvider *provider,
@@ -96,10 +100,15 @@ search_engine_start_real (NautilusSearchEngine *self)
 
     self->restart = FALSE;
 
+    self->starting = TRUE;
     search_engine_start_provider (self->localsearch, self);
     search_engine_start_provider (self->model, self);
     search_engine_start_provider (self->recent, self);
     search_engine_start_provider (self->simple, self);
+    self->starting = FALSE;
+
+    /* Providers could already be finished */
+    check_providers_status (self);
 }
 
 void
@@ -208,7 +217,7 @@ check_providers_status (NautilusSearchEngine *self)
 
     num_finished = self->providers_error + self->providers_finished;
 
-    if (num_finished < self->providers_running)
+    if (self->starting || num_finished < self->providers_running)
     {
         return;
     }
