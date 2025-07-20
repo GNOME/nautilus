814df6e51d95d5dea4516ac27c9b90b5598a0397
Peter Eisenmann <p3732@getgoogleoff.me>
2025-07-20 11:34:49 +0800

    search-provider: Claim authorship

--- src/nautilus-search-provider.c
+++ src/nautilus-search-provider.c
@@ -1,19 +1,10 @@
 /*
- *  Copyright (C) 2012 Red Hat, Inc.
+ * Copyright (C) 2012 Red Hat, Inc.
+ * Copyright © 2025 The Files contributors
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Library General Public
- *  License as published by the Free Software Foundation; either
- *  version 2 of the License, or (at your option) any later version.
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Library General Public License for more details.
- *
- *  You should have received a copy of the GNU Library General Public
- *  License along with this library; if not, see <http://www.gnu.org/licenses/>.
- *
+ * Author: Peter Eisenmann <p3732@getgoogleoff.me>
  */
 #define G_LOG_DOMAIN "nautilus-search"
 
