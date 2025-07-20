9a16d473b011544218f2b7480b17740bcf290171
Peter Eisenmann <p3732@getgoogleoff.me>
2025-07-20 11:34:49 +0800

    search-provider: Update license comments

--- src/nautilus-search-engine.h
+++ src/nautilus-search-engine.h
@@ -1,22 +1,11 @@
 /*
  * Copyright (C) 2005 Novell, Inc.
+ * Copyright © 2025 The Files contributors
  *
- * Nautilus is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * Nautilus is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program; see the file COPYING.  If not,
- * see <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: GPL-3.0-or-later
  *
  * Author: Anders Carlsson <andersca@imendio.com>
- *
+ * Author: Peter Eisenmann <p3732@getgoogleoff.me>
  */
 
 #pragma once
