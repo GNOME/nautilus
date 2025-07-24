GNOME/nautilus

commit 2e9d5fe6e083e28007c8da5df142c0e93ea2e5b8

files-view: Drop NautilusView interface
NautilusFilesView was the only implementation of NautilusView, so the
interface only added indirections and code complexity. Some users of
the interface already relied on the implementation being a FilesView.

src/nautilus-application.c
@@ -51,6 +51,7 @@
 #include "nautilus-file-operations.h"
 #include "nautilus-file-undo-manager.h"
 #include "nautilus-file-utilities.h"
+#include "nautilus-files-view.h"
 #include "nautilus-freedesktop-dbus.h"
 #include "nautilus-global-preferences.h"
 #include "nautilus-icon-info.h"
@@ -66,7 +67,6 @@
 #include "nautilus-localsearch-utilities.h"
 #include "nautilus-trash-monitor.h"
 #include "nautilus-ui-utilities.h"
-#include "nautilus-view.h"
 #include "nautilus-window-slot.h"
 #include "nautilus-window.h"
 
@@ -667,14 +667,13 @@ action_clone_window (GSimpleAction *action,
     NautilusWindow *active_window = NULL;
     GtkApplication *application = user_data;
     g_autoptr (GFile) location = NULL;
-    NautilusView *current_view;
+    NautilusFilesView *current_view = nautilus_window_slot_get_current_view (active_slot);
 
     active_window = NAUTILUS_WINDOW (gtk_application_get_active_window (application));
     active_slot = nautilus_window_get_active_slot (active_window);
-    current_view = nautilus_window_slot_get_current_view (active_slot);
 
     if (current_view != NULL &&
-        nautilus_view_is_searching (current_view))
+        nautilus_files_view_is_searching (current_view))
     {
         location = g_file_new_for_path (g_get_home_dir ());
     }

src/meson.build
@@ -133,8 +133,6 @@ libnautilus_sources = [
   'nautilus-toolbar.c',
   'nautilus-toolbar.h',
   'nautilus-toolbar-menu-sections.h',
-  'nautilus-view.c',
-  'nautilus-view.h',
   'nautilus-view-cell.c',
   'nautilus-view-cell.h',
   'nautilus-view-controls.c',

src/nautilus-files-view.h
@@ -1,4 +1,4 @@
-/* nautilus-view.h
+/* nautilus-files-view.h
  *
  * Copyright (C) 1999, 2000  Free Software Foundaton
  * Copyright (C) 2000, 2001  Eazel, Inc.
@@ -29,8 +29,7 @@
 
 #include "nautilus-directory.h"
 #include "nautilus-file.h"
-
-#include "nautilus-view.h"
+#include "nautilus-types.h"
 #include "nautilus-window-slot.h"
 
 G_BEGIN_DECLS
@@ -113,6 +112,9 @@ struct _NautilusFilesViewClass {
 
 NautilusFilesView *      nautilus_files_view_new                         (guint               id,
                                                                           NautilusWindowSlot *slot);
+
+guint
+nautilus_files_view_get_view_id (NautilusFilesView *self);
 void                nautilus_files_view_change                           (NautilusFilesView  *self,
                                                                           guint               id);
 
@@ -120,6 +122,29 @@ const char *
 nautilus_files_view_get_toggle_icon_name (NautilusFilesView *self);
 const char *
 nautilus_files_view_get_toggle_tooltip (NautilusFilesView *self);
+
+GFile *
+nautilus_files_view_get_location (NautilusFilesView *self);
+void
+nautilus_files_view_set_location (NautilusFilesView *self,
+                                  GFile             *location);
+
+NautilusQuery *
+nautilus_files_view_get_search_query (NautilusFilesView *self);
+void
+nautilus_files_view_set_search_query (NautilusFilesView *self,
+                                      NautilusQuery     *query);
+
+GList *
+nautilus_files_view_get_selection (NautilusFilesView *self);
+void
+nautilus_files_view_set_selection (NautilusFilesView *self,
+                                   GList             *selection);
+
+gboolean
+nautilus_files_view_is_loading (NautilusFilesView *self);
+gboolean
+nautilus_files_view_is_searching (NautilusFilesView *self);
 
 /* Wrappers for signal emitters. These are normally called
  * only by NautilusFilesView itself. They have corresponding signals
@@ -162,6 +187,9 @@ void              nautilus_files_view_activate_selection         (NautilusFilesV
 void              nautilus_files_view_preview_selection_event    (NautilusFilesView      *view,
                                                                   GtkDirectionType        direction);
 void              nautilus_files_view_stop_loading               (NautilusFilesView      *view);
+
+NautilusToolbarMenuSections *
+nautilus_files_view_get_toolbar_menu_sections (NautilusFilesView *self);
 
 void              nautilus_files_view_update_context_menus       (NautilusFilesView      *view);
 void              nautilus_files_view_update_toolbar_menus       (NautilusFilesView      *view);

src/nautilus-pathbar.c
@@ -29,6 +29,7 @@
 #include "nautilus-enums.h"
 #include "nautilus-enum-types.h"
 #include "nautilus-file.h"
+#include "nautilus-files-view.h"
 #include "nautilus-file-utilities.h"
 #include "nautilus-global-preferences.h"
 #include "nautilus-icon-names.h"
@@ -882,7 +883,7 @@ on_drag_drop (GtkDropTarget *target,
     }
 
     target_location = nautilus_file_get_location (button_data->file);
-    target_view = (NautilusFilesView *) nautilus_window_slot_get_current_view (self->slot);
+    target_view = nautilus_window_slot_get_current_view (self->slot);
     action = gdk_drop_get_actions (gtk_drop_target_get_current_drop (target));
 
     #ifdef GDK_WINDOWING_X11

src/nautilus-previewer.c
@@ -353,16 +353,16 @@ previewer_selection_event (GDBusConnection *connection,
         return;
     }
 
-    NautilusView *view = nautilus_window_slot_get_current_view (current_slot);
+    NautilusFilesView *view = nautilus_window_slot_get_current_view (current_slot);
     GtkDirectionType direction;
 
-    if (!NAUTILUS_IS_FILES_VIEW (view))
+    if (view == NULL)
     {
         return;
     }
 
     g_variant_get (parameters, "(u)", &direction);
-    nautilus_files_view_preview_selection_event (NAUTILUS_FILES_VIEW (view), direction);
+    nautilus_files_view_preview_selection_event (view, direction);
 }
 
 void

src/nautilus-types.h
@@ -43,6 +43,5 @@ typedef struct  NautilusMonitor             NautilusMonitor;
 typedef struct _NautilusQuery               NautilusQuery;
 typedef struct _NautilusQueryEditor         NautilusQueryEditor;
 typedef struct _NautilusToolbarMenuSections NautilusToolbarMenuSections;
-typedef struct _NautilusView                NautilusView;
 typedef struct _NautilusWindow              NautilusWindow;
 typedef struct _NautilusWindowSlot          NautilusWindowSlot;

src/nautilus-view.c
@@ -1,198 +1,0 @@
-/* nautilus-view.c
- *
- * Copyright (C) 2015 Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include "config.h"
-
-#include "nautilus-view.h"
-#include <glib/gi18n.h>
-
-G_DEFINE_INTERFACE (NautilusView, nautilus_view, GTK_TYPE_WIDGET)
-
-static void
-nautilus_view_default_init (NautilusViewInterface *iface)
-{
-}
-
-/**
- * nautilus_view_get_view_id:
- * @view: a #NautilusView
- *
- * Retrieves the view id that represents the @view type.
- *
- * Returns: a guint representing the view type
- */
-guint
-nautilus_view_get_view_id (NautilusView *view)
-{
-    g_return_val_if_fail (NAUTILUS_VIEW_GET_IFACE (view)->get_view_id, NAUTILUS_VIEW_INVALID_ID);
-
-    return NAUTILUS_VIEW_GET_IFACE (view)->get_view_id (view);
-}
-
-/**
- * nautilus_view_get_toolbar_menu_sections:
- * @view: a #NautilusView
- *
- * Retrieves the menu sections to show in the main toolbar menu when this view
- * is active
- *
- * Returns: (transfer none): a #NautilusToolbarMenuSections with the sections to
- * be displayed
- */
-NautilusToolbarMenuSections *
-nautilus_view_get_toolbar_menu_sections (NautilusView *view)
-{
-    g_return_val_if_fail (NAUTILUS_VIEW_GET_IFACE (view)->get_toolbar_menu_sections, NULL);
-
-    return NAUTILUS_VIEW_GET_IFACE (view)->get_toolbar_menu_sections (view);
-}
-
-/**
- * nautilus_view_get_search_query:
- * @view: a #NautilusView
- *
- * Retrieves the current current location of @view.
- *
- * Returns: (transfer none): a #GFile
- */
-GFile *
-nautilus_view_get_location (NautilusView *view)
-{
-    g_return_val_if_fail (NAUTILUS_VIEW_GET_IFACE (view)->get_location, NULL);
-
-    return NAUTILUS_VIEW_GET_IFACE (view)->get_location (view);
-}
-
-/**
- * nautilus_view_set_location:
- * @view: a #NautilusView
- * @location: the location displayed by @view
- *
- * Sets the location of @view.
- *
- * Returns:
- */
-void
-nautilus_view_set_location (NautilusView *view,
-                            GFile        *location)
-{
-    g_return_if_fail (NAUTILUS_VIEW_GET_IFACE (view)->set_location);
-
-    NAUTILUS_VIEW_GET_IFACE (view)->set_location (view, location);
-}
-
-/**
- * nautilus_view_get_selection:
- * @view: a #NautilusView
- *
- * Get the current selection of the view.
- *
- * Returns: (transfer full) (type GFile): a newly allocated list
- * of the currently selected files.
- */
-GList *
-nautilus_view_get_selection (NautilusView *view)
-{
-    g_return_val_if_fail (NAUTILUS_VIEW_GET_IFACE (view)->get_selection, NULL);
-
-    return NAUTILUS_VIEW_GET_IFACE (view)->get_selection (view);
-}
-
-/**
- * nautilus_view_set_selection:
- * @view: a #NautilusView
- * @selection: (nullable): a list of files
- *
- * Sets the current selection of the view.
- *
- * Returns:
- */
-void
-nautilus_view_set_selection (NautilusView *view,
-                             GList        *selection)
-{
-    g_return_if_fail (NAUTILUS_VIEW_GET_IFACE (view)->set_selection);
-
-    NAUTILUS_VIEW_GET_IFACE (view)->set_selection (view, selection);
-}
-
-/**
- * nautilus_view_get_search_query:
- * @view: a #NautilusView
- *
- * Retrieves the current search query displayed by @view.
- *
- * Returns: (transfer none): a #
- */
-NautilusQuery *
-nautilus_view_get_search_query (NautilusView *view)
-{
-    g_return_val_if_fail (NAUTILUS_VIEW_GET_IFACE (view)->get_search_query, NULL);
-
-    return NAUTILUS_VIEW_GET_IFACE (view)->get_search_query (view);
-}
-
-/**
- * nautilus_view_set_search_query:
- * @view: a #NautilusView
- * @query: the search query to be performed, or %NULL
- *
- * Sets the current search query performed by @view.
- *
- * Returns:
- */
-void
-nautilus_view_set_search_query (NautilusView  *view,
-                                NautilusQuery *query)
-{
-    g_return_if_fail (NAUTILUS_VIEW_GET_IFACE (view)->set_search_query);
-
-    NAUTILUS_VIEW_GET_IFACE (view)->set_search_query (view, query);
-}
-
-/**
- * nautilus_view_is_loading:
- * @view: a #NautilusView
- *
- * Whether @view is loading the current location.
- *
- * Returns: %TRUE if @view is loading, %FALSE otherwise.
- */
-gboolean
-nautilus_view_is_loading (NautilusView *view)
-{
-    g_return_val_if_fail (NAUTILUS_VIEW_GET_IFACE (view)->is_loading, FALSE);
-
-    return NAUTILUS_VIEW_GET_IFACE (view)->is_loading (view);
-}
-
-/**
- * nautilus_view_is_searching:
- * @view: a #NautilusView
- *
- * Whether @view is searching.
- *
- * Returns: %TRUE if @view is searching, %FALSE otherwise.
- */
-gboolean
-nautilus_view_is_searching (NautilusView *view)
-{
-    g_return_val_if_fail (NAUTILUS_VIEW_GET_IFACE (view)->is_searching, FALSE);
-
-    return NAUTILUS_VIEW_GET_IFACE (view)->is_searching (view);
-}

commit e2a398e51ce7bd73260b18cf609cc66b34c7d476

application: Fix new window variable order
This fixes a regression from https://gitlab.gnome.org/GNOME/nautilus/-/commit/2e9d5fe6e083e28007c8da5df142c0e93ea2e5b8#20a7dcb46ec25462c41c8417e1c413d681a17313_671_670

Fixes https://gitlab.gnome.org/GNOME/nautilus/-/issues/3918

src/nautilus-application.c
@@ -663,14 +663,11 @@ action_clone_window (GSimpleAction *action,
                      GVariant      *parameter,
                      gpointer       user_data)
 {
-    NautilusWindowSlot *active_slot = NULL;
-    NautilusWindow *active_window = NULL;
     GtkApplication *application = user_data;
-    g_autoptr (GFile) location = NULL;
+    NautilusWindow *active_window = NAUTILUS_WINDOW (gtk_application_get_active_window (application));
+    NautilusWindowSlot *active_slot = nautilus_window_get_active_slot (active_window);
     NautilusFilesView *current_view = nautilus_window_slot_get_current_view (active_slot);
-
-    active_window = NAUTILUS_WINDOW (gtk_application_get_active_window (application));
-    active_slot = nautilus_window_get_active_slot (active_window);
+    g_autoptr (GFile) location = NULL;
 
     if (current_view != NULL &&
         nautilus_files_view_is_searching (current_view))