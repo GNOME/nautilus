521a4bb4157b49927a458e0c0fb6862a23e7b698
Peter Eisenmann <p3732@getgoogleoff.me>
2025-07-28 14:40:26 +0200

    bookmark-list: Simplify custom name handling
    Code comments point to this being for compatibility, but this is
    de facto the only way custom bookmark names are stored. Clarify
    this and simplify the code.

--- src/nautilus-bookmark-list.c
+++ src/nautilus-bookmark-list.c
@@ -71,22 +71,6 @@ nautilus_bookmark_list_error_quark (void)
     return g_quark_from_static_string ("nautilus-bookmark-list-error-quark");
 }
 
-static NautilusBookmark *
-new_bookmark_from_uri (const char *uri,
-                       const char *label)
-{
-    NautilusBookmark *new_bookmark = NULL;
-    g_autoptr (GFile) location = NULL;
-
-    if (uri)
-    {
-        location = g_file_new_for_uri (uri);
-        new_bookmark = nautilus_bookmark_new (location, label);
-    }
-
-    return new_bookmark;
-}
-
 static GFile *
 nautilus_bookmark_list_get_file (void)
 {
@@ -459,11 +443,8 @@ load_callback (GObject      *source_object,
 {
     NautilusBookmarkList *self = NAUTILUS_BOOKMARK_LIST (source_object);
     g_autoptr (GError) error = NULL;
-    g_autofree gchar *contents = NULL;
-    char **lines;
-    int i;
+    g_autofree gchar *contents = g_task_propagate_pointer (G_TASK (res), &error);
 
-    contents = g_task_propagate_pointer (G_TASK (res), &error);
     if (error != NULL)
     {
         g_warning ("Unable to get contents of the bookmarks file: %s",
@@ -472,27 +453,32 @@ load_callback (GObject      *source_object,
         return;
     }
 
-    lines = g_strsplit (contents, "\n", -1);
-    for (i = 0; lines[i]; i++)
+    char **lines = g_strsplit (contents, "\n", -1);
+    for (guint i = 0; lines[i]; i++)
     {
+        char *uri = lines[i];
+
         /* Ignore empty or invalid lines that cannot be parsed properly */
-        if (lines[i][0] != '\0' && lines[i][0] != ' ')
+        if (uri[0] == '\0' || g_unichar_isspace (uri[0]))
         {
-            /* gtk 2.7/2.8 might have labels appended to bookmarks which are separated by a space
-             * we must separate the bookmark URI and the potential label.
-             */
-            char *space;
-            g_autofree char *label = NULL;
-
-            space = strchr (lines[i], ' ');
-            if (space)
-            {
-                *space = '\0';
-                label = g_strdup (space + 1);
-            }
-
-            insert_bookmark_internal (self, new_bookmark_from_uri (lines[i], label), -1);
+            continue;
         }
+
+        /* Split bookmarks file entries into URI and label, which are separated by
+         * a space. This behavior is used since GTK 2.7's file chooser.
+         */
+        char *label = NULL;
+        char *space = strchr (uri, ' ');
+        if (space != NULL)
+        {
+            *space = '\0';
+            label = space + 1;
+        }
+
+        g_autoptr (GFile) location = g_file_new_for_uri (uri);
+        NautilusBookmark *new_bookmark = nautilus_bookmark_new (location, label);
+
+        insert_bookmark_internal (self, new_bookmark, -1);
     }
 
     g_signal_emit (self, signals[CHANGED], 0);
@@ -624,11 +610,7 @@ static void
 save_file_async (NautilusBookmarkList *self)
 {
     g_autoptr (GTask) task = NULL;
-    GString *bookmark_string;
-    gchar *contents;
-    GList *l;
-
-    bookmark_string = g_string_new (NULL);
+    GString *bookmark_string = g_string_new (NULL);
 
     /* temporarily disable bookmark file monitoring when writing file */
     if (self->monitor != NULL)
@@ -637,38 +619,21 @@ save_file_async (NautilusBookmarkList *self)
         g_clear_object (&self->monitor);
     }
 
-    for (l = self->list; l; l = l->next)
+    for (GList *l = self->list; l != NULL; l = l->next)
     {
-        NautilusBookmark *bookmark;
+        NautilusBookmark *bookmark = NAUTILUS_BOOKMARK (l->data);
 
-        bookmark = NAUTILUS_BOOKMARK (l->data);
+        /* Store bookmark URI followed by label */
+        g_autofree char *uri = nautilus_bookmark_get_uri (bookmark);
+        const char *label = nautilus_bookmark_get_name (bookmark);
 
-        /* make sure we save label if it has one for compatibility with GTK 2.7 and 2.8 */
-        if (nautilus_bookmark_get_has_custom_name (bookmark))
-        {
-            const char *label;
-            g_autofree char *uri = NULL;
-
-            label = nautilus_bookmark_get_name (bookmark);
-            uri = nautilus_bookmark_get_uri (bookmark);
-
-            g_string_append_printf (bookmark_string,
-                                    "%s %s\n", uri, label);
-        }
-        else
-        {
-            g_autofree char *uri = NULL;
-
-            uri = nautilus_bookmark_get_uri (bookmark);
-
-            g_string_append_printf (bookmark_string, "%s\n", uri);
-        }
+        g_string_append_printf (bookmark_string, "%s %s\n", uri, label);
     }
 
     task = g_task_new (G_OBJECT (self),
                        NULL,
                        save_callback, NULL);
-    contents = g_string_free (bookmark_string, FALSE);
+    gchar *contents = g_string_free_and_steal (bookmark_string);
     g_task_set_task_data (task, contents, g_free);
 
     g_task_run_in_thread (task, save_io_thread);
