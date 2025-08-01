221b4d794dc60b3bfe1969e2d42a0a64e6458215
Peter Eisenmann <p3732@getgoogleoff.me>
2025-08-01 09:43:15 +0200

    gtk/sidebar: Modernize headers somewhat
    Use pragma once and GLib macros, also add a file history comment.

--- src/gtk/nautilusgtksidebarrow.c
+++ src/gtk/nautilusgtksidebarrow.c
@@ -1,20 +1,9 @@
-/* nautilusgtksidebarrow.c
- *
+/*
  * Copyright (C) 2015 Carlos Soriano <csoriano@gnome.org>
  *
- * This file is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of the
- * License, or (at your option) any later version.
- *
- * This file is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
+#include "nautilusgtksidebarrowprivate.h"
 
 #include "config.h"
 #include <glib/gi18n.h>
@@ -22,7 +11,6 @@
 #include "nautilus-enum-types.h"
 #include "nautilus-file.h"
 
-#include "nautilusgtksidebarrowprivate.h"
 /* For section and place type enums */
 #include "nautilusgtkplacessidebarprivate.h"
 
