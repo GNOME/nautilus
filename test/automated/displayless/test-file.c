32a20bf97f5c48bdcf68f4706470ac6d6ce152d0
Khalid Abu Shawarib <khalid.shawarib@gmail.com>
2025-09-20 00:09:41 +0300

    test/file: Add tests for permissions

--- test/automated/displayless/test-file.c
+++ test/automated/displayless/test-file.c
@@ -3,6 +3,7 @@
 #include <nautilus-directory-private.h>
 #include <nautilus-file.h>
 #include <nautilus-file-private.h>
+#include <nautilus-file-undo-manager.h>
 #include <nautilus-file-utilities.h>
 #include <nautilus-file-undo-manager.h>
 #include <test-utilities.h>
@@ -282,15 +283,141 @@ test_file_batch_rename_different_folders (void)
     }
 }
 
+static void
+test_file_permissions_can_set (void)
+{
+    g_autoptr (GFile) location = g_file_new_build_filename (test_get_tmp_dir (),
+                                                            "perm_test_file",
+                                                            NULL);
+    g_autoptr (GFileOutputStream) stream = g_file_create (location, G_FILE_CREATE_NONE, NULL, NULL);
+    gint mode = 0600;
+
+    g_assert_nonnull (stream);
+    g_assert_true (g_output_stream_close (G_OUTPUT_STREAM (stream), NULL, NULL));
+    g_assert_true (g_file_set_attribute_uint32 (location,
+                                                G_FILE_ATTRIBUTE_UNIX_MODE, mode,
+                                                G_FILE_QUERY_INFO_NONE, NULL, NULL));
+
+    g_autoptr (NautilusFile) file = nautilus_file_get (location);
+
+    file_load_attributes (file, NAUTILUS_FILE_ATTRIBUTE_INFO);
+    g_assert_true (nautilus_file_can_set_permissions (file));
+
+    test_clear_tmp_dir ();
+}
+
+typedef struct
+{
+    NautilusFile *file;
+    gboolean done;
+} PermissionTestData;
+
+static void
+file_changed_set_cb (NautilusFile *file,
+                     gboolean     *done)
+{
+    *done = TRUE;
+}
+
+static void
+permissions_success_cb (NautilusFile *file,
+                        GFile        *result_location,
+                        GError       *error,
+                        gpointer      user_data)
+{
+    PermissionTestData *perm_data = user_data;
+
+    g_assert_no_error (error);
+    g_assert_true (file == perm_data->file);
+
+    perm_data->done = TRUE;
+}
+
+static void
+test_file_permissions_set_basic (void)
+{
+    g_autoptr (GFile) location = g_file_new_build_filename (test_get_tmp_dir (),
+                                                            "perm_test_file",
+                                                            NULL);
+    g_autoptr (GFileOutputStream) stream = g_file_create (location, G_FILE_CREATE_NONE, NULL, NULL);
+
+    g_assert_nonnull (stream);
+    g_assert_true (g_output_stream_close (G_OUTPUT_STREAM (stream), NULL, NULL));
+
+    g_autoptr (NautilusFile) file = nautilus_file_get (location);
+    PermissionTestData perm_data = { file, FALSE };
+    gboolean file_changed = FALSE;
+    guint32 perms = 0600;
+
+    file_load_attributes (file, NAUTILUS_FILE_ATTRIBUTE_INFO);
+    g_signal_connect (file, "changed", G_CALLBACK (file_changed_set_cb), &file_changed);
+
+    nautilus_file_set_permissions (file, perms, permissions_success_cb, &perm_data);
+    ITER_CONTEXT_WHILE (!perm_data.done);
+
+    g_autoptr (GFileInfo) info = g_file_query_info (location, G_FILE_ATTRIBUTE_UNIX_MODE,
+                                                    G_FILE_QUERY_INFO_NONE, NULL, NULL);
+
+    g_assert_nonnull (info);
+    g_assert_true (g_file_info_has_attribute (info, G_FILE_ATTRIBUTE_UNIX_MODE));
+
+    guint32 mode = g_file_info_get_attribute_uint32 (info, G_FILE_ATTRIBUTE_UNIX_MODE);
+
+    g_assert_cmphex (mode & 0777, ==, perms);
+    g_assert_true (file_changed);
+
+    test_clear_tmp_dir ();
+}
+
+static void
+test_file_permissions_set_same (void)
+{
+    g_autoptr (GFile) location = g_file_new_build_filename (test_get_tmp_dir (),
+                                                            "perm_test_file",
+                                                            NULL);
+    g_autoptr (GFileOutputStream) stream = g_file_create (location, G_FILE_CREATE_NONE, NULL, NULL);
+
+    g_assert_nonnull (stream);
+    g_assert_true (g_output_stream_close (G_OUTPUT_STREAM (stream), NULL, NULL));
+
+    g_autoptr (NautilusFile) file = nautilus_file_get (location);
+    gboolean file_changed = FALSE;
+    g_autoptr (GFileInfo) info_before = g_file_query_info (location, G_FILE_ATTRIBUTE_UNIX_MODE,
+                                                           G_FILE_QUERY_INFO_NONE, NULL, NULL);
+    guint32 mode_before = g_file_info_get_attribute_uint32 (info_before, G_FILE_ATTRIBUTE_UNIX_MODE);
+    PermissionTestData perm_data = { file, FALSE };
+
+    file_load_attributes (file, NAUTILUS_FILE_ATTRIBUTE_INFO);
+    g_signal_connect (file, "changed", G_CALLBACK (file_changed_set_cb), &file_changed);
+
+    nautilus_file_set_permissions (file, mode_before, permissions_success_cb, &perm_data);
+    ITER_CONTEXT_WHILE (!perm_data.done);
+
+    g_autoptr (GFileInfo) info_after = g_file_query_info (location, G_FILE_ATTRIBUTE_UNIX_MODE,
+                                                          G_FILE_QUERY_INFO_NONE, NULL, NULL);
+
+    g_assert_nonnull (info_after);
+    g_assert_true (g_file_info_has_attribute (info_after, G_FILE_ATTRIBUTE_UNIX_MODE));
+
+    guint32 mode_after = g_file_info_get_attribute_uint32 (info_after, G_FILE_ATTRIBUTE_UNIX_MODE);
+
+    g_assert_cmphex (mode_after, ==, mode_before);
+    g_assert_false (file_changed);
+
+    test_clear_tmp_dir ();
+}
+
 int
 main (int   argc,
       char *argv[])
 {
-    g_autoptr (NautilusFileUndoManager) undo_manager = nautilus_file_undo_manager_new ();
+    g_autoptr (NautilusFileUndoManager) undo_manager = NULL;
 
     g_test_init (&argc, &argv, NULL);
     g_test_set_nonfatal_assertions ();
     nautilus_ensure_extension_points ();
+
+    undo_manager = nautilus_file_undo_manager_new ();
 
     g_test_add_func ("/file-refcount/single-file",
                      test_file_refcount_single_file);
@@ -312,6 +439,12 @@ main (int   argc,
                      test_file_batch_rename_replace);
     g_test_add_func ("/file-batch-rename/different-folders",
                      test_file_batch_rename_different_folders);
+    g_test_add_func ("/file/permissions/can_set",
+                     test_file_permissions_can_set);
+    g_test_add_func ("/file/permissions/basic",
+                     test_file_permissions_set_basic);
+    g_test_add_func ("/file/permissions/same",
+                     test_file_permissions_set_same);
 
     return g_test_run ();
 }
