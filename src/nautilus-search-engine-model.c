7b91cf0acbc76b34edf99324856a5ad499a8eb7c
Peter Eisenmann <p3732@getgoogleoff.me>
2025-11-20 10:58:01 +0100

    Revert "search-engine-model: Drop custom thread deduplication"
    This reverts commit 2c98e12de81a8a816e78154463b2bc84b85d9902.

--- src/nautilus-search-engine-model.c
+++ src/nautilus-search-engine-model.c
@@ -44,6 +44,7 @@ struct _NautilusSearchEngineModel
     GPtrArray *hits;
     NautilusDirectory *directory;
 
+    gboolean query_pending;
     guint finished_id;
 };
 
@@ -88,6 +89,8 @@ search_finished (NautilusSearchEngineModel *model)
         nautilus_search_provider_hits_added (NAUTILUS_SEARCH_PROVIDER (model),
                                              g_steal_pointer (&hits));
     }
+
+    model->query_pending = FALSE;
 
     g_debug ("Model engine finished");
     nautilus_search_provider_finished (NAUTILUS_SEARCH_PROVIDER (model));
@@ -231,6 +234,10 @@ search_engine_model_start (NautilusSearchProvider *provider,
     g_autoptr (NautilusDirectory) directory = nautilus_directory_get (query_location);
     g_set_object (&model->directory, directory);
 
+    if (model->query_pending)
+    {
+        return FALSE;
+    }
     if (model->directory == NULL)
     {
         return FALSE;
@@ -239,6 +246,7 @@ search_engine_model_start (NautilusSearchProvider *provider,
     g_debug ("Model engine start");
 
     g_object_ref (model);
+    model->query_pending = TRUE;
 
     nautilus_directory_call_when_ready (model->directory,
                                         NAUTILUS_FILE_ATTRIBUTE_INFO,
@@ -254,11 +262,14 @@ nautilus_search_engine_model_stop (NautilusSearchProvider *provider)
 
     model = NAUTILUS_SEARCH_ENGINE_MODEL (provider);
 
-    g_debug ("Model engine stop");
+    if (model->query_pending)
+    {
+        g_debug ("Model engine stop");
 
-    nautilus_directory_cancel_callback (model->directory,
-                                        model_directory_ready_cb, model);
-    search_finished_idle (model);
+        nautilus_directory_cancel_callback (model->directory,
+                                            model_directory_ready_cb, model);
+        search_finished_idle (model);
+    }
 
     g_clear_object (&model->directory);
 }
