b699ea511e6deac81e29fc7e1026a4ae4108f5a1
Khalid Abu Shawarib <khalid.shawarib@gmail.com>
2026-01-31 22:46:58 +0300

    templates-dialog: Changes for the dialog box method
    The current implementation of the “New Documents” submenu is a GtkPopoverMenu.
    It uses the GMenuModel, which is added as submenus in the right-click popover
    menu. This implementation has many long-standing issues, as mentioned here
    https://gitlab.gnome.org/GNOME/nautilus/-/issues/2205, so we plan to move away
    from it by using a dialog box for creating new templates.
    
    The dialog box makes it possible to view the whole list of templates in a
    single view, gives users the ability to search for templates, and makes it
    easier for everyone to use the template creation feature!

--- src/nautilus-templates-dialog.c
+++ src/nautilus-templates-dialog.c
@@ -22,14 +22,41 @@
 
 #include <glib/gi18n.h>
 
+#include "nautilus-directory.h"
 #include "nautilus-file-operations.h"
+#include "nautilus-file.h"
+#include "nautilus-file-utilities.h"
+#include "nautilus-global-preferences.h"
+
+#define MAX_MENU_LEVELS 5
+#define TEMPLATE_LIMIT 30
 
 struct _NautilusTemplatesDialog
 {
     AdwDialog parent_instance;
+
+    GtkWidget *templates_list_box;
+    GtkWidget *templates_search_entry;
+
+    GList *templates_directory_list;
+    GtkTreeListModel *templates_list_model;
+    GtkStack *templates_stack;
+    GtkFilterListModel *filter_model;
+    GtkCustomFilter *filter;
+
+    guint update_timeout_id;
+
+    gboolean show_hidden_files;
 };
 
 G_DEFINE_TYPE (NautilusTemplatesDialog, nautilus_templates_dialog, ADW_TYPE_DIALOG)
+
+/* Forward declarations */
+static void update_templates_menu (NautilusTemplatesDialog *self);
+
+static void schedule_update (NautilusTemplatesDialog *self);
+
+static void remove_update_timeout_callback (NautilusTemplatesDialog *self);
 
 static void
 on_import_response (GtkFileDialog           *chooser_dialog,
@@ -49,6 +76,8 @@ on_import_response (GtkFileDialog           *chooser_dialog,
                                              templates_location,
                                              GTK_WINDOW (gtk_widget_get_root (GTK_WIDGET (self))),
                                              NULL, NULL, NULL);
+
+        schedule_update (self);
     }
 }
 
@@ -64,6 +93,429 @@ nautilus_templates_dialog_import_template_dialog (NautilusTemplatesDialog *self 
                           NULL, (GAsyncReadyCallback) on_import_response, self);
 }
 
+static void
+templates_added_or_changed_callback (NautilusDirectory *directory,
+                                     GList             *files,
+                                     gpointer           callback_data)
+{
+    NautilusTemplatesDialog *self = NAUTILUS_TEMPLATES_DIALOG (callback_data);
+
+    schedule_update (self);
+}
+
+static void
+add_directory_to_directory_list (NautilusTemplatesDialog  *self,
+                                 NautilusDirectory        *directory,
+                                 GList                   **directory_list,
+                                 GCallback                 changed_callback)
+{
+    if (g_list_find (*directory_list, directory) != NULL)
+    {
+        return;
+    }
+
+    NautilusAttributes attributes =
+        NAUTILUS_ATTRIBUTE_INFO |
+        NAUTILUS_ATTRIBUTE_THUMBNAIL_BUFFER |
+        NAUTILUS_ATTRIBUTE_DIRECTORY_ITEM_COUNT;
+
+    nautilus_directory_ref (directory);
+    nautilus_directory_file_monitor_add (directory, directory_list,
+                                         FALSE, attributes,
+                                         (NautilusDirectoryCallback) changed_callback, self);
+
+    g_signal_connect_object (directory, "files-added",
+                             G_CALLBACK (changed_callback), self, G_CONNECT_DEFAULT);
+    g_signal_connect_object (directory, "files-changed",
+                             G_CALLBACK (changed_callback), self, G_CONNECT_DEFAULT);
+
+    *directory_list = g_list_append (*directory_list, directory);
+}
+
+static void
+remove_directory_from_directory_list (NautilusTemplatesDialog  *self,
+                                      NautilusDirectory        *directory,
+                                      GList                   **directory_list,
+                                      GCallback                 changed_callback)
+{
+    *directory_list = g_list_remove (*directory_list, directory);
+
+    g_signal_handlers_disconnect_by_func (directory,
+                                          G_CALLBACK (changed_callback),
+                                          self);
+
+    nautilus_directory_file_monitor_remove (directory, directory_list);
+
+    nautilus_directory_unref (directory);
+}
+
+static void
+add_directory_to_templates_directory_list (NautilusTemplatesDialog *self,
+                                           NautilusDirectory       *directory)
+{
+    add_directory_to_directory_list (self, directory,
+                                     &self->templates_directory_list,
+                                     G_CALLBACK (templates_added_or_changed_callback));
+}
+
+static void
+remove_directory_from_templates_directory_list (NautilusTemplatesDialog *self,
+                                                NautilusDirectory       *directory)
+{
+    remove_directory_from_directory_list (self, directory,
+                                          &self->templates_directory_list,
+                                          G_CALLBACK (templates_added_or_changed_callback));
+}
+
+static void
+nautilus_templates_dialog_dispose (GObject *object)
+{
+    NautilusTemplatesDialog *self = NAUTILUS_TEMPLATES_DIALOG (object);
+
+    /* Add other cleanup statements here*/
+    remove_update_timeout_callback (self);
+
+    g_clear_list (&self->templates_directory_list, g_object_unref);
+
+    g_clear_object (&self->templates_list_model);
+
+    G_OBJECT_CLASS (nautilus_templates_dialog_parent_class)->dispose (object);
+}
+
+static void
+remove_update_timeout_callback (NautilusTemplatesDialog *self)
+{
+    g_clear_handle_id (&self->update_timeout_id, g_source_remove);
+}
+
+static gboolean
+update_timeout_callback (gpointer data)
+{
+    NautilusTemplatesDialog *self = NAUTILUS_TEMPLATES_DIALOG (data);
+
+    g_object_ref (G_OBJECT (self));
+
+    self->update_timeout_id = 0;
+    update_templates_menu (self);
+
+    g_object_unref (G_OBJECT (self));
+
+    return FALSE;
+}
+
+static gboolean
+directory_belongs_in_templates_menu (const char *templates_directory_uri,
+                                     const char *uri)
+{
+    int num_levels;
+    int i;
+
+    if (templates_directory_uri == NULL)
+    {
+        return FALSE;
+    }
+
+    if (!g_str_has_prefix (uri, templates_directory_uri))
+    {
+        return FALSE;
+    }
+
+    num_levels = 0;
+    for (i = strlen (templates_directory_uri); uri[i] != '\0'; i++)
+    {
+        if (uri[i] == '/')
+        {
+            num_levels++;
+        }
+    }
+
+    if (num_levels > MAX_MENU_LEVELS)
+    {
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+static gboolean
+filter_templates_callback (NautilusFile *file,
+                           gpointer      callback_data)
+{
+    /*
+     * We want to show hidden files, but not directories. This is a compromise
+     * to allow creating hidden files but to prevent content from .git directory
+     * for example. See https://gitlab.gnome.org/GNOME/nautilus/issues/1413.
+     */
+    NautilusTemplatesDialog *self = callback_data;
+
+    if (nautilus_file_is_hidden_file (file))
+    {
+        if (!self->show_hidden_files)
+        {
+            return FALSE;
+        }
+
+        if (nautilus_file_is_directory (file))
+        {
+            return FALSE;
+        }
+    }
+
+    return TRUE;
+}
+
+static GListStore *
+update_directory_in_templates_menu (NautilusTemplatesDialog *self,
+                                    NautilusDirectory       *directory)
+{
+    g_return_val_if_fail (NAUTILUS_IS_DIRECTORY (directory), NULL);
+
+    g_autolist (NautilusFile) file_list = nautilus_directory_get_file_list (directory);
+    int num = 0;
+    GListStore *store = g_list_store_new (NAUTILUS_TYPE_FILE);
+    gboolean any_templates = FALSE;
+
+    file_list = nautilus_file_list_filter (file_list, filter_templates_callback, self);
+    file_list = nautilus_file_list_sort_by_display_name (file_list);
+
+    for (GList *node = file_list; num < TEMPLATE_LIMIT && node != NULL; node = node->next, num++)
+    {
+        NautilusFile *file = node->data;
+
+        g_list_store_append (store, file);
+        any_templates = TRUE;
+    }
+
+    if (!any_templates)
+    {
+        g_object_unref (store);
+        store = NULL;
+    }
+
+    return G_LIST_STORE (store);
+}
+
+static GListModel *
+templates_tree_list_model (GObject  *item,
+                           gpointer  user_data)
+{
+    NautilusTemplatesDialog *self = NAUTILUS_TEMPLATES_DIALOG (user_data);
+    NautilusFile *file = NAUTILUS_FILE (item);
+    char *templates_directory_uri = nautilus_get_templates_directory_uri ();
+
+    if (nautilus_file_is_directory (file))
+    {
+        g_autofree char *uri = nautilus_file_get_uri (file);
+
+        if (directory_belongs_in_templates_menu (templates_directory_uri, uri))
+        {
+            NautilusDirectory *dir = nautilus_directory_get_by_uri (uri);
+
+            add_directory_to_templates_directory_list (self, dir);
+
+            return G_LIST_MODEL (update_directory_in_templates_menu (self, dir));
+        }
+    }
+
+    return NULL;
+}
+
+static void
+update_templates_menu (NautilusTemplatesDialog *self)
+{
+    if (!nautilus_should_use_templates_directory ())
+    {
+        return;
+    }
+
+    g_autofree char *templates_directory_uri = nautilus_get_templates_directory_uri ();
+    g_autolist (NautilusDirectory) sorted_copy = NULL;
+    g_autoptr (NautilusDirectory) directory = NULL;
+    GListStore *model;
+    GtkTreeListModel *treemodel;
+
+    sorted_copy = nautilus_directory_list_sort_by_uri
+                      (nautilus_directory_list_copy (self->templates_directory_list));
+
+    for (GList *dir_l = sorted_copy; dir_l != NULL; dir_l = dir_l->next)
+    {
+        g_autofree char *uri = nautilus_directory_get_uri (dir_l->data);
+        if (!directory_belongs_in_templates_menu (templates_directory_uri, uri))
+        {
+            remove_directory_from_templates_directory_list (self, dir_l->data);
+        }
+    }
+
+    directory = nautilus_directory_get_by_uri (templates_directory_uri);
+
+    model = update_directory_in_templates_menu (self, directory);
+    treemodel = gtk_tree_list_model_new (G_LIST_MODEL (model),
+                                         FALSE, FALSE,
+                                         (GtkTreeListModelCreateModelFunc) templates_tree_list_model,
+                                         self, NULL);
+
+    self->templates_list_model = treemodel;
+    gtk_filter_list_model_set_model (self->filter_model, G_LIST_MODEL (self->templates_list_model));
+}
+
+static void
+row_activated (GtkListBox    *self,
+               GtkListBoxRow *row,
+               gpointer       user_data)
+{
+    GtkTreeExpander *expander = GTK_TREE_EXPANDER (gtk_list_box_row_get_child (row));
+    g_autoptr (NautilusFile) file = (NautilusFile *) gtk_tree_expander_get_item (expander);
+
+    if (file == NULL)
+    {
+        return;
+    }
+
+    if (nautilus_file_is_directory (file))
+    {
+        if (!gtk_tree_list_row_get_expanded (gtk_tree_expander_get_list_row (expander)))
+        {
+            gtk_tree_list_row_set_expanded (gtk_tree_expander_get_list_row (expander), TRUE);
+        }
+        else
+        {
+            gtk_tree_list_row_set_expanded (gtk_tree_expander_get_list_row (expander), FALSE);
+        }
+    }
+}
+
+static GtkWidget *
+templates_list_box_widgets (GObject  *item,
+                            gpointer  user_data)
+{
+    NautilusFile *file = (NautilusFile *) gtk_tree_list_row_get_item (GTK_TREE_LIST_ROW (item));
+
+    g_return_val_if_fail (NAUTILUS_IS_FILE (file), NULL);
+
+    GtkWidget *expander = gtk_tree_expander_new ();
+    GtkWidget *box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 12);
+    GtkWidget *label = gtk_label_new (nautilus_file_get_display_name (file));
+    GtkWidget *image = gtk_image_new ();
+    GtkWidget *list_box_row;
+    NautilusFileIconFlags flags = 0;
+
+    gtk_tree_expander_set_list_row (GTK_TREE_EXPANDER (expander), GTK_TREE_LIST_ROW (item));
+
+    gtk_image_set_from_gicon (GTK_IMAGE (image), nautilus_file_get_gicon (file, flags));
+    gtk_image_set_icon_size (GTK_IMAGE (image), GTK_ICON_SIZE_LARGE);
+    gtk_tree_expander_set_child (GTK_TREE_EXPANDER (expander), box);
+    gtk_box_append (GTK_BOX (box), image);
+    gtk_box_append (GTK_BOX (box), label);
+
+    gtk_widget_set_margin_top (expander, 12);
+    gtk_widget_set_margin_bottom (expander, 12);
+    gtk_widget_set_margin_start (expander, 12);
+    gtk_widget_set_margin_end (expander, 12);
+
+    list_box_row = gtk_list_box_row_new ();
+    gtk_list_box_row_set_child (GTK_LIST_BOX_ROW (list_box_row), expander);
+
+    if (nautilus_file_is_directory (file))
+    {
+        gtk_list_box_row_set_selectable (GTK_LIST_BOX_ROW (list_box_row), FALSE);
+    }
+
+    g_object_unref (file);
+
+    return list_box_row;
+}
+
+static void
+set_expanded_recursive (GtkTreeListRow *row,
+                        gboolean        expanded)
+{
+    guint i = 0;
+    GtkTreeListRow *child_row;
+
+    gtk_tree_list_row_set_expanded (row, expanded);
+
+    while ((child_row = gtk_tree_list_row_get_child_row (row, i)) != NULL)
+    {
+        set_expanded_recursive (child_row, expanded);
+        i += 1;
+    }
+}
+
+static gboolean
+match_function (GObject  *item,
+                gpointer  user_data)
+{
+    NautilusFile *file = (NautilusFile *) gtk_tree_list_row_get_item (GTK_TREE_LIST_ROW (item));
+
+    if (file == NULL)
+    {
+        return FALSE;
+    }
+
+    const gchar *file_name = nautilus_file_get_display_name (file);
+    const gchar *text = gtk_editable_get_text (GTK_EDITABLE (user_data));
+
+    g_return_val_if_fail (text != NULL, TRUE);
+
+    return g_str_match_string (text, file_name, TRUE);
+}
+
+static void
+search_changed (GtkSearchEntry *entry,
+                gpointer        user_data)
+{
+    NautilusTemplatesDialog *self = NAUTILUS_TEMPLATES_DIALOG (user_data);
+    guint i = 0;
+    GtkTreeListRow *child_row;
+    const gchar *text = gtk_editable_get_text (GTK_EDITABLE (entry));
+    gboolean check;
+
+    g_return_if_fail (text != NULL);
+
+    if (g_strcmp0 ("", text) != 0)
+    {
+        check = TRUE;
+    }
+    else
+    {
+        check = FALSE;
+    }
+
+    while ((child_row = gtk_tree_list_model_get_child_row (self->templates_list_model, i)) != NULL)
+    {
+        set_expanded_recursive (child_row, check);
+        i += 1;
+    }
+
+    gtk_filter_changed (GTK_FILTER (self->filter), GTK_FILTER_CHANGE_DIFFERENT);
+}
+
+static void
+schedule_update (NautilusTemplatesDialog *self)
+{
+    if (self->update_timeout_id == 0)
+    {
+        self->update_timeout_id
+            = g_timeout_add (100, update_timeout_callback, self);
+    }
+}
+
+NautilusFile *
+nautilus_templates_dialog_get_selected_file (NautilusTemplatesDialog *self)
+{
+    GtkListBoxRow *row = gtk_list_box_get_selected_row (GTK_LIST_BOX (self->templates_list_box));
+    NautilusFile *file = NULL;
+
+    if (row != NULL)
+    {
+        GtkTreeExpander *expander = GTK_TREE_EXPANDER (gtk_list_box_row_get_child (row));
+
+        file = NAUTILUS_FILE (gtk_tree_expander_get_item (expander));
+    }
+
+    return file;
+}
+
 void
 nautilus_templates_dialog_new (GtkWindow *parent_window)
 {
@@ -77,13 +529,47 @@ static void
 nautilus_templates_dialog_class_init (NautilusTemplatesDialogClass *klass)
 {
     GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+    GObjectClass *oclass = G_OBJECT_CLASS (klass);
+
+    oclass->dispose = nautilus_templates_dialog_dispose;
 
     gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/nautilus/ui/nautilus-templates-dialog.ui");
+
+    gtk_widget_class_bind_template_child (widget_class, NautilusTemplatesDialog, templates_stack);
+    gtk_widget_class_bind_template_child (widget_class, NautilusTemplatesDialog, templates_list_box);
+    gtk_widget_class_bind_template_child (widget_class, NautilusTemplatesDialog, templates_search_entry);
     gtk_widget_class_bind_template_callback (widget_class, nautilus_templates_dialog_import_template_dialog);
+    gtk_widget_class_bind_template_callback (widget_class, row_activated);
+    gtk_widget_class_bind_template_callback (widget_class, search_changed);
 }
 
 static void
 nautilus_templates_dialog_init (NautilusTemplatesDialog *self)
 {
     gtk_widget_init_template (GTK_WIDGET (self));
+
+    self->filter = gtk_custom_filter_new ((GtkCustomFilterFunc) match_function,
+                                          self->templates_search_entry, NULL);
+    self->filter_model = gtk_filter_list_model_new (NULL, GTK_FILTER (self->filter));
+
+    /* Make templates model */
+    update_templates_menu (self);
+
+    gtk_list_box_bind_model (GTK_LIST_BOX (self->templates_list_box),
+                             G_LIST_MODEL (self->filter_model),
+                             (GtkListBoxCreateWidgetFunc) templates_list_box_widgets,
+                             NULL,
+                             NULL);
+
+    if (g_list_model_get_n_items (G_LIST_MODEL (self->templates_list_model)) == 0)
+    {
+        gtk_stack_set_visible_child_name (self->templates_stack, "no-templates");
+    }
+    else
+    {
+        gtk_stack_set_visible_child_name (self->templates_stack, "templates-list");
+    }
+
+    self->show_hidden_files = g_settings_get_boolean (gtk_filechooser_preferences,
+                                                      NAUTILUS_PREFERENCES_SHOW_HIDDEN_FILES);
 }
