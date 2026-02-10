5f72a8350a5ecc5c300c149412bb999e379fcbb9
Peter Eisenmann <p3732@getgoogleoff.me>
2026-02-10 02:31:11 +0100

    file-ops: Drop redundant empty function with test
    The test was including the whole operation .c file which made it the
    file that took the compiler the longest to parse.

--- src/nautilus-file-operations.c
+++ src/nautilus-file-operations.c
@@ -2202,36 +2202,6 @@ do_unmount (UnmountData *data)
     g_object_unref (mount_op);
 }
 
-static gboolean
-dir_has_files (GFile *dir)
-{
-    GFileEnumerator *enumerator;
-    gboolean res;
-    GFileInfo *file_info;
-
-    res = FALSE;
-
-    enumerator = g_file_enumerate_children (dir,
-                                            G_FILE_ATTRIBUTE_STANDARD_NAME,
-                                            0,
-                                            NULL, NULL);
-    if (enumerator)
-    {
-        file_info = g_file_enumerator_next_file (enumerator, NULL, NULL);
-        if (file_info != NULL)
-        {
-            res = TRUE;
-            g_object_unref (file_info);
-        }
-
-        g_file_enumerator_close (enumerator, NULL, NULL);
-        g_object_unref (enumerator);
-    }
-
-
-    return res;
-}
-
 static GList *
 get_trash_dirs_for_mount (GMount *mount)
 {
