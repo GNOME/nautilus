f71698cd156c2bb8aab65d3d66639477d467da8f
Peter Eisenmann <p3732@getgoogleoff.me>
2026-02-11 02:09:16 +0100

    test/dir: Use main loop for callback test

--- test/automated/displayless/test-directory.c
+++ test/automated/displayless/test-directory.c
@@ -9,6 +9,55 @@
 
 static int data_dummy;
 
+typedef struct
+{
+    guint expected_number_callbacks;
+    GMainLoop *loop;
+} CallbackCounter;
+
+static CallbackCounter *
+callback_counter_new (guint expected_number_callbacks)
+{
+    CallbackCounter *data = g_new0 (CallbackCounter, 1);
+
+    data->expected_number_callbacks = expected_number_callbacks;
+    data->loop = g_main_loop_new (NULL, FALSE);
+
+    return data;
+}
+
+static void
+callback_counter_free (CallbackCounter *data)
+{
+    g_clear_pointer (&data->loop, g_main_loop_unref);
+
+    g_free (data);
+}
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (CallbackCounter, callback_counter_free)
+
+static void
+callback_counter_callback (CallbackCounter *data)
+{
+    data->expected_number_callbacks -= 1;
+
+    if (data->expected_number_callbacks == 0)
+    {
+        g_assert_nonnull (data->loop);
+
+        g_main_loop_quit (data->loop);
+        g_clear_pointer (&data->loop, g_main_loop_unref);
+    }
+}
+
+static void
+callback_counter_await (CallbackCounter *data)
+{
+    if (data->loop != NULL)
+    {
+        g_main_loop_run (data->loop);
+    }
+}
 
 /** Check for same directory object for duplicates */
 static void
@@ -50,18 +99,12 @@ test_directory_hash_table_cleanup (void)
     g_assert_cmpuint (nautilus_directory_number_outstanding (), ==, 0);
 }
 
-static gboolean got_files_flag;
-
 static void
-got_files_callback (NautilusDirectory *directory,
-                    GList             *files,
-                    gpointer           callback_data)
+callback_counter_directory_callback (NautilusDirectory *directory,
+                                     NautilusFileList  *files,
+                                     gpointer           user_data)
 {
-    g_assert_true (NAUTILUS_IS_DIRECTORY (directory));
-    g_assert_cmpint (g_list_length (files), >, 10);
-    g_assert_true (callback_data == &data_dummy);
-
-    got_files_flag = TRUE;
+    callback_counter_callback (user_data);
 }
 
 /** Check that call-when-ready works */
@@ -69,20 +112,18 @@ static void
 test_directory_call_when_ready (void)
 {
     g_autoptr (NautilusDirectory) directory = nautilus_directory_get_by_uri ("file:///etc");
+    g_autoptr (CallbackCounter) data = callback_counter_new (1);
+
     g_assert_cmpuint (nautilus_directory_number_outstanding (), ==, 1);
 
-    got_files_flag = FALSE;
     nautilus_directory_call_when_ready (directory,
                                         NAUTILUS_FILE_ATTRIBUTE_INFO |
                                         NAUTILUS_FILE_ATTRIBUTE_DEEP_COUNTS,
                                         TRUE,
-                                        got_files_callback, &data_dummy);
-    for (guint i = 0; !got_files_flag && i < 100000; i++)
-    {
-        g_main_context_iteration (NULL, TRUE);
-    }
+                                        callback_counter_directory_callback, data);
 
-    g_assert_true (got_files_flag);
+    callback_counter_await (data);
+
     /* Every NautilusFile created by call_when_ready must have been
      * unref'd and destroyed after the NautilusDirectoryCallback returns */
     g_assert_null (directory->details->file_list);
