383f5b9a9109c02ce9e448be131b9cfd7194106e
Peter Eisenmann <p3732@getgoogleoff.me>
2026-02-25 14:30:12 +0100

    test/file-util: Bundle two string prefix tests

--- test/automated/displayless/test-file-utilities-get-common-filename-prefix.c
+++ test/automated/displayless/test-file-utilities-get-common-filename-prefix.c
@@ -7,256 +7,55 @@
 
 #include "test-utilities.h"
 
-static void
-test_has_large_enough_common_prefix (void)
+typedef struct
 {
-    const char *list[] =
-    {
-        "test",
-        "tests",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("test", ==, actual);
-}
+    const char *a;
+    const char *b;
+    const char *prefix;
+    int min_length;
+} TwoStringPrefixTest;
 
 static void
-test_has_large_enough_common_prefix_with_spaces_in_middle (void)
+test_common_prefix_two_strings (void)
 {
-    const char *list[] =
+    const TwoStringPrefixTest tests[] =
     {
-        "Cpt J Yossarian r1",
-        "Cpt J Yossarian a1",
-        NULL
+        { "test", "tests", "test", 4 },
+        { "Cpt-J_Yossarian r1", "Cpt-J_Yossarian a1", "Cpt-J_Yossarian", 4 },
+        { "Cpt-J, Yossarian.xml", "Cpt-J, Yossarian.xsl", "Cpt-J, Yossarian", 4 },
+        { "foo", "foob", NULL, 4 },
+        { "this string really", "isn't the same as the other", NULL, 4 },
+        { "foo", "roo", NULL, 4 },
+        { "fools", "rools", NULL, 4 },
+        { "tes.txt", "tes.tar", NULL, 4 },
+        { "nau tilus.c", "nau tilus.cpp", "nau tilus", 4 },
+        { "nautilus ", "nautilus two", "nautilus", 4 },
+        { "nautilus !£ $\"    foo.tar.gz", "nautilus !£ $\"  .lzma", "nautilus !£ $\"", 4 },
+        {
+            "nautilus (2018!£$%^&* ()_+-={}[ ];':@#~<>?,./\".mp4",
+            "nautilus (2018!£$%^&* ()_+-={}[ ];':@#~<>?,./\".srt",
+            "nautilus (2018!£$%^&* ()_+-={}[ ];':@#~<>?,./\"",
+            4
+        },
+        { "ӶtestӶ234", "ӶtestӶ1", "ӶtestӶ", 4 },
+        { "QQӶtestӶabb234", "QQӶtestӶabb1", "QQӶtestӶabb", 4 },
+        { "ǣȸʸͻͻΎΘΛ", "ǣȸʸͻͻΎΘ", "ǣȸʸͻͻΎΘ", 4 },
+        { "test.ǣȸʸͻͻΎΘΛ", "test.ǣȸʸͻͻΎΘ", "test", 4 },
+        { "ǣȸʸ- ͻͻΎΘ$%%^", "ǣȸʸ- ͻͻΎΘ$%%&", "ǣȸʸ- ͻͻΎΘ$%%", 4 },
+        { "CA", "CB", "C", 1 },
+        { "CA", "BB", NULL, 1 },
+        { NULL, NULL, NULL, 0 }
     };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
 
-    g_assert_cmpstr ("Cpt J Yossarian", ==, actual);
-}
-
-static void
-test_has_large_enough_common_prefix_with_punctuation_in_middle (void)
-{
-    const char *list[] =
+    for (guint i = 0; tests[i].a != NULL; i += 1)
     {
-        "Cpt-J_Yossarian r1",
-        "Cpt-J_Yossarian a1",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
+        const TwoStringPrefixTest *test = &tests[i];
+        const char * const filenames[3] = { test->a, test->b, NULL };
+        g_autofree char *actual =
+            nautilus_get_common_filename_prefix_from_filenames (filenames, test->min_length);
 
-    g_assert_cmpstr ("Cpt-J_Yossarian", ==, actual);
-}
-
-static void
-test_has_large_enough_common_prefix_with_punctuation_in_middle_and_extension (void)
-{
-    const char *list[] =
-    {
-        "Cpt-J, Yossarian.xml",
-        "Cpt-J, Yossarian.xsl",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("Cpt-J, Yossarian", ==, actual);
-}
-
-static void
-test_doesnt_have_large_enough_common_prefix (void)
-{
-    const char *list[] =
-    {
-        "foo",
-        "foob",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_null (actual);
-}
-
-static void
-test_doesnt_have_large_enough_common_prefix_completely_different_strings (void)
-{
-    const char *list[] =
-    {
-        "this string really",
-        "isn't the same as the other",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_null (actual);
-}
-
-static void
-test_doesnt_have_large_enough_common_prefix_first_character_differs (void)
-{
-    const char *list[] =
-    {
-        "foo",
-        "roo",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_null (actual);
-}
-
-static void
-test_doesnt_have_large_enough_common_prefix_first_character_differs_longer_string (void)
-{
-    const char *list[] =
-    {
-        "fools",
-        "rools",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_null (actual);
-}
-
-static void
-test_has_large_enough_common_prefix_until_extension_removed (void)
-{
-    const char *list[] =
-    {
-        "tes.txt",
-        "tes.tar",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_null (actual);
-}
-
-static void
-test_extension_is_removed (void)
-{
-    const char *list[] =
-    {
-        "nau tilus.c",
-        "nau tilus.cpp",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("nau tilus", ==, actual);
-}
-
-static void
-test_whitespace_is_removed (void)
-{
-    const char *list[] =
-    {
-        "nautilus ",
-        "nautilus two",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("nautilus", ==, actual);
-}
-
-static void
-test_whitespace_and_extension_are_removed (void)
-{
-    const char *list[] =
-    {
-        "nautilus !£ $\"    foo.tar.gz",
-        "nautilus !£ $\"  .lzma",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("nautilus !£ $\"", ==, actual);
-}
-
-static void
-test_punctuation_is_preserved (void)
-{
-    const char *list[] =
-    {
-        "nautilus (2018!£$%^&* ()_+-={}[ ];':@#~<>?,./\".mp4",
-        "nautilus (2018!£$%^&* ()_+-={}[ ];':@#~<>?,./\".srt",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("nautilus (2018!£$%^&* ()_+-={}[ ];':@#~<>?,./\"", ==, actual);
-}
-
-static void
-test_unicode_on_outside (void)
-{
-    const char *list[] =
-    {
-        "ӶtestӶ234",
-        "ӶtestӶ1",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("ӶtestӶ", ==, actual);
-}
-
-static void
-test_unicode_on_inside (void)
-{
-    const char *list[] =
-    {
-        "QQӶtestӶabb234",
-        "QQӶtestӶabb1",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("QQӶtestӶabb", ==, actual);
-}
-
-static void
-test_unicode_whole_string (void)
-{
-    const char *list[] =
-    {
-        "ǣȸʸͻͻΎΘΛ",
-        "ǣȸʸͻͻΎΘ",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("ǣȸʸͻͻΎΘ", ==, actual);
-}
-
-static void
-test_unicode_extension (void)
-{
-    const char *list[] =
-    {
-        "test.ǣȸʸͻͻΎΘΛ",
-        "test.ǣȸʸͻͻΎΘ",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("test", ==, actual);
-}
-
-static void
-test_unicode_with_punctuation (void)
-{
-    const char *list[] =
-    {
-        "ǣȸʸ- ͻͻΎΘ$%%^",
-        "ǣȸʸ- ͻͻΎΘ$%%&",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 4);
-
-    g_assert_cmpstr ("ǣȸʸ- ͻͻΎΘ$%%", ==, actual);
+        g_assert_cmpstr (actual, ==, test->prefix);
+    }
 }
 
 static void
@@ -323,34 +122,6 @@ test_many_strings_first_differs (void)
     list[n_strings] = NULL;
 
     actual = nautilus_get_common_filename_prefix_from_filenames ((const char * const *) list, 4);
-    g_assert_null (actual);
-}
-
-static void
-test_smaller_min_length_and_does_have_common_prefix (void)
-{
-    const char *list[] =
-    {
-        "CA",
-        "CB",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 1);
-
-    g_assert_cmpstr ("C", ==, actual);
-}
-
-static void
-test_smaller_min_length_and_doesnt_have_common_prefix (void)
-{
-    const char *list[] =
-    {
-        "CA",
-        "BB",
-        NULL
-    };
-    g_autofree char *actual = nautilus_get_common_filename_prefix_from_filenames (list, 1);
-
     g_assert_null (actual);
 }
 
@@ -438,58 +209,12 @@ static void
 setup_test_suite (void)
 {
     /* nautilus_get_common_filename_prefix_from_filenames () */
-    g_test_add_func ("/get-common-filename-prefix/string-list/1.0",
-                     test_has_large_enough_common_prefix);
-    g_test_add_func ("/get-common-filename-prefix/string-list/1.1",
-                     test_has_large_enough_common_prefix_with_spaces_in_middle);
-    g_test_add_func ("/get-common-filename-prefix/string-list/1.2",
-                     test_has_large_enough_common_prefix_with_punctuation_in_middle);
-    g_test_add_func ("/get-common-filename-prefix/string-list/1.3",
-                     test_has_large_enough_common_prefix_with_punctuation_in_middle_and_extension);
-
-    g_test_add_func ("/get-common-filename-prefix/string-list/2.0",
-                     test_doesnt_have_large_enough_common_prefix);
-    g_test_add_func ("/get-common-filename-prefix/string-list/2.1",
-                     test_doesnt_have_large_enough_common_prefix_completely_different_strings);
-    g_test_add_func ("/get-common-filename-prefix/string-list/2.2",
-                     test_doesnt_have_large_enough_common_prefix_first_character_differs);
-    g_test_add_func ("/get-common-filename-prefix/string-list/2.3",
-                     test_doesnt_have_large_enough_common_prefix_first_character_differs_longer_string);
-
-    g_test_add_func ("/get-common-filename-prefix/string-list/3.0",
-                     test_has_large_enough_common_prefix_until_extension_removed);
-
-    g_test_add_func ("/get-common-filename-prefix/string-list/4.0",
-                     test_extension_is_removed);
-    g_test_add_func ("/get-common-filename-prefix/string-list/4.1",
-                     test_whitespace_is_removed);
-    g_test_add_func ("/get-common-filename-prefix/string-list/4.2",
-                     test_whitespace_and_extension_are_removed);
-    g_test_add_func ("/get-common-filename-prefix/string-list/4.3",
-                     test_punctuation_is_preserved);
-
-    g_test_add_func ("/get-common-filename-prefix/string-list/5.0",
-                     test_unicode_on_inside);
-    g_test_add_func ("/get-common-filename-prefix/string-list/5.1",
-                     test_unicode_on_outside);
-    g_test_add_func ("/get-common-filename-prefix/string-list/5.2",
-                     test_unicode_whole_string);
-    g_test_add_func ("/get-common-filename-prefix/string-list/5.3",
-                     test_unicode_extension);
-    g_test_add_func ("/get-common-filename-prefix/string-list/5.4",
-                     test_unicode_with_punctuation);
-
     g_test_add_func ("/get-common-filename-prefix/string-list/6.0",
                      test_many_strings);
     g_test_add_func ("/get-common-filename-prefix/string-list/6.1",
                      test_many_strings_last_differs);
     g_test_add_func ("/get-common-filename-prefix/string-list/6.2",
                      test_many_strings_first_differs);
-
-    g_test_add_func ("/get-common-filename-prefix/string-list/7.0",
-                     test_smaller_min_length_and_does_have_common_prefix);
-    g_test_add_func ("/get-common-filename-prefix/string-list/7.1",
-                     test_smaller_min_length_and_doesnt_have_common_prefix);
 
     /* nautilus_get_common_filename_prefix () */
     g_test_add_func ("/get-common-filename-prefix/file-list/1.0",
@@ -514,6 +239,8 @@ main (int   argc,
     g_test_set_nonfatal_assertions ();
     nautilus_ensure_extension_points ();
 
+    g_test_add_func ("/common-prefix/two-strings",
+                     test_common_prefix_two_strings);
     setup_test_suite ();
 
     return g_test_run ();
