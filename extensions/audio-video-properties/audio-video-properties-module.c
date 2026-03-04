83ec224826273c92ac3602441ee52f199392119d
Peter Eisenmann <p3732@getgoogleoff.me>
2026-03-04 01:57:30 +0100

    ext/av-props: Rename functions to generic name
    Also avoid forward declaring properties_model_provider_iface_init.

--- extensions/audio-video-properties/audio-video-properties-module.c
+++ extensions/audio-video-properties/audio-video-properties-module.c
@@ -18,11 +18,9 @@
 
 static GType tpp_type = 0;
 static void properties_model_provider_iface_init (NautilusPropertiesModelProviderInterface *iface);
-static GList *totem_properties_get_models (NautilusPropertiesModelProvider *provider,
-                                           GList                           *files);
 
 static void
-totem_properties_plugin_register_type (GTypeModule *module)
+audio_video_properties_plugin_register_type (GTypeModule *module)
 {
     const GTypeInfo info =
     {
@@ -45,7 +43,7 @@ totem_properties_plugin_register_type (GTypeModule *module)
     };
 
     tpp_type = g_type_module_register_type (module, G_TYPE_OBJECT,
-                                            "TotemPropertiesPlugin",
+                                            "AVPropertiesPlugin",
                                             &info, 0);
     g_type_module_add_interface (module,
                                  tpp_type,
@@ -83,12 +81,6 @@ disable_gst_display_decoders (void)
     }
 }
 
-static void
-properties_model_provider_iface_init (NautilusPropertiesModelProviderInterface *iface)
-{
-    iface->get_models = totem_properties_get_models;
-}
-
 static gpointer
 init_backend (gpointer data)
 {
@@ -98,8 +90,8 @@ init_backend (gpointer data)
 }
 
 static GList *
-totem_properties_get_models (NautilusPropertiesModelProvider *provider,
-                             GList                           *files)
+audio_video_properties_get_models (NautilusPropertiesModelProvider *provider,
+                                   GList                           *files)
 {
     /* only add properties model if a single file is selected */
     if (files == NULL || files->next != NULL)
@@ -124,7 +116,13 @@ totem_properties_get_models (NautilusPropertiesModelProvider *provider,
 
     g_once (&backend_inited, init_backend, NULL);
 
-    return g_list_prepend (NULL, totem_properties_view_new (uri));
+    return g_list_prepend (NULL, audio_video_properties_model_new (uri));
+}
+
+static void
+properties_model_provider_iface_init (NautilusPropertiesModelProviderInterface *iface)
+{
+    iface->get_models = audio_video_properties_get_models;
 }
 
 /* --- extension interface --- */
@@ -135,8 +133,8 @@ nautilus_module_initialize (GTypeModule *module)
     bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
     bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 
-    totem_properties_plugin_register_type (module);
-    totem_properties_view_register_type (module);
+    audio_video_properties_plugin_register_type (module);
+    audio_video_properties_model_register_type (module);
 }
 
 void
