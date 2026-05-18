fe7f98aff44173d840e9191228b4700eaef22f42
Peter Eisenmann <p3732@getgoogleoff.me>
2026-05-19 01:21:37 +0200

    application: Add last-window-closed signal

--- src/nautilus-application.c
+++ src/nautilus-application.c
@@ -103,6 +103,14 @@ struct _NautilusApplication
 
 G_DEFINE_FINAL_TYPE (NautilusApplication, nautilus_application, ADW_TYPE_APPLICATION)
 
+enum
+{
+    LAST_WINDOW_CLOSED,
+    LAST_SIGNAL
+};
+
+static guint signals[LAST_SIGNAL];
+
 static void
 on_window_is_active_changed (NautilusWindow      *window,
                              GParamSpec          *pspec,
@@ -1278,6 +1286,7 @@ nautilus_application_window_removed (GtkApplication *app,
     {
         nautilus_previewer_call_close ();
         nautilus_progress_persistence_handler_make_persistent (self->progress_handler);
+        g_signal_emit (self, signals[LAST_WINDOW_CLOSED], 0);
     }
 
     schedule_dbus_location_update (self);
@@ -1322,6 +1331,14 @@ nautilus_application_class_init (NautilusApplicationClass *class)
     gtkapp_class = GTK_APPLICATION_CLASS (class);
     gtkapp_class->window_added = nautilus_application_window_added;
     gtkapp_class->window_removed = nautilus_application_window_removed;
+
+    signals[LAST_WINDOW_CLOSED] = g_signal_new ("last-window-closed",
+                                                G_TYPE_FROM_CLASS (object_class),
+                                                G_SIGNAL_RUN_LAST,
+                                                0,
+                                                NULL, NULL,
+                                                g_cclosure_marshal_VOID__VOID,
+                                                G_TYPE_NONE, 0);
 }
 
 NautilusApplication *
