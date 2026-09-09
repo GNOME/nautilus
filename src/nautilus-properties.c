463123bc25086c6963a330327857aff3efeab54c
Khalid Abu Shawarib <khalid.shawarib@gmail.com>
2026-09-09 20:36:48 +0300

    properties: Force fitting thumbnails into a square
    This prevents folder thumbnail edit buttons from being too compressed.

--- src/nautilus-properties.c
+++ src/nautilus-properties.c
@@ -76,6 +76,7 @@ struct _NautilusPropertiesWidget
     /* Basic page */
 
     GtkStack *icon_stack;
+    GtkWidget *icon_bin;
     GtkWidget *icon_image;
     GtkWidget *icon_overlay;
     GtkWidget *select_icon_button;
@@ -814,7 +815,7 @@ update_image_widget (NautilusPropertiesWidget *self)
 
     if (!show_select && !show_reset)
     {
-        gtk_stack_set_visible_child (self->icon_stack, self->icon_image);
+        gtk_stack_set_visible_child (self->icon_stack, self->icon_bin);
     }
     else
     {
@@ -4026,6 +4027,7 @@ nautilus_properties_widget_class_init (NautilusPropertiesWidgetClass *klass)
     gtk_widget_class_bind_template_child (widget_class, NautilusPropertiesWidget, toast_overlay);
     gtk_widget_class_bind_template_child (widget_class, NautilusPropertiesWidget, nav_view);
     gtk_widget_class_bind_template_child (widget_class, NautilusPropertiesWidget, icon_stack);
+    gtk_widget_class_bind_template_child (widget_class, NautilusPropertiesWidget, icon_bin);
     gtk_widget_class_bind_template_child (widget_class, NautilusPropertiesWidget, icon_image);
     gtk_widget_class_bind_template_child (widget_class, NautilusPropertiesWidget, icon_overlay);
     gtk_widget_class_bind_template_child (widget_class, NautilusPropertiesWidget, select_icon_button);
